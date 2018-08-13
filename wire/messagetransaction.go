2565dc2ee73deaa70f278ce15a9416b66c74cfa2
thaibaoautonomous <thaibao@autonomous.nyc>
2018-08-13 13:56:11 +0700

    - prototype for blockchain

--- wire/messagetransaction.go
+++ wire/messagetransaction.go
@@ -2,16 +2,26 @@ package wire
 
 import (
 	"github.com/internet-cash/prototype/transaction"
+	"encoding/json"
 )
 
 type MessageTransaction struct {
-	Transaction *transaction.Tx
+	Transaction transaction.Tx
 }
 
-func (msg MessageTransaction) MessageType() string {
+func (self MessageTransaction) MessageType() string {
 	return CmdTx
 }
 
-func (msg MessageTransaction) MaxPayloadLength(pver int) int {
+func (self MessageTransaction) MaxPayloadLength(pver int) int {
 	return MaxBlockPayload
+}
+
+func (self MessageTransaction) JsonSerialize() string {
+	jsonStr, _ := json.Marshal(self)
+	return string(jsonStr)
+}
+
+func (self MessageTransaction) JsonDeserialize(jsonStr string) {
+	_ = json.Unmarshal([]byte(jsonStr), self)
 }
