df7383ada88d6232df848ede7cd370cc3fcc48e6
thaibaoautonomous <thaibao@autonomous.nyc>
2018-08-16 11:48:12 +0700

    - prototype for blockchain

--- config.go
+++ config.go
@@ -146,6 +146,16 @@ type serviceOptions struct {
 	ServiceCommand string `short:"s" long:"service" description:"Service command {install, remove, start, stop}"`
 }
 
+// filesExists reports whether the named file or directory exists.
+func fileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
 // newConfigParser returns a new command line flags parser.
 func newConfigParser(cfg *config, so *serviceOptions, options flags.Options) *flags.Parser {
 	parser := flags.NewParser(cfg, options)
