4f2468d99e3bf0a143481fcf579eb40e2c06d5b0
thaibaoautonomous <thaibao@autonomous.nyc>
2018-08-16 14:37:40 +0700

    - rpc

--- jsonrpc/request.go
+++ jsonrpc/request.go
@@ -1,36 +1,8 @@
 package jsonrpc
 
-import "encoding/json"
-
-// Standard JSON-RPC 2.0 errors.
-var (
-	ErrRPCInvalidRequest = &RPCError{
-		Code:    -32600,
-		Message: "Invalid request",
-	}
-	ErrRPCMethodNotFound = &RPCError{
-		Code:    -32601,
-		Message: "Method not found",
-	}
-	ErrRPCInvalidParams = &RPCError{
-		Code:    -32602,
-		Message: "Invalid parameters",
-	}
-	ErrRPCInternal = &RPCError{
-		Code:    -32603,
-		Message: "Internal error",
-	}
-	ErrRPCParse = &RPCError{
-		Code:    -32700,
-		Message: "Parse error",
-	}
+import (
+	"encoding/json"
 )
-
-// RPCErrorCode represents an error code to be used as a part of an RPCError
-// which is in turn used in a JSON-RPC Response object.
-//
-// A specific type is used to help ensure the wrong errors aren't used.
-type RPCErrorCode int
 
 // Request is a type for raw JSON-RPC 1.0 requests.  The Method field identifies
 // the specific command type which in turns leads to different parameters.
@@ -44,11 +16,4 @@ type Request struct {
 	Method  string            `json:"method"`
 	Params  []json.RawMessage `json:"params"`
 	ID      interface{}       `json:"id"`
-}
-
-// RPCError represents an error that is used as a part of a JSON-RPC Response
-// object.
-type RPCError struct {
-	Code    RPCErrorCode `json:"code,omitempty"`
-	Message string       `json:"message,omitempty"`
 }
---
+++ jsonrpc/response.go
@@ -0,0 +1,70 @@
+package jsonrpc
+
+import (
+	"fmt"
+	"encoding/json"
+)
+
+// Response is the general form of a JSON-RPC response.  The type of the Result
+// field varies from one command to the next, so it is implemented as an
+// interface.  The ID field has to be a pointer for Go to put a null in it when
+// empty.
+type Response struct {
+	Result json.RawMessage `json:"result"`
+	Error  *RPCError       `json:"error"`
+	ID     *interface{}    `json:"id"`
+}
+
+// NewResponse returns a new JSON-RPC response object given the provided id,
+// marshalled result, and RPC error.  This function is only provided in case the
+// caller wants to construct raw responses for some reason.
+//
+// Typically callers will instead want to create the fully marshalled JSON-RPC
+// response to send over the wire with the MarshalResponse function.
+func NewResponse(id interface{}, marshalledResult []byte, rpcErr *RPCError) (*Response, error) {
+	if !IsValidIDType(id) {
+		str := fmt.Sprintf("the id of type '%T' is invalid", id)
+		return nil, makeError(ErrInvalidType, str)
+	}
+
+	pid := &id
+	return &Response{
+		Result: marshalledResult,
+		Error:  rpcErr,
+		ID:     pid,
+	}, nil
+}
+
+// IsValidIDType checks that the ID field (which can go in any of the JSON-RPC
+// requests, responses, or notifications) is valid.  JSON-RPC 1.0 allows any
+// valid JSON type.  JSON-RPC 2.0 (which bitcoind follows for some parts) only
+// allows string, number, or null, so this function restricts the allowed types
+// to that list.  This function is only provided in case the caller is manually
+// marshalling for some reason.    The functions which accept an ID in this
+// package already call this function to ensure the provided id is valid.
+func IsValidIDType(id interface{}) bool {
+	switch id.(type) {
+	case int, int8, int16, int32, int64,
+	uint, uint8, uint16, uint32, uint64,
+	float32, float64,
+	string,
+	nil:
+		return true
+	default:
+		return false
+	}
+}
+
+// MarshalResponse marshals the passed id, result, and RPCError to a JSON-RPC
+// response byte slice that is suitable for transmission to a JSON-RPC client.
+func MarshalResponse(id interface{}, result interface{}, rpcErr *RPCError) ([]byte, error) {
+	marshalledResult, err := json.Marshal(result)
+	if err != nil {
+		return nil, err
+	}
+	response, err := NewResponse(id, marshalledResult, rpcErr)
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(&response)
+}
