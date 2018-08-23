77c2112c9c8bb700a2a67b1894920c66d6c4dcf5
thaibaoautonomous <thaibao@autonomous.nyc>
2018-08-23 10:11:28 +0700

    - Update mining

--- mining/miner/miner.go
+++ mining/miner/miner.go
@@ -111,7 +111,7 @@ out:
 
 		template, err := m.g.NewBlockTemplate(payToAddr, m.cfg.Chain)
 		m.submitBlockLock.Unlock()
-		if err != nil && len(template.Block.Transactions) == 0 {
+		if err != nil || len(template.Block.Transactions) == 0 {
 			fmt.Sprint("Failed to create new block template: %v", err)
 			continue
 		}
