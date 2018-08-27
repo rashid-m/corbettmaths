c96bc28ea3304c7523177d7c498cc1254493122a
thaibaoautonomous <thaibao@autonomous.nyc>
2018-08-27 15:36:52 +0700

    - implement wallet

---
+++ wallet/wordlist.go
@@ -0,0 +1,21 @@
+package wallet
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+func NewWordList() []string {
+	englishBytes, err := ioutil.ReadFile("english.txt")
+	english := string(englishBytes)
+	if err != nil {
+		Logger.log.Error(err)
+	}
+	//checksum := crc32.ChecksumIEEE([]byte(english))
+	//c1dbd296 := fmt.Sprintf("%x", checksum)
+	//if c1dbd296 != "c1dbd296" {
+	//	panic("english.txt checksum invalid")
+	//}
+	var englishWords = strings.Split(strings.TrimSpace(english), "\n")
+	return englishWords
+}
