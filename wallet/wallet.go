0cc38682c6302553875efc49395ce91c227d0764
thaibaoautonomous <thaibao@autonomous.nyc>
2018-08-29 10:20:41 +0700

    - wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,5 +1,11 @@
 package wallet
 
+import (
+	"bytes"
+	"encoding/binary"
+	"io/ioutil"
+)
+
 type Account struct {
 	key        Key
 	child      []Account
@@ -8,9 +14,16 @@ type Account struct {
 type Wallet struct {
 	seed          []byte
 	entropy       []byte
-	passpharse    string
+	passPhrase    string
 	mnemonic      string
 	masterAccount Account
+
+	Config *WalletConfig
+}
+
+type WalletConfig struct {
+	DataDir  string
+	DataFile string
 }
 
 func (self *Wallet) Init(passPhrase string, numOfAccount uint32) {
@@ -65,4 +78,35 @@ func (self *Wallet) ImportAccount(privateKey string) {
 
 func (self *Wallet) ListAccounts() ([]Account) {
 	return self.masterAccount.child
+}
+
+func (self *Wallet) Save(password string) (error) {
+	if password == "" {
+		password = self.passPhrase
+	}
+
+	// encrypt
+	buf := &bytes.Buffer{}
+	err := binary.Write(buf, binary.BigEndian, &self)
+	if err != nil {
+		return err
+	}
+	cipherText := AES{}.Encrypt(password, buf.Bytes())
+
+	// save file
+	err = ioutil.WriteFile(self.Config.DataDir+self.Config.DataFile, []byte(cipherText), 0644)
+	return err
+}
+
+func (self *Wallet) LoadWallet(password string) (error) {
+	bytes, err := ioutil.ReadFile(self.Config.DataDir + self.Config.DataFile)
+	if err != nil {
+		return err
+	}
+	bufBytes := AES{}.Decrypt(password, string(bytes))
+
+	buf := &bytes.Buffer{}
+	buf.Read(bufBytes)
+	err = binary.Read(buf, binary.BigEndian, &self)
+	return err
 }
