8e9bf2fb7e59e4e6a51ee7a65f5626ea36d7ce53
hieuqautonomous <hieu.q@autonomous.nyc>
2018-09-06 17:29:31 +0700

    broadcast connected addr to all peers

--- peer/peer.go
+++ peer/peer.go
@@ -198,7 +198,7 @@ func (self *Peer) Start() error {
 }
 
 func (self *Peer) NewPeerConnection(peer *Peer) (*PeerConn, error) {
-	Logger.log.Infof("Opening stream to PEER ID - %s \n", self.PeerId.String())
+	Logger.log.Infof("Opening stream to PEER ID - %s \n", peer.PeerId.String())
 
 	self.outboundMutex.Lock()
 
@@ -218,7 +218,7 @@ func (self *Peer) NewPeerConnection(peer *Peer) (*PeerConn, error) {
 	}
 
 	if len(self.PeerConns) >= self.MaxOutbound {
-		Logger.log.Infof("Max Peer Conn")
+		Logger.log.Infof("Max Peer Outbound Connection")
 
 		self.outboundMutex.Unlock()
 		return nil, nil
