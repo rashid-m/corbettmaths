34f1a4f07482d6fc82e3b14ece7c0d5a4ca68a08
dbchiem <baoduy@autonomous.nyc>
2018-09-06 21:06:01 +0700

    Debugging verify proof

--- privacy/main.go
+++ privacy/main.go
@@ -7,17 +7,24 @@ import (
 	"github.com/ninjadotorg/cash-prototype/privacy/proto/zksnark"
 )
 
-func runProve() (*zksnark.PHGRProof, error) {
+type ProveResult struct {
+	Proof  *zksnark.PHGRProof
+	Nf, Cm [][]byte
+	Reward uint64
+}
+
+func runProve() (*ProveResult, error) {
 	// ask := client.RandSpendingKey()
 	ask := client.SpendingKey{127, 9, 42, 195, 53, 40, 231, 23, 127, 206, 167, 170, 20, 82, 217, 40, 248, 110, 181, 16, 253, 131, 117, 145, 0, 30, 57, 18, 84, 57, 189, 8}
 	outApk := client.GenSpendingAddress(ask)
 	skenc := client.GenReceivingKey(ask)
 	ekey := client.GenTransmissionKey(skenc)
-	outNote1 := &client.Note{Value: 0, Apk: outApk}
+	outNote1 := &client.Note{Value: 1, Apk: outApk}
 	outNote2 := &client.Note{Value: 0, Apk: outApk}
 	outputs := []*client.JSOutput{
 		&client.JSOutput{EncKey: ekey, OutputNote: outNote1},
-		&client.JSOutput{EncKey: ekey, OutputNote: outNote2}}
+		&client.JSOutput{EncKey: ekey, OutputNote: outNote2},
+	}
 
 	const merkleTreeDepth = 29
 	hash1 := [32]byte{3}
@@ -55,11 +62,38 @@ func runProve() (*zksnark.PHGRProof, error) {
 
 	pubKey := [32]byte{9}
 	rt := [32]byte{10}
-	return client.Prove(inputs, outputs, pubKey[:], rt[:])
+	var reward uint64 = 1
+	proof, err := client.Prove(inputs, outputs, pubKey[:], rt[:], reward)
+	// var proof *zksnark.PHGRProof
+	// var err error
+	// fmt.Println(pubKey, rt)
+	if err != nil {
+		return nil, err
+	}
+
+	var nf, cm [][]byte
+	for _, input := range inputs {
+		nf = append(nf, input.InputNote.Nf)
+	}
+	for _, output := range outputs {
+		cm = append(cm, output.OutputNote.Cm)
+	}
+	result := &ProveResult{
+		Proof:  proof,
+		Nf:     nf,
+		Cm:     cm,
+		Reward: reward,
+	}
+	return result, nil
 }
 
-func runVerify(proof *zksnark.PHGRProof) {
-	if proof == nil {
+func runVerify(proveResult *ProveResult) {
+	var nf [][]byte
+	var cm [][]byte
+	var proof *zksnark.PHGRProof
+	var reward uint64
+
+	if proveResult == nil {
 		gA := [33]byte{0, 7}
 		gAPrime := [33]byte{0, 8}
 		gB := [65]byte{0, 9}
@@ -73,41 +107,43 @@ func runVerify(proof *zksnark.PHGRProof) {
 			G_A: gA[:], G_APrime: gAPrime[:],
 			G_B: gB[:], G_BPrime: gBPrime[:],
 			G_C: gC[:], G_CPrime: gCPrime[:],
-			G_H: gH[:], G_K: gK[:]}
+			G_H: gH[:], G_K: gK[:],
+		}
+
+		nf1 := [32]byte{1}
+		nf2 := [32]byte{2}
+		nf = append(nf, nf1[:])
+		nf = append(nf, nf2[:])
+		cm1 := [32]byte{3}
+		cm2 := [32]byte{4}
+		cm = append(cm, cm1[:])
+		cm = append(cm, cm2[:])
+	} else {
+		proof = proveResult.Proof
+		nf = proveResult.Nf
+		cm = proveResult.Cm
+		reward = proveResult.Reward
 	}
 
-	nf1 := [32]byte{1}
-	nf2 := [32]byte{2}
-	var nf [][]byte
-	nf = append(nf, nf1[:])
-	nf = append(nf, nf2[:])
-
-	cm1 := [32]byte{3}
-	cm2 := [32]byte{4}
-	var cm [][]byte
-	cm = append(cm, cm1[:])
-	cm = append(cm, cm2[:])
 	rt := [32]byte{5}
 	hSig := [32]byte{6}
-	client.Verify(proof, &nf, &cm, rt[:], hSig[:])
+	valid, err := client.Verify(proof, &nf, &cm, rt[:], hSig[:], reward)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Verify valid:", valid)
 }
 
 func runProveThenVerify() {
-	if proof, err := runProve(); err != nil {
-		runVerify(proof)
+	if proveResult, err := runProve(); err == nil {
+		runVerify(proveResult)
+	} else {
+		panic(err)
 	}
-}
-
-func test(a [3]int) {
-	a[0] = 12
 }
 
 func main() {
 	// runProve()
 	// runVerify()
-	// runProveThenVerify()
-
-	b := [3]int{1, 2, 3}
-	test(b)
-	fmt.Println(b)
+	runProveThenVerify()
 }
