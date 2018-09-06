109455805648e3bf0b581f7b9247791d05647ef1
0xgenoskawa <stone@Stones-MacBook-Pro.local>
2018-09-06 13:20:23 +0700

    DEBUG bootnode

--- bootnode/server/handler.go
+++ bootnode/server/handler.go
@@ -1,5 +1,7 @@
 package server
 
+import "fmt"
+
 type Handler struct {
 	server *RpcServer
 }
@@ -14,11 +16,14 @@ type PingArgs struct {
 	SealerPrvKey string
 }
 func (s Handler) Ping(args *PingArgs, peers *[]RawPeer) error {
+	fmt.Println("Ping", args)
 	s.server.AddPeer(args.ID, args.SealerPrvKey)
 
 	for _, p := range s.server.Peers {
 		*peers = append(*peers, RawPeer{p.ID, p.SealerPrvKey})
 	}
+
+	fmt.Println("Response", *peers)
 
 	return nil
 }
