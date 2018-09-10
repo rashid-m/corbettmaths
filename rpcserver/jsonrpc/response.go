894a427b9abff2c8c15d22f42eea7c683f82bad2
0xsirrush <thaibao@autonomous.nyc>
2018-09-10 11:49:55 +0700

    - list unspent

--- rpcserver/jsonrpc/response.go
+++ rpcserver/jsonrpc/response.go
@@ -91,7 +91,7 @@ type GetBlockChainInfoResult struct {
 
 // ListUnspentResult models a successful response from the listunspent request.
 type ListUnspentResult struct {
-	TxID          string  `json:"TxID"`
+	/*TxID          string  `json:"TxID"`
 	Vout          int     `json:"Vout"`
 	Address       string  `json:"Address"`
 	Account       string  `json:"Account"`
@@ -100,8 +100,22 @@ type ListUnspentResult struct {
 	Amount        float64 `json:"Amount"`
 	Confirmations int64   `json:"Confirmations"`
 	Spendable     bool    `json:"Spendable"`
-	TxOutType     string  `json:"TxOutType"`
+	TxOutType     string  `json:"TxOutType"`*/
+	ListUnspentResultItems map[string][]ListUnspentResultItem `json:"ListUnspentResultItems"`
 }
+
+type ListUnspentResultItem struct {
+	TxId          string          `json:"TxId"`
+	JoinSplitDesc []JoinSplitDesc `json:"JoinSplitDesc"`
+}
+
+type JoinSplitDesc struct {
+	Commitments [][]byte `json:"Commitments"`
+	Amount      uint64   `json:"Amount"`
+	Anchor      []byte   `json:"Anchor"`
+}
+
+// end
 
 type GetHeaderResult struct {
 	BlockNum  int                    `json:"blocknum"`
