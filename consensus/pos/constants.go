5f97692efca7d26ccb34ff836160bbfac17e668f
0xMothman <tamle@autonomous.nyc>
2018-09-12 17:25:58 +0700

    add preValidateBlock

--- consensus/pos/constants.go
+++ consensus/pos/constants.go
@@ -1,8 +1,9 @@
 package pos
 
 const (
-	MAX_BLOCKSIZE       = 1000000 //byte
-	MAX_TXSIZE          = 1000    //byte
-	MAX_TXs_IN_BLOCK    = 1000
-	MAX_BLOCK_WAIT_TIME = 5 // second
+	MAX_BLOCKSIZE        = 1000000 //byte
+	MAX_TXSIZE           = 1000    //byte
+	MAX_TXs_IN_BLOCK     = 1000
+	MAX_BLOCK_WAIT_TIME  = 5 // second
+	MAX_SYNC_CHAINS_TIME = 5 // second
 )
