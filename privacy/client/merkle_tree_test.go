30da5cc1cfd7275eda8d5dc05f8d9f54b7d13880
dbchiem <baoduy@autonomous.nyc>
2018-09-14 14:10:35 +0700

    MerkleTree height doesn't count rt

--- privacy/client/merkle_tree_test.go
+++ privacy/client/merkle_tree_test.go
@@ -57,8 +57,13 @@ func TestBuildWitness(t *testing.T) {
 	if err != nil {
 		t.Errorf("error: %s", err.Error())
 	}
-	fmt.Printf("left: %x\n", tree.left)
-	fmt.Printf("right: %x\n", tree.right)
-	fmt.Printf("nodes: %v\n", tree.nodes)
-	fmt.Printf("witness path: %v\n", notes[0].WitnessPath)
+	// fmt.Printf("left: %x\n", tree.left)
+	// fmt.Printf("right: %x\n", tree.right)
+	// fmt.Printf("nodes: %v\n", tree.nodes)
+	fmt.Printf("witness path: %x\n\n", notes[0].WitnessPath.AuthPath)
+	fmt.Printf("2+3: %x\n", combineAndHash(bytes[2], bytes[3]))
+	fmt.Printf("4+5: %x\n", combineAndHash(bytes[4], bytes[5]))
+	fmt.Printf("6+nil: %x\n", combineAndHash(bytes[6], uncommittedNodeHash))
+	fmt.Printf("(4+5)+(6+nil): %x\n", combineAndHash(combineAndHash(bytes[4], bytes[5]), combineAndHash(bytes[6], uncommittedNodeHash)))
+	fmt.Printf("padding@3: %x\n", getPaddingAtDepth(27))
 }
