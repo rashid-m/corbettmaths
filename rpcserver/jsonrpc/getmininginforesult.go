4e3103faf77e27588aea04e1da26f0647b33b311
0xsirrush <thaibao@autonomous.nyc>
2018-09-19 11:58:29 +0700

    - add more rpc api

---
+++ rpcserver/jsonrpc/getmininginforesult.go
@@ -0,0 +1,9 @@
+package jsonrpc
+
+type GetMiningInfoResult struct {
+	Blocks         uint64 `json:"Blocks"`
+	CurrentBlockTx int    `json:"CurrentBlockTx"`
+	Difficulty     uint32 `json:"Difficulty"`
+	PoolSize       int    `json:"PoolSize"`
+	Chain          string `json:"Chain"`
+}
