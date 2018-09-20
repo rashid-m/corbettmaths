fc804818c7aff66dd04d14a5168930bd65d455bb
0xsirrush <thaibao@autonomous.nyc>
2018-09-20 16:15:37 +0700

    - fee estimator

--- rpcserver/rpcfunction.go
+++ rpcserver/rpcfunction.go
@@ -46,6 +46,7 @@ var RpcHandler = map[string]commandHandler{
 	"getmininginfo":                 RpcServer.handleGetMiningInfo,
 	"getrawmempool":                 RpcServer.handleGetRawMempool,
 	"getmempoolentry":               RpcServer.handleMempoolEntry,
+	"estimatefee":                   RpcServer.handleEstimateFee,
 
 	//POS
 	"votecandidate": RpcServer.handleVoteCandidate,
@@ -1128,4 +1129,16 @@ func (self RpcServer) handleMempoolEntry(params interface{}, closeChan <-chan st
 
 	tx, err := self.Config.TxMemPool.GetTx(txId)
 	return tx, err
+}
+
+/**
+handleEstimateFee - RPC estimates the transaction fee per kilobyte that needs to be paid for a transaction to be included within a certain number of blocks.
+ */
+func (self RpcServer) handleEstimateFee(params interface{}, closeChan <-chan struct{}) (interface{}, error) {
+	// Param #1: —how many blocks the transaction may wait before being included
+	feeRate, err := self.Config.FeeEstimator.EstimateFee(uint32(params.(float64)))
+	if err != nil {
+		return -1, err
+	}
+	return uint64(feeRate), nil
 }
