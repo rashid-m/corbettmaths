df3977d4cffe7d0d1531e38778393520b3cf7c6d
0xsirrush <thaibao@autonomous.nyc>
2018-09-20 14:06:29 +0700

    - estimate fee for transaction

--- mining/constants.go
+++ mining/constants.go
@@ -7,3 +7,10 @@ const (
 	DEFAULT_COINS                    = 5
 	DEFAULT_BONDS                    = 0
 )
+
+const (
+	// UnminedHeight is the height used for the "block" height field of the
+	// contextual transaction information provided in a transaction store
+	// when it has not yet been mined into a block.
+	UnminedHeight = 0x7fffffff
+)
