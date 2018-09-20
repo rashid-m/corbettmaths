67d1cd6d4ceb5632affca061be0aa621efa10c0e
0xsirrush <thaibao@autonomous.nyc>
2018-09-20 16:52:47 +0700

    - register block into fee estimator

--- server.go
+++ server.go
@@ -183,10 +183,11 @@ func (self *Server) NewServer(listenAddrs []string, db database.DB, chainParams 
 
 	// Init Net Sync manager to process messages
 	self.NetSync, err = netsync.NetSync{}.New(&netsync.NetSyncConfig{
-		BlockChain: self.BlockChain,
-		ChainParam: chainParams,
-		MemPool:    self.MemPool,
-		Server:     self,
+		BlockChain:   self.BlockChain,
+		ChainParam:   chainParams,
+		MemPool:      self.MemPool,
+		Server:       self,
+		FeeEstimator: self.FeeEstimator,
 	})
 	if err != nil {
 		return err
