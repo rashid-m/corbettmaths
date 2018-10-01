952ee35cea2704ff9ad7b48a5bd9252eca5e98a2
0xsirrush <thaibao@autonomous.nyc>
2018-10-01 11:04:47 +0700

    - refactor code

--- cashec/keysetprivacy.go
+++ cashec/keysetprivacy.go
@@ -53,33 +53,3 @@ func (self *KeySet) CreateSealerKeySet() (*KeySetSealer, error) {
 	sealerKeySet.ReceivingKey = self.ReadonlyKey.Skenc
 	return &sealerKeySet, nil
 }
-
-// func (self *KeySet) GenerateSignKey() (client.PrivateKey, error){
-// 	// Generate signing key
-// 	privKey, err := client.GenerateKey(rand.Reader)
-// 	return *privKey, err
-// }
-
-// func (self *KeySet) Verify(data, signature []byte, pubKey client.PublicKey) (bool, error) {
-// 	jsSig := new(JSSig)
-// 	err := json.Unmarshal(signature, jsSig)
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	valid := client.VerifySign(&pubKey, data[:], jsSig.R, jsSig.S)
-// 	return valid, nil
-// }
-
-// func (self *KeySet) Sign(data []byte, privKey client.PrivateKey) ([]byte, error) {
-// 	// TODO(@0xkraken): implement signing using keypair
-// 	jsSig := *new(JSSig)
-// 	jsSig.R, jsSig.S, _= client.Sign(rand.Reader, &privKey, data[:])
-
-// 	signed_data, err := json.Marshal(jsSig)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	//Calculate hi
-// 	return signed_data, nil
-// }
