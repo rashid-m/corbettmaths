304d7f26d7f61e63a0e9e9d85ae5958e703b9343
0xBahamoot <lam@autonomous.nyc>
2018-10-01 13:38:40 +0700

    refactor InMessageHandler

--- peer/peerconn.go
+++ peer/peerconn.go
@@ -67,118 +67,119 @@ func (self *PeerConn) InMessageHandler(rw *bufio.ReadWriter) {
 
 		//Logger.log.Infof("Received message: %s \n", str)
 		if str != "\n" {
+			go func(msgStr string) {
+				// Parse Message header
+				jsonDecodeString, _ := hex.DecodeString(msgStr)
+				messageHeader := jsonDecodeString[len(jsonDecodeString)-wire.MessageHeaderSize:]
 
-			// Parse Message header
-			jsonDecodeString, _ := hex.DecodeString(str)
-			messageHeader := jsonDecodeString[len(jsonDecodeString)-wire.MessageHeaderSize:]
+				//Logger.log.Infof("Received message: %s \n", jsonDecodeString)
 
-			//Logger.log.Infof("Received message: %s \n", jsonDecodeString)
+				commandInHeader := messageHeader[:12]
+				commandInHeader = bytes.Trim(messageHeader, "\x00")
+				Logger.log.Infof("Received message Type - %s %s", string(commandInHeader), self.PeerID)
+				commandType := string(messageHeader[:len(commandInHeader)])
+				var message, err = wire.MakeEmptyMessage(string(commandType))
 
-			commandInHeader := messageHeader[:12]
-			commandInHeader = bytes.Trim(messageHeader, "\x00")
-			Logger.log.Infof("Received message Type - %s %s", string(commandInHeader), self.PeerID)
-			commandType := string(messageHeader[:len(commandInHeader)])
-			var message, err = wire.MakeEmptyMessage(string(commandType))
-
-			// Parse Message body
-			messageBody := jsonDecodeString[:len(jsonDecodeString)-wire.MessageHeaderSize]
-			//Logger.log.Infof("Message Body - %s %s", string(messageBody), self.PeerID)
-			if err != nil {
-				Logger.log.Error(err)
-				continue
-			}
-			if commandType != wire.CmdBlock {
-				err = json.Unmarshal(messageBody, &message)
-			} else {
-				err = json.Unmarshal(messageBody, &message)
-			}
-			//temp := message.(map[string]interface{})
-			if err != nil {
-				Logger.log.Error(err)
-				continue
-			}
-			realType := reflect.TypeOf(message)
-			log.Print(realType)
-			// check type of Message
-			switch realType {
-			case reflect.TypeOf(&wire.MessageTx{}):
-				if self.Config.MessageListeners.OnTx != nil {
+				// Parse Message body
+				messageBody := jsonDecodeString[:len(jsonDecodeString)-wire.MessageHeaderSize]
+				//Logger.log.Infof("Message Body - %s %s", string(messageBody), self.PeerID)
+				if err != nil {
+					Logger.log.Error(err)
+					return
+				}
+				if commandType != wire.CmdBlock {
+					err = json.Unmarshal(messageBody, &message)
+				} else {
+					err = json.Unmarshal(messageBody, &message)
+				}
+				//temp := message.(map[string]interface{})
+				if err != nil {
+					Logger.log.Error(err)
+					return
+				}
+				realType := reflect.TypeOf(message)
+				log.Print(realType)
+				// check type of Message
+				switch realType {
+				case reflect.TypeOf(&wire.MessageTx{}):
+					if self.Config.MessageListeners.OnTx != nil {
+						//self.flagMutex.Lock()
+						self.Config.MessageListeners.OnTx(self, message.(*wire.MessageTx))
+						//self.flagMutex.Unlock()
+					}
+				case reflect.TypeOf(&wire.MessageBlock{}):
+					if self.Config.MessageListeners.OnBlock != nil {
+						//self.flagMutex.Lock()
+						self.Config.MessageListeners.OnBlock(self, message.(*wire.MessageBlock))
+						//self.flagMutex.Unlock()
+					}
+				case reflect.TypeOf(&wire.MessageGetBlocks{}):
+					if self.Config.MessageListeners.OnGetBlocks != nil {
+						//self.flagMutex.Lock()
+						self.Config.MessageListeners.OnGetBlocks(self, message.(*wire.MessageGetBlocks))
+						//self.flagMutex.Unlock()
+					}
+				case reflect.TypeOf(&wire.MessageVersion{}):
+					if self.Config.MessageListeners.OnVersion != nil {
+						//self.flagMutex.Lock()
+						versionMessage := message.(*wire.MessageVersion)
+						self.Config.MessageListeners.OnVersion(self, versionMessage)
+						//self.flagMutex.Unlock()
+					}
+				case reflect.TypeOf(&wire.MessageVerAck{}):
 					//self.flagMutex.Lock()
-					go self.Config.MessageListeners.OnTx(self, message.(*wire.MessageTx))
+					self.verAckReceived = true
+					if self.Config.MessageListeners.OnVerAck != nil {
+						self.Config.MessageListeners.OnVerAck(self, message.(*wire.MessageVerAck))
+					}
 					//self.flagMutex.Unlock()
-				}
-			case reflect.TypeOf(&wire.MessageBlock{}):
-				if self.Config.MessageListeners.OnBlock != nil {
+				case reflect.TypeOf(&wire.MessageGetAddr{}):
 					//self.flagMutex.Lock()
-					go self.Config.MessageListeners.OnBlock(self, message.(*wire.MessageBlock))
+					if self.Config.MessageListeners.OnGetAddr != nil {
+						self.Config.MessageListeners.OnGetAddr(self, message.(*wire.MessageGetAddr))
+					}
 					//self.flagMutex.Unlock()
-				}
-			case reflect.TypeOf(&wire.MessageGetBlocks{}):
-				if self.Config.MessageListeners.OnGetBlocks != nil {
+				case reflect.TypeOf(&wire.MessageAddr{}):
 					//self.flagMutex.Lock()
-					go self.Config.MessageListeners.OnGetBlocks(self, message.(*wire.MessageGetBlocks))
+					if self.Config.MessageListeners.OnGetAddr != nil {
+						self.Config.MessageListeners.OnAddr(self, message.(*wire.MessageAddr))
+					}
 					//self.flagMutex.Unlock()
+				case reflect.TypeOf(&wire.MessageRequestSign{}):
+					if self.Config.MessageListeners.OnRequestSign != nil {
+						//self.flagMutex.Lock()
+						self.Config.MessageListeners.OnRequestSign(self, message.(*wire.MessageRequestSign))
+						//self.flagMutex.Unlock()
+					}
+				case reflect.TypeOf(&wire.MessageInvalidBlock{}):
+					if self.Config.MessageListeners.OnInvalidBlock != nil {
+						//self.flagMutex.Lock()
+						self.Config.MessageListeners.OnInvalidBlock(self, message.(*wire.MessageInvalidBlock))
+						//self.flagMutex.Unlock()
+					}
+				case reflect.TypeOf(&wire.MessageBlockSig{}):
+					if self.Config.MessageListeners.OnBlockSig != nil {
+						//self.flagMutex.Lock()
+						self.Config.MessageListeners.OnBlockSig(self, message.(*wire.MessageBlockSig))
+						//self.flagMutex.Unlock()
+					}
+				case reflect.TypeOf(&wire.MessageGetChainState{}):
+					if self.Config.MessageListeners.OnGetChainState != nil {
+						//self.flagMutex.Lock()
+						self.Config.MessageListeners.OnGetChainState(self, message.(*wire.MessageGetChainState))
+						//self.flagMutex.Unlock()
+					}
+				case reflect.TypeOf(&wire.MessageChainState{}):
+					if self.Config.MessageListeners.OnChainState != nil {
+						//self.flagMutex.Lock()
+						self.Config.MessageListeners.OnChainState(self, message.(*wire.MessageChainState))
+						//self.flagMutex.Unlock()
+					}
+				default:
+					Logger.log.Warnf("InMessageHandler Received unhandled message of type %v "+
+						"from %v", realType, self)
 				}
-			case reflect.TypeOf(&wire.MessageVersion{}):
-				if self.Config.MessageListeners.OnVersion != nil {
-					//self.flagMutex.Lock()
-					versionMessage := message.(*wire.MessageVersion)
-					go self.Config.MessageListeners.OnVersion(self, versionMessage)
-					//self.flagMutex.Unlock()
-				}
-			case reflect.TypeOf(&wire.MessageVerAck{}):
-				//self.flagMutex.Lock()
-				self.verAckReceived = true
-				if self.Config.MessageListeners.OnVerAck != nil {
-					go self.Config.MessageListeners.OnVerAck(self, message.(*wire.MessageVerAck))
-				}
-				//self.flagMutex.Unlock()
-			case reflect.TypeOf(&wire.MessageGetAddr{}):
-				//self.flagMutex.Lock()
-				if self.Config.MessageListeners.OnGetAddr != nil {
-					go self.Config.MessageListeners.OnGetAddr(self, message.(*wire.MessageGetAddr))
-				}
-				//self.flagMutex.Unlock()
-			case reflect.TypeOf(&wire.MessageAddr{}):
-				//self.flagMutex.Lock()
-				if self.Config.MessageListeners.OnGetAddr != nil {
-					go self.Config.MessageListeners.OnAddr(self, message.(*wire.MessageAddr))
-				}
-				//self.flagMutex.Unlock()
-			case reflect.TypeOf(&wire.MessageRequestSign{}):
-				if self.Config.MessageListeners.OnRequestSign != nil {
-					//self.flagMutex.Lock()
-					go self.Config.MessageListeners.OnRequestSign(self, message.(*wire.MessageRequestSign))
-					//self.flagMutex.Unlock()
-				}
-			case reflect.TypeOf(&wire.MessageInvalidBlock{}):
-				if self.Config.MessageListeners.OnInvalidBlock != nil {
-					//self.flagMutex.Lock()
-					go self.Config.MessageListeners.OnInvalidBlock(self, message.(*wire.MessageInvalidBlock))
-					//self.flagMutex.Unlock()
-				}
-			case reflect.TypeOf(&wire.MessageBlockSig{}):
-				if self.Config.MessageListeners.OnBlockSig != nil {
-					//self.flagMutex.Lock()
-					go self.Config.MessageListeners.OnBlockSig(self, message.(*wire.MessageBlockSig))
-					//self.flagMutex.Unlock()
-				}
-			case reflect.TypeOf(&wire.MessageGetChainState{}):
-				if self.Config.MessageListeners.OnGetChainState != nil {
-					//self.flagMutex.Lock()
-					go self.Config.MessageListeners.OnGetChainState(self, message.(*wire.MessageGetChainState))
-					//self.flagMutex.Unlock()
-				}
-			case reflect.TypeOf(&wire.MessageChainState{}):
-				if self.Config.MessageListeners.OnChainState != nil {
-					//self.flagMutex.Lock()
-					go self.Config.MessageListeners.OnChainState(self, message.(*wire.MessageChainState))
-					//self.flagMutex.Unlock()
-				}
-			default:
-				Logger.log.Warnf("InMessageHandler Received unhandled message of type %v "+
-					"from %v", realType, self)
-			}
+			}(str)
 		}
 	}
 }
