9d94d2455b82ba82f2d83243039d6a9ba2726603
0xgenoskawa <stone@Stones-MacBook-Pro.local>
2018-10-02 11:13:56 +0700

    commit build_linux.sh add -j4 to make faster (1)

--- privacy/server/build_linux.sh
+++ privacy/server/build_linux.sh
@@ -1,43 +1,43 @@
 echo "Pre-require install"
 echo "apt install build-essential autoconf libtool pkg-config unzip -y"
-apt install build-essential autoconf libtool pkg-config unzip -y
+sudo apt install build-essential autoconf libtool pkg-config unzip -y
 
 echo "install Lib"
 
 # install boost
 echo "install boost ... "
 echo "apt install libboost* -y"
-apt install libboost* -y
+sudo apt install libboost* -y
 
 # install lib sodium
 echo "install libsodium ... "
 echo "apt install libsodium-dev -y"
-apt install libsodium-dev -y
+sudo apt install libsodium-dev -y
 
 # install gmp
 echo "install gmp ... "
 echo "apt install libgmp3-dev -y"
-apt install libgmp3-dev -y
+sudo apt install libgmp3-dev -y
 
 # install grpc
 echo "install grpc ... "
 git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
-make -j4
-make install
+sudo make -j4
+sudo make install
 cd ..
 
 # install lib protobuf
 echo "install protobuf ... "
-curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.zip
-unzip protobuf-cpp-3.6.1.zip
+sudo curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protobuf-cpp-3.6.1.zip
+sudo unzip protobuf-cpp-3.6.1.zip
 cd protobuf-3.6.1
-./configure
-make -j4
-make check
-make install
-ldconfig # refresh shared library cache.
+sudo ./configure
+sudo make -j4
+sudo make check
+sudo make install
+sudo ldconfig # refresh shared library cache.
 cd ..
 
 # install lib protobuf-compiler
@@ -52,8 +52,12 @@ cd ..
 
 # install lib cmake
 echo "install cmake ... "
-echo "apt install cmake -y"
-apt install cmake -y
+sudo wget http://www.cmake.org/files/v3.5/cmake-3.5.2.tar.gz
+sudo tar xf cmake-3.5.2.tar.gz
+cd cmake-3.5.2
+sudo ./configure
+sudo make -j4
+cd ..
 
 BUILD_DIR="build"
 if [ ! -d "$BUILD_DIR" ]; then
@@ -62,15 +66,15 @@ fi
 
 cd build
 
-cmake ..
-make -j4
+sudo cmake ..
+sudo make -j4
 
 file="./proving.key"
 if [ -f "$file" ]
 then
 	echo "$file found."
 else
-	wget https://github.com/ninjadotorg/cash-prototype/releases/download/zkpp-v0.0.3/proving.key
+	sudo wget https://github.com/ninjadotorg/cash-prototype/releases/download/zkpp-v0.0.3/proving.key
 fi
 
 file="./verifying.key"
@@ -78,5 +82,5 @@ if [ -f "$file" ]
 then
 	echo "$file found."
 else
-	wget https://github.com/ninjadotorg/cash-prototype/releases/download/zkpp-v0.0.3/verifying.key
+	sudo wget https://github.com/ninjadotorg/cash-prototype/releases/download/zkpp-v0.0.3/verifying.key
 fi
