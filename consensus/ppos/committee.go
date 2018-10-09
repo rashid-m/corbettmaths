c9755ebe4c5a4216e31336e34ae0d50fa98b7ae8
0xBahamoot <lam@autonomous.nyc>
2018-10-09 21:28:25 +0700

    refactor

--- consensus/ppos/committee.go
+++ consensus/ppos/committee.go
@@ -22,18 +22,6 @@ func (self *Engine) ProposeCandidate() {
 
 }
 
-func (self *Engine) OnCandidateProposal() {
-
-}
-
-func (self *Engine) OnCandidateVote() {
-
-}
-
-func (self *Engine) OnCandidateRequestTx() {
-
-}
-
 func (self *Engine) CheckCommittee(committee []string, blockHeight int, chainID byte) bool {
 
 	return true
@@ -47,6 +35,14 @@ func (self *Engine) signData(data []byte) (string, error) {
 	return base58.Base58Check{}.Encode(signatureByte, byte(0x00)), nil
 }
 
-// func getChainValidator(chainID byte, committee []string) (string, error) {
-// 	return committee[int((1+int(chainID))%TOTAL_VALIDATORS)], nil
-// }
+// getMyChain validator chainID and committee of that chainID
+func (self *Engine) getMyChain() byte {
+	pkey := base58.Base58Check{}.Encode(self.config.ValidatorKeySet.SpublicKey, byte(0x00))
+	for idx := byte(0); idx < byte(TOTAL_VALIDATORS); idx++ {
+		validator := self.currentCommittee[int((1+int(idx))%TOTAL_VALIDATORS)]
+		if pkey == validator {
+			return idx
+		}
+	}
+	return TOTAL_VALIDATORS // nope, you're not in the committee
+}
