f840fb195147dd9ec1633643ce2789edd1628e09
0xsirrush <thaibao@autonomous.nyc>
2018-10-15 13:54:40 +0700

    - update

--- wallet/error.go
+++ wallet/error.go
@@ -1,20 +1,34 @@
 package wallet
 
+import "fmt"
+
 const (
-	ErrInvalidChecksum = "ErrInvalidChecksum"
+	InvalidChecksumErr    = "InvalidChecksumErr"
+	WrongPassphraseErr    = "WrongPassphraseErr"
+	ExistedAccountErr     = "ExistedAccountErr"
+	ExistedAccountNameErr = "ExistedAccountNameErr"
+	UnexpectedErr         = "UnexpectedErr"
 )
 
 var ErrCodeMessage = map[string]struct {
 	code    int
 	message string
 }{
-	ErrInvalidChecksum: {-1000, "Checksum does not match"},
+	UnexpectedErr:         {-1, "Unexpected error"},
+	InvalidChecksumErr:    {-1000, "Checksum does not match"},
+	WrongPassphraseErr:    {-1001, "Wrong passphrase"},
+	ExistedAccountErr:     {-1002, "Existed account"},
+	ExistedAccountNameErr: {-1002, "Existed account name"},
 }
 
 type WalletError struct {
 	code    int
 	message string
 	err     error
+}
+
+func (e WalletError) Error() string {
+	return fmt.Sprintf("%v: %v", e.code, e.message)
 }
 
 func NewWalletError(key string, err error) *WalletError {
