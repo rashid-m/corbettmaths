5c8148af51f58b52c54b55ea577311c37d9a5b9c
0xsirrush <thaibao@autonomous.nyc>
2018-10-15 10:06:27 +0700

    - remove not use code

--- mempool/constants.go
+++ mempool/constants.go
@@ -1,22 +1,6 @@
 package mempool
 
-import "time"
-
 const (
-	// DefaultBlockPrioritySize is the default size in bytes for high-
-	// priority / low-fee transactions.  It is used to help determine which
-	// are allowed into the mempool and consequently affects their relay and
-	// inclusion when generating block templates.
-	DefaultBlockPrioritySize = 50000
-
-	// orphanTTL is the maximum amount of time an orphan is allowed to
-	// stay in the orphan pool before it expires and is evicted during the
-	// next scan.
-	//orphanTTL = time.Minute * 15
-
-	// orphanExpireScanInterval is the minimum amount of time in between
-	// scans of the orphan pool to evict expired transactions.
-	orphanExpireScanInterval = time.Minute * 5
 	// UnminedHeight is the height used for the "block" height field of the
 	// contextual transaction information provided in a transaction store
 	// when it has not yet been mined into a block.
