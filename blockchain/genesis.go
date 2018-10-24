8c9bde5e9aebfaaad2062f0e1b0a88385d2dd7d8
0xsirrush <thaibao@autonomous.nyc>
2018-10-24 11:32:27 +0700

    - get basic salary from prev-block in chain

--- blockchain/genesis.go
+++ blockchain/genesis.go
@@ -247,7 +247,7 @@ func (self GenesisBlockGenerator) calcCommitmentMerkleRoot(tx *transaction.Tx) c
 	return &genesisBlock
 }*/
 
-func (self GenesisBlockGenerator) CreateGenesisBlockPoSParallel(nonce int, difficulty uint32, version int, initialAddress string, preSelectValidators []string, initSalaryFund uint64) *Block {
+func (self GenesisBlockGenerator) CreateGenesisBlockPoSParallel(nonce int, difficulty uint32, version int, initialAddress string, preSelectValidators []string, initSalaryFund uint64, basicSalary uint64) *Block {
 	//init the loc
 	loc, _ := time.LoadLocation("America/New_York")
 	time := time.Date(2018, 8, 1, 0, 0, 0, 0, loc)
@@ -256,6 +256,9 @@ func (self GenesisBlockGenerator) CreateGenesisBlockPoSParallel(nonce int, diffi
 	genesisBlock.Header.Timestamp = time.Unix()
 	genesisBlock.Header.Version = version
 	genesisBlock.Header.Committee = make([]string, len(preSelectValidators))
+	genesisBlock.Header.GovernanceParams = GovernanceParams{
+		BasicSalary: basicSalary,
+	}
 	copy(genesisBlock.Header.Committee, preSelectValidators)
 
 	genesisBlock.Height = 1
