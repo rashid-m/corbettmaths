52dc3ceb2a52341c496c7f418b36b7e44f9fef05
0xsirrush <thaibao@autonomous.nyc>
2018-10-25 13:44:32 +0700

    - response more info about chain info

--- rpcserver/jsonresult/getbestblockresult.go
+++ rpcserver/jsonresult/getbestblockresult.go
@@ -5,11 +5,13 @@ type GetBestBlockResult struct {
 }
 
 type GetBestBlockItem struct {
-	Height      int32  `json:"Height"`
-	Hash        string `json:"Hash"`
-	TotalTxs    uint64 `json:"TotalTxs"`
-	SalaryFund  uint64 `json:"SalaryFund"`
-	BasicSalary uint64 `json:"BasicSalary"`
+	Height           int32  `json:"Height"`
+	Hash             string `json:"Hash"`
+	TotalTxs         uint64 `json:"TotalTxs"`
+	SalaryFund       uint64 `json:"SalaryFund"`
+	BasicSalary      uint64 `json:"BasicSalary"`
+	BlockProducer    string `json:"BlockProducer"`
+	BlockProducerSig string `json:"BlockProducerSig"`
 }
 
 type GetBestBlockHashResult struct {
