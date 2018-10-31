84e6da1bcd0a26aabc7e3e916754f2338b37566c
0xsirrush <thaibao@autonomous.nyc>
2018-10-31 16:23:29 +0700

    - update build file

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,1 +1,14 @@
-go build -o cashctl
+echo "Start build constantctl"
+
+echo "go get"
+go get -d
+
+APP_NAME="constantctl"
+
+echo "go build -o $APP_NAME"
+go build -o $APP_NAME
+
+echo "cp ./$APP_NAME $GOPATH/bin/$APP_NAME"
+mv ./$APP_NAME $GOPATH/bin/$APP_NAME
+
+echo "Build constantctl success!"
