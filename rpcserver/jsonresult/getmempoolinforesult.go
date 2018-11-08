faf881dd58837e8c5fd2f42c8ac64bc0cfb356bb
0xsirrush <thaibao@autonomous.nyc>
2018-11-08 11:27:53 +0700

    - get mempool info

--- rpcserver/jsonresult/getmempoolinforesult.go
+++ rpcserver/jsonresult/getmempoolinforesult.go
@@ -1,10 +1,11 @@
 package jsonresult
 
 type GetMempoolInfo struct {
-	Size          int    `json:"Size"`
-	Bytes         uint64 `json:"Bytes"`
-	Usage         uint64 `json:"Usage"`
-	MaxMempool    uint64 `json:"MaxMempool"`
-	MempoolMinFee uint64 `json:"MempoolMinFee"`
-	MempoolMaxFee uint64 `json:"MempoolMaxFee"`
+	Size          int      `json:"Size"`
+	Bytes         uint64   `json:"Bytes"`
+	Usage         uint64   `json:"Usage"`
+	MaxMempool    uint64   `json:"MaxMempool"`
+	MempoolMinFee uint64   `json:"MempoolMinFee"`
+	MempoolMaxFee uint64   `json:"MempoolMaxFee"`
+	ListTxs       []string `json:"ListTxs"`
 }
