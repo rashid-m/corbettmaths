6011247b5a3801716f94ae5fc459558c4a9d046e
0xsirrush <thaibao@autonomous.nyc>
2018-11-08 15:41:32 +0700

    - transfer custom token

--- database/lvdb/customtoken.go
+++ database/lvdb/customtoken.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"github.com/syndtr/goleveldb/leveldb/util"
 	"github.com/ninjadotorg/constant/common"
+	"log"
 )
 
 func (db *db) StoreCustomToken(tokenID *common.Hash, txHash []byte) error {
@@ -15,15 +16,16 @@ func (db *db) StoreCustomToken(tokenID *common.Hash, txHash []byte) error {
 }
 
 func (db *db) StoreCustomTokenTx(tokenID *common.Hash, chainID byte, blockHeight int32, txIndex int32, txHash []byte) error {
-	key := db.getKey(string(tokenPrefix), tokenID) // token-{tokenID}-chainID-(999999999-blockHeight)-txIndex
-	key = append(key, tokenID[:]...)
+	bigNumber := int32(999999999)
+	key := db.getKey(string(tokenPrefix), tokenID) // token-{tokenID}-chainID-(999999999-blockHeight)-(999999999-txIndex)
 	key = append(key, chainID)
 	bs := make([]byte, 4)
-	binary.LittleEndian.PutUint32(bs, uint32(999999999-blockHeight))
+	binary.LittleEndian.PutUint32(bs, uint32(bigNumber-blockHeight))
 	key = append(key, bs...)
 	bs = make([]byte, 4)
-	binary.LittleEndian.PutUint32(bs, uint32(999999999-txIndex))
+	binary.LittleEndian.PutUint32(bs, uint32(bigNumber-txIndex))
 	key = append(key, bs...)
+	log.Println(string(key))
 	if err := db.lvdb.Put(key, txHash, nil); err != nil {
 		return err
 	}
@@ -40,14 +42,17 @@ func (db *db) ListCustomToken() ([][]byte, error) {
 	return result, nil
 }
 
-func (db *db) CustomTokenTxs(tokenID *common.Hash) ([][]byte, error) {
-	result := make([][]byte, 0)
-	key := tokenPrefix
-	key = append(key, tokenID[:]...)
+func (db *db) CustomTokenTxs(tokenID *common.Hash) ([]*common.Hash, error) {
+	result := make([]*common.Hash, 0)
+	key := db.getKey(string(tokenPrefix), tokenID)
 	// key = token-{tokenID}
 	iter := db.lvdb.NewIterator(util.BytesPrefix(key), nil)
+	log.Println(string(key))
 	for iter.Next() {
-		result = append(result, iter.Value())
+		value := iter.Value()
+		hash := common.Hash{}
+		hash.SetBytes(value)
+		result = append(result, &hash)
 	}
 	iter.Release()
 	return result, nil
