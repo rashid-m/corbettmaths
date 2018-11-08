0d42c3c4416e714752f98de2a28d6b0320882f0e
0xsirrush <thaibao@autonomous.nyc>
2018-11-08 10:03:34 +0700

    - get list token

---
+++ rpcserver/jsonresult/listcustomtoken.go
@@ -0,0 +1,21 @@
+package jsonresult
+
+import "github.com/ninjadotorg/constant/transaction"
+
+type CustomToken struct {
+	ID     string `json:"ID"`
+	Name   string `json:"Name"`
+	Symbol string `json:"Symbol"`
+	Amount uint64 `json:"Amount"`
+}
+
+func (self *CustomToken) Init(obj transaction.TxCustomToken) {
+	self.ID = obj.TxToken.PropertyID.String()
+	self.Symbol = obj.TxToken.PropertySymbol
+	self.Name = obj.TxToken.PropertyName
+	self.Amount = obj.TxToken.Amount
+}
+
+type ListCustomToken struct {
+	ListCustomToken []CustomToken `json:"ListCustomToken"`
+}
