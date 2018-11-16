a8ac883573eaef3915c015d32872ad179a71556d
0xjackalope <huu@ninja.org>
2018-11-16 12:14:32 +0700

    add hash function to new transaction

--- transaction/acceptproposal.go
+++ transaction/acceptproposal.go
@@ -1,9 +1,27 @@
 package transaction
 
-type TxlAcceptGovProposal struct {
+import "github.com/ninjadotorg/constant/common"
 
+type TxlAcceptGovProposal struct {
+	Tx
+	GovProposalTXID *common.Hash
 }
 
 type TxlAcceptDCBProposal struct {
+	Tx
+	DCBProposalTXID *common.Hash
+}
 
+func (thisTx TxlAcceptDCBProposal) Hash() *common.Hash {
+	record := string(common.ToBytes(thisTx.Tx.Hash()))
+	record += string(common.ToBytes(thisTx.DCBProposalTXID))
+	hash := common.DoubleHashH([]byte(record))
+	return &hash
+}
+
+func (thisTx TxlAcceptGovProposal) Hash() *common.Hash {
+	record := string(common.ToBytes(thisTx.Tx.Hash()))
+	record += string(common.ToBytes(thisTx.GovProposalTXID))
+	hash := common.DoubleHashH([]byte(record))
+	return &hash
 }
--- transaction/votingtx.go
+++ transaction/votingtx.go
@@ -1,19 +1,52 @@
 package transaction
 
+import "github.com/ninjadotorg/constant/common"
+
 type TxVoteDCBProposal struct {
 	Tx
-	TxVoteDCBProposalData TxVoteDCBProposalData
+	VoteDCBProposalData VoteDCBProposalData
 }
 
 type TxVoteGovProposal struct {
 	Tx
-	TxVoteGovProposalData TxVoteGovProposalData
+	VoteGovProposalData VoteGovProposalData
 }
 
-type TxVoteGovProposalData struct {
+type VoteGovProposalData struct {
+	GovProposalTXID *common.Hash
 	AmountVoteToken uint32
 }
 
-type TxVoteDCBProposalData struct{
+type VoteDCBProposalData struct{
+	DCBProposalTXID *common.Hash
 	AmountVoteToken uint32
 }
+
+func (VoteDCBProposalData VoteDCBProposalData) Hash() *common.Hash {
+	record := string(common.ToBytes(VoteDCBProposalData))
+	record += string(VoteDCBProposalData.AmountVoteToken)
+	hash := common.DoubleHashH([]byte(record))
+	return &hash
+}
+
+func (VoteGovProposalData VoteGovProposalData) Hash() *common.Hash {
+	record := string(common.ToBytes(VoteGovProposalData))
+	record += string(VoteGovProposalData.AmountVoteToken)
+	hash := common.DoubleHashH([]byte(record))
+	return &hash
+}
+
+func (thisTx TxVoteDCBProposal) Hash() *common.Hash{
+	record := string(common.ToBytes(thisTx.Tx.Hash()))
+	record += string(common.ToBytes(thisTx.VoteDCBProposalData.Hash()))
+	hash := common.DoubleHashH([]byte(record))
+	return &hash
+}
+
+func (thisTx TxVoteGovProposal) Hash() *common.Hash{
+	record := string(common.ToBytes(thisTx.Tx.Hash()))
+	record += string(common.ToBytes(thisTx.VoteGovProposalData.Hash()))
+	hash := common.DoubleHashH([]byte(record))
+	return &hash
+}
+
