560920a157da911a84121c1775a70447b1e6a35b
0xjackalope <huu@ninja.org>
2018-11-20 23:15:04 +0700

    validate in 4 places

--- transaction/txvotingboard.go
+++ transaction/txvotingboard.go
@@ -3,47 +3,45 @@ package transaction
 import "github.com/ninjadotorg/constant/common"
 
 type TxVoteDCBBoard struct {
-	Tx
+	TxCustomToken
 	VoteDCBBoardData VoteDCBBoardData
 }
 
 type VoteDCBBoardData struct {
 	CandidatePubKey string
-	AmountDCBToken uint32
 }
 
 type TxVoteGOVBoard struct {
-	Tx
+	TxCustomToken
 	VoteGOVBoardData VoteGOVBoardData
 }
 
 type VoteGOVBoardData struct {
 	CandidatePubKey string
-	AmountGOVToken uint32
 }
 
 func (thisTx TxVoteDCBBoard) Hash() *common.Hash{
-	record := string(common.ToBytes(thisTx.Hash()))
+	record := string(common.ToBytes(thisTx.TxCustomToken.Hash()))
 	record += string(common.ToBytes(thisTx.VoteDCBBoardData.Hash()))
 	hash := common.DoubleHashH([]byte(record))
 	return &hash
 }
 
 func (VoteDCBBoardData VoteDCBBoardData) Hash() *common.Hash {
-	record := string(VoteDCBBoardData.AmountDCBToken) + VoteDCBBoardData.CandidatePubKey
+	record := VoteDCBBoardData.CandidatePubKey
 	hash := common.DoubleHashH([]byte(record))
 	return &hash
 }
 
 func (thisTx TxVoteGOVBoard) Hash() *common.Hash{
-	record := string(common.ToBytes(thisTx.Hash()))
+	record := string(common.ToBytes(thisTx.TxCustomToken.Hash()))
 	record += string(common.ToBytes(thisTx.VoteGOVBoardData.Hash()))
 	hash := common.DoubleHashH([]byte(record))
 	return &hash
 }
 
 func (VoteGOVBoardData VoteGOVBoardData) Hash() *common.Hash {
-	record := string(VoteGOVBoardData.AmountGOVToken) + VoteGOVBoardData.CandidatePubKey
+	record := VoteGOVBoardData.CandidatePubKey
 	hash := common.DoubleHashH([]byte(record))
 	return &hash
 }
