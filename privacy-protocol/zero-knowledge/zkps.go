370c1c6547f877934c53090dc6f1240ac52c43de
0xkraken <hien.n@autonomous.nyc>
2018-11-21 14:27:40 +0700

    replacing HashGenerator to HashPoint in ecc.go

--- privacy-protocol/zero-knowledge/zkps.go
+++ privacy-protocol/zero-knowledge/zkps.go
@@ -13,3 +13,16 @@ type ZKProtocols interface {
 	SetProof(proof Proof)
 	Verify() bool
 }
+
+
+func Prove(){
+	// Commit each component of coins being spent
+
+
+	// Summing all commitments into one commitment and proving the knowledge of its openings
+	// Proving one-out-of-N commitments is a commitment to the coins being spent
+	// Proving that serial number is derived from the committed derivator
+	// Proving that output values do not exceed v_max
+	// Proving that sum of inputs equals sum of outputs
+	// Proving ciphertexts encrypting for coins' details are well-formed
+}
