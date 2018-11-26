95c057b06fd3d585e5c3ea8dedc3514a516d37fc
dbchiem <baoduy@autonomous.nyc>
2018-11-26 10:40:22 +0700

    Deploy SimpleLoan on rinkeby

--- stability/migrations/2_loan.js
+++ stability/migrations/2_loan.js
@@ -2,7 +2,7 @@ var MultiSigWallet = artifacts.require('MultiSigWallet')
 var SimpleLoan = artifacts.require('SimpleLoan')
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(MultiSigWallet, [accounts[0]], 1).then(() => {
+    deployer.deploy(MultiSigWallet, ["0x9dC6Bb8F3Fb33FECe7a89e0b1a8655A474172152"], 1).then(() => {
         return deployer.deploy(SimpleLoan, MultiSigWallet.address)
     })
 }
