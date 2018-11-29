109d5839a6d015131d8402f9a7b77ab8709c47fb
0xkraken <hien.n@autonomous.nyc>
2018-11-29 17:27:20 +0700

    update verify the proof

--- privacy-protocol/ecc.go
+++ privacy-protocol/ecc.go
@@ -287,19 +287,19 @@ func TestECC() bool {
 These functions have worked for my range proof protocol
 If there are any changes, please inform me first
 						TRONG-DAT														***********/
-func (eccPoint EllipticPoint) AddPoint(p EllipticPoint) EllipticPoint {
-	var res EllipticPoint
+func (eccPoint EllipticPoint) AddPoint(p *EllipticPoint) *EllipticPoint {
+	res := new(EllipticPoint)
 	res.X, res.Y = Curve.Add(eccPoint.X, eccPoint.Y, p.X, p.Y)
 	return res
 }
-func (eccPoint EllipticPoint) IsEqual(p EllipticPoint) bool {
+func (eccPoint EllipticPoint) IsEqual(p *EllipticPoint) bool {
 	if eccPoint.X.Cmp(p.X) == 0 && eccPoint.Y.Cmp(p.Y) == 0 {
 		return true
 	}
 	return false
 }
-func (eccPoint EllipticPoint) ScalarMulPoint(factor *big.Int) EllipticPoint {
-	var res EllipticPoint
+func (eccPoint EllipticPoint) ScalarMulPoint(factor *big.Int) *EllipticPoint {
+	res := new(EllipticPoint)
 	res.X, res.Y = Curve.ScalarMult(eccPoint.X, eccPoint.Y, factor.Bytes())
 	return res
 }
