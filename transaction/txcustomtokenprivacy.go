92768194d8a67fa239ef0bbb106254e9cd9ac8c2
0xsirrush <thaibao@autonomous.nyc>
2018-11-30 15:50:22 +0700

    - add txcustomtokenprivacy to support privacy on custom token

--- transaction/txcustomtokenprivacy.go
+++ transaction/txcustomtokenprivacy.go
@@ -4,7 +4,8 @@ package transaction
 // and contain data(with supporting privacy format) to support issuing and transfer a custom token(token from end-user, look like erc-20)
 // Dev or end-user can use this class tx to create an token type which use personal purpose
 // TxCustomTokenPrivacy is an advance format of TxCustomToken
+// so that user need to spend a lot fee to create this class tx
 type TxCustomTokenPrivacy struct {
-	Tx                                    // inherit from normal tx of constant(supporting privacy)
-	TxTokenPrivacyData TxTokenPrivacyData // v
+	Tx                                    // inherit from normal tx of constant(supporting privacy) with a high fee to ensure that tx could contain a big data of privacy for token
+	TxTokenPrivacyData TxTokenPrivacyData // supporting privacy format
 }
