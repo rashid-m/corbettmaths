023919e50ea8f8ba09dae1a8ff55a53190145298
0xsirrush <thaibao@autonomous.nyc>
2018-12-03 09:43:35 +0700

    - add txcustomtokenprivacy to support privacy on custom token

--- transaction/txcustomtokenprivacydata.go
+++ transaction/txcustomtokenprivacydata.go
@@ -1,6 +1,10 @@
 package transaction
 
-import "github.com/ninjadotorg/constant/common"
+import (
+	"github.com/ninjadotorg/constant/common"
+	"errors"
+	"fmt"
+)
 
 type TxTokenPrivacyData struct {
 	PropertyID     common.Hash // = hash of TxTokenData data
@@ -10,4 +14,18 @@ type TxTokenPrivacyData struct {
 	Type   int    // action type
 	Amount uint64 // init amount
 	Descs  []*JoinSplitDesc `json:"Descs"`
+}
+
+// Hash - return hash of token data, be used as Token ID
+func (self TxTokenPrivacyData) Hash() (*common.Hash, error) {
+	if self.Descs == nil {
+		return nil, errors.New("Vout is empty")
+	}
+	record := self.PropertyName + self.PropertySymbol + fmt.Sprintf("%d", self.Amount)
+	for _, out := range self.Descs {
+		record += out.toString()
+	}
+	// final hash
+	hash := common.DoubleHashH([]byte(record))
+	return &hash, nil
 }
