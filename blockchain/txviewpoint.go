94415b9411a8d4066ccf4b0b91ef1a3e2f292638
0xsirrush <thaibao@autonomous.nyc>
2018-12-04 17:30:15 +0700

    - update tx with privacy

--- blockchain/txviewpoint.go
+++ blockchain/txviewpoint.go
@@ -6,7 +6,6 @@ import (
 	"github.com/ninjadotorg/constant/common"
 	"github.com/ninjadotorg/constant/database"
 	"github.com/ninjadotorg/constant/transaction"
-	"github.com/ninjadotorg/constant/privacy-protocol"
 	"math/big"
 	"github.com/ninjadotorg/constant/privacy-protocol/zero-knowledge"
 )
@@ -15,6 +14,7 @@ type TxViewPoint struct {
 	chainID         byte
 	listNullifiers  [][]byte
 	listCommitments [][]byte
+	listSnD         []big.Int
 	customTokenTxs  map[int32]*transaction.TxCustomToken
 
 	// hash of best block in current
@@ -46,25 +46,38 @@ func (view *TxViewPoint) CurrentBestBlockHash() *common.Hash {
 }
 
 // fetch from desc of tx to get nullifiers and commitments
-func (view *TxViewPoint) processFetchTxViewPoint(block *Block, db database.DatabaseInterface, proof *zkp.PaymentProof) ([]byte, []byte, big.Int, error) {
-	acceptedNullifiers := []byte{}
-	acceptedCommitments := []byte{}
-	acceptedSnD := big.Int{}
-	temp, err := db.HasNullifier(proof.InputCoins.SerialNumber.Compress(), block.Header.ChainID)
-	if err != nil {
-		return nil, nil, 0, err
+func (view *TxViewPoint) processFetchTxViewPoint(block *Block, db database.DatabaseInterface, proof *zkp.PaymentProof) ([][]byte, [][]byte, []big.Int, error) {
+	acceptedNullifiers := make([][]byte, 0)
+	acceptedCommitments := make([][]byte, 0)
+	acceptedSnD := make([]big.Int, 0)
+	for _, item := range proof.InputCoins {
+		serialNum := item.CoinDetails.SerialNumber.Compress()
+		temp, err := db.HasNullifier(serialNum, block.Header.ChainID)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		if !temp {
+			acceptedNullifiers = append(acceptedNullifiers, serialNum)
+		}
 	}
-	if !temp {
-		acceptedNullifiers = outcoin.CoinDetails.SerialNumber.Compress()
+	for _, item := range proof.OutputCoins {
+		commitment := item.CoinDetails.CoinCommitment.Compress()
+		temp, err := db.HasCommitment(commitment, block.Header.ChainID)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		if !temp {
+			acceptedCommitments = append(acceptedCommitments, item.CoinDetails.CoinCommitment.Compress())
+		}
+
+		snD := item.CoinDetails.SNDerivator
+		// TODO
+		//temp, err := db.HasSND(snD, block.Header.ChainID)
+		if !temp {
+			acceptedSnD = append(acceptedSnD, *snD)
+		}
 	}
-	temp, err = db.HasCommitment(outcoin.CoinDetails.CoinCommitment.Compress(), block.Header.ChainID)
-	if err != nil {
-		return nil, nil, 0, err
-	}
-	if !temp {
-		acceptedCommitments = outcoin.CoinDetails.CoinCommitment.Compress()
-	}
-	return acceptedNullifiers, acceptedCommitments, outcoin.CoinDetails., nil
+	return acceptedNullifiers, acceptedCommitments, acceptedSnD, nil
 }
 
 /*
@@ -76,42 +89,40 @@ func (view *TxViewPoint) fetchTxViewPoint(db database.DatabaseInterface, block *
 	// Loop through all of the transaction descs (except for the salary tx)
 	acceptedNullifiers := make([][]byte, 0)
 	acceptedCommitments := make([][]byte, 0)
+	acceptedSnD := make([]big.Int, 0)
 	for indexTx, tx := range transactions {
 		switch tx.GetType() {
 		case common.TxNormalType:
 			{
-				normalTx := tx.(*transaction.TxNormal)
-				for _, desc := range normalTx.Descs {
-					temp1, temp2, err := view.processFetchTxViewPoint(block, db, desc)
-					acceptedNullifiers = append(acceptedNullifiers, temp1...)
-					acceptedCommitments = append(acceptedCommitments, temp2...)
-					if err != nil {
-						return NewBlockChainError(UnExpectedError, err)
-					}
+				normalTx := tx.(*transaction.Tx)
+				temp1, temp2, temp3, err := view.processFetchTxViewPoint(block, db, normalTx.Proof)
+				acceptedNullifiers = append(acceptedNullifiers, temp1...)
+				acceptedCommitments = append(acceptedCommitments, temp2...)
+				acceptedSnD = append(acceptedSnD, temp3...)
+				if err != nil {
+					return NewBlockChainError(UnExpectedError, err)
 				}
 			}
 		case common.TxSalaryType:
 			{
-				normalTx := tx.(*transaction.TxNormal)
-				for _, desc := range normalTx.Descs {
-					temp1, temp2, err := view.processFetchTxViewPoint(block, db, desc)
-					acceptedNullifiers = append(acceptedNullifiers, temp1...)
-					acceptedCommitments = append(acceptedCommitments, temp2...)
-					if err != nil {
-						return NewBlockChainError(UnExpectedError, err)
-					}
+				normalTx := tx.(*transaction.Tx)
+				temp1, temp2, temp3, err := view.processFetchTxViewPoint(block, db, normalTx.Proof)
+				acceptedNullifiers = append(acceptedNullifiers, temp1...)
+				acceptedCommitments = append(acceptedCommitments, temp2...)
+				acceptedSnD = append(acceptedSnD, temp3...)
+				if err != nil {
+					return NewBlockChainError(UnExpectedError, err)
 				}
 			}
 		case common.TxCustomTokenType:
 			{
 				tx := tx.(*transaction.TxCustomToken)
-				for _, outcoin := range tx.Proof.OutputCoins {
-					temp1, temp2, err := view.processFetchTxViewPoint(block, db, outcoin)
-					acceptedNullifiers = append(acceptedNullifiers, temp1...)
-					acceptedCommitments = append(acceptedCommitments, temp2...)
-					if err != nil {
-						return NewBlockChainError(UnExpectedError, err)
-					}
+				temp1, temp2, temp3, err := view.processFetchTxViewPoint(block, db, tx.Proof)
+				acceptedNullifiers = append(acceptedNullifiers, temp1...)
+				acceptedCommitments = append(acceptedCommitments, temp2...)
+				acceptedSnD = append(acceptedSnD, temp3...)
+				if err != nil {
+					return NewBlockChainError(UnExpectedError, err)
 				}
 				view.customTokenTxs[int32(indexTx)] = tx
 			}
@@ -141,6 +152,7 @@ func NewTxViewPoint(chainId byte) *TxViewPoint {
 		chainID:         chainId,
 		listNullifiers:  make([][]byte, 0),
 		listCommitments: make([][]byte, 0),
+		listSnD:         make([]big.Int, 0),
 		customTokenTxs:  make(map[int32]*transaction.TxCustomToken, 0),
 	}
 }
