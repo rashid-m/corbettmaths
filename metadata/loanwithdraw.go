50db8435e73ba45dedd4eeeef0f2803e8d523887
dbchiem <baoduy@autonomous.nyc>
2018-12-06 15:51:06 +0700

    Check valid until for loan withdraw

--- metadata/loanwithdraw.go
+++ metadata/loanwithdraw.go
@@ -47,6 +47,7 @@ func (lw *LoanWithdraw) ValidateTxWithBlockChain(txr Transaction, bcr Blockchain
 	}
 	foundResponse := 0
 	keyCorrect := false
+	validUntil := int32(0)
 	for _, txHash := range txHashes {
 		hash := &common.Hash{}
 		copy(hash[:], txHash)
@@ -85,6 +86,7 @@ func (lw *LoanWithdraw) ValidateTxWithBlockChain(txr Transaction, bcr Blockchain
 				}
 				if responseMeta.Response == Accept {
 					foundResponse += 1
+					validUntil = responseMeta.ValidUntil
 				}
 			}
 		}
@@ -93,6 +95,9 @@ func (lw *LoanWithdraw) ValidateTxWithBlockChain(txr Transaction, bcr Blockchain
 	minResponse := bcr.GetDCBParams().MinLoanResponseRequire
 	if foundResponse < int(minResponse) {
 		return false, fmt.Errorf("Not enough loan accepted response")
+	}
+	if bcr.GetHeight() >= validUntil {
+		return false, fmt.Errorf("Loan is not valid anymore, cannot claim Constant")
 	}
 	if !keyCorrect {
 		return false, fmt.Errorf("Provided key is incorrect")
