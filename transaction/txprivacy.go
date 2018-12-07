aabfdca9e284d346ffd640636a454f76626851e8
0xsirrush <thaibao@autonomous.nyc>
2018-12-07 14:32:43 +0700

    - refactor code

--- transaction/txprivacy.go
+++ transaction/txprivacy.go
@@ -180,7 +180,7 @@ func (tx *Tx) CreateTx(
 			sndOuts = append(sndOuts, sndOut)
 		}
 
-		ok = CheckDuplicate(sndOuts)
+		ok = common.CheckDuplicateBigInt(sndOuts)
 	}
 
 	// create new output coins with info: Pk, value, last byte of pk, snd
@@ -471,10 +471,5 @@ func EstimateTxSize(usableTx []*Tx, payments []*privacy.PaymentInfo) uint64 {
 // CheckSND return true if snd exists in snDerivators list
 func CheckSNDExistence(snd *big.Int) bool {
 	//todo: query from db to get snDerivators
-	return false
-}
-
-// CheckDuplicate returns true if there are at least 2 elements in array have same values
-func CheckDuplicate(arr []*big.Int) bool {
 	return false
 }
