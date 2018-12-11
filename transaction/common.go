03d3d9ee9fe71f48b141552abdacf1d652285f2e
0xsirrush <thaibao@autonomous.nyc>
2018-12-11 10:56:55 +0700

    - refactor code

--- transaction/common.go
+++ transaction/common.go
@@ -2,11 +2,12 @@ package transaction
 
 import "github.com/ninjadotorg/constant/privacy-protocol"
 
-func GetInputCoins(usableTx []*Tx) []*privacy.InputCoin {
+// ConvertOutputCoinToInputCoin - convert output coin from old tx to input coin for new tx
+func ConvertOutputCoinToInputCoin(usableTxOfOld []*Tx) []*privacy.InputCoin {
 	var inputCoins []*privacy.InputCoin
 	inCoin := new(privacy.InputCoin)
 
-	for _, tx := range usableTx {
+	for _, tx := range usableTxOfOld {
 		for _, coin := range tx.Proof.OutputCoins {
 			inCoin.CoinDetails = coin.CoinDetails
 			inputCoins = append(inputCoins, inCoin)
