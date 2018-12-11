73bc0e8fc2f80022f506ad043f099206636e18b7
0xsirrush <thaibao@autonomous.nyc>
2018-12-11 16:39:40 +0700

    - update

--- rpcserver/constants.go
+++ rpcserver/constants.go
@@ -19,7 +19,6 @@ const (
 	GetBlockCount     = "getblockcount"
 	GetBlockHash      = "getblockhash"
 
-	ListUnspentTxByPaymentAddress       = "listunspentbypaymentaddress"
 	ListTransactions                    = "listtransactions"
 	CreateRawTransaction                = "createtransaction"
 	SendRawTransaction                  = "sendtransaction"
