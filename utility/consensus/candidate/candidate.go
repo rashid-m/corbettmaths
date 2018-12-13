0c13e8c33c187ec170ea962d07ad9e92e4e35da2
Unknown <1411604@hcmut.edu.vn>
2018-12-13 17:35:27 +0700

    assign unAssignCandidate to assignCandidate	& clear UnassignShardCandidate

--- utility/consensus/candidate/candidate.go
+++ utility/consensus/candidate/candidate.go
@@ -3,12 +3,14 @@ package candidate
 import (
 	"crypto/sha256"
 	"errors"
-	"github.com/ninjadotorg/constant/blockchain"
+	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/ninjadotorg/constant/blockchain"
 )
 
-func UpdateBeaconBestState(beaconBestState *blockchain.BestStateBeacon, newBlock *blockchain.BlockV2) *blockchain.BestStateBeacon {
+func UpdateBeaconBestState(beaconBestState *blockchain.BestStateBeacon, newBlock *blockchain.BlockV2) (*blockchain.BestStateBeacon, error) {
 	// TODO:
 	// update BestShardHash, BestBlock, BestBlockHash
 	// unassign -> remove out the candidate
@@ -18,7 +20,45 @@ func UpdateBeaconBestState(beaconBestState *blockchain.BestStateBeacon, newBlock
 		beaconBestState.UnassignBeaconCandidate = append(beaconBestState.UnassignBeaconCandidate, newBeaconNode...)
 		beaconBestState.UnassignShardCandidate = append(beaconBestState.UnassignShardCandidate, newShardNode...)
 		//TODO: assign unAssignCandidate to assignCandidate	& clear UnassignShardCandidate
+		// ShardCandidate         map[byte][]string
+		// ShardPendingCandidate  map[byte][]string
+		// BeaconCandidate        []string
+		// BeaconPendingCandidate []string
 
+		/// Shuffle candidate for shard
+		// assign UnassignShardCandidate to ShardPendingCandidate with CurrentRandom this shard
+		err := AssignValidatorShard(beaconBestState.ShardPendingCandidate, beaconBestState.UnassignShardCandidate, beaconBestState.CurrentRandomNumber)
+		// reset beaconBestState.UnassignShardCandidate
+		beaconBestState.UnassignShardCandidate = []string{}
+
+		if err != nil {
+			return beaconBestState, err
+		}
+
+		// swap 3 validators each time
+		const offset = int(3)
+		for i := 0; i < 256; i++ {
+			shardID := byte(i)
+			//swap validator for each shard
+			beaconBestState.ShardPendingCandidate[shardID], beaconBestState.ShardCandidate[shardID], err = SwapValidator(beaconBestState.ShardPendingCandidate[shardID], beaconBestState.ShardCandidate[shardID], offset)
+			if err != nil {
+				return beaconBestState, err
+			}
+		}
+		// ShuffleCandidate
+		shuffleBeaconCandidate, err := ShuffleCandidate(beaconBestState.UnassignBeaconCandidate, beaconBestState.CurrentRandomNumber)
+		if err != nil {
+			return beaconBestState, err
+		}
+		// append new candidate to pending validator in beacon
+		beaconBestState.BeaconPendingCandidate = append(beaconBestState.BeaconPendingCandidate, shuffleBeaconCandidate...)
+		// reset UnassignBeaconCandidate
+		beaconBestState.UnassignBeaconCandidate = []string{}
+		//swap validator in beacon
+		beaconBestState.BeaconPendingCandidate, beaconBestState.BeaconCandidate, err = SwapValidator(beaconBestState.BeaconPendingCandidate, beaconBestState.BeaconCandidate, offset)
+		if err != nil {
+			return beaconBestState, err
+		}
 		// update random number for new epoch
 		beaconBestState.CurrentRandomNumber = beaconBestState.NextRandomNumber
 	} else {
@@ -29,7 +69,7 @@ func UpdateBeaconBestState(beaconBestState *blockchain.BestStateBeacon, newBlock
 	}
 	// TODO: Param "set" "del"
 
-	return beaconBestState
+	return beaconBestState, nil
 }
 
 func GetStakingCandidate(beaconBlock *blockchain.BlockV2) (beacon []string, shard []string) {
@@ -60,6 +100,15 @@ func AssignValidator(candidates []string, rand int64) (map[byte][]string, error)
 		pendingValidators[shardID] = append(pendingValidators[shardID], candidate)
 	}
 	return pendingValidators, nil
+}
+
+// AssignValidatorShard, param for better convenice than AssignValidator
+func AssignValidatorShard(currentCandidates map[byte][]string, shardCandidates []string, rand int64) error {
+	for _, candidate := range shardCandidates {
+		shardID := calculateHash(candidate, rand)
+		currentCandidates[shardID] = append(currentCandidates[shardID], candidate)
+	}
+	return nil
 }
 
 func VerifyValidator(candidate string, rand int64, shardID byte) (bool, error) {
@@ -113,4 +162,21 @@ func SwapValidator(pendingValidators []string, currentValidators []string, offse
 	// enqueue new validator to the remaning of current validators list
 	currentValidators = append(currentValidators, tempValidators...)
 	return pendingValidators, currentValidators, nil
+}
+
+func ShuffleCandidate(candidates []string, rand int64) ([]string, error) {
+	hashes := []string{}
+	m := make(map[string]string)
+	sortedCandidate := []string{}
+	for _, candidate := range candidates {
+		seed := candidate + strconv.Itoa(int(rand))
+		hash := sha256.Sum256([]byte(seed))
+		hashes = append(hashes, string(hash[:32]))
+		m[string(hash[:32])] = candidate
+	}
+	sort.Strings(hashes)
+	for _, candidate := range hashes {
+		sortedCandidate = append(sortedCandidate, candidate)
+	}
+	return hashes, nil
 }
