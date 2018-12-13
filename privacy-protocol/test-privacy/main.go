14c42fddbb89ce5795723b5f9064d7c36b28d821
0xkraken <hien.n@autonomous.nyc>
2018-12-13 15:59:11 +0700

    Fixtypo

--- privacy-protocol/test-privacy/main.go
+++ privacy-protocol/test-privacy/main.go
@@ -260,7 +260,7 @@ func main() {
 	//fmt.Println(coin.SerialNumber == nil)
 	//fmt.Printf("coin.Serial numbre: %v\n", coin.SerialNumber)
 
-	num := 12456
+	num := 0
 	bytes := privacy.IntToByteArr(num)
 	fmt.Printf("bytes: %v\n", bytes)
 
