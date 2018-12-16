ed53a9be57a30033e915a7434698bb5035a8e9ee
Duc Dinh <autonomous@Autonomouss-MacBook-Pro-2.local>
2018-12-16 18:53:06 +0700

    Add Contracting request metadata

--- metadata/constants.go
+++ metadata/constants.go
@@ -19,6 +19,7 @@ const (
 	BuyBackResponseMeta
 	IssuingRequestMeta
 	IssuingResponseMeta
+	ContractingRequestMeta
 	DividendMeta
 
 	CrowdsaleRequestMeta
--- rpcserver/rpcfunctiondcb.go
+++ rpcserver/rpcfunctiondcb.go
@@ -1,5 +1,15 @@
 package rpcserver
 
+import (
+	"encoding/hex"
+	"encoding/json"
+
+	"github.com/ninjadotorg/constant/common"
+	"github.com/ninjadotorg/constant/metadata"
+	privacy "github.com/ninjadotorg/constant/privacy-protocol"
+	"github.com/ninjadotorg/constant/rpcserver/jsonresult"
+)
+
 // handleGetDCBParams - get dcb params
 func (self RpcServer) handleGetDCBParams(params interface{}, closeChan <-chan struct{}) (interface{}, error) {
 	dcbParam := self.config.BlockChain.BestState[0].BestBlock.Header.DCBConstitution.DCBParams
@@ -15,4 +25,106 @@ func (self RpcServer) handleGetDCBConstitution(params interface{}, closeChan <-c
 // handleGetListDCBBoard - return list payment address of DCB board
 func (self RpcServer) handleGetListDCBBoard(params interface{}, closeChan <-chan struct{}) (interface{}, error) {
 	return self.config.BlockChain.BestState[0].BestBlock.Header.DCBGovernor.DCBBoardPubKeys, nil
+}
+
+func (self RpcServer) handleCreateRawTxWithIssuingRequest(params interface{}, closeChan <-chan struct{}) (interface{}, error) {
+	arrayParams := common.InterfaceSlice(params)
+	normalTx, err := self.buildRawTransaction(params)
+	if err != nil {
+		Logger.log.Error(err)
+		return nil, NewRPCError(ErrUnexpected, err)
+	}
+	// Req param #4: issuing request info
+	issuingReq := arrayParams[4].(map[string]interface{})
+	depositedAmount := uint64(issuingReq["depositedAmount"].(float64))
+	assetTypeBytes := []byte(issuingReq["assetType"].(string))
+	assetType := common.Hash{}
+	copy(assetType[:], assetTypeBytes)
+	metaType := metadata.IssuingRequestMeta
+	receiverAddressMap := issuingReq["receiverAddress"].(map[string]interface{})
+	receiverAddress := privacy.PaymentAddress{
+		Pk: []byte(receiverAddressMap["pk"].(string)),
+		Tk: []byte(receiverAddressMap["tk"].(string)),
+	}
+
+	normalTx.Metadata = metadata.NewIssuingRequest(
+		receiverAddress,
+		depositedAmount,
+		assetType,
+		metaType,
+	)
+	byteArrays, err := json.Marshal(normalTx)
+	if err != nil {
+		Logger.log.Error(err)
+		return nil, NewRPCError(ErrUnexpected, err)
+	}
+	hexData := hex.EncodeToString(byteArrays)
+	result := jsonresult.CreateTransactionResult{
+		HexData: hexData,
+	}
+	return result, nil
+}
+
+func (self RpcServer) handleCreateAndSendTxWithIssuingRequest(params interface{}, closeChan <-chan struct{}) (interface{}, error) {
+	data, err := self.handleCreateRawTxWithIssuingRequest(params, closeChan)
+	if err != nil {
+		return nil, err
+	}
+	tx := data.(jsonresult.CreateTransactionResult)
+	hexStrOfTx := tx.HexData
+	if err != nil {
+		return nil, NewRPCError(ErrUnexpected, err)
+	}
+	newParam := make([]interface{}, 0)
+	newParam = append(newParam, hexStrOfTx)
+	sendResult, err := self.handleSendRawTransaction(newParam, closeChan)
+	if err != nil {
+		return nil, NewRPCError(ErrUnexpected, err)
+	}
+	result := jsonresult.CreateTransactionResult{
+		TxID: sendResult.(jsonresult.CreateTransactionResult).TxID,
+	}
+	return result, nil
+}
+
+func (self RpcServer) handleCreateRawTxWithContractingRequest(params interface{}, closeChan <-chan struct{}) (interface{}, error) {
+	normalTx, err := self.buildRawTransaction(params)
+	if err != nil {
+		Logger.log.Error(err)
+		return nil, NewRPCError(ErrUnexpected, err)
+	}
+	metaType := metadata.ContractingRequestMeta
+	normalTx.Metadata = metadata.NewContractingRequest(metaType)
+	byteArrays, err := json.Marshal(normalTx)
+	if err != nil {
+		Logger.log.Error(err)
+		return nil, NewRPCError(ErrUnexpected, err)
+	}
+	hexData := hex.EncodeToString(byteArrays)
+	result := jsonresult.CreateTransactionResult{
+		HexData: hexData,
+	}
+	return result, nil
+}
+
+func (self RpcServer) handleCreateAndSendTxWithContractingRequest(params interface{}, closeChan <-chan struct{}) (interface{}, error) {
+	data, err := self.handleCreateRawTxWithContractingRequest(params, closeChan)
+	if err != nil {
+		return nil, err
+	}
+	tx := data.(jsonresult.CreateTransactionResult)
+	hexStrOfTx := tx.HexData
+	if err != nil {
+		return nil, NewRPCError(ErrUnexpected, err)
+	}
+	newParam := make([]interface{}, 0)
+	newParam = append(newParam, hexStrOfTx)
+	sendResult, err := self.handleSendRawTransaction(newParam, closeChan)
+	if err != nil {
+		return nil, NewRPCError(ErrUnexpected, err)
+	}
+	result := jsonresult.CreateTransactionResult{
+		TxID: sendResult.(jsonresult.CreateTransactionResult).TxID,
+	}
+	return result, nil
 }
