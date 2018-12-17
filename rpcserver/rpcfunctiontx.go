bb7612a18dfe4fcf321a53a9385f92efa28fefb3
0xsirrush <thaibao@autonomous.nyc>
2018-12-17 11:40:26 +0700

    - fee estimator

--- rpcserver/rpcfunctiontx.go
+++ rpcserver/rpcfunctiontx.go
@@ -151,7 +151,6 @@ func (self RpcServer) buildRawTransaction(params interface{}) (*transaction.Tx, 
 		estimateFeeCoinPerKb = int64(temp)
 	}
 	estimateFeeCoinPerKb += int64(self.config.Wallet.Config.IncrementalFee)
-	// TODO
 	estimateTxSizeInKb := transaction.EstimateTxSize(candidateOutputCoins, nil)
 	realFee = uint64(estimateFeeCoinPerKb) * uint64(estimateTxSizeInKb)
 
