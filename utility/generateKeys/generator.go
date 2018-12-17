89a5fd74c0f4d8cd62acb1d3a62640ffb6d2bd9d
Unknown <1411604@hcmut.edu.vn>
2018-12-17 17:23:37 +0700

    change public key value

--- utility/generateKeys/generator.go
+++ utility/generateKeys/generator.go
@@ -7,16 +7,17 @@ import (
 )
 
 func main() {
-	inst := [][]string{}
+	fmt.Println(generator.GenerateAddress(generator.PreSelectShardNodeTestnet))
+	// inst := [][]string{}
 	// build validator beacon
-	strBeacon := []string{"assign"}
-	strBeacon = append(strBeacon, generator.PreSelectBeaconNodeTestnetSerializedPubkey...)
-	strBeacon = append(strBeacon, "beacon")
+	// strBeacon := []string{"assign"}
+	// strBeacon = append(strBeacon, generator.PreSelectBeaconNodeTestnetSerializedPubkey...)
+	// strBeacon = append(strBeacon, "beacon")
 
-	strShard := []string{"assign"}
-	strShard = append(strShard, generator.PreSelectShardNodeTestnetSerializedPubkey...)
-	strShard = append(strShard, "shard")
-	inst = append(inst, strBeacon)
-	inst = append(inst, strShard)
-	fmt.Println(inst)
+	// strShard := []string{"assign"}
+	// strShard = append(strShard, generator.PreSelectShardNodeTestnetSerializedPubkey...)
+	// strShard = append(strShard, "shard")
+	// inst = append(inst, strBeacon)
+	// inst = append(inst, strShard)
+	// fmt.Println(inst)
 }
