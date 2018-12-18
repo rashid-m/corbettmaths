1af72ed6554c9e8b79954d9f9d657668652c105b
0xBahamoot <lam@autonomous.nyc>
2018-12-18 11:50:36 +0700

    update related to chainstate

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -3,9 +3,11 @@ package blockchain
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"math"
 	"math/big"
 	"strconv"
 	"strings"
@@ -189,10 +191,28 @@ func (self *BlockChain) initChainState() error {
 	// Determine the state of the chain database. We may need to initialize
 	// everything from scratch or upgrade certain buckets.
 	var initialized bool
-	nodeRole := self.config.NodeRole
-	if nodeRole == "beacon" || nodeRole == "shard" {
+	bestStateBeaconBytes, err := self.config.DataBase.FetchBeaconBestState()
+	if err == nil {
+		err = json.Unmarshal(bestStateBeaconBytes, self.BestState.Beacon)
+		if err != nil {
+			initialized = false
+		} else {
+			initialized = true
+		}
+	} else {
+		initialized = false
+	}
+
+	if !initialized {
+		// At this point the database has not already been initialized, so
+		// initialize both it and the chain state to the genesis block.
+		err := self.createBeaconState()
+		if err != nil {
+			return err
+		}
 
 	}
+
 	self.BestState.Shard = make(map[byte]*BestStateShard)
 	for shard := 1; shard < self.config.ChainParams.ShardsNum; shard++ {
 		shardID := byte(shard - 1)
@@ -1359,4 +1379,146 @@ func (self BlockChain) RandomCommitmentsProcess(usableInputCoins []*privacy.Inpu
 
 func (self BlockChain) CheckSNDerivatorExistence(snd *big.Int, shardID byte) (bool, error) {
 	return transaction.CheckSNDerivatorExistence(snd, shardID, self.config.DataBase)
+}
+
+func (self *BlockChain) GetAmountPerAccount(proposal *metadata.DividendProposal) (uint64, []string, []uint64, error) {
+	// TODO(@0xsirrush): cache list so that list of receivers is fixed across blocks
+	tokenHolders, err := self.config.DataBase.GetCustomTokenPaymentAddressesBalance(proposal.TokenID)
+	if err != nil {
+		return 0, nil, nil, err
+	}
+
+	// Get total token supply
+	totalTokenSupply := uint64(0)
+	for _, value := range tokenHolders {
+		totalTokenSupply += value
+	}
+
+	// Get amount per account (only count unrewarded utxo)
+	rewardHolders := []string{}
+	amounts := []uint64{}
+	for holder, _ := range tokenHolders {
+		temp, _ := hex.DecodeString(holder)
+		paymentAddress := (&privacy.PaymentAddress{}).FromBytes(temp)
+		vouts, err := self.parseCustomTokenUTXO(proposal.TokenID, paymentAddress.Pk)
+		if err != nil {
+			return 0, nil, nil, err
+		}
+		amount := uint64(0)
+		for _, vout := range vouts {
+			amount += vout.Value
+		}
+
+		if amount > 0 {
+			rewardHolders = append(rewardHolders, holder)
+			amounts = append(amounts, amount)
+		}
+	}
+	return totalTokenSupply, rewardHolders, amounts, nil
+}
+
+func isAnyBoardAddressInVins(customToken *transaction.TxCustomToken) bool {
+	GOVAddressStr := string(common.GOVAddress)
+	DCBAddressStr := string(common.DCBAddress)
+	for _, vin := range customToken.TxTokenData.Vins {
+		apkStr := string(vin.PaymentAddress.Pk[:])
+		if apkStr == GOVAddressStr || apkStr == DCBAddressStr {
+			return true
+		}
+	}
+	return false
+}
+
+func isAllBoardAddressesInVins(
+	customToken *transaction.TxCustomToken,
+	boardAddrStr string,
+) bool {
+	for _, vin := range customToken.TxTokenData.Vins {
+		apkStr := string(vin.PaymentAddress.Pk[:])
+		if apkStr != boardAddrStr {
+			return false
+		}
+	}
+	return true
+}
+
+func verifySignatures(
+	tx *transaction.TxCustomToken,
+	boardPubKeys [][]byte,
+) bool {
+	boardLen := len(boardPubKeys)
+	if boardLen == 0 {
+		return false
+	}
+
+	signs := tx.BoardSigns
+	verifiedSignCount := 0
+	tx.BoardSigns = nil
+
+	for _, pubKey := range boardPubKeys {
+		sign, ok := signs[string(pubKey)]
+		if !ok {
+			continue
+		}
+		keyObj, err := wallet.Base58CheckDeserialize(string(pubKey))
+		if err != nil {
+			Logger.log.Info(err)
+			continue
+		}
+		isValid, err := keyObj.KeySet.Verify(common.ToBytes(*tx), common.ToBytes(sign))
+		if err != nil {
+			Logger.log.Info(err)
+			continue
+		}
+		if isValid {
+			verifiedSignCount += 1
+		}
+	}
+
+	if verifiedSignCount >= int(math.Floor(float64(boardLen/2)))+1 {
+		return true
+	}
+	return false
+}
+
+func (bc *BlockChain) verifyByBoard(
+	boardType uint8,
+	customToken *transaction.TxCustomToken,
+) bool {
+	// var address string
+	// var pubKeys [][]byte
+	// if boardType == common.DCB {
+	// 	address = string(common.DCBAddress)
+	// 	pubKeys = bc.BestState[0].BestBlock.Header.DCBGovernor.DCBBoardPubKeys
+	// } else if boardType == common.GOV {
+	// 	govAccount, _ := wallet.Base58CheckDeserialize(common.GOVAddress)
+	// 	address = string(govAccount.KeySet.PaymentAddress.Pk)
+	// 	pubKeys = bc.BestState[0].BestBlock.Header.GOVGovernor.GOVBoardPubKeys
+	// } else {
+	// 	return false
+	// }
+
+	// if !isAllBoardAddressesInVins(customToken, address) {
+	// 	return false
+	// }
+	// return verifySignatures(customToken, pubKeys)
+	return true
+}
+
+// VerifyMultiSigByBoard: verify multisig if the tx is for board's spending
+func (bc *BlockChain) VerifyCustomTokenSigns(tx metadata.Transaction) bool {
+	customToken, ok := tx.(*transaction.TxCustomToken)
+	if !ok {
+		return false
+	}
+
+	boardType := customToken.BoardType
+	if boardType == 0 { // this tx is not for board's spending so no need to verify multisig
+		if isAnyBoardAddressInVins(customToken) {
+			return false
+		}
+		return true
+	}
+
+	return bc.verifyByBoard(boardType, customToken)
 }
