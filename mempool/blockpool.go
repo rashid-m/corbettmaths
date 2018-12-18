d8dc80b9efa0abd6fa3ea33e835dc3445a0a9f67
Duc Hung <duchung@autonomous.nyc>
2018-12-18 17:36:36 +0700

    blockpool --- add block

--- mempool/blockpool.go
+++ mempool/blockpool.go
@@ -1,27 +1,52 @@
 package mempool
 
 import (
-	"github.com/ninjadotorg/constant/blockchain"
+	"errors"
 	"sync"
+
+	"github.com/ninjadotorg/constant/blockchain/blocktype"
 )
 
 type BlockPool struct {
 	mtx  sync.RWMutex
-	pool []blockchain.BlockV2
+	pool []blocktype.ShardToBeaconBlock
 }
 
-func (self *BlockPool) addBlock() {
+type ShardPoolItem struct {
+	state bool
+	block blocktype.ShardToBeaconBlock
 }
 
-func (self *BlockPool) getBlock() {
+var shardMap = map[byte][]ShardPoolItem{}
+
+func (self *BlockPool) AddBlock(newBlock blocktype.ShardToBeaconBlock) error {
+	blockHeader := newBlock.Header
+	ShardID := blockHeader.ShardID
+	if ShardID <= 0 {
+		return errors.New("invalid Shard ID")
+	}
+
+	if _, ok := shardMap[ShardID]; ok {
+		return nil
+	}
+	shardPoolItem := ShardPoolItem{
+		state: false,
+		block: newBlock,
+	}
+	shardMap[ShardID] = append(shardMap[ShardID], shardPoolItem)
+
+	return nil
 }
 
-func (self *BlockPool) removeBlock() {
+func (self *BlockPool) GetBlock() {
 }
 
-func (self *BlockPool) getAllBlocks() {
+func (self *BlockPool) RemoveBlock() {
 }
 
-func (self *BlockPool) validateBlock() {
+func (self *BlockPool) GetAllBlocks() {
+}
+
+func (self *BlockPool) ValidateBlock() {
 
 }
