306e714a9808ca0746f61b50d0c8dcb2b7ce3cde
0xsirrush <thaibao@autonomous.nyc>
2018-12-21 14:26:31 +0700

    - add more constant

--- common/constants.go
+++ common/constants.go
@@ -2,6 +2,8 @@ package common
 
 const (
 	EmptyString         = ""
+	TrueValue           = true
+	FalseValue          = false
 	NanoConstant        = 2 // 1 constant = 10^2 nano constant, we will use 1 miliconstant as minimum unit constant in tx
 	IncMerkleTreeHeight = 29
 	RefundPeriod        = 1000 // after 1000 blocks since a tx (small & no-privacy) happens, the network will refund an amount of constants to tx initiator automatically
