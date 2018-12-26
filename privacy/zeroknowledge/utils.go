8a3f7ded8bc0d2df62edb3e13b290faef29caaed
0xkraken <hien.n@autonomous.nyc>
2018-12-26 17:15:56 +0700

    refactor code

--- privacy/zeroknowledge/utils.go
+++ privacy/zeroknowledge/utils.go
@@ -1,55 +1,51 @@
 package zkp
 
 import (
+	"github.com/ninjadotorg/constant/common"
 	"math"
 	"math/big"
 
 	"github.com/ninjadotorg/constant/privacy"
-
-	"github.com/minio/blake2b-simd"
 )
 
 // GenerateChallengeFromPoint get hash of n points in G append with input values
 // return blake_2b(G[0]||G[1]||...||G[CM_CAPACITY-1]||<values>)
 // G[i] is list of all generator point of Curve
 func GenerateChallengeFromPoint(values []*privacy.EllipticPoint) *big.Int {
-	appendStr := privacy.PedCom.G[0].Compress()
+	bytes := privacy.PedCom.G[0].Compress()
 	for i := 1; i < privacy.PedCom.Capacity; i++ {
-		appendStr = append(appendStr, privacy.PedCom.G[i].Compress()...)
+		bytes = append(bytes, privacy.PedCom.G[i].Compress()...)
 	}
-	//fmt.Printf("len values: %v\n", len(values))
 
-	//fmt.Printf("values[0]: %v\n", values[0])
 	for i := 0; i < len(values); i++ {
-		appendStr = append(appendStr, values[i].Compress()...)
+		bytes = append(bytes, values[i].Compress()...)
 	}
-	hashFunc := blake2b.New256()
-	hashFunc.Write(appendStr)
-	hashValue := hashFunc.Sum(nil)
-	result := big.NewInt(0)
-	result.SetBytes(hashValue)
-	result.Mod(result, privacy.Curve.Params().N)
-	return result
+
+	hash := common.HashB(bytes)
+
+	res := new(big.Int).SetBytes(hash)
+	res.Mod(res, privacy.Curve.Params().N)
+	return res
 }
 
 // GenerateChallengeFromByte get hash of n points in G append with input values
 // return blake_2b(G[0]||G[1]||...||G[CM_CAPACITY-1]||<values>)
 // G[i] is list of all generator point of Curve
 func GenerateChallengeFromByte(values [][]byte) *big.Int {
-	appendStr := privacy.PedCom.G[0].Compress()
+	bytes := privacy.PedCom.G[0].Compress()
 	for i := 1; i < privacy.PedCom.Capacity; i++ {
-		appendStr = append(appendStr, privacy.PedCom.G[i].Compress()...)
+		bytes = append(bytes, privacy.PedCom.G[i].Compress()...)
 	}
+
 	for i := 0; i < len(values); i++ {
-		appendStr = append(appendStr, values[i]...)
+		bytes = append(bytes, values[i]...)
 	}
-	hashFunc := blake2b.New256()
-	hashFunc.Write(appendStr)
-	hashValue := hashFunc.Sum(nil)
-	result := big.NewInt(0)
-	result.SetBytes(hashValue)
-	result.Mod(result, privacy.Curve.Params().N)
-	return result
+
+	hash := common.HashB(bytes)
+
+	res := new(big.Int).SetBytes(hash)
+	res.Mod(res, privacy.Curve.Params().N)
+	return res
 }
 
 // EstimateProofSize returns the estimated size of the proof in kilobyte
