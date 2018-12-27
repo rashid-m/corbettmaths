67452ef6770ccbf203ecf3c373d29bdb9c03c61d
0xMothman <tamle@autonomous.nyc>
2018-12-27 17:20:11 +0700

    init shard genesis block

--- blockchain/shard_genesis.go
+++ blockchain/shard_genesis.go
@@ -1,6 +1,45 @@
 package blockchain
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"github.com/ninjadotorg/constant/cashec"
+
+	"github.com/ninjadotorg/constant/common"
+	privacy "github.com/ninjadotorg/constant/privacy-protocol"
+	"github.com/ninjadotorg/constant/transaction"
+)
+
+func createSpecialTokenTx(
+	tokenID common.Hash,
+	tokenName string,
+	tokenSymbol string,
+	amount uint64,
+	initialAddress privacy.PaymentAddress,
+) transaction.TxCustomToken {
+	log.Printf("Init token %s: %s \n", tokenSymbol, tokenID.String())
+	paymentAddr := initialAddress
+	vout := transaction.TxTokenVout{
+		Value:          amount,
+		PaymentAddress: paymentAddr,
+	}
+	vout.SetIndex(0)
+	txTokenData := transaction.TxTokenData{
+		PropertyID:     tokenID,
+		PropertyName:   tokenName,
+		PropertySymbol: tokenSymbol,
+		Type:           transaction.CustomTokenInit,
+		Amount:         amount,
+		Vins:           []transaction.TxTokenVin{},
+		Vouts:          []transaction.TxTokenVout{vout},
+	}
+	result := transaction.TxCustomToken{
+		TxTokenData: txTokenData,
+	}
+	result.Type = common.TxCustomTokenType
+	return result
+}
 
 //TODO Write function to create Shard block of shard chain here
 func CreateShardGenesisBlock(
@@ -9,19 +48,85 @@ func CreateShardGenesisBlock(
 	icoParams IcoParams,
 ) *ShardBlock {
 
+	ks := &cashec.KeySet{}
+
+	initialKeySet, err := ks.DecodeToKeySet(icoParams.InitialPaymentAddress)
+	if err != nil {
+		return nil
+	}
+
 	body := ShardBody{}
 	header := ShardHeader{
 		Timestamp: time.Date(2018, 8, 1, 0, 0, 0, 0, time.UTC).Unix(),
 		Height:    1,
 		Version:   1,
 		//TODO:
-
+		SalaryFund: icoParams.InitFundSalary,
 	}
 
 	block := &ShardBlock{
 		Body:   body,
 		Header: header,
 	}
+
+	// Create genesis token tx for DCB
+	dcbTokenTx := createSpecialTokenTx( // DCB
+		common.Hash(common.DCBTokenID),
+		"Decentralized central bank token",
+		"DCB",
+		icoParams.InitialDCBToken,
+		initialKeySet.PaymentAddress,
+	)
+	block.Body.Transactions = append(block.Body.Transactions, &dcbTokenTx)
+
+	// Create genesis token tx for GOV
+	govTokenTx := createSpecialTokenTx(
+		common.Hash(common.GOVTokenID),
+		"Government token",
+		"GOV",
+		icoParams.InitialGOVToken,
+		initialKeySet.PaymentAddress,
+	)
+	block.Body.Transactions = append(block.Body.Transactions, &govTokenTx)
+
+	// Create genesis token tx for CMB
+	cmbTokenTx := createSpecialTokenTx(
+		common.Hash(common.CMBTokenID),
+		"Commercial bank token",
+		"CMB",
+		icoParams.InitialCMBToken,
+		initialKeySet.PaymentAddress,
+	)
+	block.Body.Transactions = append(block.Body.Transactions, &cmbTokenTx)
+
+	// Create genesis token tx for BOND test
+	bondTokenTx := createSpecialTokenTx(
+		common.Hash([common.HashSize]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}),
+		"BondTest",
+		"BONTest",
+		icoParams.InitialBondToken,
+		initialKeySet.PaymentAddress,
+	)
+	block.Body.Transactions = append(block.Body.Transactions, &bondTokenTx)
+
+	// Create genesis vote token tx for DCB
+	voteDCBTokenTx := createSpecialTokenTx(
+		common.Hash(common.VoteDCBTokenID),
+		"Bond",
+		"BON",
+		icoParams.InitialVoteDCBToken,
+		initialKeySet.PaymentAddress,
+	)
+	block.Body.Transactions = append(block.Body.Transactions, &voteDCBTokenTx)
+
+	voteGOVTokenTx := createSpecialTokenTx(
+		common.Hash(common.VoteGOVTokenID),
+		"Bond",
+		"BON",
+		icoParams.InitialVoteGOVToken,
+		initialKeySet.PaymentAddress,
+	)
+	block.Body.Transactions = append(block.Body.Transactions, &voteGOVTokenTx)
 
 	return block
 }
