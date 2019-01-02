219e8c529fa149b5cfbf62f9097b477a56e0d586
0xkraken <hien.n@autonomous.nyc>
2019-01-02 12:22:26 +0700

    refactor code

--- privacy/schnorr_test.go
+++ privacy/schnorr_test.go
@@ -1,7 +1,6 @@
 package privacy
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -9,16 +8,10 @@ import (
 func TestSchn(t *testing.T) {
 	schnPrivKey := new(SchnPrivKey)
 	schnPrivKey.GenKey()
-	fmt.Printf("Private key SK: %v\n", schnPrivKey.SK.Bytes())
-	fmt.Printf("Private key R: %v\n", schnPrivKey.R.Bytes())
-	fmt.Printf("Public key G: %v\n", schnPrivKey.PubKey.G.Compress())
-	fmt.Printf("Public key H: %v\n", schnPrivKey.PubKey.H.Compress())
 
 	data := RandBytes(SpendingKeySize)
-	fmt.Printf("Data: %v\n", data)
 
 	signature, _ := schnPrivKey.Sign(data)
-	fmt.Printf("Signature: %+v\n", signature.Bytes())
 
 	res := schnPrivKey.PubKey.Verify(signature, data)
 
