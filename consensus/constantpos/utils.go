bc25ced99aacb57a481f180e4d29f698dbb866f7
0xBahamoot <lam@autonomous.nyc>
2019-01-02 16:31:40 +0700

    refactor bft

---
+++ consensus/constantpos/utils.go
@@ -0,0 +1,20 @@
+package constantpos
+
+import (
+	"github.com/ninjadotorg/constant/common/base58"
+	privacy "github.com/ninjadotorg/constant/privacy"
+)
+
+func GetPubKeysFromIdx(pubkeyList []string, idxs []int) []*privacy.PublicKey {
+	listPubkeyOfSigners := make([]*privacy.PublicKey, len(idxs))
+	for i := 0; i < len(idxs); i++ {
+		listPubkeyOfSigners[i] = new(privacy.PublicKey)
+		pubKeyTemp, byteVersion, err := base58.Base58Check{}.Decode(pubkeyList[idxs[i]])
+		if (err != nil) || (byteVersion != byte(0x00)) {
+			Logger.log.Info(err)
+			continue
+		}
+		*listPubkeyOfSigners[i] = pubKeyTemp
+	}
+	return listPubkeyOfSigners
+}
