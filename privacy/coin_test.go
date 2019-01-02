03fb6e00338c3de35c9d70ec894f31fcb4379947
0xkraken <hien.n@autonomous.nyc>
2019-01-02 14:07:01 +0700

    refactor code

--- privacy/coin_test.go
+++ privacy/coin_test.go
@@ -1,6 +1,7 @@
 package privacy
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"math/big"
 	"testing"
@@ -32,6 +33,14 @@ func TestEncryptionCoin(t *testing.T){
 	}
 
 	coin.Decrypt(viewingKey)
+
+	coin3Bytes := []byte{33, 3, 71, 166, 83, 226, 71, 95, 14, 188, 57, 177, 14, 85, 249, 136, 146, 169, 160, 86, 50, 207, 24, 120, 71, 251, 247, 227, 93, 147, 22, 190, 2, 80, 0, 32, 147, 64, 112, 6, 102, 82, 136,
+		65, 199, 170, 115, 230, 118, 88, 199, 83, 112, 100, 141, 105, 0, 143, 141, 66, 128, 108, 255, 232, 4, 126, 88, 110, 0, 0, 1, 10, 0}
+	coin3 := new(Coin)
+	coin3.SetBytes(coin3Bytes)
+
+	fmt.Printf("coin3 info: %+v", coin3)
+	fmt.Printf("Public key: %v\n", coin3.PublicKey)
 
 	assert.Equal(t, coin.CoinDetails.Randomness , coin2.CoinDetails.Randomness)
 	assert.Equal(t, coin.CoinDetails.Value , coin2.CoinDetails.Value)
