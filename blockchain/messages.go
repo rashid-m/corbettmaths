73cc4f688418260c84f2bbde5172a5a81a5da2e5
0xBahamoot <lam@autonomous.nyc>
2019-01-03 15:56:59 +0700

    add syncblock

--- blockchain/messages.go
+++ blockchain/messages.go
@@ -1,8 +1,12 @@
 package blockchain
 
 func (self *BlockChain) OnBlockShardReceived(block *ShardBlock) {
+	if self.newShardBlkCh != nil {
 
+	}
 }
 func (self *BlockChain) OnBlockBeaconReceived(block *BeaconBlock) {
-
+	if self.newBeaconBlkCh != nil {
+		self.newBeaconBlkCh <- block
+	}
 }
