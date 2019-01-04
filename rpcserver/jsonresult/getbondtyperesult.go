f3e94f234561caf5ddfe27a358bf58ff9f41fdc2
Duc Dinh <autonomous@Autonomouss-MacBook-Pro-3.local>
2019-01-04 10:49:54 +0700

    Update bond types rpc func response

--- rpcserver/jsonresult/getbondtyperesult.go
+++ rpcserver/jsonresult/getbondtyperesult.go
@@ -3,6 +3,9 @@ package jsonresult
 type GetBondTypeResult struct {
 	BondID         []byte `json:"bondId"`
 	StartSellingAt uint32 `json:"startSellingAt"`
-	Maturity       uint32 `json:"maturity"`
+	ExpiredDate    uint32 `json:"expiredDate"`
 	BuyBackPrice   uint64 `json:"buyBackPrice"`
+	BuyPrice       uint64 `json:"buyPrice"`
+	TotalIssue     uint64 `json:"totalIssue"`
+	Available      uint64 `json:"available"`
 }
