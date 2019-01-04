98f334b390665d437b322dc6958f85c7af156542
0xkraken <hien.n@autonomous.nyc>
2019-01-04 09:20:01 +0700

    update unit test

--- privacy/coin_test.go
+++ privacy/coin_test.go
@@ -17,6 +17,8 @@ func TestEncryptionCoin(t *testing.T){
 	paymentAddress := GeneratePaymentAddress(spendingKey)
 	viewingKey := GenerateViewingKey(spendingKey)
 
+	fmt.Printf("viewing key: %+v\n", viewingKey)
+
 	coin.CoinDetails.PublicKey, _ = DecompressKey(paymentAddress.Pk)
 
 	err := coin.Encrypt(paymentAddress.Tk)
@@ -43,6 +45,20 @@ func TestEncryptionCoin(t *testing.T){
 	//fmt.Printf("Public key: %v\n", coin3.PublicKey.Compress())
 	fmt.Printf("SND: %v\n", coin3.SNDerivator.Bytes())
 	//fmt.Printf("Public key: %v\n", coin3.PublicKey)
+
+	outCoin := new(OutputCoin)
+	outCoin.CoinDetails = new(Coin)
+	outCoin.CoinDetailsEncrypted = new(CoinDetailsEncrypted)
+	outCoin.CoinDetailsEncrypted.SetBytes([]byte{223, 198, 8, 136, 78, 57, 73, 132, 70, 137, 192, 152, 232, 168, 205, 40, 83, 235, 96, 71, 95, 216, 33, 195, 251, 59, 58, 26, 204, 71, 148, 31, 21, 47, 1, 168, 194, 71, 248, 147, 195, 146, 44, 151, 55, 116, 68, 95, 2, 50, 67, 64, 175, 218, 141, 69, 113, 134, 11, 130, 200, 66, 73, 237, 36, 91, 63, 149, 193, 47, 14, 42, 61, 122, 81, 183, 222, 92, 100, 125, 170, 3, 123, 246, 152, 211, 39, 110, 93, 59, 0, 95, 92, 44, 249, 195, 145, 1, 55, 136, 18, 233, 125, 7, 81, 37, 12, 177, 93, 35, 99, 241, 215, 199, 12, 114, 95, 83, 164, 137, 229, 9, 170, 21, 65, 111, 97, 26, 18, 17, 8})
+	fmt.Printf("Out coin set byte: %+v\n", outCoin.CoinDetailsEncrypted)
+	fmt.Printf("Out coin set byte randomness: %+v\n", len(outCoin.CoinDetailsEncrypted.EncryptedRandomness))
+	fmt.Printf("Out coin set byte value: %+v\n", len(outCoin.CoinDetailsEncrypted.EncryptedValue))
+	fmt.Printf("Out coin set byte sym key: %+v\n", len(outCoin.CoinDetailsEncrypted.EncryptedSymKey))
+
+	outCoin.Decrypt(viewingKey)
+
+	fmt.Printf("Out coin decrypted randomness: %+v\n", outCoin.CoinDetails.Randomness.Bytes())
+	fmt.Printf("Out coin decrypted value: %+v\n", outCoin.CoinDetails.Value)
 
 
 	assert.Equal(t, coin.CoinDetails.Randomness , coin2.CoinDetails.Randomness)
