0d5251841fac24bccea85324f6daa941664d7f03
0xBahamoot <lam@autonomous.nyc>
2019-01-04 17:21:59 +0700

    update & refactor

--- blockchain/interface.go
+++ blockchain/interface.go
@@ -16,7 +16,13 @@ type CrossShardPool interface {
 }
 
 type NodeShardPool interface {
+	PushBlock(ShardBlock) error
+	GetBlocks(byte, uint64) ([]ShardBlock, error)
+	RemoveBlocks(byte, uint64) error
 }
 
 type NodeBeaconPool interface {
+	PushBlock(BeaconBlock) error
+	GetBlocks(uint64) ([]BeaconBlock, error)
+	RemoveBlocks(uint64) error
 }
