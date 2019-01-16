8f3a28ba24e65eac1cdf63613f582a425fdb6eef
Duc Hung <duchung@autonomous.nyc>
2019-01-16 17:41:38 +0700

    add elastic service

--- common/aggregatelog/constants.go
+++ common/aggregatelog/constants.go
@@ -3,7 +3,9 @@ package aggregatelog
 import "os"
 
 const (
-	SENTRY_LOG_SERVICENAME = "sentry"
+	SENTRY_LOG_SERVICENAME  = "sentry"
+	ELASTIC_LOG_SERVICENAME = "elastic"
 )
 
 var SENTRY_DSN = os.Getenv("SENTRY_DSN")
+var ELASTIC_URL = os.Getenv("ELASTIC_URL")
