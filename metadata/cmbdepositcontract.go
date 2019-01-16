5f82fc75e536545ae17f0594325ac7ce49447683
0xsirrush <thaibao@autonomous.nyc>
2019-01-16 09:44:03 +0700

    - refactor code

--- metadata/cmbdepositcontract.go
+++ metadata/cmbdepositcontract.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/ninjadotorg/constant/common"
 	"github.com/ninjadotorg/constant/database"
-	privacy "github.com/ninjadotorg/constant/privacy"
+	"github.com/ninjadotorg/constant/privacy"
 	"github.com/ninjadotorg/constant/wallet"
 	"github.com/pkg/errors"
 )
