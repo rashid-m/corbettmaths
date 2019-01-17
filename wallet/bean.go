b15d74dfe800055badc2b4b37a8e7a63874fb6a5
0xsirrush <thaibao@autonomous.nyc>
2019-01-17 12:35:08 +0700

    - update

--- wallet/bean.go
+++ wallet/bean.go
@@ -3,6 +3,6 @@ package wallet
 type KeySerializedData struct {
 	PrivateKey     string `json:"PrivateKey"`
 	PaymentAddress string `json:"PaymentAddress"`
-	Pubkey         string `json:"Pubkey"`
+	Pubkey         string `json:"Pubkey"` // in hex encode string
 	ReadonlyKey    string `json:"ReadonlyKey"`
 }
