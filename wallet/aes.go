bfe03b5ab5efc00add2f341032d5095d6d1ebc06
0xsirrush <thaibao@autonomous.nyc>
2019-01-17 16:48:19 +0700

    - refactor

--- wallet/aes.go
+++ wallet/aes.go
@@ -15,7 +15,7 @@ type AES struct {
 }
 
 // create a salt
-func (self AES) deriveKey(passPhrase string, salt []byte) ([]byte, []byte) {
+func (aesAl AES) deriveKey(passPhrase string, salt []byte) ([]byte, []byte) {
 	if salt == nil {
 		salt = make([]byte, 8)
 		rand.Read(salt)
@@ -24,27 +24,48 @@ func (self AES) deriveKey(passPhrase string, salt []byte) ([]byte, []byte) {
 }
 
 // encrypt with AES
-func (self AES) Encrypt(passphrase string, plaintext []byte) (string, error) {
-	key, salt := self.deriveKey(passphrase, nil)
+func (aesAl AES) Encrypt(passphrase string, plaintext []byte) (string, error) {
+	key, salt := aesAl.deriveKey(passphrase, nil)
 	iv := make([]byte, 12)
 	// http://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
 	// Section 8.2
 	rand.Read(iv)
 	b, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
 	aesgcm, err := cipher.NewGCM(b)
 	data := aesgcm.Seal(nil, iv, plaintext, nil)
 	return hex.EncodeToString(salt) + "-" + hex.EncodeToString(iv) + "-" + hex.EncodeToString(data), err
 }
 
 // decrypt with AES
-func (self AES) Decrypt(passPhrase, cipherText string) ([]byte, error) {
+func (aesAl AES) Decrypt(passPhrase, cipherText string) ([]byte, error) {
 	arr := strings.Split(cipherText, "-")
 	salt, err := hex.DecodeString(arr[0])
+	if err != nil {
+		return nil, err
+	}
 	iv, err := hex.DecodeString(arr[1])
+	if err != nil {
+		return nil, err
+	}
 	data, err := hex.DecodeString(arr[2])
-	key, _ := self.deriveKey(passPhrase, salt)
+	if err != nil {
+		return nil, err
+	}
+	key, _ := aesAl.deriveKey(passPhrase, salt)
 	b, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
 	aesgcm, err := cipher.NewGCM(b)
+	if err != nil {
+		return nil, err
+	}
 	data, err = aesgcm.Open(nil, iv, data, nil)
+	if err != nil {
+		return nil, err
+	}
 	return data, err
 }
