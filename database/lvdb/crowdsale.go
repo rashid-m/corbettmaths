1d8f23d4bbc696c6fc8f8abb18fc506e2076bb5d
dbchiem <baoduy@autonomous.nyc>
2019-01-19 18:27:01 +0700

    Update crowdsale data for crowdsale payment

--- database/lvdb/crowdsale.go
+++ database/lvdb/crowdsale.go
@@ -1,27 +1,25 @@
 package lvdb
 
+import "github.com/ninjadotorg/constant/common"
+
 // TODO(@0xbunyip): implement
 
 func (db *db) StoreCrowdsaleData(
 	saleID []byte,
 	endBlock uint64,
-	buyingAsset []byte,
-	amountBuying uint64,
-	sellingAsset []byte,
-	amountSelling uint64,
+	buyingAsset common.Hash,
+	buyingAmount uint64,
+	sellingAsset common.Hash,
+	sellingAmount uint64,
 ) error {
 	return nil
 }
 
-func (db *db) GetCrowdsaleData(saleID []byte) (uint64, []byte, uint64, []byte, uint64, error) {
-	return 0, nil, 0, nil, 0, nil
+func (db *db) GetCrowdsaleData(saleID []byte) (uint64, common.Hash, uint64, common.Hash, uint64, error) {
+	return 0, common.Hash{}, 0, common.Hash{}, 0, nil
 }
 
 func (db *db) StoreCrowdsaleRequest(requestTxHash, saleID, pk, tk []byte) error {
-	return nil
-}
-
-func (db *db) StoreCrowdsaleResponse(requestTxHash, responseTxHash []byte) error {
 	return nil
 }
 
