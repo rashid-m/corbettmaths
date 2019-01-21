fb50eaaeb996b3a4ea2f15c8e96e9c012daaa9a0
0xsirrush <thaibao@autonomous.nyc>
2019-01-21 10:42:34 +0700

    - update

--- privacy/elgamal.go
+++ privacy/elgamal.go
@@ -98,6 +98,27 @@ func (pub *ElGamalPubKey) Encrypt(plaintext *EllipticPoint) *ElGamalCiphertext {
 	return ciphertext
 }
 
-func (priv *ElGamalPrivKey) Decrypt(ciphertext *ElGamalCiphertext) *EllipticPoint {
+func (priv *ElGamalPrivKey) Decrypt(ciphertext *ElGamalCiphertext) (*EllipticPoint, error) {
 	return ciphertext.C2.Sub(ciphertext.C1.ScalarMult(priv.X))
+}
+
+func ElGamalEncrypt(pubKey []byte, data *EllipticPoint) ([]byte, error) {
+	elgamalPub := ElGamalPubKey{}
+	err := elgamalPub.H.Decompress(pubKey)
+	if err != nil {
+		return nil, err
+	}
+	cipher := elgamalPub.Encrypt(data)
+	return cipher.Bytes(), nil
+}
+
+func ElGamalDecrypt(privKey []byte, cipher []byte) (*EllipticPoint, error) {
+	elgamalPri := ElGamalPrivKey{
+		X: new(big.Int),
+	}
+	elgamalPri.X.SetBytes(privKey)
+	ciphertext := &ElGamalCiphertext{}
+	ciphertext.SetBytes(cipher)
+	result, err := elgamalPri.Decrypt(ciphertext)
+	return result, err
 }
