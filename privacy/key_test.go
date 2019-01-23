d935eb2c04a2c734cf20859fd8418755d1598625
0xkraken <hien.n@autonomous.nyc>
2019-01-23 16:04:11 +0700

    update unit test generate spending key

--- privacy/key_test.go
+++ privacy/key_test.go
@@ -10,7 +10,9 @@ func TestGenerateSpendingKey(t *testing.T) {
 	spendingKey := GenerateSpendingKey(new(big.Int).SetInt64(123).Bytes())
 	expectedResult := []byte{2, 31, 181, 150, 219, 129, 230, 208, 43, 243, 210, 88, 110, 227, 152, 31, 229, 25, 242, 117, 192, 172, 156, 167, 107, 188, 242, 235, 180, 9, 125, 150}
 
-	assert.Equal(t, expectedResult, spendingKey)
+	spendingKeyArray := make([]uint8, SpendingKeySize)
+	copy(spendingKeyArray, spendingKey)
+	assert.Equal(t, expectedResult, spendingKeyArray)
 }
 
 func TestPAdd1Div4(t *testing.T) {
