f5b8e250fa0045c6a50dbf34624844dd0db1e76c
hieuqautonomous <hieu.q@autonomous.nyc>
2019-02-11 11:15:47 +0700

    bash script for deploy

--- bin/run_bootnode.sh
+++ bin/run_bootnode.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-kill -9 $(pgrep -u cashadmin bootnode)
+kill -9 $(pgrep -u root bootnode)
 
 cd ~/go/src/github.com/ninjadotorg/constant/bootnode
 git pull
-/usr/local/go/bin/go build main.go -o ./bootnode
+/usr/local/go/bin/go build -o bootnode *.go
 ./bootnode
--- bin/run_constant.sh
+++ bin/run_constant.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 KEY=""
+BOOTNODE=""
 
-kill -9 $(pgrep -u cashadmin constant)
+kill -9 $(pgrep -u root constant)
 
 cd ~/go/src/github.com/ninjadotorg/constant
 git pull
-/usr/local/go/bin/go build -o ./constant
+/usr/local/go/bin/go build -o constant
 ./constant
