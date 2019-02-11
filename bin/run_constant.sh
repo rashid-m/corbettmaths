c9843dd09643a09c9954d2ca48f1f113a2cfbeca
hieuqautonomous <hieu.q@autonomous.nyc>
2019-02-11 14:06:43 +0700

    bash script for deploy

--- bin/run_constant.sh
+++ bin/run_constant.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-KEY=""
-BOOTNODE=""
+SPENDINGKEY=""
+EXTERNALADDRESS="127.0.0.1"
+PORT="9433"
+DISCOVERPEERSADDRESS="127.0.0.1:9330"
 
 kill -9 $(pgrep -u root constant)
 
 cd ~/go/src/github.com/ninjadotorg/constant
 git pull
 /usr/local/go/bin/go build -o constant
-./constant
+./constant --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --spendingkey $SPENDINGKEY --nodemode "auto" --datadir "data-dir" --listen "127.0.0.1:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "wallet" --walletpassphrase "12345678" --walletautoinit
