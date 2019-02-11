02e956f3019150340db4fd9da67d6c2a125f2434
hieuqautonomous <hieu.q@autonomous.nyc>
2019-02-11 14:52:28 +0700

    mempool for in coin

--- bin/install_env.sh
+++ bin/install_env.sh
@@ -4,8 +4,8 @@ apt update
 apt -y upgrade
 
 echo "Install wget git"
-apt install wget
-apt install git
+apt install -y wget
+apt install -y git
 
 echo "Install golang..."
 wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
@@ -13,18 +13,22 @@ tar -xvf go1.11.5.linux-amd64.tar.gz
 mv go /usr/local
 
 mkdir ~/go/bin -p
-mkdir ~/go/src/github.com/ninjadotorg -p
 
-echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+if !(grep -q "GOROOT" ~/.bashrc); then
+    echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+    echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+    echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+fi
 
-source ~/.bashrc
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 echo "Install dep..."
 go get -u github.com/golang/dep/cmd/dep
 
 echo "Clone constant..."
+mkdir ~/go/src/github.com/ninjadotorg -p
 cd ~/go/src/github.com/ninjadotorg
 git clone https://github.com/ninjadotorg/constant -b master
 
