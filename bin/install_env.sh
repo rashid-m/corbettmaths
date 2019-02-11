f2611f04224056d1a9fc29cd21c3434367a0823f
hieuqautonomous <hieu.q@autonomous.nyc>
2019-02-11 15:07:42 +0700

    bash script for deploy

--- bin/install_env.sh
+++ bin/install_env.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
+echo "Apt update upgrade"
 apt update
 apt -y upgrade
 
 echo "Install wget git"
-apt install -y wget
-apt install -y git
+apt install -y wget git
 
 echo "Install golang..."
 wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
 tar -xvf go1.11.5.linux-amd64.tar.gz
 mv go /usr/local
 
-mkdir ~/go/bin -p
-
 echo "Setup env GOROOT GOPATH..."
+mkdir ~/go/bin -p
 if !(grep -q "GOROOT" ~/.bashrc); then
     echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
     echo 'export GOPATH=$HOME/go' >> ~/.bashrc
     echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
 fi
-
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
