332af5c07ebe0e11525b6fc4b85176e0bdaba782
hieuqautonomous <hieu.q@autonomous.nyc>
2019-02-12 15:26:07 +0700

    mempool for in coin

--- bin/run_bootnode.sh
+++ bin/run_bootnode.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-kill -9 $(pgrep -u root bootnode)
-
-cd ~/go/src/github.com/ninjadotorg/constant/bootnode
-git pull
-/usr/local/go/bin/go build -o bootnode *.go
+cd /root/go/src/github.com/ninjadotorg/constant/bootnode
 ./bootnode
-cd ~/go/src/github.com/ninjadotorg/constant/bin
