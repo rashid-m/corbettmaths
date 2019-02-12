ef448d202d1cd2163a8b533acd10fba1463597a6
hieuqautonomous <hieu.q@autonomous.nyc>
2019-02-12 16:27:35 +0700

    mempool for in coin

--- bin/run_constant.sh
+++ bin/run_constant.sh
@@ -1,4 +1,2 @@
-#!/usr/bin/env bash
-
-cd /root/go/src/github.com/ninjadotorg/constant
-./constant --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --spendingkey $SPENDINGKEY --nodemode "auto" --datadir "data/constant" --listen "127.0.0.1:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit
+#!/bin/sh
+/constant --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --spendingkey $SPENDINGKEY --nodemode "auto" --datadir "data/constant" --listen "127.0.0.1:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit
