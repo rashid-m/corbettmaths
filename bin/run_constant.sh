70c1d8dd04f0e0960e61705b57983e1eaac3ab94
Kumi <dung.q@autonomous.nyc>
2019-02-12 18:03:06 +0700

    Update docker build

--- bin/run_constant.sh
+++ bin/run_constant.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/constant --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --spendingkey $SPENDINGKEY --nodemode "auto" --datadir "data/constant" --listen "127.0.0.1:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit
+/constant --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --spendingkey $SPENDINGKEY --nodemode "auto" --datadir "/data/constant" --listen "0.0.0.0:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit
