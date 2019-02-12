378b26e615850da1478b31a79e09c97a3d6b1ae7
Kumi <dung.q@autonomous.nyc>
2019-02-12 18:03:06 +0700

    Update docker build

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ fi
 if [ -f ./bootnode ]; then
     rm -rf ./bootnode
 fi
-env GOOS=linux GOARCH=amd64 go build github.com/ninjadotorg/constant
-env GOOS=linux GOARCH=amd64 go build -o bootnode ../bootnode/*.go
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' github.com/ninjadotorg/constant
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o bootnode ../bootnode/*.go
 
-docker build . -t constant
+docker build . -t dungvanautonomous/constant
