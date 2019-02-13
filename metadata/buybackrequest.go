4891d5f66925aaa9707c8d19ec4de1818c4da794
0xankylosaurus <minhduccm90@gmail.com>
2019-02-13 15:07:54 +0700

    Add process for buy back bonds feature

--- metadata/buybackrequest.go
+++ metadata/buybackrequest.go
@@ -1,7 +1,10 @@
 package metadata
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"errors"
+	"strconv"
 
 	"github.com/ninjadotorg/constant/common"
 	"github.com/ninjadotorg/constant/database"
@@ -76,16 +79,16 @@ func (bbReq *BuyBackRequest) Hash() *common.Hash {
 	return &hash
 }
 
-// func (bbReq *BuyBackRequest) BuildReqActions(tx Transaction, bcr BlockchainRetriever, shardID byte) ([][]string, error) {
-// 	actionContent := map[string]interface{}{
-// 		"txReqId": *(tx.Hash()),
-// 		"meta":    *bsReq,
-// 	}
-// 	actionContentBytes, err := json.Marshal(actionContent)
-// 	if err != nil {
-// 		return [][]string{}, err
-// 	}
-// 	actionContentBase64Str := base64.StdEncoding.EncodeToString(actionContentBytes)
-// 	action := []string{strconv.Itoa(BuyFromGOVRequestMeta), actionContentBase64Str}
-// 	return [][]string{action}, nil
-// }
+func (bbReq *BuyBackRequest) BuildReqActions(tx Transaction, bcr BlockchainRetriever, shardID byte) ([][]string, error) {
+	actionContent := map[string]interface{}{
+		"reqTx":       tx,
+		"buyBackMeta": *bbReq,
+	}
+	actionContentBytes, err := json.Marshal(actionContent)
+	if err != nil {
+		return [][]string{}, err
+	}
+	actionContentBase64Str := base64.StdEncoding.EncodeToString(actionContentBytes)
+	action := []string{strconv.Itoa(BuyBackRequestMeta), actionContentBase64Str}
+	return [][]string{action}, nil
+}
