3ff329e40a92dd1ab7b1a3570584391cfb80ce7b
0xmerman <hung@autonomous.nyv>
2019-02-16 23:25:23 +0700

    update rpc function

--- rpcserver/jsonresult/gettransactiondetailresult.go
+++ rpcserver/jsonresult/gettransactiondetailresult.go
@@ -5,15 +5,16 @@ import (
 )
 
 type TransactionDetail struct {
-	BlockHash string `json:"BlockHash"`
-	Index     uint64 `json:"index"`
-	ShardID   byte   `json:"shardID"`
-	Hash      string `json:"Hash"`
-	Version   int8   `json:"Version"`
-	Type      string `json:"Type"` // Transaction type
-	LockTime  string `json:"LockTime"`
-	Fee       uint64 `json:"Fee"` // Fee applies: always consant
-	Image     string `json:"Image"`
+	BlockHash   string `json:"BlockHash"`
+	BlockHeight uint64 `json:"BlockHeight"`
+	Index       uint64 `json:"index"`
+	ShardID     byte   `json:"shardID"`
+	Hash        string `json:"Hash"`
+	Version     int8   `json:"Version"`
+	Type        string `json:"Type"` // Transaction type
+	LockTime    string `json:"LockTime"`
+	Fee         uint64 `json:"Fee"` // Fee applies: always consant
+	Image       string `json:"Image"`
 
 	Proof           *zkp.PaymentProof `json:"Proof"`
 	InputCoinPubKey string            `json:"InputCoinPubKey"`
