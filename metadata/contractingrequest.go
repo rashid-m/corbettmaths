de608950e1b06bc6859730817b1321ebcde07a0b
0xankylosaurus <minhduccm90@gmail.com>
2019-02-18 08:11:29 +0700

    Update issuing/contracting tokens to new consensus

--- metadata/contractingrequest.go
+++ metadata/contractingrequest.go
@@ -1,11 +1,17 @@
 package metadata
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"strconv"
+
 	"github.com/ninjadotorg/constant/common"
 	"github.com/ninjadotorg/constant/database"
 )
 
 type ContractingRequest struct {
+	CurrencyType common.Hash // USD or ETH for now
 	MetadataBase
 }
 
@@ -42,8 +48,26 @@ func (cReq *ContractingRequest) ValidateMetadataByItself() bool {
 
 func (cReq *ContractingRequest) Hash() *common.Hash {
 	record := cReq.MetadataBase.Hash().String()
+	record += cReq.CurrencyType.String()
 
 	// final hash
 	hash := common.DoubleHashH([]byte(record))
 	return &hash
+}
+
+func (cReq *ContractingRequest) BuildReqActions(tx Transaction, bcr BlockchainRetriever, shardID byte) ([][]string, error) {
+	if bytes.Equal(cReq.CurrencyType[:], common.USDAssetID[:]) {
+		return [][]string{}, nil
+	}
+	actionContent := map[string]interface{}{
+		"txReqId": *(tx.Hash()),
+		"meta":    *cReq,
+	}
+	actionContentBytes, err := json.Marshal(actionContent)
+	if err != nil {
+		return [][]string{}, err
+	}
+	actionContentBase64Str := base64.StdEncoding.EncodeToString(actionContentBytes)
+	action := []string{strconv.Itoa(ContractingRequestMeta), actionContentBase64Str}
+	return [][]string{action}, nil
 }
