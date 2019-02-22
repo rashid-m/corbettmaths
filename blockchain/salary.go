e4c14cd68f5b548b5acb07e91054c0257e80c067
0xankylosaurus <minhduccm90@gmail.com>
2019-02-22 10:09:46 +0700

    Update salary on beacon chain

--- blockchain/salary.go
+++ blockchain/salary.go
@@ -5,104 +5,116 @@ import (
 	"strconv"
 
 	"github.com/ninjadotorg/constant/metadata"
-	"github.com/ninjadotorg/constant/privacy"
-	"github.com/ninjadotorg/constant/transaction"
 )
 
-type SalaryReqInfo struct {
-	PayToAddress     *privacy.PaymentAddress
-	ShardID          byte
-	ShardBlockHeight uint64
-	TotalTxsSizeInKb uint64
-	TotalTxs         uint64
+type ShardBlockSalaryUpdateInfo struct {
+	ShardBlockSalary uint64
+	ShardBlockFee    uint64
 }
 
-func createSalaryReqAction(
-	payToAddress *privacy.PaymentAddress,
-	shardID byte,
-	shardBlockHeight uint64,
-	totalTxsSizeInKb uint64,
-	totalTxs uint64,
+func createShardBlockSalaryUpdateAction(
+	shardBlockSalary uint64,
+	shardBlockFee uint64,
 ) ([][]string, error) {
-	salaryReqInfo := SalaryReqInfo{
-		PayToAddress:     payToAddress,
-		ShardID:          shardID,
-		ShardBlockHeight: shardBlockHeight,
-		TotalTxsSizeInKb: totalTxsSizeInKb,
-		TotalTxs:         totalTxs,
+	shardBlockSalaryUpdateInfo := ShardBlockSalaryUpdateInfo{
+		ShardBlockSalary: shardBlockSalary,
+		ShardBlockFee:    shardBlockFee,
 	}
-	salaryReqInfoBytes, err := json.Marshal(salaryReqInfo)
+	shardBlockSalaryUpdateInfoBytes, err := json.Marshal(shardBlockSalaryUpdateInfo)
 	if err != nil {
 		return [][]string{}, err
 	}
-	action := []string{strconv.Itoa(metadata.SalaryRequestMeta), string(salaryReqInfoBytes)}
+	action := []string{strconv.Itoa(metadata.ShardBlockSalaryUpdateMeta), string(shardBlockSalaryUpdateInfoBytes)}
 	return [][]string{action}, nil
 }
 
-type SalaryResContent struct {
-	ShardBlockSalary uint64
-	PayToAddress     *privacy.PaymentAddress
-	ShardBlockHeight uint64
-}
-
-func buildInstructionsForSalaryReq(
+func buildInstForShardBlockSalaryUpdate(
 	shardID byte,
 	contentStr string,
 	beaconBestState *BestStateBeacon,
 	accumulativeValues *accumulativeValues,
 ) ([][]string, error) {
-	var salaryReqInfo SalaryReqInfo
-	err := json.Unmarshal([]byte(contentStr), &salaryReqInfo)
+	var shardBlockSalaryUpdateInfo ShardBlockSalaryUpdateInfo
+	err := json.Unmarshal([]byte(contentStr), &shardBlockSalaryUpdateInfo)
 	if err != nil {
 		return nil, err
 	}
 	instructions := [][]string{}
-	stabilityInfo := beaconBestState.StabilityInfo
-	govParams := stabilityInfo.GOVConstitution.GOVParams
-	salaryPerTx := govParams.SalaryPerTx
-	feePerKbTx := govParams.FeePerKbTx
-	basicSalary := govParams.BasicSalary
-	shardBlockSalary := basicSalary + salaryPerTx*salaryReqInfo.TotalTxs
-	shardBlockFee := salaryReqInfo.TotalTxsSizeInKb * feePerKbTx
-	accumulativeValues.totalFee += shardBlockFee
-	accumulativeValues.totalSalary += shardBlockSalary
-	salaryResContent := SalaryResContent{
-		ShardBlockSalary: shardBlockSalary,
-		PayToAddress:     salaryReqInfo.PayToAddress,
-		ShardBlockHeight: salaryReqInfo.ShardBlockHeight,
-	}
-	salaryResContentBytes, err := json.Marshal(salaryResContent)
-	if err != nil {
-		return nil, err
-	}
+	accumulativeValues.totalFee += shardBlockSalaryUpdateInfo.ShardBlockFee
+	accumulativeValues.totalSalary += shardBlockSalaryUpdateInfo.ShardBlockSalary
 	returnedInst := []string{
-		strconv.Itoa(metadata.SalaryResponseMeta),
+		strconv.Itoa(metadata.ShardBlockSalaryUpdateMeta),
 		strconv.Itoa(int(shardID)),
-		string(salaryResContentBytes),
+		contentStr,
 	}
 	instructions = append(instructions, returnedInst)
 	return instructions, nil
 }
 
-func (blockgen *BlkTmplGenerator) buildSalaryRes(
-	salaryResContentStr string,
-	blkProducerPrivateKey *privacy.SpendingKey,
-) ([]metadata.Transaction, error) {
-	var salaryResContent SalaryResContent
-	err := json.Unmarshal([]byte(salaryResContentStr), &salaryResContent)
-	if err != nil {
-		return nil, err
-	}
-	salaryResTx := new(transaction.Tx)
-	err = salaryResTx.InitTxSalary(
-		salaryResContent.ShardBlockSalary,
-		salaryResContent.PayToAddress,
-		blkProducerPrivateKey,
-		blockgen.chain.GetDatabase(),
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return []metadata.Transaction{salaryResTx}, nil
-}
+// type SalaryResContent struct {
+// 	ShardBlockSalary uint64
+// 	PayToAddress     *privacy.PaymentAddress
+// 	ShardBlockHeight uint64
+// }
+
+// func buildInstructionsForSalaryReq(
+// 	shardID byte,
+// 	contentStr string,
+// 	beaconBestState *BestStateBeacon,
+// 	accumulativeValues *accumulativeValues,
+// ) ([][]string, error) {
+// 	var salaryReqInfo SalaryReqInfo
+// 	err := json.Unmarshal([]byte(contentStr), &salaryReqInfo)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	instructions := [][]string{}
+// 	stabilityInfo := beaconBestState.StabilityInfo
+// 	govParams := stabilityInfo.GOVConstitution.GOVParams
+// 	salaryPerTx := govParams.SalaryPerTx
+// 	feePerKbTx := govParams.FeePerKbTx
+// 	basicSalary := govParams.BasicSalary
+// 	shardBlockSalary := basicSalary + salaryPerTx*salaryReqInfo.TotalTxs
+// 	shardBlockFee := salaryReqInfo.TotalTxsSizeInKb * feePerKbTx
+// 	accumulativeValues.totalFee += shardBlockFee
+// 	accumulativeValues.totalSalary += shardBlockSalary
+// 	salaryResContent := SalaryResContent{
+// 		ShardBlockSalary: shardBlockSalary,
+// 		PayToAddress:     salaryReqInfo.PayToAddress,
+// 		ShardBlockHeight: salaryReqInfo.ShardBlockHeight,
+// 	}
+// 	salaryResContentBytes, err := json.Marshal(salaryResContent)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	returnedInst := []string{
+// 		strconv.Itoa(metadata.SalaryResponseMeta),
+// 		strconv.Itoa(int(shardID)),
+// 		string(salaryResContentBytes),
+// 	}
+// 	instructions = append(instructions, returnedInst)
+// 	return instructions, nil
+// }
+
+// func (blockgen *BlkTmplGenerator) buildSalaryRes(
+// 	salaryResContentStr string,
+// 	blkProducerPrivateKey *privacy.SpendingKey,
+// ) ([]metadata.Transaction, error) {
+// 	var salaryResContent SalaryResContent
+// 	err := json.Unmarshal([]byte(salaryResContentStr), &salaryResContent)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	salaryResTx := new(transaction.Tx)
+// 	err = salaryResTx.InitTxSalary(
+// 		salaryResContent.ShardBlockSalary,
+// 		salaryResContent.PayToAddress,
+// 		blkProducerPrivateKey,
+// 		blockgen.chain.GetDatabase(),
+// 		nil,
+// 	)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return []metadata.Transaction{salaryResTx}, nil
+// }
