5d7ca3a7e709bfa053ad398ff39ed365ba9aa965
dbchiem <baoduy@autonomous.nyc>
2019-02-27 02:33:57 +0700

    Update loan payment info on beacon

--- database/lvdb/db.go
+++ database/lvdb/db.go
@@ -42,6 +42,7 @@ var (
 	loanIDKeyPrefix           = []byte("loanID-")
 	loanTxKeyPrefix           = []byte("loanTx-")
 	loanPaymentKeyPrefix      = []byte("loanPayment-")
+	loanWithdrawedPrefix      = []byte("loanWithdrawed-")
 	loanRequestPostfix        = []byte("-req")
 	loanResponsePostfix       = []byte("-res")
 	rewared                   = []byte("reward")
