acc17d0146802f9c9f13c1d924f08d092121fb2f
0xkumi <dung.q@autonomous.nyc>
2019-03-12 16:01:21 +0700

    Update

--- bin/run_constant.sh
+++ bin/run_constant.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-mkdir -p /data/constant/
-/constant --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --spendingkey $SPENDINGKEY --nodemode "auto" --datadir "/data/constant" --listen "0.0.0.0:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit 2>&1 > /data/constant/log.txt
+mkdir -p /data
+if [ "$1" == "y" ]; then
+    rm -rf /data/*
+fi
+/constant --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --spendingkey $SPENDINGKEY --nodemode "auto" --datadir "/data" --listen "0.0.0.0:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit --rpclisten "0.0.0.0:$RPC_PORT" > /data/log.txt
