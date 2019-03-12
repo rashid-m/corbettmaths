83c2437b2595efee49b9c165e03249270768a6f8
0xkumi <dung.q@autonomous.nyc>
2019-03-12 16:01:21 +0700

    Update

---
+++ bin/local.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -f ./constant ]; then
+    rm -rf ./constant
+fi
+if [ -f ./bootnode ]; then
+    rm -rf ./bootnode
+fi
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o constant github.com/constant-money/constant-chain
+env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o bootnode ../bootnode/*.go
+
+commit=`git show --summary --oneline | cut -d ' ' -f 1`
+docker build --build-arg commit=$commit . -t constant && echo "Commit: $commit"
