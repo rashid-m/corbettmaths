5ba68d89b904fe71a763ce2e52e494ee02a79dca
0xjackalope <huu@ninja.org>
2019-03-12 11:57:27 +0700

    fix

--- metadata/fromshardins/voteboardins.go
+++ metadata/fromshardins/voteboardins.go
@@ -2,8 +2,10 @@ package fromshardins
 
 import (
 	"encoding/json"
+	"github.com/constant-money/constant-chain/blockchain/component"
 	"github.com/constant-money/constant-chain/common"
 	"github.com/constant-money/constant-chain/privacy"
+	"strconv"
 )
 
 type VoteBoardIns struct {
@@ -24,18 +26,25 @@ func NewVoteBoardIns(
 	return &VoteBoardIns{BoardType: boardType, CandidatePaymentAddress: candidatePaymentAddress, VoterPaymentAddress: voterPaymentAddress, BoardIndex: boardIndex, AmountOfVote: amountOfVote}
 }
 
-func (VoteBoardIns) GetStringFormat() ([]string, error) {
-	panic("implement me")
-}
-
-func NewVoteDCBBoardInsFromStr(inst string) (*VoteBoardIns, error) {
-	voteDCBBoardIns := &VoteBoardIns{}
-	err := json.Unmarshal([]byte(inst), voteDCBBoardIns)
-	voteDCBBoardIns.BoardType = common.DCBBoard
+func (voteBoardIns VoteBoardIns) GetStringFormat() ([]string, error) {
+	content, err := json.Marshal(voteBoardIns)
 	if err != nil {
 		return nil, err
 	}
-	return voteDCBBoardIns, nil
+	return []string{
+		strconv.Itoa(component.VoteBoardIns),
+		strconv.Itoa(-1),
+		string(content),
+	}, nil
+}
+
+func NewVoteBoardInsFromStr(inst string) (*VoteBoardIns, error) {
+	voteBoardIns := &VoteBoardIns{}
+	err := json.Unmarshal([]byte(inst), voteBoardIns)
+	if err != nil {
+		return nil, err
+	}
+	return voteBoardIns, nil
 }
 
 func NewVoteGOVBoardInsFromStr(inst string) (*VoteBoardIns, error) {
