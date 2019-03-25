d6d9266c815de0c9e0eb45796e52b3b5aa2f506f
0xkraken <hien.n@autonomous.nyc>
2019-03-25 11:22:44 +0700

    update

--- rpcserver/constants.go
+++ rpcserver/constants.go
@@ -178,7 +178,7 @@ const (
 	CreateAndSendTxWithCMBDepositSend     = "createandsendtxwithcmbdepositsend"
 	CreateAndSendTxWithCMBWithdrawRequest = "createandsendtxwithcmbwithdrawrequest"
 
-	// wallet
+	// walletsta
 	GetPublicKeyFromPaymentAddress = "getpublickeyfrompaymentaddress"
 	DefragmentAccount              = "defragmentaccount"
 )
