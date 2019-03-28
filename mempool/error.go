627f6fa4b35d3cd893816d9178f7f6d77bb4add4
0xsirrush <thaibao@autonomous.nyc>
2019-03-28 22:50:12 +0700

    - mempool error

--- mempool/error.go
+++ mempool/error.go
@@ -37,20 +37,20 @@ var ErrCodeMessage = map[int]struct {
 }
 
 type MempoolTxError struct {
-	code    int    // The code to send with reject messages
-	message string // Human readable message of the issue
-	err     error
+	Code    int    // The code to send with reject messages
+	Message string // Human readable message of the issue
+	Err     error
 }
 
 // Error satisfies the error interface and prints human-readable errors.
 func (e MempoolTxError) Error() string {
-	return fmt.Sprintf("%d: %s %+v", e.code, e.message, e.err)
+	return fmt.Sprintf("%d: %s %+v", e.Code, e.Message, e.Err)
 }
 
 // txRuleError creates an underlying MempoolTxError with the given a set of
 // arguments and returns a RuleError that encapsulates it.
 func (e *MempoolTxError) Init(key int, err error) {
-	e.code = ErrCodeMessage[key].code
-	e.message = ErrCodeMessage[key].message
-	e.err = errors.Wrap(err, e.message)
+	e.Code = ErrCodeMessage[key].code
+	e.Message = ErrCodeMessage[key].message
+	e.Err = errors.Wrap(err, e.Message)
 }
