32c0fd3061cea20afb3db7261d009aa420a0e98b
0xBahamoot <lam@autonomous.nyc>
2019-03-28 10:57:46 +0700

    refactor & rmv todo

--- blockchain/blockgen.go
+++ blockchain/blockgen.go
@@ -6,15 +6,13 @@ type BlkTmplGenerator struct {
 	shardToBeaconPool ShardToBeaconPool
 	crossShardPool    map[byte]CrossShardPool
 	chain             *BlockChain
-	rewardAgent       RewardAgent
 }
 
-func (blkTmplGenerator BlkTmplGenerator) Init(txPool TxPool, chain *BlockChain, rewardAgent RewardAgent, shardToBeaconPool ShardToBeaconPool, crossShardPool map[byte]CrossShardPool) (*BlkTmplGenerator, error) {
+func (blkTmplGenerator BlkTmplGenerator) Init(txPool TxPool, chain *BlockChain, shardToBeaconPool ShardToBeaconPool, crossShardPool map[byte]CrossShardPool) (*BlkTmplGenerator, error) {
 	return &BlkTmplGenerator{
 		txPool:            txPool,
 		shardToBeaconPool: shardToBeaconPool,
 		crossShardPool:    crossShardPool,
 		chain:             chain,
-		rewardAgent:       rewardAgent,
 	}, nil
 }
