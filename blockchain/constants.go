43225e9bad60d81b62410a85551d099ed4bd4393
0xakk0r0kamui <hy@ninja.org>
2019-03-29 15:52:58 +0700

    fix

--- blockchain/constants.go
+++ blockchain/constants.go
@@ -17,7 +17,7 @@ const (
 	ThresholdRatioOfDCBCrisis = 9000
 	ThresholdRatioOfGOVCrisis = 9000
 	ConstitutionPerBoard      = 10
-	EndOfFirstBoard           = 30
+	EndOfFirstBoard           = 90
 	BaseSalaryBoard           = 10000
 )
 
