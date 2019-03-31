22c50ad11f1a11b53e13368153ec0303be4406ee
0xkumi <dung.q@autonomous.nyc>
2019-03-31 12:31:15 +0700

    Fix ready status & update debug log

--- bin/local.sh
+++ bin/local.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [ -f ./constant ]; then
     rm -rf ./constant
@@ -6,9 +6,10 @@ fi
 if [ -f ./bootnode ]; then
     rm -rf ./bootnode
 fi
+
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o constant github.com/constant-money/constant-chain
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o bootnode ../bootnode/*.go
 
 commit=`git show --summary --oneline | cut -d ' ' -f 1`
 docker build --build-arg commit=$commit . -t constant && echo "Commit: $commit"
-docker rmi -f $(docker images --filter "dangling=true" -q)
+# docker rmi -f $(docker images --filter "dangling=true" -q)
