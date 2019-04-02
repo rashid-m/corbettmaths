8c3abaeaaaed09f54fe71eddfe681b0cb1d9645b
dbchiem <baoduy@autonomous.nyc>
2019-04-02 10:47:49 +0700

    Use correct list of insts to validate

--- blockchain/stabilityvalidation.go
+++ blockchain/stabilityvalidation.go
@@ -1,11 +1,14 @@
 package blockchain
 
 import (
+	"bytes"
+	"strconv"
+
 	"github.com/constant-money/constant-chain/metadata"
 	"github.com/pkg/errors"
 )
 
-func (bc *BlockChain) verifyBuyFromGOVRequestTx(tx metadata.Transaction, insts []string, instUsed []int) error {
+func (bc *BlockChain) verifyBuyFromGOVRequestTx(tx metadata.Transaction, insts [][]string, instUsed []int) error {
 	meta, ok := tx.GetMetadata().(*metadata.BuySellRequest)
 	if !ok {
 		return errors.Errorf("error parsing metadata BuySellRequest of tx %s", tx.Hash().String())
@@ -14,10 +17,28 @@ func (bc *BlockChain) verifyBuyFromGOVRequestTx(tx metadata.Transaction, insts [
 		return nil
 	}
 
-	return nil
+	for i, inst := range insts {
+		// Find corresponding instruction in block
+		if instUsed[i] > 0 || inst[0] != strconv.Itoa(metadata.TradeActivationMeta) {
+			continue
+		}
+		tradeID, reqAmount, err := metadata.ParseTradeActivationActionValue(inst[2])
+		if err != nil || !bytes.Equal(meta.TradeID, tradeID) {
+			continue
+		}
+
+		// Check if amount is correct
+		if meta.Amount != reqAmount {
+			return errors.Errorf("invalid amount for trade bond BuySellRequest tx: got %d instead of %d", meta.Amount, reqAmount)
+		}
+
+		instUsed[i] += 1
+	}
+
+	return errors.Errorf("no instruction found for BuySellRequest tx %s", tx.Hash().String())
 }
 
-func (bc *BlockChain) VerifyStabilityTransactionsForNewBlock(insts []string, block *ShardBlock) error {
+func (bc *BlockChain) VerifyStabilityTransactionsForNewBlock(insts [][]string, block *ShardBlock) error {
 	instUsed := make([]int, len(insts)) // Count how many times an inst is used by a tx
 	for _, tx := range block.Body.Transactions {
 		if tx.GetMetadata() == nil {
