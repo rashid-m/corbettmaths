e731331bd7d35b65a6256178fea15757392f525c
0xmerman <hung@autonomous.nyc>
2019-04-05 16:33:02 +0700

    Hot Fix For Hy

--- blockchain/beaconprocess.go
+++ blockchain/beaconprocess.go
@@ -344,9 +344,25 @@ func (blockchain *BlockChain) VerifyPreProcessingBeaconBlock(block *BeaconBlock,
 					validSwappers[shardID] = append(validSwappers[shardID], validSwapper[shardID]...)
 					stabilityInstructions = append(stabilityInstructions, stabilityInstruction...)
 				}
+				votingInstruction, err := blockchain.generateVotingInstructionWOIns(DCBConstitutionHelper{})
+				if err != nil {
+					fmt.Println("[voting]-Build DCB voting instruction failed: ", err)
+				} else {
+					if len(votingInstruction) != 0 {
+						stabilityInstructions = append(stabilityInstructions, votingInstruction...)
+					}
+				}
+				votingInstruction, err = blockchain.generateVotingInstructionWOIns(GOVConstitutionHelper{})
+				if err != nil {
+					fmt.Println("[voting]-Build GOV voting instruction failed: ", err)
+				} else {
+					if len(votingInstruction) != 0 {
+						stabilityInstructions = append(stabilityInstructions, votingInstruction...)
+					}
+				}
 				oracleInsts, err := blockchain.buildOracleRewardInstructions(&beaconBestState)
 				if err != nil {
-					Logger.log.Error("Build oracle reward instructions failed: ", err)
+					fmt.Println("Build oracle reward instructions failed: ", err)
 				} else if len(oracleInsts) > 0 {
 					stabilityInstructions = append(stabilityInstructions, oracleInsts...)
 				}
