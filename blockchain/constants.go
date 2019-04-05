1d97e4cbbe2c880cd65fa94ccfcf9948f6a4c7d7
0xBahamoot <lam@autonomous.nyc>
2019-04-05 19:31:45 +0700

    update to multi-node

--- blockchain/constants.go
+++ blockchain/constants.go
@@ -29,8 +29,8 @@ const (
 	MainetName         = "mainnet"
 	MainnetDefaultPort = "9333"
 
-	MainNetShardCommitteeSize  = 1
-	MainNetBeaconCommitteeSize = 1
+	MainNetShardCommitteeSize  = 3
+	MainNetBeaconCommitteeSize = 3
 	MainNetActiveShards        = 2
 
 	//board and proposal parameters
@@ -64,8 +64,8 @@ const (
 	TestnetName        = "testnet"
 	TestnetDefaultPort = "9444"
 
-	TestNetShardCommitteeSize  = 1
-	TestNetBeaconCommitteeSize = 1
+	TestNetShardCommitteeSize  = 4
+	TestNetBeaconCommitteeSize = 4
 	TestNetActiveShards        = 2
 
 	//board and proposal parameters
