cca36c6501c425b8dced0a5ad83f3a3bde1df5c8
0xankylosaurus <minhduccm90@gmail.com>
2019-04-07 11:07:58 +0700

    Reset default gov params

--- blockchain/constants.go
+++ blockchain/constants.go
@@ -69,24 +69,14 @@ const (
 	TestNetActiveShards        = 2
 
 	//board and proposal parameters
-	// TestnetSalaryPerTx                = 1
-	// TestnetBasicSalary                = 0
-	// TestnetInitFundSalary             = 0
-	// TestnetInitDCBToken               = 10000
-	// TestnetInitGovToken               = 10000
-	// TestnetInitCmBToken               = 0
-	// TestnetInitBondToken              = 0
-	// TestnetFeePerTxKb                 = 2
-	// TestnetGenesisBlockPaymentAddress = "1Uv2zzR4LgfX8ToQe8ub3bYcCLk3uDU1sm9U9hiu9EKYXoS77UdikfT9s8d5YjhsTJm61eazsMwk2otFZBYpPHwiMn8z6bKWWJRspsLky"
-
 	TestnetSalaryPerTx                = 1
-	TestnetBasicSalary                = 2
-	TestnetInitFundSalary             = 1000
+	TestnetBasicSalary                = 0
+	TestnetInitFundSalary             = 0
 	TestnetInitDCBToken               = 10000
 	TestnetInitGovToken               = 10000
 	TestnetInitCmBToken               = 0
 	TestnetInitBondToken              = 0
-	TestnetFeePerTxKb                 = 0
+	TestnetFeePerTxKb                 = 2
 	TestnetGenesisBlockPaymentAddress = "1Uv2zzR4LgfX8ToQe8ub3bYcCLk3uDU1sm9U9hiu9EKYXoS77UdikfT9s8d5YjhsTJm61eazsMwk2otFZBYpPHwiMn8z6bKWWJRspsLky"
 )
 
