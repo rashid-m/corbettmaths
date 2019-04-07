0646c546fc01e2cc56615df793aae4b1e307de51
0xankylosaurus <minhduccm90@gmail.com>
2019-04-07 10:39:13 +0700

    Fix potential async best state beacon at shard

--- blockchain/beaconprocess.go
+++ blockchain/beaconprocess.go
@@ -74,6 +74,9 @@ func (blockchain *BlockChain) VerifyPreSignBeaconBlock(block *BeaconBlock, isCom
 }
 
 func (blockchain *BlockChain) InsertBeaconBlock(block *BeaconBlock, isCommittee bool) error {
+
+	fmt.Println("fuck InsertBeaconBlock")
+
 	blockchain.chainLock.Lock()
 	defer blockchain.chainLock.Unlock()
 
@@ -172,6 +175,7 @@ func (blockchain *BlockChain) InsertBeaconBlock(block *BeaconBlock, isCommittee 
 	}
 	// ************ Store block at last
 	Logger.log.Infof("Store StabilityInfo %+v \n")
+	fmt.Println("Store StabilityInfo")
 	if err := blockchain.config.DataBase.StoreStabilityInfoByHeight(block.Header.Height, bestStateBeacon.StabilityInfo); err != nil {
 		return err
 	}
