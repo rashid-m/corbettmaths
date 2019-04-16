fb9dab2c012a2049879ce955c77e1de9d4c391c4
0xsirrush <thaibao@autonomous.nyc>
2019-04-16 15:11:55 +0700

    - check node mode

--- server.go
+++ server.go
@@ -130,7 +130,7 @@ func (serverObj *Server) NewServer(listenAddrs string, db database.DatabaseInter
 
 	serverObj.userKeySet, err = cfg.GetUserKeySet()
 	if err != nil {
-		if cfg.NodeMode == "auto" || cfg.NodeMode == "beacon" || cfg.NodeMode == "shard" {
+		if cfg.NodeMode == common.NODEMODE_AUTO || cfg.NodeMode == common.NODEMODE_BEACON || cfg.NodeMode == common.NODEMODE_SHARD {
 			Logger.log.Critical(err)
 			return err
 		} else {
@@ -350,6 +350,7 @@ func (serverObj *Server) NewServer(listenAddrs string, db database.DatabaseInter
 			FeeEstimator:    serverObj.feeEstimator,
 			ProtocolVersion: serverObj.protocolVersion,
 			Database:        &serverObj.dataBase,
+			IsMiningNode:    cfg.NodeMode != common.NODEMODE_RELAY && cfg.PrivateKey != "", // a node is mining if it constains this condiction when runing
 		}
 		serverObj.rpcServer = &rpcserver.RpcServer{}
 		serverObj.rpcServer.Init(&rpcConfig)
@@ -500,7 +501,7 @@ func (serverObj Server) Start() {
 	}
 	go serverObj.blockChain.StartSyncBlk()
 
-	if cfg.NodeMode != "relay" {
+	if cfg.NodeMode != common.NODEMODE_RELAY {
 		err := serverObj.consensusEngine.Start()
 		if err != nil {
 			Logger.log.Error(err)
@@ -1250,7 +1251,7 @@ func (serverObj *Server) BoardcastNodeState() error {
 	msg.(*wire.MessagePeerState).ShardToBeaconPool = serverObj.shardToBeaconPool.GetValidPendingBlockHeight()
 	if serverObj.userKeySet != nil {
 		userRole, shardID := serverObj.blockChain.BestState.Beacon.GetPubkeyRole(serverObj.userKeySet.GetPublicKeyB58(), serverObj.blockChain.BestState.Beacon.BestBlock.Header.Round)
-		if (cfg.NodeMode == "auto" || cfg.NodeMode == "shard") && userRole == "shard" {
+		if (cfg.NodeMode == common.NODEMODE_AUTO || cfg.NodeMode == common.NODEMODE_SHARD) && userRole == common.NODEMODE_SHARD {
 			userRole = serverObj.blockChain.BestState.Shard[shardID].GetPubkeyRole(serverObj.userKeySet.GetPublicKeyB58(), serverObj.blockChain.BestState.Shard[shardID].BestBlock.Header.Round)
 			if userRole == "shard-proposer" || userRole == "shard-validator" {
 				msg.(*wire.MessagePeerState).CrossShardPool[shardID] = serverObj.crossShardPool[shardID].GetValidBlockHeight()
