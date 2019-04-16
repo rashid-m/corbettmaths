d868fc736abf0641763637574622207f178e7154
0xkumi <dung.q@autonomous.nyc>
2019-04-16 10:42:52 +0700

    Update Run file

--- bin/run_constant.sh
+++ bin/run_constant.sh
@@ -6,4 +6,4 @@ fi
 
 echo ./constant -n $NAME --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --privatekey $PRIVATEKEY --nodemode "auto" --datadir "/data" --listen "0.0.0.0:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit --rpclisten "0.0.0.0:$RPC_PORT" > cmd.sh
 
-./constant -n $NAME --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --privatekey $PRIVATEKEY --nodemode "auto" --datadir "/data" --listen "0.0.0.0:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit --rpclisten "0.0.0.0:$RPC_PORT" > /data/log.txt
+./constant -n $NAME --discoverpeers --discoverpeersaddress $DISCOVERPEERSADDRESS --privatekey $PRIVATEKEY --nodemode "auto" --datadir "/data" --listen "0.0.0.0:$PORT" --externaladdress "$EXTERNALADDRESS:$PORT" --norpcauth --enablewallet --wallet "constant" --walletpassphrase "12345678" --walletautoinit --rpclisten "0.0.0.0:$RPC_PORT" > /data/log.txt 2>&1
