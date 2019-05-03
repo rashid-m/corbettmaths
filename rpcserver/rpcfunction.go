139c4d5fef475204bd8f2c436a3776b4540b10a6
0xbunyip <baoduy@autonomous.nyc>
2019-05-03 11:54:30 +0700

    Add rpc GetConstantCirculating which returns 0

--- rpcserver/rpcfunction.go
+++ rpcserver/rpcfunction.go
@@ -172,6 +172,7 @@ var RpcHandler = map[string]commandHandler{
 	GetGOVBoardIndex:   RpcServer.handleGetGOVBoardIndex,
 	// CreateAndSendTxWithIssuingRequest:     RpcServer.handleCreateAndSendTxWithIssuingRequest,
 	// CreateAndSendTxWithContractingRequest: RpcServer.handleCreateAndSendTxWithContractingRequest,
+	GetConstantCirculating: RpcServer.handleGetConstantCirculating,
 
 	// gov
 	GetBondTypes:                           RpcServer.handleGetBondTypes,
