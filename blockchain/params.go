1696679d6156101d3a3dde1d699e98301f533615
0xmerman <hung@autonomous.nyc>
2019-05-14 12:57:03 +0700

    Update for testing

--- blockchain/params.go
+++ blockchain/params.go
@@ -62,7 +62,7 @@ var genesisParamsTestnetNew = GenesisParams{
 	//@Notice: InitTxsForBenchmark is for testing and benchmark only
 	//InitialConstant: append(TestnetInitConstant, append(InitTxsShard0, append(InitTxsShard1, InitTxsForBenchmark...)...)...),
 	//InitialConstant: append(TestnetInitConstant, InitTxsShard0...),
-	InitialConstant: TestnetInitConstant,
+	InitialConstant: append(TestnetInitConstant, InitTxsShard0...),
 }
 
 var ChainTestParam = Params{
