b510dc1c40f3c078c0beda49a2929bcde5a16024
0xankylosaurus <minhduccm90@gmail.com>
2019-05-15 17:43:40 +0700

    Remove metadata types

--- metadata/constants.go
+++ metadata/constants.go
@@ -3,35 +3,12 @@ package metadata
 const (
 	InvalidMeta = 1
 
-	LoanRequestMeta  = 2
-	LoanResponseMeta = 3
-	LoanWithdrawMeta = 4
-	LoanUnlockMeta   = 5
-	LoanPaymentMeta  = 6
+	IssuingRequestMeta      = 24
+	IssuingResponseMeta     = 25
+	ContractingRequestMeta  = 26
+	ContractingResponseMeta = 27
 
-	// Dividend: removed 7-8
-
-	// Crowdsale: removed 10-11
-
-	// CMB: removed 12-19
-
-	BuyFromGOVRequestMeta        = 20
-	BuyFromGOVResponseMeta       = 21
-	BuyBackRequestMeta           = 22
-	BuyBackResponseMeta          = 23
-	IssuingRequestMeta           = 24
-	IssuingResponseMeta          = 25
-	ContractingRequestMeta       = 26
-	ContractingResponseMeta      = 27
-	OracleFeedMeta               = 28
-	OracleRewardMeta             = 29
-	RefundMeta                   = 30
-	UpdatingOracleBoardMeta      = 31
-	MultiSigsRegistrationMeta    = 32
-	MultiSigsSpendingMeta        = 33
-	WithSenderAddressMeta        = 34
 	ResponseBaseMeta             = 35
-	BuyGOVTokenRequestMeta       = 36
 	ShardBlockSalaryRequestMeta  = 37
 	ShardBlockSalaryResponseMeta = 38
 	BeaconSalaryRequestMeta      = 39
@@ -54,8 +31,6 @@ const (
 )
 
 var minerCreatedMetaTypes = []int{
-	BuyFromGOVRequestMeta,
-	BuyBackRequestMeta,
 	ShardBlockSalaryResponseMeta,
 	IssuingResponseMeta,
 	ContractingResponseMeta,
