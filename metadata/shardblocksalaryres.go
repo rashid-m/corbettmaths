80c651c49b0c9bc8459fc9b40de82a000fa87107
0xakk0r0kamui <hy@ninja.org>
2019-05-20 11:58:02 +0700

    ShardReward ok

--- metadata/shardblocksalaryres.go
+++ metadata/shardblocksalaryres.go
@@ -1,53 +1,47 @@
 package metadata
 
 import (
-	"bytes"
-	"encoding/json"
 
 	// "errors"
-	"strconv"
 
-	"github.com/constant-money/constant-chain/common"
 	"github.com/constant-money/constant-chain/database"
-	"github.com/constant-money/constant-chain/privacy"
-	"github.com/pkg/errors"
 )
 
-type ShardBlockSalaryRes struct {
-	MetadataBase
-	ShardBlockHeight         uint64
-	ProducerAddress          privacy.PaymentAddress
-	ShardBlockSalaryInfoHash common.Hash
-}
+// type ShardBlockSalaryRes struct {
+// 	MetadataBase
+// 	ShardBlockHeight         uint64
+// 	ProducerAddress          privacy.PaymentAddress
+// 	ShardBlockSalaryInfoHash common.Hash
+// }
 
 type ShardBlockRewardMeta struct {
 	MetadataBase
 }
 
-type ShardBlockSalaryInfo struct {
-	ShardBlockSalary uint64
-	ShardBlockFee    uint64
-	PayToAddress     *privacy.PaymentAddress
-	ShardBlockHeight uint64
-	InfoHash         *common.Hash
-}
+// type ShardBlockSalaryInfo struct {
+// 	ShardBlockSalary uint64
+// 	ShardBlockFee    uint64
+// 	PayToAddress     *privacy.PaymentAddress
+// 	ShardBlockHeight uint64
+// 	InfoHash         *common.Hash
+// }
 
-func NewShardBlockSalaryRes(
-	shardBlockHeight uint64,
-	producerAddress privacy.PaymentAddress,
-	shardBlockSalaryInfoHash common.Hash,
-	metaType int,
-) *ShardBlockSalaryRes {
-	metadataBase := MetadataBase{
-		Type: metaType,
-	}
-	return &ShardBlockSalaryRes{
-		ShardBlockHeight:         shardBlockHeight,
-		ProducerAddress:          producerAddress,
-		ShardBlockSalaryInfoHash: shardBlockSalaryInfoHash,
-		MetadataBase:             metadataBase,
-	}
-}
+// func NewShardBlockSalaryRes(
+// 	shardBlockHeight uint64,
+// 	producerAddress privacy.PaymentAddress,
+// 	shardBlockSalaryInfoHash common.Hash,
+// 	metaType int,
+// ) *ShardBlockSalaryRes {
+// 	metadataBase := MetadataBase{
+// 		Type: metaType,
+// 	}
+// 	return &ShardBlockSalaryRes{
+// 		ShardBlockHeight:         shardBlockHeight,
+// 		ProducerAddress:          producerAddress,
+// 		ShardBlockSalaryInfoHash: shardBlockSalaryInfoHash,
+// 		MetadataBase:             metadataBase,
+// 	}
+// }
 
 func NewShardBlockRewardMeta() *ShardBlockRewardMeta {
 	metadataBase := MetadataBase{
@@ -58,33 +52,33 @@ func NewShardBlockRewardMeta() *ShardBlockRewardMeta {
 	}
 }
 
-func (sbsRes *ShardBlockSalaryRes) CheckTransactionFee(tr Transaction, minFee uint64) bool {
-	// no need to have fee for this tx
-	return true
-}
+// func (sbsRes *ShardBlockSalaryRes) CheckTransactionFee(tr Transaction, minFee uint64) bool {
+// 	// no need to have fee for this tx
+// 	return true
+// }
 
-func (sbsRes *ShardBlockSalaryRes) ValidateTxWithBlockChain(txr Transaction, bcr BlockchainRetriever, shardID byte, db database.DatabaseInterface) (bool, error) {
-	// no need to validate tx with blockchain, just need to validate with request tx (via RequestedTxID) in current block
-	return false, nil
-}
+// func (sbsRes *ShardBlockSalaryRes) ValidateTxWithBlockChain(txr Transaction, bcr BlockchainRetriever, shardID byte, db database.DatabaseInterface) (bool, error) {
+// 	// no need to validate tx with blockchain, just need to validate with request tx (via RequestedTxID) in current block
+// 	return false, nil
+// }
 
 func (shardBlockRewardMeta *ShardBlockRewardMeta) ValidateTxWithBlockChain(txr Transaction, bcr BlockchainRetriever, shardID byte, db database.DatabaseInterface) (bool, error) {
 	// no need to validate tx with blockchain, just need to validate with request tx (via RequestedTxID) in current block
 	return false, nil
 }
 
-func (sbsRes *ShardBlockSalaryRes) ValidateSanityData(bcr BlockchainRetriever, txr Transaction) (bool, bool, error) {
-	if len(sbsRes.ProducerAddress.Pk) == 0 {
-		return false, false, errors.New("Wrong request info's producer address")
-	}
-	if len(sbsRes.ProducerAddress.Tk) == 0 {
-		return false, false, errors.New("Wrong request info's producer address")
-	}
-	// if sbsRes.ShardBlockHeight == 0 {
-	// 	return false, false, errors.New("Wrong request info's shard block height")
-	// }
-	return false, true, nil
-}
+// func (sbsRes *ShardBlockSalaryRes) ValidateSanityData(bcr BlockchainRetriever, txr Transaction) (bool, bool, error) {
+// 	if len(sbsRes.ProducerAddress.Pk) == 0 {
+// 		return false, false, errors.New("Wrong request info's producer address")
+// 	}
+// 	if len(sbsRes.ProducerAddress.Tk) == 0 {
+// 		return false, false, errors.New("Wrong request info's producer address")
+// 	}
+// 	// if sbsRes.ShardBlockHeight == 0 {
+// 	// 	return false, false, errors.New("Wrong request info's shard block height")
+// 	// }
+// 	return false, true, nil
+// }
 
 func (shardBlockRewardMeta *ShardBlockRewardMeta) ValidateMetadataByItself() bool {
 	// The validation just need to check at tx level, so returning true here
@@ -93,71 +87,71 @@ func (shardBlockRewardMeta *ShardBlockRewardMeta) ValidateMetadataByItself() boo
 
 func (shardBlockRewardMeta *ShardBlockRewardMeta) ValidateSanityData(bcr BlockchainRetriever, txr Transaction) (bool, bool, error) {
 	// The validation just need to check at tx level, so returning true here
-	return false, false, nil
+	return true, true, nil
 }
 
-func (sbsRes *ShardBlockSalaryRes) ValidateMetadataByItself() bool {
-	// The validation just need to check at tx level, so returning true here
-	return true
-}
+// func (sbsRes *ShardBlockSalaryRes) ValidateMetadataByItself() bool {
+// 	// The validation just need to check at tx level, so returning true here
+// 	return true
+// }
 
-func (sbsRes *ShardBlockSalaryRes) Hash() *common.Hash {
-	record := sbsRes.ProducerAddress.String()
-	record += string(sbsRes.ShardBlockHeight)
-	record += sbsRes.ShardBlockSalaryInfoHash.String()
+// func (sbsRes *ShardBlockSalaryRes) Hash() *common.Hash {
+// 	record := sbsRes.ProducerAddress.String()
+// 	record += string(sbsRes.ShardBlockHeight)
+// 	record += sbsRes.ShardBlockSalaryInfoHash.String()
 
-	// final hash
-	record += sbsRes.MetadataBase.Hash().String()
-	hash := common.HashH([]byte(record))
-	return &hash
-}
+// 	// final hash
+// 	record += sbsRes.MetadataBase.Hash().String()
+// 	hash := common.HashH([]byte(record))
+// 	return &hash
+// }
 
-func (sbsRes *ShardBlockSalaryRes) VerifyMinerCreatedTxBeforeGettingInBlock(
-	insts [][]string,
-	instUsed []int,
-	shardID byte,
-	tx Transaction,
-	bcr BlockchainRetriever,
-) (bool, error) {
-	instIdx := -1
-	var shardBlockSalaryInfo ShardBlockSalaryInfo
-	for i, inst := range insts {
-		if instUsed[i] > 0 {
-			continue
-		}
-		if inst[0] != strconv.Itoa(ShardBlockSalaryRequestMeta) {
-			continue
-		}
-		if inst[1] != strconv.Itoa(int(shardID)) {
-			continue
-		}
-		if inst[2] != "accepted" {
-			continue
-		}
-		contentStr := inst[3]
-		err := json.Unmarshal([]byte(contentStr), &shardBlockSalaryInfo)
-		if err != nil {
-			return false, err
-		}
-		if !bytes.Equal(shardBlockSalaryInfo.InfoHash[:], sbsRes.ShardBlockSalaryInfoHash[:]) {
-			continue
-		}
-		instIdx = i
-		instUsed[i] += 1
-		break
-	}
-	if instIdx == -1 {
-		return false, errors.Errorf("no instruction found for ShardBlockSalaryRes tx %s", tx.Hash().String())
-	}
-	if (!bytes.Equal(shardBlockSalaryInfo.PayToAddress.Pk[:], sbsRes.ProducerAddress.Pk[:])) ||
-		(!bytes.Equal(shardBlockSalaryInfo.PayToAddress.Tk[:], sbsRes.ProducerAddress.Tk[:])) {
-		return false, errors.Errorf("Producer address in ShardBlockSalaryRes tx %s is not matched to instruction's", tx.Hash().String())
-	}
-	if shardBlockSalaryInfo.ShardBlockHeight != sbsRes.ShardBlockHeight {
-		return false, errors.Errorf("ShardBlockHeight in ShardBlockSalaryRes tx %s is not matched to instruction's", tx.Hash().String())
-	}
-	if shardBlockSalaryInfo.ShardBlockSalary != tx.CalculateTxValue() {
-		return false, errors.Errorf("Salary amount in ShardBlockSalaryRes tx %s is not matched to instruction's", tx.Hash().String())
-	}
-	return true, nil
-}
+// func (sbsRes *ShardBlockSalaryRes) VerifyMinerCreatedTxBeforeGettingInBlock(
+// 	insts [][]string,
+// 	instUsed []int,
+// 	shardID byte,
+// 	tx Transaction,
+// 	bcr BlockchainRetriever,
+// ) (bool, error) {
+// 	instIdx := -1
+// 	var shardBlockSalaryInfo ShardBlockSalaryInfo
+// 	for i, inst := range insts {
+// 		if instUsed[i] > 0 {
+// 			continue
+// 		}
+// 		if inst[0] != strconv.Itoa(ShardBlockSalaryRequestMeta) {
+// 			continue
+// 		}
+// 		if inst[1] != strconv.Itoa(int(shardID)) {
+// 			continue
+// 		}
+// 		if inst[2] != "accepted" {
+// 			continue
+// 		}
+// 		contentStr := inst[3]
+// 		err := json.Unmarshal([]byte(contentStr), &shardBlockSalaryInfo)
+// 		if err != nil {
+// 			return false, err
+// 		}
+// 		if !bytes.Equal(shardBlockSalaryInfo.InfoHash[:], sbsRes.ShardBlockSalaryInfoHash[:]) {
+// 			continue
+// 		}
+// 		instIdx = i
+// 		instUsed[i] += 1
+// 		break
+// 	}
+// 	if instIdx == -1 {
+// 		return false, errors.Errorf("no instruction found for ShardBlockSalaryRes tx %s", tx.Hash().String())
+// 	}
+// 	if (!bytes.Equal(shardBlockSalaryInfo.PayToAddress.Pk[:], sbsRes.ProducerAddress.Pk[:])) ||
+// 		(!bytes.Equal(shardBlockSalaryInfo.PayToAddress.Tk[:], sbsRes.ProducerAddress.Tk[:])) {
+// 		return false, errors.Errorf("Producer address in ShardBlockSalaryRes tx %s is not matched to instruction's", tx.Hash().String())
+// 	}
+// 	if shardBlockSalaryInfo.ShardBlockHeight != sbsRes.ShardBlockHeight {
+// 		return false, errors.Errorf("ShardBlockHeight in ShardBlockSalaryRes tx %s is not matched to instruction's", tx.Hash().String())
+// 	}
+// 	if shardBlockSalaryInfo.ShardBlockSalary != tx.CalculateTxValue() {
+// 		return false, errors.Errorf("Salary amount in ShardBlockSalaryRes tx %s is not matched to instruction's", tx.Hash().String())
+// 	}
+// 	return true, nil
+// }
