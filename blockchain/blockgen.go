22f9b0619aa255a2c12dcf8045604a156f9b3ace
0xmerman <hung@autonomous.nyc>
2019-05-21 10:32:01 +0700

    Add Worker to Blockgen and Netsync

--- blockchain/blockgen.go
+++ blockchain/blockgen.go
@@ -2,11 +2,15 @@ package blockchain
 
 import (
 	"sync"
+	"time"
 	
 	"github.com/constant-money/constant-chain/common"
 	"github.com/constant-money/constant-chain/metadata"
 )
 
+const (
+	workers = 5
+)
 type BlkTmplGenerator struct {
 	// blockpool   BlockPool
 	txPool            TxPool
@@ -14,13 +18,13 @@ type BlkTmplGenerator struct {
 	crossShardPool    map[byte]CrossShardPool
 	chain             *BlockChain
 	CQuit             chan struct{}
-	CPendingTxs       chan []metadata.Transaction
+	CPendingTxs       chan metadata.Transaction
 	CRemovedTxs       chan metadata.Transaction
 	PendingTxs        map[common.Hash]metadata.Transaction
 	mtx               sync.RWMutex
 }
 
-func (blkTmplGenerator BlkTmplGenerator) Init(txPool TxPool, chain *BlockChain, shardToBeaconPool ShardToBeaconPool, crossShardPool map[byte]CrossShardPool, cPendingTxs chan []metadata.Transaction, cRemovedTxs chan metadata.Transaction) (*BlkTmplGenerator, error) {
+func (blkTmplGenerator BlkTmplGenerator) Init(txPool TxPool, chain *BlockChain, shardToBeaconPool ShardToBeaconPool, crossShardPool map[byte]CrossShardPool, cPendingTxs chan metadata.Transaction, cRemovedTxs chan metadata.Transaction) (*BlkTmplGenerator, error) {
 	return &BlkTmplGenerator{
 		txPool:            txPool,
 		shardToBeaconPool: shardToBeaconPool,
@@ -34,14 +38,17 @@ func (blkTmplGenerator BlkTmplGenerator) Init(txPool TxPool, chain *BlockChain, 
 
 func (blkTmplGenerator *BlkTmplGenerator) Start(cQuit chan struct{}) {
 	Logger.log.Critical("Block Gen is starting")
+	for w:=0; w < workers; w++ {
+		go blkTmplGenerator.AddTransactionV2Worker(blkTmplGenerator.CPendingTxs)
+	}
 	for {
 		select {
 		case <-cQuit:
 			return
-		case addTxs := <-blkTmplGenerator.CPendingTxs:
-			{
-				go blkTmplGenerator.AddTransactionV2(addTxs)
-			}
+		//case addTxs := <-blkTmplGenerator.CPendingTxs:
+		//	{
+		//		go blkTmplGenerator.AddTransactionV2(addTxs)
+		//	}
 		case removeTxs := <-blkTmplGenerator.CRemovedTxs:
 			{
 				go blkTmplGenerator.RemoveTransactionV2(removeTxs)
@@ -49,37 +56,41 @@ func (blkTmplGenerator *BlkTmplGenerator) Start(cQuit chan struct{}) {
 		}
 	}
 }
-func (blkTmplGenerator *BlkTmplGenerator) AddTransaction(txs []metadata.Transaction) {
-	blkTmplGenerator.mtx.Lock()
-	defer blkTmplGenerator.mtx.Unlock()
-	Logger.log.Critical("Number of transaction get from pool: ", len(txs))
-	blkTmplGenerator.PendingTxs = make(map[common.Hash]metadata.Transaction)
-	for _, tx := range txs {
-		blkTmplGenerator.PendingTxs[*tx.Hash()] = tx
-	}
-}
-func (blkTmplGenerator *BlkTmplGenerator) RemoveTransaction(txs []metadata.Transaction) {
-	blkTmplGenerator.mtx.Lock()
-	defer blkTmplGenerator.mtx.Unlock()
-	//Notice: just reset
-	blkTmplGenerator.PendingTxs = make(map[common.Hash]metadata.Transaction)
-}
-func (blkTmplGenerator *BlkTmplGenerator) GetPendingTxs() []metadata.Transaction {
-	blkTmplGenerator.mtx.Lock()
-	defer blkTmplGenerator.mtx.Unlock()
-	pendingTxs := []metadata.Transaction{}
-	for _, tx := range blkTmplGenerator.PendingTxs{
-		pendingTxs = append(pendingTxs, tx)
-	}
-	blkTmplGenerator.PendingTxs = make(map[common.Hash]metadata.Transaction)
-	return pendingTxs
-}
+//func (blkTmplGenerator *BlkTmplGenerator) AddTransaction(txs []metadata.Transaction) {
+//	blkTmplGenerator.mtx.Lock()
+//	defer blkTmplGenerator.mtx.Unlock()
+//	Logger.log.Critical("Number of transaction get from pool: ", len(txs))
+//	blkTmplGenerator.PendingTxs = make(map[common.Hash]metadata.Transaction)
+//	for _, tx := range txs {
+//		blkTmplGenerator.PendingTxs[*tx.Hash()] = tx
+//	}
+//}
+//func (blkTmplGenerator *BlkTmplGenerator) RemoveTransaction(txs []metadata.Transaction) {
+//	blkTmplGenerator.mtx.Lock()
+//	defer blkTmplGenerator.mtx.Unlock()
+//	//Notice: just reset
+//	blkTmplGenerator.PendingTxs = make(map[common.Hash]metadata.Transaction)
+//}
+//func (blkTmplGenerator *BlkTmplGenerator) GetPendingTxs() []metadata.Transaction {
+//	blkTmplGenerator.mtx.Lock()
+//	defer blkTmplGenerator.mtx.Unlock()
+//	pendingTxs := []metadata.Transaction{}
+//	for _, tx := range blkTmplGenerator.PendingTxs{
+//		pendingTxs = append(pendingTxs, tx)
+//	}
+//	blkTmplGenerator.PendingTxs = make(map[common.Hash]metadata.Transaction)
+//	return pendingTxs
+//}
 
-func (blkTmplGenerator *BlkTmplGenerator) AddTransactionV2(txs []metadata.Transaction) {
+func (blkTmplGenerator *BlkTmplGenerator) AddTransactionV2(tx metadata.Transaction) {
 	blkTmplGenerator.mtx.Lock()
 	defer blkTmplGenerator.mtx.Unlock()
-	for _, tx := range txs {
-		blkTmplGenerator.PendingTxs[*tx.Hash()] = tx
+	blkTmplGenerator.PendingTxs[*tx.Hash()] = tx
+}
+func (blkTmplGenerator *BlkTmplGenerator) AddTransactionV2Worker(cPendingTx <-chan metadata.Transaction) {
+	for tx := range cPendingTx {
+		blkTmplGenerator.AddTransactionV2(tx)
+		time.Sleep(time.Nanosecond)
 	}
 }
 func (blkTmplGenerator *BlkTmplGenerator) RemoveTransactionV2(tx metadata.Transaction) {
