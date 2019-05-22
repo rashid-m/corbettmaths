7c5ce84b3900c14b11550a940f8634f1b09eddf0
0xBahamoot <lam@autonomous.nyc>
2019-05-22 23:40:59 +0700

    update

--- blockchain/shardprocess.go
+++ blockchain/shardprocess.go
@@ -121,6 +121,7 @@ func (blockchain *BlockChain) InsertShardBlock(block *ShardBlock, isValidated bo
 	if blockchain.config.UserKeySet != nil {
 		userRole := blockchain.BestState.Shard[shardID].GetPubkeyRole(blockchain.config.UserKeySet.GetPublicKeyB58(), 0)
 		if userRole == common.PROPOSER_ROLE || userRole == common.VALIDATOR_ROLE {
+			blockchain.config.DataBase.CleanBackup(true, block.Header.ShardID)
 			err = blockchain.BackupCurrentShardState(block)
 			if err != nil {
 				return err
