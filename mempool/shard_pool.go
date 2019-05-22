453d794d174dd4db27b5ddc787f15da76bfa9ba3
0xmerman <hung@autonomous.nyc>
2019-05-22 12:13:04 +0700

    HOT FIX

--- mempool/shard_pool.go
+++ mempool/shard_pool.go
@@ -48,7 +48,7 @@ func init() {
 		for _ = range ticker {
 			for k, _ := range shardPoolMap {
 				GetShardPool(k).RemoveBlock(blockchain.GetBestStateShard(k).ShardHeight)
-				GetShardPool(k).CleanOldBlock(blockchain.GetBestStateShard(k).ShardHeight)
+				//GetShardPool(k).CleanOldBlock(blockchain.GetBestStateShard(k).ShardHeight)
 				GetShardPool(k).PromotePendingPool()
 			}
 		}
@@ -106,7 +106,7 @@ func (self *ShardPool) AddShardBlock(block *blockchain.ShardBlock) error {
 	}
 	self.insertNewShardBlockToPoolV2(block)
 	self.promotePendingPool()
-	self.CleanOldBlock(self.latestValidHeight)
+	//self.CleanOldBlock(blockchain.GetBestStateShard(self.shardID).ShardHeight)
 	return nil
 }
 
@@ -279,15 +279,23 @@ func (self *ShardPool) removeBlock(lastBlockHeight uint64) {
 func (self *ShardPool) CleanOldBlock(latestBlockHeight uint64) {
 	self.mtx.Lock()
 	defer self.mtx.Unlock()
-	for height, block := range self.pendingPool {
-		if block.Header.Height <= latestBlockHeight {
-			delete(self.pendingPool, height)
+	toBeRemovedHeight := []uint64{}
+	toBeRemovedHash := []common.Hash{}
+	for height, _ := range self.pendingPool {
+		if height <= latestBlockHeight {
+			toBeRemovedHeight = append(toBeRemovedHeight, height)
 		}
 	}
 	for hash, block := range self.conflictedPool {
 		if block.Header.Height < latestBlockHeight-2 {
-			delete(self.conflictedPool, hash)
+			toBeRemovedHash = append(toBeRemovedHash, hash)
 		}
+	}
+	for _, height := range toBeRemovedHeight {
+		delete(self.pendingPool, height)
+	}
+	for _, hash := range toBeRemovedHash {
+		delete(self.conflictedPool, hash)
 	}
 }
 
