472c4a8851748994701d5208db07e1aa35864837
0xBahamoot <lam@autonomous.nyc>
2019-05-29 12:04:24 +0700

    update new sync

--- blockchain/syncblock_utils.go
+++ blockchain/syncblock_utils.go
@@ -8,7 +8,7 @@ import (
 	cache "github.com/patrickmn/go-cache"
 )
 
-func getBlkNeedToGetByHash(blksHash []common.Hash, cachedItems map[string]cache.Item, peerID libp2p.ID) []common.Hash {
+func getBlkNeedToGetByHash(prefix string, blksHash []common.Hash, cachedItems map[string]cache.Item, peerID libp2p.ID) []common.Hash {
 	var blksNeedToGet []common.Hash
 	for _, blkHash := range blksHash {
 		if _, ok := cachedItems[blkHash.String()]; !ok {
@@ -18,37 +18,69 @@ func getBlkNeedToGetByHash(blksHash []common.Hash, cachedItems map[string]cache.
 	return blksNeedToGet
 }
 
-func getBlkNeedToGetByHeight(fromHeight uint64, toHeight uint64, cachedItems map[string]cache.Item, peerID libp2p.ID) map[uint64]uint64 {
+func getBlkNeedToGetByHeight(prefix string, fromHeight uint64, toHeight uint64, cachedItems map[string]cache.Item, poolItems []uint64, peerID libp2p.ID) map[uint64]uint64 {
 	blkBatchsNeedToGet := make(map[uint64]uint64)
 
 	latestBatchBegin := uint64(0)
 	for blkHeight := fromHeight; blkHeight <= toHeight; blkHeight++ {
-		if _, ok := cachedItems[fmt.Sprint(blkHeight)]; !ok {
-			if latestBatchEnd, ok := blkBatchsNeedToGet[latestBatchBegin]; !ok {
-				blkBatchsNeedToGet[blkHeight] = blkHeight
-				latestBatchBegin = blkHeight
-			} else {
-				if latestBatchEnd+1 == blkHeight {
-					blkBatchsNeedToGet[latestBatchBegin] = blkHeight
-				} else {
+		if exist, _ := common.SliceExists(poolItems, blkHeight); !exist {
+			if _, ok := cachedItems[fmt.Sprint(blkHeight)]; !ok {
+				if latestBatchEnd, ok := blkBatchsNeedToGet[latestBatchBegin]; !ok {
 					blkBatchsNeedToGet[blkHeight] = blkHeight
 					latestBatchBegin = blkHeight
+				} else {
+					if latestBatchEnd+1 == blkHeight {
+						blkBatchsNeedToGet[latestBatchBegin] = blkHeight
+					} else {
+						blkBatchsNeedToGet[blkHeight] = blkHeight
+						latestBatchBegin = blkHeight
+					}
 				}
+			} else {
+				blkBatchsNeedToGet[blkHeight] = blkHeight
+				latestBatchBegin = blkHeight
 			}
-		} else {
-			blkBatchsNeedToGet[blkHeight] = blkHeight
-			latestBatchBegin = blkHeight
 		}
 	}
 	return blkBatchsNeedToGet
 }
 
-func getBlkNeedToGetBySpecificHeight(blksHeight []uint64, cachedItems map[string]cache.Item, peerID libp2p.ID) []uint64 {
+func getBlkNeedToGetBySpecificHeight(prefix string, blksHeight []uint64, cachedItems map[string]cache.Item, poolItems []uint64, peerID libp2p.ID) []uint64 {
 	var blksNeedToGet []uint64
 	for _, blkHeight := range blksHeight {
 		if _, ok := cachedItems[fmt.Sprint(blkHeight)]; !ok {
-			blksNeedToGet = append(blksNeedToGet, blkHeight)
+			if exist, _ := common.SliceExists(poolItems, blkHeight); !exist {
+				blksNeedToGet = append(blksNeedToGet, blkHeight)
+			}
 		}
 	}
 	return blksNeedToGet
+}
+
+type blockType int
+
+const (
+	beaconBlk blockType = iota
+	shardBlk
+	shardToBeaconBlk
+	crossShardBlk
+)
+
+func getBlkPrefixSyncKey(isByHash bool, blkType blockType, shardID byte, fromShard byte) string {
+	key := "height"
+	if isByHash {
+		key = "hash"
+	}
+	switch blkType {
+	case beaconBlk:
+		return fmt.Sprintf("%v-%v-", key, "beablk")
+	case shardBlk:
+		return fmt.Sprintf("%v-%v-%v-", key, "shardblk", shardID)
+	case shardToBeaconBlk:
+		return fmt.Sprintf("%v-%v-", key, "shtobblk")
+	case crossShardBlk:
+		return fmt.Sprintf("%v-%v-%v-%v-", key, "crossblk", shardID, fromShard)
+	default:
+		return ""
+	}
 }
