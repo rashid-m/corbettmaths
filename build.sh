c36ce8c2259b14e0b3c3506d679602e795ec0c99
sirrush <thaibao@autonomous.nyc>
2019-05-31 14:49:57 +0700

    - rename costant -> incognito

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-echo "Start build Constant"
+echo "Start build Incognito"
 
 git pull
 
 echo "Package install"
 dep ensure -v
 
-APP_NAME="constant"
+APP_NAME="incognito"
 
 echo "go build -o $APP_NAME"
 go build -o $APP_NAME
@@ -14,4 +14,4 @@ go build -o $APP_NAME
 echo "cp ./$APP_NAME $GOPATH/bin/$APP_NAME"
 mv ./$APP_NAME $GOPATH/bin/$APP_NAME
 
-echo "Build Constant success!"
+echo "Build Incognito success!"
