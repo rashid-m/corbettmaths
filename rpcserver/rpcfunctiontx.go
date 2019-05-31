4b59dce201c7405312c86c79504bf9cb5c3e135b
sirrush <thaibao@autonomous.nyc>
2019-05-31 14:41:44 +0700

    - rename CONST coin to PRV coin

--- rpcserver/rpcfunctiontx.go
+++ rpcserver/rpcfunctiontx.go
@@ -59,7 +59,7 @@ func (rpcServer RpcServer) handleListOutputCoins(params interface{}, closeChan <
 
 	//#4: optional token type - default constant coin
 	tokenID := &common.Hash{}
-	tokenID.SetBytes(common.ConstantID[:])
+	tokenID.SetBytes(common.PRVCoinID[:])
 	if len(paramsArray) > 3 {
 		var err1 error
 		tokenID, err1 = common.Hash{}.NewHashFromStr(paramsArray[3].(string))
@@ -621,7 +621,7 @@ func (rpcServer RpcServer) handleGetListPrivacyCustomTokenBalance(params interfa
 		lastByte := account.KeySet.PaymentAddress.Pk[len(account.KeySet.PaymentAddress.Pk)-1]
 		shardIDSender := common.GetShardIDFromLastByte(lastByte)
 		constantTokenID := &common.Hash{}
-		constantTokenID.SetBytes(common.ConstantID[:])
+		constantTokenID.SetBytes(common.PRVCoinID[:])
 		outcoints, err := rpcServer.config.BlockChain.GetListOutputCoinsByKeyset(&account.KeySet, shardIDSender, &tokenID)
 		if err != nil {
 			Logger.log.Infof("handleGetListPrivacyCustomTokenBalance result: %+v, err: %+v", nil, err)
@@ -652,7 +652,7 @@ func (rpcServer RpcServer) handleGetListPrivacyCustomTokenBalance(params interfa
 		lastByte := account.KeySet.PaymentAddress.Pk[len(account.KeySet.PaymentAddress.Pk)-1]
 		shardIDSender := common.GetShardIDFromLastByte(lastByte)
 		constantTokenID := &common.Hash{}
-		constantTokenID.SetBytes(common.ConstantID[:])
+		constantTokenID.SetBytes(common.PRVCoinID[:])
 		outcoints, err := rpcServer.config.BlockChain.GetListOutputCoinsByKeyset(&account.KeySet, shardIDSender, &tokenID)
 		if err != nil {
 			return nil, NewRPCError(ErrUnexpected, err)
@@ -876,7 +876,7 @@ func (rpcServer RpcServer) handleRandomCommitments(params interface{}, closeChan
 
 	//#3 - tokenID - default constant
 	tokenID := &common.Hash{}
-	tokenID.SetBytes(common.ConstantID[:])
+	tokenID.SetBytes(common.PRVCoinID[:])
 	if len(arrayParams) > 2 {
 		tokenIDTemp, ok := arrayParams[2].(string)
 		if !ok {
@@ -931,7 +931,7 @@ func (rpcServer RpcServer) handleHasSerialNumbers(params interface{}, closeChan 
 
 	// #3: optional - token ID - default is constant coin
 	tokenID := &common.Hash{}
-	tokenID.SetBytes(common.ConstantID[:]) // default is constant
+	tokenID.SetBytes(common.PRVCoinID[:]) // default is constant
 	if len(arrayParams) > 2 {
 		tokenIDTemp, ok := arrayParams[2].(string)
 		if !ok {
@@ -992,7 +992,7 @@ func (rpcServer RpcServer) handleHasSnDerivators(params interface{}, closeChan <
 
 	// #3: optional - token ID - default is constant coin
 	tokenID := &common.Hash{}
-	tokenID.SetBytes(common.ConstantID[:]) // default is constant
+	tokenID.SetBytes(common.PRVCoinID[:]) // default is constant
 	if len(arrayParams) > 2 {
 		tokenIDTemp, ok := arrayParams[1].(string)
 		if !ok {
