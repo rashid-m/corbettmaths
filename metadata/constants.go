ecbb569deca0b92aea42d3073af7bf4dc4889e67
sirrush <thaibao@autonomous.nyc>
2019-05-31 17:22:03 +0700

    - remove not use code shardblockreward, ...

--- metadata/constants.go
+++ metadata/constants.go
@@ -7,21 +7,15 @@ const (
 	IssuingResponseMeta    = 25
 	ContractingRequestMeta = 26
 
-	ResponseBaseMeta             = 35
-	ShardBlockReward             = 36
-	ShardBlockSalaryRequestMeta  = 37
-	ShardBlockSalaryResponseMeta = 38
-	BeaconSalaryRequestMeta      = 39
-	BeaconSalaryResponseMeta     = 40
-	ReturnStakingMeta            = 41
+	ResponseBaseMeta         = 35
+	ShardBlockReward         = 36
+	BeaconSalaryRequestMeta  = 39
+	BeaconSalaryResponseMeta = 40
+	ReturnStakingMeta        = 41
 
 	//statking
 	ShardStakingMeta  = 63
 	BeaconStakingMeta = 64
-)
-
-const (
-	MaxDivTxsPerBlock = 1000
 )
 
 var minerCreatedMetaTypes = []int{
@@ -30,5 +24,3 @@ var minerCreatedMetaTypes = []int{
 	IssuingResponseMeta,
 	ReturnStakingMeta,
 }
-
-// Special rules for shardID: stored as 2nd param of instruction of BeaconBlock
