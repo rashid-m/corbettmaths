4bbccb252511c2118c0e8828fb21e8545afd6f71
0xmerman <hung@autonomous.nyc>
2019-06-05 17:08:13 +0700

    update readme.md

--- blockchain/btc/btcapi/btcclient.go
+++ blockchain/btc/btcapi/btcclient.go
@@ -49,3 +49,32 @@ func (btcClient *BTCClient) GetBlockchainInfo() (map[string]interface{}, error) 
 	}
 	return result ,nil
 }
+/*
+
+*/
+func (btcClient *BTCClient) GetBestBlockHeight() (int, error) {
+	var result = make(map[string]interface{})
+	var err error
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getblockcount\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://" + btcClient.IP + ":" + btcClient.Port, body)
+	if err != nil {
+		return -1, NewBTCAPIError(APIError, err)
+	}
+	req.SetBasicAuth(btcClient.User, btcClient.Password)
+	req.Header.Set("Content-Type", "text/plain;")
+	
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return -1, NewBTCAPIError(APIError, err)
+	}
+	defer resp.Body.Close()
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return -1, NewBTCAPIError(APIError, err)
+	}
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return -1, NewBTCAPIError(APIError, err)
+	}
+	return result ,nil
+}
