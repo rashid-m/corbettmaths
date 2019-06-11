30d80251c72b3238abf3062ad7fd725b17acf125
0xBahamoot <lam@autonomous.nyc>
2019-06-11 11:42:37 +0700

    update

--- blockchain/shardproducer.go
+++ blockchain/shardproducer.go
@@ -198,24 +198,42 @@ func (blockgen *BlkTmplGenerator) getTransactionForNewBlock(privatekey *privacy.
 	if len(txsToAdd) == 0 {
 		Logger.log.Info("Creating empty block...")
 	}
+	go blockgen.txPool.RemoveTxList(txToRemove, false)
+
+	// Process stability tx, create response txs if needed
+	var respTxsShard, respTxsBeacon []metadata.Transaction
+	var errCh chan error
+	errCh = make(chan error)
+
 	go func() {
-		for _, tx := range txToRemove {
-			blockgen.txPool.RemoveTx(tx, false)
+		var err error
+		respTxsShard, err = blockgen.buildStabilityResponseTxsAtShardOnly(txsToAdd, privatekey, shardID)
+		if err != nil {
+			errCh <- err
 		}
 	}()
 
-	// Process stability tx, create response txs if needed
-	stabilityResponseTxs, err := blockgen.buildStabilityResponseTxsAtShardOnly(txsToAdd, privatekey, shardID)
-	if err != nil {
-		return nil, err
-	}
-	txsToAdd = append(txsToAdd, stabilityResponseTxs...)
+	go func() {
+		var err error
+		respTxsBeacon, err = blockgen.buildResponseTxsFromBeaconInstructions(beaconBlocks, privatekey, shardID)
+		if err != nil {
+			errCh <- err
+		}
+	}()
 
-	stabilityResponseTxs, err = blockgen.buildResponseTxsFromBeaconInstructions(beaconBlocks, privatekey, shardID)
-	if err != nil {
-		return nil, err
+	nilCount := 0
+	for {
+		err := <-errCh
+		if err != nil {
+			return nil, err
+		}
+		nilCount++
+		if nilCount == 2 {
+			break
+		}
 	}
-	txsToAdd = append(txsToAdd, stabilityResponseTxs...)
+	txsToAdd = append(txsToAdd, respTxsShard...)
+	txsToAdd = append(txsToAdd, respTxsBeacon...)
 	return txsToAdd, nil
 }
 
