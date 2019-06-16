52ef123346282b32ac73af65921919d5173ee374
0xkumi <dung.q@autonomous.nyc>
2019-06-16 13:12:57 +0700

    update

--- bin/local.sh
+++ bin/local.sh
@@ -9,6 +9,7 @@ fi
 
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o incognito github.com/incognitochain/incognito-chain
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o bootnode ../bootnode/*.go
+cp ../keylist.json .
 
 commit=`git show --summary --oneline | cut -d ' ' -f 1`
 docker build --build-arg commit=$commit . -t incognito && echo "Commit: $commit"
