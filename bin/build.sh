eeb4873c72522cf6ed5cefea115a5fa7388fd2f1
0xkumi <dung.q@autonomous.nyc>
2019-06-16 13:12:57 +0700

    update

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,8 @@ if [ -f ./bootnode ]; then
 fi
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o incognito github.com/incognitochain/incognito-chain
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o bootnode ../bootnode/*.go
+cp ../keylist.json .
 
 commit=`git show --summary --oneline | cut -d ' ' -f 1`
-docker build --build-arg commit=$commit . -t hungngoautonomous/incognito && docker push hungngoautonomous/incognito && echo "Commit: $commit"
+docker build --build-arg commit=$commit . -t dungvanautonomous/incognito && docker push dungvanautonomous/incognito && echo "Commit: $commit"
 docker rmi -f $(docker images --filter "dangling=true" -q)
