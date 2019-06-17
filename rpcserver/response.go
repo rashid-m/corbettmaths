1cfdc8dc1cf5f83cef2174e1c0f114ca49d24ee9
0xmerman <hung@autonomous.nyc>
2019-06-18 00:22:22 +0700

    Refactor Code and Rename

--- rpcserver/response.go
+++ rpcserver/response.go
@@ -6,14 +6,21 @@ import (
 	"github.com/pkg/errors"
 )
 
-// RPCResponse is the general form of a JSON-RPC response.  The type of the Result
+// JsonResponse is the general form of a JSON-RPC response.  The type of the Result
 // field varies from one command to the next, so it is implemented as an
 // interface.  The Id field has to be a pointer for Go to put a null in it when
 // empty.
-type RPCResponse struct {
-	Result json.RawMessage `json:"Result"`
-	Error  *RPCError       `json:"Error"`
-	Id     *interface{}    `json:"Id"`
+type JsonResponse struct {
+	Id      *interface{}    `json:"Id"`
+	Result  json.RawMessage `json:"Result"`
+	Error   *RPCError       `json:"Error"`
+	Params  interface{}     `json:"Params"`
+	Method  string          `json:"Method"`
+	Jsonrpc string          `json:"Jsonrpc"`
+}
+type SubcriptionResult struct {
+	SubscriptionID string          `json:"SubscriptionID"`
+	Result         json.RawMessage `json:"Result"`
 }
 
 // NewResponse returns a new JSON-RPC response object given the provided id,
@@ -22,17 +29,19 @@ type RPCResponse struct {
 //
 // Typically callers will instead want to create the fully marshalled JSON-RPC
 // response to send over the wire with the MarshalResponse function.
-func NewResponse(id interface{}, marshalledResult []byte, rpcErr *RPCError) (*RPCResponse, error) {
+func newResponse(request *JsonRequest, id interface{}, marshalledResult []byte, rpcErr *RPCError) (*JsonResponse, error) {
 	if !IsValidIDType(id) {
 		str := fmt.Sprintf("The id of type '%T' is invalid", id)
 		return nil, NewRPCError(ErrInvalidType, errors.New(str))
 	}
-
 	pid := &id
-	resp := &RPCResponse{
-		Result: marshalledResult,
-		Error:  rpcErr,
-		Id:     pid,
+	resp := &JsonResponse{
+		Id:      pid,
+		Result:  marshalledResult,
+		Error:   rpcErr,
+		Params:  request.Params,
+		Method:  request.Method,
+		Jsonrpc: request.Jsonrpc,
 	}
 	if resp.Error != nil {
 		resp.Error.StackTrace = rpcErr.Error()
@@ -60,28 +69,10 @@ func IsValidIDType(id interface{}) bool {
 	}
 }
 
-// MarshalResponse marshals the passed id, result, and RPCError to a JSON-RPC
-// response byte slice that is suitable for transmission to a JSON-RPC client.
-func MarshalResponse(id interface{}, result interface{}, rpcErr *RPCError) ([]byte, error) {
-	marshalledResult, err := json.Marshal(result)
-	if err != nil {
-		return nil, err
-	}
-	response, err := NewResponse(id, marshalledResult, rpcErr)
-	if err != nil {
-		return nil, err
-	}
-	resultResp, err := json.MarshalIndent(&response, "", "\t")
-	if err != nil {
-		return nil, err
-	}
-	return resultResp, nil
-}
-
-// createMarshalledReply returns a new marshalled JSON-RPC response given the
+// createMarshalledResponse returns a new marshalled JSON-RPC response given the
 // passed parameters.  It will automatically convert errors that are not of
 // the type *btcjson.RPCError to the appropriate type as needed.
-func createMarshalledReply(id, result interface{}, replyErr error) ([]byte, error) {
+func createMarshalledResponse(request *JsonRequest, result interface{}, replyErr error) ([]byte, error) {
 	var jsonErr *RPCError
 	if replyErr != nil {
 		if jErr, ok := replyErr.(*RPCError); ok {
@@ -90,19 +81,19 @@ func createMarshalledReply(id, result interface{}, replyErr error) ([]byte, erro
 			jsonErr = internalRPCError(replyErr.Error(), "")
 		}
 	}
-	return MarshalResponse(id, result, jsonErr)
-}
-
-// internalRPCError is a convenience function to convert an internal error to
-// an RPC error with the appropriate Code set.  It also logs the error to the
-// RPC server subsystem since internal errors really should not occur.  The
-// context parameter is only used in the log Message and may be empty if it's
-// not needed.
-func internalRPCError(errStr, context string) *RPCError {
-	logStr := errStr
-	if context != "" {
-		logStr = context + ": " + errStr
+	// MarshalResponse marshals the passed id, result, and RPCError to a JSON-RPC
+	// response byte slice that is suitable for transmission to a JSON-RPC client.
+	marshalledResult, err := json.Marshal(result)
+	if err != nil {
+		return nil, err
 	}
-	Logger.log.Info(logStr)
-	return NewRPCError(ErrRPCInternal, errors.New(errStr))
+	response, err := newResponse(request, request.Id, marshalledResult, jsonErr)
+	if err != nil {
+		return nil, err
+	}
+	resultResp, err := json.MarshalIndent(&response, "", "\t")
+	if err != nil {
+		return nil, err
+	}
+	return resultResp, nil
 }
--- rpcserver/ws_testws.go
+++ rpcserver/ws_testws.go
@@ -6,8 +6,8 @@ import (
 
 func (wsServer *WsServer) handleSubcribeNewBlock(params interface{}, result chan interface{}, error chan *RPCError, closeChan <-chan struct{}) {
 	for i := 0; i < 10; i++ {
-		result<-i
-		<-time.Tick(1*time.Second)
+		result <- i
+		<-time.Tick(1 * time.Second)
 	}
 	close(result)
 }
