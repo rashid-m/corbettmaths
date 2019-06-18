eb0e1d91cc2d4d04ef8ec950b18aaa7809e1d4e5
0xmerman <hung@autonomous.nyc>
2019-06-18 18:42:32 +0700

    Refactor code after test

--- blockchain/benchmark/Index.go
+++ blockchain/benchmark/Index.go
@@ -1,9 +1,9 @@
 package benchmark
 
 func GetInitTransaction() []string{
-	Shard0 := []string{}
-	Shard1 := []string{}
-	Shard0 = append(Shard0, InitTxsShard0...)
+	//Shard0 := []string{}
+	//Shard1 := []string{}
+	//Shard0 = append(Shard0, InitTxsShard0...)
 	//Shard0 = append(Shard0, InitTxsShard0_1...)
 	//Shard0 = append(Shard0, InitTxsShard0_2...)
 	//Shard0 = append(Shard0, InitTxsShard0_3...)
@@ -13,7 +13,7 @@ func GetInitTransaction() []string{
 	//Shard0 = append(Shard0, InitTxsShard0_7...)
 	//Shard0 = append(Shard0, InitTxsShard0_8...)
 	//Shard0 = append(Shard0, InitTxsShard0_9...)
-	Shard1 = append(Shard1, InitTxsShard1...)
+	//Shard1 = append(Shard1, InitTxsShard1...)
 	//Shard1 = append(Shard1, InitTxsShard1_1...)
 	//Shard1 = append(Shard1, InitTxsShard1_2...)
 	//Shard1 = append(Shard1, InitTxsShard1_3...)
@@ -23,6 +23,7 @@ func GetInitTransaction() []string{
 	//Shard1 = append(Shard1, InitTxsShard1_7...)
 	//Shard1 = append(Shard1, InitTxsShard1_8...)
 	//Shard1 = append(Shard1, InitTxsShard1_9...)
-	return append(Shard0, Shard1...)
+	//return append(Shard0, Shard1...)
+	return []string{`{"Version":1,"Type":"s","LockTime":1553762692,"Fee":0,"Info":null,"SigPubKey":"A6zmFqIlTKgsV23Qk9jz2roo3VhisVy5Flg6EGuOKaQA","Sig":"UndOECFI+/lIN+MjBwiFmmWQiHMDcNsTiVJ1yBdx5DEqBs1kj2gdxXsm6Etq29qtFASNJ3GhQgB1W2Z90ImNww==","Proof":"11111112F9nPGbqyBE7fPpGeJQACt1PrSaUj1kShmorbXzMnn3VJXPtUnqEpM1ZhU8uKk2TnaoHnCbw6tyk1hXQFcaSxDSt351633NZC2jz2oSnhikaVMJkdihtwDWkYD7VsKsBzS2WKwBqNmDwYDs297tRonb6ZntrF1dPKXtg47SETSoJnmzof2FfMQpzPTj8qJJEWPWCHFSYMLdvpr7j25","PubKeyLastByteSender":0,"Metadata":null}`,}
 }
 
