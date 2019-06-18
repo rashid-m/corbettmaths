c0631ae9badf03f0efd20e01816007d62bf57587
0xkraken <hien.n@autonomous.nyc>
2019-06-18 15:21:03 +0700

    sayHello function for testing

--- privacy/wasm/main.go
+++ privacy/wasm/main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"github.com/incognitochain/incognito-chain/privacy/zeroknowledge"
 	"syscall/js"
+	"time"
 )
 
-func AggregatedRangeWitnessProve(this js.Value, args []js.Value) interface{} {
+
+func sayHello(this js.Value, args []js.Value) interface{} {
+	fmt.Printf("Hello, %v\n", args[0].String())
 	return nil
+}
+
+func AggregatedRangeWitnessProve(this js.Value, args []js.Value) interface{} {
+	bytes := []byte(args[0].String())
+
+	wit := new(zkp.AggregatedRangeWitness)
+
+	json.Unmarshal(bytes, &wit)
+
+	start := time.Now()
+	proof, err := wit.Prove()
+	if err != nil {
+		fmt.Printf("Err: %v\n", err)
+	}
+	end := time.Since(start)
+	fmt.Printf("Aggregated range proving time: %v\n", end)
+
+	fmt.Printf("Proof json marshal: %v\n", json.Marshal(proof))
+
+	proofBase64 := base64.StdEncoding.EncodeToString(proof.Bytes())
+
+	fmt.Printf("proofBase64: %v\n", proofBase64)
+
+	return proofBase64
 }
 
 func main() {
@@ -15,5 +45,8 @@ func main() {
 
 	// register add function in onclick event with golang add funtion
 	js.Global().Set("add", js.FuncOf(AggregatedRangeWitnessProve))
+
+	// register sayHello function
+	js.Global().Set("sayHello", js.FuncOf(sayHello))
 	<-c
 }
