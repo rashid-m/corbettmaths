a44f1a98e8b8f83d04fd3a9f45a6a9e51c5820d3
sirrush <thaibao@autonomous.nyc>
2019-06-18 13:51:23 +0700

    - update

--- mempool/estimatefee.go
+++ mempool/estimatefee.go
@@ -41,8 +41,6 @@ const (
 	// number of blocks which must be observed by the fee estimator before
 	// it will provide fee estimations.
 	DefaultEstimateFeeMinRegisteredBlocks = 3
-
-	bytePerKb = 1000
 )
 
 var (
@@ -555,7 +553,7 @@ func (ef *FeeEstimator) newEstimateFeeSet(tokenID *common.Hash) *estimateFeeSet 
 		for _, o := range b {
 			set.feeRate[i] = o.feeRate
 
-			if tokenID != nil{
+			if tokenID != nil {
 				for key, value := range o.feeRateForToken {
 					if key.IsEqual(tokenID) {
 						set.feeRateForToken[key] = append(set.feeRateForToken[key], value)
@@ -569,7 +567,8 @@ func (ef *FeeEstimator) newEstimateFeeSet(tokenID *common.Hash) *estimateFeeSet 
 
 	if tokenID == nil {
 		sort.Sort(set)
-	} else {}
+	} else {
+	}
 
 	return set
 }
@@ -582,7 +581,7 @@ func (ef *FeeEstimator) estimates(tokenID *common.Hash) []CoinPerKilobyte {
 	estimates := make([]CoinPerKilobyte, estimateFeeDepth)
 	for i := 0; i < estimateFeeDepth; i++ {
 		if tokenID != nil {
-			estimates[i] = set.estimateFeeForToken(i + 1, tokenID)
+			estimates[i] = set.estimateFeeForToken(i+1, tokenID)
 		} else {
 			estimates[i] = set.estimateFee(i + 1)
 		}
