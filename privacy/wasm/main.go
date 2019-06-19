f494cc6b89de29089604d48f38c7c128ddb84f56
sirrush <thaibao@autonomous.nyc>
2019-06-19 09:40:08 +0700

    - wasm for privacy

--- privacy/wasm/main.go
+++ privacy/wasm/main.go
@@ -1,52 +1,30 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"github.com/incognitochain/incognito-chain/privacy/zeroknowledge"
+	"strconv"
 	"syscall/js"
-	"time"
 )
 
+func add(this js.Value, i []js.Value) (interface{}, error) {
+	ret := 0
 
-func sayHello(this js.Value, args []js.Value) interface{} {
-	fmt.Printf("Hello, %v\n", args[0].String())
-	return nil
+	for _, item := range i {
+		val, _ := strconv.Atoi(item.String())
+		ret += val
+	}
+
+	return ret, nil
 }
 
-func AggregatedRangeWitnessProve(this js.Value, args []js.Value) interface{} {
-	bytes := []byte(args[0].String())
-
-	wit := new(zkp.AggregatedRangeWitness)
-
-	json.Unmarshal(bytes, &wit)
-
-	start := time.Now()
-	proof, err := wit.Prove()
-	if err != nil {
-		fmt.Printf("Err: %v\n", err)
-	}
-	end := time.Since(start)
-	fmt.Printf("Aggregated range proving time: %v\n", end)
-
-	fmt.Printf("Proof json marshal: %v\n", json.Marshal(proof))
-
-	proofBase64 := base64.StdEncoding.EncodeToString(proof.Bytes())
-
-	fmt.Printf("proofBase64: %v\n", proofBase64)
-
-	return proofBase64
+func sayHello(this js.Value, i []js.Value) (interface{}, error) {
+	println("Hello %s", i[0].String())
+	return i[0].String(), nil
 }
 
 func main() {
 	c := make(chan struct{}, 0)
-	fmt.Println("Hello, WebAssembly!")
-
-	// register add function in onclick event with golang add funtion
-	js.Global().Set("add", js.FuncOf(AggregatedRangeWitnessProve))
-
-	// register sayHello function
-	js.Global().Set("sayHello", js.FuncOf(sayHello))
+	println("Hello WASM")
+	RegisterCallback("add", add)
+	RegisterCallback("sayHello", sayHello)
 	<-c
 }
