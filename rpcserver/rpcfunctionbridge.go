15b12d44c5ccb3e9a33cd6649dad6921e434f515
0xbunyip <baoduy@autonomous.nyc>
2019-06-19 12:30:59 +0700

    Build bridge block signers' merkle paths

--- rpcserver/rpcfunctionbridge.go
+++ rpcserver/rpcfunctionbridge.go
@@ -37,8 +37,8 @@ func (rpcServer RpcServer) handleGetBeaconSwapProof(params interface{}, closeCha
 		return nil, nil
 	}
 
-	// Build instruction merkle proof for bridge block
-	bridgeProof := buildKeccak256MerkleProof(shardInsts, bridgeInstId)
+	// Build merkle proof for instruction in bridge block
+	bridgeProof := buildInstProof(shardInsts, bridgeInstId)
 
 	flattenbridgeInst := []byte{}
 	for _, part := range bridgeInst {
@@ -46,13 +46,22 @@ func (rpcServer RpcServer) handleGetBeaconSwapProof(params interface{}, closeCha
 	}
 
 	// Get committee pubkey and signature
-	pubkeys, err := getBridgeInstSignerPubkeys(shardBlock, db)
+	pubkeys, signerIdxs, err := getBridgeSignerPubkeys(shardBlock, db)
 	if err != nil {
 		return nil, NewRPCError(ErrUnexpected, err)
 	}
 	bridgeSignerPubkeys := make([]string, len(pubkeys))
 	for i, pk := range pubkeys {
 		bridgeSignerPubkeys[i] = hex.EncodeToString(pk)
+	}
+
+	// Build merkle proof for signer pubkeys
+	signerProof := buildSignersProof(pubkeys, signerIdxs)
+	bridgeSignerPaths := make([][]string, len(pubkeys))
+	bridgeSignerPathIsLeft := make([][]bool, len(pubkeys))
+	for i, p := range signerProof {
+		bridgeSignerPaths[i] = p.getPath()
+		bridgeSignerPathIsLeft[i] = p.left
 	}
 
 	// Get meta hash and block hash
@@ -69,8 +78,8 @@ func (rpcServer RpcServer) handleGetBeaconSwapProof(params interface{}, closeCha
 		BridgeBlkHash:          hex.EncodeToString(bridgeBlkHash[:]),
 		BridgeSignerPubkeys:    bridgeSignerPubkeys,
 		BridgeSignerSig:        shardBlock.AggregatedSig,
-		BridgeSignerPaths:      nil,
-		BridgeSignerPathIsLeft: nil,
+		BridgeSignerPaths:      bridgeSignerPaths,
+		BridgeSignerPathIsLeft: bridgeSignerPathIsLeft,
 	}, nil
 
 	// Get the corresponding beacon block with the swap instruction
@@ -134,28 +143,36 @@ func (p *keccak256MerkleProof) getPath() []string {
 	return path
 }
 
-func buildKeccak256MerkleProof(insts [][]string, id int) *keccak256MerkleProof {
-	flattenInsts := common.FlattenAndConvertStringInst(insts)
-	fmt.Println("[db] flattenInsts", flattenInsts)
-	path, left := blockchain.GetKeccak256MerkleProof(flattenInsts, id)
+func buildProofFromTree(merkles [][]byte, id int) *keccak256MerkleProof {
+	path, left := blockchain.GetKeccak256MerkleProofFromTree(merkles, id)
 	return &keccak256MerkleProof{path: path, left: left}
 }
 
-func getBridgeInstSignerPubkeys(shardBlock *blockchain.ShardBlock, db database.DatabaseInterface) ([][]byte, error) {
+func buildProof(data [][]byte, id int) *keccak256MerkleProof {
+	merkles := blockchain.BuildKeccak256MerkleTree(data)
+	return buildProofFromTree(merkles, id)
+}
+
+func buildInstProof(insts [][]string, id int) *keccak256MerkleProof {
+	flattenInsts := common.FlattenAndConvertStringInst(insts)
+	return buildProof(flattenInsts, id)
+}
+
+func getBridgeSignerPubkeys(shardBlock *blockchain.ShardBlock, db database.DatabaseInterface) ([][]byte, []int, error) {
 	commsRaw, err := db.FetchCommitteeByEpoch(shardBlock.Header.Epoch)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	comms := make(map[byte][]string)
 	err = json.Unmarshal(commsRaw, &comms)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	comm, ok := comms[shardBlock.Header.ShardID]
 	if !ok {
-		return nil, fmt.Errorf("no committee member found for shard block %d", shardBlock.Header.ShardID)
+		return nil, nil, fmt.Errorf("no committee member found for shard block %d", shardBlock.Header.ShardID)
 	}
 
 	signerIdxs := shardBlock.ValidatorsIdx[1] // List of signers
@@ -163,9 +180,20 @@ func getBridgeInstSignerPubkeys(shardBlock *blockchain.ShardBlock, db database.D
 	for i, signerID := range signerIdxs {
 		pubkey, _, err := base58.Base58Check{}.Decode(comm[signerID])
 		if err != nil {
-			return nil, err
+			return nil, nil, err
 		}
 		pubkeys[i] = pubkey
 	}
-	return pubkeys, nil
+	return pubkeys, signerIdxs, nil
+}
+
+func buildSignersProof(pubkeys [][]byte, idxs []int) []*keccak256MerkleProof {
+	merkles := blockchain.BuildKeccak256MerkleTree(pubkeys)
+	fmt.Printf("[db] pubkeys: %x\n", pubkeys)
+	fmt.Printf("[db] merkles: %x\n", merkles)
+	proofs := make([]*keccak256MerkleProof, len(pubkeys))
+	for i, pid := range idxs {
+		proofs[i] = buildProofFromTree(merkles, pid)
+	}
+	return proofs
 }
