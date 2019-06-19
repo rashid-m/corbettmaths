91f2eba91bc05d4eba83d7539fb4f3f4eaa2f0fe
0xmerman <hung@autonomous.nyc>
2019-06-19 17:32:10 +0700

    Init Interface

---
+++ pubusb/log.go
@@ -0,0 +1,17 @@
+package pubusb
+
+import (
+	"github.com/incognitochain/incognito-chain/common"
+)
+
+type PubsubLogger struct {
+	log common.Logger
+}
+
+func (metricLogger *PubsubLogger) Init(inst common.Logger) {
+	metricLogger.log = inst
+}
+
+// Global instant to use
+var Logger = PubsubLogger{}
+
