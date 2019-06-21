994308a38143d352bbcf202a356b178214cf3003
0xmerman <hung@autonomous.nyc>
2019-06-21 22:15:33 +0700

    subcribe beacon and shard pool

---
+++ rpcserver/ws_pool.go
@@ -0,0 +1,123 @@
+package rpcserver
+
+import (
+	"errors"
+	"github.com/incognitochain/incognito-chain/common"
+	"github.com/incognitochain/incognito-chain/mempool"
+	"github.com/incognitochain/incognito-chain/pubsub"
+	"github.com/incognitochain/incognito-chain/rpcserver/jsonresult"
+	"reflect"
+	"time"
+)
+
+func (wsServer *WsServer) handleSubcribeMempoolInfo(params interface{}, subcription string, cResult chan RpcSubResult, closeChan <-chan struct{}) {
+	Logger.log.Info("Handle Subcribe Mempool Informantion", params, subcription)
+	arrayParams := common.InterfaceSlice(params)
+	if len(arrayParams) != 0 {
+		err := NewRPCError(ErrRPCInvalidParams, errors.New("Methods should only contain NO params"))
+		cResult <- RpcSubResult{Error: err}
+		return
+	}
+	subId, subChan, err := wsServer.config.PubsubManager.RegisterNewSubcriber(pubsub.MempoolInfoTopic)
+	if err != nil {
+		err := NewRPCError(ErrSubcribe, err)
+		cResult <- RpcSubResult{Error: err}
+		return
+	}
+	defer func() {
+		Logger.log.Info("Finish Subcribe Mempool Informantion")
+		wsServer.config.PubsubManager.Unsubcribe(pubsub.MempoolInfoTopic, subId)
+		close(cResult)
+	}()
+	for {
+		select {
+		case msg := <-subChan:
+			{
+				listTxs, ok := msg.Value.([]string)
+				if !ok {
+					Logger.log.Errorf("Wrong Message Type from Pubsub Manager, wanted []string, have %+v", reflect.TypeOf(msg.Value))
+					continue
+				}
+				cResult <- RpcSubResult{Result: listTxs, Error: nil}
+			}
+		case <-closeChan:
+			{
+				cResult <- RpcSubResult{Result: jsonresult.UnsubcribeResult{Message: "Unsubcribe Mempool Info"}}
+				return
+			}
+		}
+	}
+}
+
+
+func (wsServer *WsServer) handleSubcribeBeaconPoolBeststate(params interface{}, subcription string, cResult chan RpcSubResult, closeChan <-chan struct{}) {
+	Logger.log.Info("Handle Subcribe Beacon Pool Beststate", params, subcription)
+	arrayParams := common.InterfaceSlice(params)
+	if len(arrayParams) != 0 {
+		err := NewRPCError(ErrRPCInvalidParams, errors.New("Methods should only contain NO params"))
+		cResult <- RpcSubResult{Error: err}
+		return
+	}
+	beaconPool := mempool.GetBeaconPool()
+	if beaconPool == nil {
+		Logger.log.Error("Beacon pool not found")
+		return
+	}
+	defer func() {
+		Logger.log.Info("Finish Subcribe Beacon Pool Beststate")
+		close(cResult)
+	}()
+	for {
+		select {
+		case <-closeChan:
+			{
+				cResult <- RpcSubResult{Result: jsonresult.UnsubcribeResult{Message: "Unsubcribe Mempool Info"}}
+				return
+			}
+			default:
+				{
+					result := Blocks{Valid: beaconPool.GetValidBlockHeight(), Pending: beaconPool.GetPendingBlockHeight(), Latest: beaconPool.GetBeaconState()}
+					cResult <- RpcSubResult{Result: result, Error: nil}
+					<-time.Tick(1*time.Second)
+			}
+		}
+	}
+}
+
+
+func (wsServer *WsServer) handleSubcribeShardPoolBeststate(params interface{}, subcription string, cResult chan RpcSubResult, closeChan <-chan struct{}) {
+	Logger.log.Info("Handle Subcribe Shard Pool Beststate", params, subcription)
+	arrayParams := common.InterfaceSlice(params)
+	if len(arrayParams) != 1 {
+		err := NewRPCError(ErrRPCInvalidParams, errors.New("Methods should only contain 1 params"))
+		cResult <- RpcSubResult{Error: err}
+		return
+	}
+	shardID := byte(arrayParams[0].(float64))
+	shardPool := mempool.GetShardPool(shardID)
+	if shardPool == nil {
+		Logger.log.Errorf("Shard pool SHARDID %+v not found\n", shardID)
+		return
+	}
+	defer func() {
+		Logger.log.Info("Finish Subcribe Shard Pool Beststate")
+		close(cResult)
+	}()
+	for {
+		select {
+		
+		case <-closeChan:
+			{
+				cResult <- RpcSubResult{Result: jsonresult.UnsubcribeResult{Message: "Unsubcribe Mempool Info"}}
+				return
+			}
+		default:
+			{
+				result := Blocks{Valid: shardPool.GetValidBlockHeight(), Pending: shardPool.GetPendingBlockHeight(), Latest: shardPool.GetShardState()}
+				cResult <- RpcSubResult{Result: result, Error: nil}
+				<-time.Tick(1*time.Second)
+			}
+		}
+	}
+}
+
