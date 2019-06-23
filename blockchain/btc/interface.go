d985c913d9b0c668ea749a9fd0d35d59f928e936
0xmerman <hung@autonomous.nyc>
2019-06-23 21:13:39 +0700

    update random client

--- blockchain/btc/interface.go
+++ blockchain/btc/interface.go
@@ -1,7 +1,7 @@
 package btc
 
 type RandomClient interface {
-	GetNonceByTimestamp(timestamp int64) (int, int64, int64, error)
+	GetNonceByTimestamp(timestamp int64) (int, int64, int64, error) // return blockHeight, timestamp, nonce, int
 	VerifyNonceWithTimestamp(timestamp int64, nonce int64) (bool, error)
 	GetCurrentChainTimeStamp() (int64, error)
 	GetTimeStampAndNonceByBlockHeight(blockHeight int) (int64, int64, error)
