5a7f668a02441873dea5bd8303850b38be1eb9bc
0xakk0r0kamui <hy@autonomous.nyc>
2019-06-24 14:18:14 +0700

    Remove local key

--- blockchain/beaconproducer.go
+++ blockchain/beaconproducer.go
@@ -369,12 +369,12 @@ func (blockChain *BlockChain) GetShardStateFromBlock(
 	stabilityInstructions := [][]string{}
 	acceptedBlockRewardInfo := metadata.NewAcceptedBlockRewardInfo(shardID, shardBlock.Header.TotalTxsFee, shardBlock.Header.Height)
 	// str, _ := acceptedBlockRewardInfo.GetStringFormat()
-	fmt.Printf("[ndh] - - - - - - aaaaaaaaaaaaaaa\n\n\n")
-	for key, value := range shardBlock.Header.TotalTxsFee {
-		fmt.Printf("[ndh] ======================= %+v %+v \n", key, value)
-	}
+	// fmt.Printf("[ndh] - - - - - - aaaaaaaaaaaaaaa\n\n\n")
+	// for key, value := range shardBlock.Header.TotalTxsFee {
+	// 	fmt.Printf("[ndh] ======================= %+v %+v \n", key, value)
+	// }
 	acceptedRewardInstructions, err := acceptedBlockRewardInfo.GetStringFormat()
-	fmt.Printf("[ndh] ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ %+v\n", acceptedRewardInstructions)
+	// fmt.Printf("[ndh] ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ %+v\n", acceptedRewardInstructions)
 	if err != nil {
 		panic("[ndh] Cant create acceptedRewardInstructions")
 	}
