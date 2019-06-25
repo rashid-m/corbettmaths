3741d7bdda92819b1bd55ccb6e99258db16eff63
0xbunyip <baoduy@autonomous.nyc>
2019-06-25 15:36:09 +0700

    Save BurningConfirm instructions in bridge block
    - Pick BurningConfirm instructions and save to bridge block
    - Add RequestedTxID to BurningRequest action

--- blockchain/shardutils.go
+++ blockchain/shardutils.go
@@ -139,12 +139,18 @@ func CreateShardInstructionsFromTransactionAndIns(
 	}
 
 	// Pick instruction with merkle root of beacon committee's pubkeys and save to bridge block
+	// Also, pick BurningConfirm inst and save to bridge block
 	if shardID == byte(1) { // TODO(@0xbunyip): replace with bridge's shardID
 		// Instruction created here won't be sent to validators in block.Body.Instructions
 		// Instead, validators will call CreateShardInstructionsFromTransactionAndIns again and check if inst is correct
 		commPubkeyInst := pickBeaconPubkeyRootInstruction(beaconBlocks)
 		if len(commPubkeyInst) > 0 {
 			instructions = append(instructions, commPubkeyInst...)
+		}
+
+		confirmInsts := pickBurningConfirmInstruction(beaconBlocks)
+		if len(confirmInsts) > 0 {
+			instructions = append(instructions, confirmInsts...)
 		}
 	}
 
