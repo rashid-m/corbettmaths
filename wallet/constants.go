115e33a749c08cba1d1340c9eab513b06aca3732
0xkraken <hien.n@autonomous.nyc>
2019-06-26 18:43:33 +0700

    unit test for init wallet func

--- wallet/constants.go
+++ wallet/constants.go
@@ -4,4 +4,7 @@ const (
 	PriKeyType         = byte(0x0) // Serialize wallet account key into string with only PRIVATE KEY of account keyset
 	PaymentAddressType = byte(0x1) // Serialize wallet account key into string with only PAYMENT ADDRESS of account keyset
 	ReadonlyKeyType    = byte(0x2) // Serialize wallet account key into string with only READONLY KEY of account keyset
+
+	WalletNameDefault = "Incognito Wallet"
+	SeedKeyLen        = 64 //bytes
 )
