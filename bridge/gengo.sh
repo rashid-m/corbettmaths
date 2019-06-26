f44a9dd95fd597bed65dbb20adde1ab3f37579a3
0xbunyip <baoduy@autonomous.nyc>
2019-06-26 14:27:29 +0700

    Update vault contract to call incognito_proxy

--- bridge/gengo.sh
+++ bridge/gengo.sh
@@ -7,4 +7,6 @@ filename="${filename%.*}"
 
 vyper -f bytecode $fullfile > $filename.bin
 vyper -f abi $fullfile > $filename.abi
+vyper -f external_interface $fullfile > ${filename}_interface.vy
 abigen -abi $filename.abi -bin $filename.bin -pkg bridge -out $filename.go
+
