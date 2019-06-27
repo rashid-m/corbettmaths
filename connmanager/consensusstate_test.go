d30010b6488912129e5d3ed4a2e9aad740093702
sirrush <thaibao@autonomous.nyc>
2019-06-27 16:59:06 +0700

    - document and refactor code for connManager - unit test

---
+++ connmanager/consensusstate_test.go
@@ -0,0 +1,15 @@
+package connmanager
+
+import "testing"
+
+func TestConsensusState_GetCommittee(t *testing.T) {
+
+}
+
+func TestConsensusState_GetBeaconCommittee(t *testing.T) {
+
+}
+
+func TestConsensusState_GetShardCommittee(t *testing.T) {
+
+}
