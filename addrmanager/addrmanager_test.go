d07a51b5ab3b7276f9ea256008242f94613c3837
sirrush <thaibao@autonomous.nyc>
2019-06-27 10:33:53 +0700

    - add unit test

---
+++ addrmanager/addrmanager_test.go
@@ -0,0 +1,35 @@
+package addrmanager
+
+import (
+	"fmt"
+	"testing"
+)
+
+var _ = func() (_ struct{}) {
+	fmt.Println("This runs before init()!")
+	return
+}()
+
+func TestAddrManager_New(t *testing.T) {
+
+}
+
+func TestAddrManager_Good(t *testing.T) {
+
+}
+
+func TestAddrManager_Start(t *testing.T) {
+
+}
+
+func TestAddrManager_Stop(t *testing.T) {
+
+}
+
+func TestAddrManager_AddressCache(t *testing.T) {
+
+}
+
+func TestNewAddrManager_SavePeer(t *testing.T) {
+
+}
