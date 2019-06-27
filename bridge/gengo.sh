990c188297f93d656dbf50228e18cea21eade5ad
0xbunyip <baoduy@autonomous.nyc>
2019-06-27 11:18:11 +0700

    Implement vault with external contract call
    - Implement vault with withdraw() calling incognito_proxy
    - Draft TestBurn in swap_test
    - gengo generates interface as well
    - Generate incognito and vault in separate packages

--- bridge/gengo.sh
+++ bridge/gengo.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 fullfile=$1
+pkgname=$2
 filename=$(basename -- "$fullfile")
+filepath=${fullfile%/*}
 extension="${filename##*.}"
 filename="${filename%.*}"
 
-vyper -f bytecode $fullfile > $filename.bin
-vyper -f abi $fullfile > $filename.abi
-vyper -f external_interface $fullfile > ${filename}_interface.vy
-abigen -abi $filename.abi -bin $filename.bin -pkg bridge -out $filename.go
+vyper -f bytecode $fullfile > $filepath/$filename.bin
+vyper -f abi $fullfile > $filepath/$filename.abi
+vyper -f external_interface $fullfile > $filepath/${filename}_interface.vy
+
+abigen -abi $filepath/$filename.abi -bin $filepath/$filename.bin -pkg $pkgname -out $filepath/$filename.go
 
