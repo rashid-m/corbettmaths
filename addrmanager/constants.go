dc2d72423b619bb631f52005a454feffa078f54e
sirrush <thaibao@autonomous.nyc>
2019-06-27 09:38:06 +0700

    - documentation in code

--- addrmanager/constants.go
+++ addrmanager/constants.go
@@ -5,7 +5,8 @@ import "time"
 const (
 	version = 1
 
-	// dumpAddressInterval is the interval used to dump the address
-	// cache to disk for future use.
-	dumpAddressInterval = time.Second * 10
+	// DumpAddressInterval is the interval used to dump the address
+	// cache to disk for future use. Every 10 second, saving all
+	// connected address into file to reuse in the future
+	DumpAddressInterval = time.Second * 10
 )
