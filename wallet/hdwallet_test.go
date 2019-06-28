88fe8325a848fc925b7bde63e62f4637a72ebf9c
0xkraken <hien.n@autonomous.nyc>
2019-06-28 15:19:16 +0700

    unit test for NewChildKey func

--- wallet/hdwallet_test.go
+++ wallet/hdwallet_test.go
@@ -1,6 +1,7 @@
 package wallet
 
 import (
+	"github.com/incognitochain/incognito-chain/common"
 	"github.com/incognitochain/incognito-chain/privacy"
 	"github.com/magiconair/properties/assert"
 	"testing"
@@ -36,3 +37,68 @@ func TestHDWalletNewMasterKey(t *testing.T){
 /*
 		Unit test for NewChildKey function
  */
+
+func TestHDWalletNewChildKey(t *testing.T) {
+	seed := []byte{1,2,3}
+	masterKey, _ := NewMasterKey(seed)
+
+	data := []struct{
+		childIdx uint32
+	}{
+		{uint32(0)},
+		{uint32(1)},
+		{uint32(2)},
+		{uint32(3)},
+		{uint32(4)},
+	}
+
+	for _, item := range data {
+		childKey, err := masterKey.NewChildKey(item.childIdx)
+
+		assert.Equal(t, nil, err)
+		assert.Equal(t, common.Uint32ToBytes(item.childIdx), childKey.ChildNumber)
+		assert.Equal(t, ChainCodeLen, len(childKey.ChainCode))
+		assert.Equal(t, masterKey.Depth + 1, childKey.Depth)
+		assert.Equal(t, privacy.PublicKeySize, len(childKey.KeySet.PaymentAddress.Pk))
+		assert.Equal(t, privacy.TransmissionKeySize, len(childKey.KeySet.PaymentAddress.Tk))
+		assert.Equal(t, privacy.PrivateKeySize, len(childKey.KeySet.PrivateKey))
+		assert.Equal(t, privacy.ReceivingKeySize, len(childKey.KeySet.ReadonlyKey.Rk))
+	}
+}
+
+func TestHDWalletNewChildKeyFromOtherChildKey(t *testing.T) {
+	seed := []byte{1,2,3}
+	masterKey, _ := NewMasterKey(seed)
+	childKey1, _ := masterKey.NewChildKey(uint32(1))
+
+	childIndex := uint32(10)
+	childKey2, err := childKey1.NewChildKey(childIndex)
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, common.Uint32ToBytes(childIndex), childKey2.ChildNumber)
+	assert.Equal(t, ChainCodeLen, len(childKey2.ChainCode))
+	assert.Equal(t, childKey1.Depth + 1, childKey2.Depth)
+	assert.Equal(t, privacy.PublicKeySize, len(childKey2.KeySet.PaymentAddress.Pk))
+	assert.Equal(t, privacy.TransmissionKeySize, len(childKey2.KeySet.PaymentAddress.Tk))
+	assert.Equal(t, privacy.PrivateKeySize, len(childKey2.KeySet.PrivateKey))
+	assert.Equal(t, privacy.ReceivingKeySize, len(childKey2.KeySet.ReadonlyKey.Rk))
+}
+
+func TestHDWalletNewChildKeyWithSameChildIdx(t *testing.T) {
+	seed := []byte{1,2,3}
+	masterKey, _ := NewMasterKey(seed)
+
+	childIndex := uint32(10)
+	childKey1, err1 := masterKey.NewChildKey(childIndex)
+	childKey2, err2 := masterKey.NewChildKey(childIndex)
+
+	assert.Equal(t, nil, err1)
+	assert.Equal(t, nil, err2)
+	assert.Equal(t, childKey1.ChildNumber, childKey2.ChildNumber)
+	assert.Equal(t, childKey1.ChainCode, childKey2.ChainCode)
+	assert.Equal(t, childKey1.Depth, childKey2.Depth)
+	assert.Equal(t, childKey1.KeySet.PaymentAddress.Pk, childKey2.KeySet.PaymentAddress.Pk)
+	assert.Equal(t, ChainCodeLen, len(childKey2.ChainCode))
+	assert.Equal(t, masterKey.Depth + 1, childKey2.Depth)
+}
+
