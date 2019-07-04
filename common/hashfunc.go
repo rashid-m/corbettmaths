8489cc8751a89fd5a10580cba68b7741e5d4831b
0xkraken <hien.n@autonomous.nyc>
2019-07-04 17:21:38 +0700

    unit test hashfunc.go file

--- common/hashfunc.go
+++ common/hashfunc.go
@@ -4,13 +4,15 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-// HashB calculates hash(b) and returns the resulting bytes.
+// HashB calculates SHA3-256 hashing of input b
+// and returns the result in bytes array.
 func HashB(b []byte) []byte {
 	hash := sha3.Sum256(b)
 	return hash[:]
 }
 
-// HashH calculates hash(b) and returns the resulting bytes as a Hash.
+// HashB calculates SHA3-256 hashing of input b
+// and returns the result in Hash.
 func HashH(b []byte) Hash {
 	return Hash(sha3.Sum256(b))
 }
