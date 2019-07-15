98683671ec92242ee52404a042829cd5b53a76fc
sirrush <thaibao@autonomous.nyc>
2019-07-15 11:32:29 +0700

    - memcache for key-value

--- memcache/memcache.go
+++ memcache/memcache.go
@@ -9,7 +9,6 @@ import (
 	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethdb"
 )
 
 var (
@@ -106,14 +105,6 @@ func (db *MemoryCache) Delete(key []byte) error {
 	return nil
 }
 
-// NewBatch creates a write-only key-value store that buffers changes to its host
-// database until a final write is called.
-func (db *MemoryCache) NewBatch() ethdb.Batch {
-	return &batch{
-		db: db,
-	}
-}
-
 // NewIterator creates a binary-alphabetical iterator over the entire keyspace
 // contained within the memory database.
 func (db *MemoryCache) NewIterator() Iterator {
@@ -204,54 +195,6 @@ type keyvalue struct {
 	key    []byte
 	value  []byte
 	delete bool
-}
-
-// batch is a write-only memory batch that commits changes to its host
-// database when Write is called. A batch cannot be used concurrently.
-type batch struct {
-	db     *MemoryCache
-	writes []keyvalue
-	size   int
-}
-
-// Put inserts the given value into the batch for later committing.
-func (b *batch) Put(key, value []byte) error {
-	b.writes = append(b.writes, keyvalue{common.CopyBytes(key), common.CopyBytes(value), false})
-	b.size += len(value)
-	return nil
-}
-
-// Delete inserts the a key removal into the batch for later committing.
-func (b *batch) Delete(key []byte) error {
-	b.writes = append(b.writes, keyvalue{common.CopyBytes(key), nil, true})
-	b.size += 1
-	return nil
-}
-
-// ValueSize retrieves the amount of data queued up for writing.
-func (b *batch) ValueSize() int {
-	return b.size
-}
-
-// Write flushes any accumulated data to the memory database.
-func (b *batch) Write() error {
-	b.db.lock.Lock()
-	defer b.db.lock.Unlock()
-
-	for _, keyvalue := range b.writes {
-		if keyvalue.delete {
-			delete(b.db.db, string(keyvalue.key))
-			continue
-		}
-		b.db.db[string(keyvalue.key)] = keyvalue.value
-	}
-	return nil
-}
-
-// Reset resets the batch for reuse.
-func (b *batch) Reset() {
-	b.writes = b.writes[:0]
-	b.size = 0
 }
 
 // iterator can walk over the (potentially partial) keyspace of a memory key
