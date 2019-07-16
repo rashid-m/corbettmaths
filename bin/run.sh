bea17692f7bf67ff649677849bdb28b8ef7a4442
minhduccm <minhduccm90@gmail.com>
2019-07-16 18:02:54 +0700

    Add running eth light node to run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,7 @@
 
 docker -v || bash -c "wget -qO- https://get.docker.com/ | sh"
 dataDir="/data"
+ethDataDir="eth-data"
 
 source miner.conf
 
@@ -27,3 +28,11 @@ docker pull hungngoautonomous/incognito
 docker rm -f constant_miner
 
 docker run -e NAME=miner -p $miner_port:$miner_port -p $miner_rpc:$miner_rpc -e DISCOVERPEERSADDRESS='172.104.39.6:9330' -v /${dataDir}:/data -e PRIVATEKEY="${privatekey}" -e EXTERNALADDRESS="${ip}:9330"  -e PORT=$miner_port -e RPC_PORT=$miner_rpc -d --name constant_miner hungngoautonomous/incognito /run_constant.sh $clear
+
+
+runETHLightNode="n"
+read -p "Run ethereum light node? [y/n] "  runETHLightNode
+if [ $runETHLightNode == "y" ]; then
+    docker pull ethereum/client-go
+    docker run -it -p 8545:8545 -d -v /${ethDataDir}:/root/.ethereum ethereum/client-go --testnet --syncmode "light" --maxpeers 0 --rpc --rpcaddr "0.0.0.0" --rpcapi db,eth,net,web3,personal,admin --rpccorsdomain "*"
+fi
