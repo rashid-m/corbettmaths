0fd1e459d5ef47b0b8a76323e320cbfae3848b59
sirrush <thaibao@autonomous.nyc>
2019-07-18 16:13:33 +0700

    - upgrade network

--- peer/constants.go
+++ peer/constants.go
@@ -7,7 +7,7 @@ const (
 	LocalHost         = "0.0.0.0"
 	MaxRetryConn      = 15
 	RetryConnDuration = 10 * time.Second
-	ProtocolId        = "/blockchain/1.0.0"
+	ProtocolId        = "/incognito/0.1.2"
 	DelimMessageByte  = '\n'
 	DelimMessageStr   = "\n"
 
