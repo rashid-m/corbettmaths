26e45cc27a2bbe95d2e1ff8266976e38f694143b
minhduccm <minhduccm90@gmail.com>
2019-07-19 14:43:31 +0700

    Update run.sh so that incognito & geth containers can talk to each other

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker -v || bash -c "wget -qO- https://get.docker.com/ | sh"
-dataDir="/data"
+dataDir="data"
 ethDataDir="eth-data"
 
 source miner.conf
@@ -23,16 +23,16 @@ clear="n"
 read -p "Clear database?[y/n]"  clear
 
 ip=`curl http://checkip.amazonaws.com/`
-docker pull hungngoautonomous/incognito
+docker pull incognitochain/incognito
 
-docker rm -f constant_miner
+docker rm -f inc_miner
+docker rm -f inc_geth
 
-docker run -e NAME=miner -p $miner_port:$miner_port -p $miner_rpc:$miner_rpc -e DISCOVERPEERSADDRESS='172.104.39.6:9330' -v /${dataDir}:/data -e PRIVATEKEY="${privatekey}" -e EXTERNALADDRESS="${ip}:9330"  -e PORT=$miner_port -e RPC_PORT=$miner_rpc -d --name constant_miner hungngoautonomous/incognito /run_constant.sh $clear
+# docker run -e NAME=miner -p $miner_port:$miner_port -p $miner_rpc:$miner_rpc -e DISCOVERPEERSADDRESS='172.104.39.6:9330' -v /${dataDir}:/data -e PRIVATEKEY="${privatekey}" -e EXTERNALADDRESS="${ip}:9330"  -e PORT=$miner_port -e RPC_PORT=$miner_rpc -d --name constant_miner hungngoautonomous/incognito /run_constant.sh $clear
 
 
-runETHLightNode="n"
-read -p "Run ethereum light node? [y/n] "  runETHLightNode
-if [ $runETHLightNode == "y" ]; then
-    docker pull ethereum/client-go
-    docker run -it -p 8545:8545 -d -v /${ethDataDir}:/root/.ethereum ethereum/client-go --testnet --syncmode "light" --maxpeers 0 --rpc --rpcaddr "0.0.0.0" --rpcapi db,eth,net,web3,personal,admin --rpccorsdomain "*"
-fi
+docker network create --driver bridge inc_net || true
+
+docker run -it --net inc_net -p 8545:8545 -p 30303:30303 -d -v $PWD/${ethDataDir}:/root/.ethereum --name inc_geth ethereum/client-go --testnet --syncmode "light" --maxpeers 0 --rpc --rpcaddr "0.0.0.0" --rpcapi db,eth,net,web3,personal,admin --rpccorsdomain "*" --rpcvhosts="*"
+
+docker run --net inc_net -d -e GETH_NAME=inc_geth -e NAME=miner -p $miner_port:$miner_port -p $miner_rpc:$miner_rpc -e DISCOVERPEERSADDRESS='172.104.39.6:9330' -v $PWD/${dataDir}:/data -e PRIVATEKEY="${privatekey}" -e EXTERNALADDRESS="${ip}:9330" -e PORT=$miner_port -e RPC_PORT=$miner_rpc --name inc_miner incognitochain/incognito /run_incognito.sh $clear
