412ef106a7f88f69e0e8e17a7b08b1d383685b4d
minhduccm <minhduccm90@gmail.com>
2019-07-24 10:40:46 +0700

    Update deposit direction to new flow

--- blockchain/bridgetxsbuilder.go
+++ blockchain/bridgetxsbuilder.go
@@ -2,6 +2,8 @@ package blockchain
 
 import (
 	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"math/big"
 	"strconv"
@@ -14,71 +16,224 @@ import (
 	"github.com/incognitochain/incognito-chain/wallet"
 )
 
-func buildInstructionsForIssuingReq(
+func buildInstruction(
+	metaType int,
+	shardID byte,
+	instStatus string,
+	contentStr string,
+) []string {
+	return []string{
+		strconv.Itoa(metaType),
+		strconv.Itoa(int(shardID)),
+		instStatus,
+		contentStr,
+	}
+}
+
+func (chain *BlockChain) buildInstructionsForIssuingReq(
 	contentStr string,
 	shardID byte,
 	metaType int,
+	ac *metadata.AccumulatedValues,
 ) ([][]string, error) {
 	instructions := [][]string{}
-	returnedInst := []string{
-		strconv.Itoa(metaType),
-		strconv.Itoa(int(shardID)),
-		"accepted",
-		contentStr,
+	rejectedInst := buildInstruction(metaType, shardID, "rejected", contentStr)
+	db := chain.GetDatabase()
+	issuingReqAction, err := metadata.ParseIssuingInstContent(contentStr)
+	if err != nil {
+		fmt.Println("WARNING: an issue occured while parsing issuing action content: ", err)
+		return [][]string{}, err
 	}
-	instructions = append(instructions, returnedInst)
-	return instructions, nil
+
+	issuingReq := issuingReqAction.Meta
+	issuingTokenID := issuingReq.TokenID
+	issuingTokenName := issuingReq.TokenName
+	if !ac.CanProcessCIncToken(issuingTokenID) {
+		fmt.Printf("WARNING: The issuing token (%s) was already used in the current block.", issuingTokenID.String())
+		return append(instructions, rejectedInst), nil
+	}
+
+	ok, err := db.CanProcessCIncToken(issuingTokenID)
+	if err != nil {
+		fmt.Println("WARNING: an issue occured while checking it can process for the incognito token or not: ", err)
+		return nil, err
+	}
+	if !ok {
+		fmt.Printf("WARNING: The issuing token (%s) was already used in the previous blocks.", issuingTokenID.String())
+		return append(instructions, rejectedInst), nil
+	}
+
+	issuingAcceptedInst := metadata.IssuingAcceptedInst{
+		ShardID:         shardID,
+		DepositedAmount: issuingReq.DepositedAmount,
+		ReceiverAddr:    issuingReq.ReceiverAddress,
+		IncTokenID:      issuingTokenID,
+		IncTokenName:    issuingTokenName,
+		TxReqID:         issuingReqAction.TxReqID,
+	}
+	issuingAcceptedInstBytes, err := json.Marshal(issuingAcceptedInst)
+	if err != nil {
+		fmt.Println("WARNING: an error occured while marshaling issuingAccepted instruction: ", err)
+		return nil, err
+	}
+
+	ac.CBridgeTokens = append(ac.CBridgeTokens, &issuingTokenID)
+	returnedInst := buildInstruction(metaType, shardID, "accepted", string(issuingAcceptedInstBytes))
+	return append(instructions, returnedInst), nil
+}
+
+func (chain *BlockChain) buildInstructionsForIssuingETHReq(
+	contentStr string,
+	shardID byte,
+	metaType int,
+	ac *metadata.AccumulatedValues,
+) ([][]string, error) {
+	instructions := [][]string{}
+	rejectedInst := buildInstruction(metaType, shardID, "rejected", contentStr)
+	db := chain.GetDatabase()
+	issuingETHReqAction, err := metadata.ParseETHIssuingInstContent(contentStr)
+	if err != nil {
+		fmt.Println("WARNING: an issue occured while parsing issuing action content: ", err)
+		return [][]string{}, err
+	}
+	md := issuingETHReqAction.Meta
+	ethReceipt := issuingETHReqAction.ETHReceipt
+	if ethReceipt == nil {
+		fmt.Println("WARNING: eth receipt is null.")
+		return append(instructions, rejectedInst), nil
+	}
+
+	// NOTE: since TxHash from constructedReceipt is always '0x0000000000000000000000000000000000000000000000000000000000000000'
+	// so must build unique eth tx as combination of block hash and tx index.
+	uniqETHTx := append(md.BlockHash[:], []byte(strconv.Itoa(int(md.TxIndex)))...)
+	isUsedInBlock := metadata.IsETHTxHashUsedInBlock(uniqETHTx, ac.UniqETHTxsUsed)
+	if isUsedInBlock {
+		fmt.Println("WARNING: already issued for the hash in current block: ", uniqETHTx)
+		return append(instructions, rejectedInst), nil
+	}
+	isIssued, err := db.IsETHTxHashIssued(uniqETHTx)
+	if err != nil {
+		fmt.Println("WARNING: an issue occured while checking the eth tx hash is issued or not: ", err)
+		return nil, err
+	}
+	if isIssued {
+		fmt.Println("WARNING: already issued for the hash in previous blocks: ", uniqETHTx)
+		return append(instructions, rejectedInst), nil
+	}
+
+	logMap, err := metadata.PickNParseLogMapFromReceipt(ethReceipt)
+	if err != nil {
+		fmt.Println("WARNING: an error occured while parsing log map from receipt: ", err)
+		return nil, err
+	}
+	if logMap == nil {
+		fmt.Println("WARNING: could not find log map out from receipt")
+		return append(instructions, rejectedInst), nil
+	}
+
+	// the token might be ETH/ERC20
+	ethereumAddr, ok := logMap["_token"].(rCommon.Address)
+	if !ok {
+		fmt.Println("WARNING: could not parse eth token id from log map.")
+		return append(instructions, rejectedInst), nil
+	}
+	ethereumToken := ethereumAddr.Bytes()
+	canProcess, err := ac.CanProcessTokenPair(ethereumToken, md.IncTokenID)
+	if err != nil {
+		fmt.Println("WARNING: an error occured while checking it can process for token pair on the current block or not: ", err)
+		return nil, err
+	}
+	if !canProcess {
+		fmt.Println("WARNING: pair of incognito token id & ethereum's id is invalid in current block")
+		return append(instructions, rejectedInst), nil
+	}
+
+	isValid, err := db.CanProcessTokenPair(ethereumToken, md.IncTokenID)
+	if err != nil {
+		fmt.Println("WARNING: an error occured while checking it can process for token pair on the previous blocks or not: ", err)
+		return nil, err
+	}
+	if !isValid {
+		fmt.Println("WARNING: pair of incognito token id & ethereum's id is invalid with previous blocks")
+		return append(instructions, rejectedInst), nil
+	}
+
+	addressStr, ok := logMap["_incognito_address"].(string)
+	if !ok {
+		fmt.Println("WARNING: could not parse incognito address from eth log map.")
+		return append(instructions, rejectedInst), nil
+	}
+	amt, ok := logMap["_amount"].(*big.Int)
+	if !ok {
+		fmt.Println("WARNING: could not parse amount from eth log map.")
+		return append(instructions, rejectedInst), nil
+	}
+	amount := uint64(0)
+	if bytes.Equal(rCommon.HexToAddress(common.ETH_ADDR_STR).Bytes(), ethereumToken) {
+		// convert amt from wei (10^18) to nano eth (10^9)
+		amount = big.NewInt(0).Div(amt, big.NewInt(1000000000)).Uint64()
+	} else { // ERC20
+		amount = amt.Uint64()
+	}
+
+	issuingETHAcceptedInst := metadata.IssuingETHAcceptedInst{
+		ShardID:         shardID,
+		IssuingAmount:   amount,
+		ReceiverAddrStr: addressStr,
+		IncTokenID:      md.IncTokenID,
+		TxReqID:         issuingETHReqAction.TxReqID,
+		UniqETHTx:       uniqETHTx,
+		ExternalTokenID: ethereumToken,
+	}
+	issuingETHAcceptedInstBytes, err := json.Marshal(issuingETHAcceptedInst)
+	if err != nil {
+		fmt.Println("WARNING: an error occured while marshaling issuingETHAccepted instruction: ", err)
+		return nil, err
+	}
+	ac.UniqETHTxsUsed = append(ac.UniqETHTxsUsed, uniqETHTx)
+	ac.DBridgeTokenPair[md.IncTokenID.String()] = ethereumToken
+
+	acceptedInst := buildInstruction(metaType, shardID, "accepted", string(issuingETHAcceptedInstBytes))
+	return append(instructions, acceptedInst), nil
 }
 
 func (blockgen *BlkTmplGenerator) buildIssuanceTx(
 	contentStr string,
 	producerPrivateKey *privacy.PrivateKey,
 	shardID byte,
-	ac *metadata.AccumulatedValues,
 ) (metadata.Transaction, error) {
-	if shardID != common.BRIDGE_SHARD_ID {
-		return nil, nil
-	}
-
-	db := blockgen.chain.GetDatabase()
-	issuingReqAction, err := metadata.ParseIssuingInstContent(contentStr)
+	fmt.Println("[Centralized bridge token issuance] Starting...")
+	contentBytes, err := base64.StdEncoding.DecodeString(contentStr)
 	if err != nil {
 		return nil, err
 	}
-	issuingReq := issuingReqAction.Meta
-	issuingTokenID := issuingReq.TokenID
-	issuingTokenName := issuingReq.TokenName
-	if !ac.CanProcessCIncToken(issuingTokenID) {
-		fmt.Printf("WARNING: The issuing token (%s) was already used in the current block.", issuingTokenID.String())
-		return nil, nil
-	}
-
-	ok, err := db.CanProcessCIncToken(issuingTokenID)
+	var issuingAcceptedInst metadata.IssuingAcceptedInst
+	err = json.Unmarshal(contentBytes, &issuingAcceptedInst)
 	if err != nil {
 		return nil, err
 	}
-	if !ok {
-		fmt.Printf("WARNING: The issuing token (%s) was already used in the previous blocks.", issuingTokenID.String())
+
+	if shardID != issuingAcceptedInst.ShardID {
 		return nil, nil
 	}
 
 	issuingRes := metadata.NewIssuingResponse(
-		issuingReqAction.TxReqID,
+		issuingAcceptedInst.TxReqID,
 		metadata.IssuingResponseMeta,
 	)
 
 	receiver := &privacy.PaymentInfo{
-		Amount:         issuingReq.DepositedAmount,
-		PaymentAddress: issuingReq.ReceiverAddress,
+		Amount:         issuingAcceptedInst.DepositedAmount,
+		PaymentAddress: issuingAcceptedInst.ReceiverAddr,
 	}
 	var propertyID [common.HashSize]byte
-	copy(propertyID[:], issuingTokenID[:])
+	copy(propertyID[:], issuingAcceptedInst.IncTokenID[:])
 	propID := common.Hash(propertyID)
 	tokenParams := &transaction.CustomTokenPrivacyParamTx{
 		PropertyID:     propID.String(),
-		PropertyName:   issuingTokenName,
-		PropertySymbol: issuingTokenName,
-		Amount:         issuingReq.DepositedAmount,
+		PropertyName:   issuingAcceptedInst.IncTokenName,
+		PropertySymbol: issuingAcceptedInst.IncTokenName,
+		Amount:         issuingAcceptedInst.DepositedAmount,
 		TokenTxType:    transaction.CustomTokenInit,
 		Receiver:       []*privacy.PaymentInfo{receiver},
 		TokenInput:     []*privacy.InputCoin{},
@@ -103,7 +258,6 @@ func (blockgen *BlkTmplGenerator) buildIssuanceTx(
 		Logger.log.Error(initErr)
 		return nil, initErr
 	}
-	ac.CBridgeTokens = append(ac.CBridgeTokens, &issuingTokenID)
 	fmt.Println("[Centralized token issuance] Create tx ok.")
 	return resTx, nil
 }
@@ -112,106 +266,39 @@ func (blockgen *BlkTmplGenerator) buildETHIssuanceTx(
 	contentStr string,
 	producerPrivateKey *privacy.PrivateKey,
 	shardID byte,
-	ac *metadata.AccumulatedValues,
 ) (metadata.Transaction, error) {
-	if shardID != common.BRIDGE_SHARD_ID {
-		return nil, nil
-	}
-
 	fmt.Println("[Decentralized bridge token issuance] Starting...")
-	db := blockgen.chain.GetDatabase()
-	issuingETHReqAction, err := metadata.ParseETHIssuingInstContent(contentStr)
+	contentBytes, err := base64.StdEncoding.DecodeString(contentStr)
+	if err != nil {
+		return nil, err
+	}
+	var issuingETHAcceptedInst metadata.IssuingETHAcceptedInst
+	err = json.Unmarshal(contentBytes, &issuingETHAcceptedInst)
 	if err != nil {
 		return nil, err
 	}
 
-	md := issuingETHReqAction.Meta
-	constructedReceipt, err := metadata.VerifyProofAndParseReceipt(issuingETHReqAction, blockgen.chain)
-	if err != nil {
-		fmt.Println("WARNING: an error occured during verifying proof & parsing receipt: ", err)
-		return nil, err
-	}
-	if constructedReceipt == nil {
+	if shardID != issuingETHAcceptedInst.ShardID {
 		return nil, nil
 	}
 
-	// NOTE: since TxHash from constructedReceipt is always '0x0000000000000000000000000000000000000000000000000000000000000000'
-	// so must build unique eth tx as combination of block hash and tx index.
-	uniqETHTx := append(md.BlockHash[:], []byte(strconv.Itoa(int(md.TxIndex)))...)
-	isUsedInBlock := metadata.IsETHTxHashUsedInBlock(uniqETHTx, ac.UniqETHTxsUsed)
-	if isUsedInBlock {
-		fmt.Println("WARNING: already issued for the hash in current block: ", uniqETHTx)
-		return nil, nil
-	}
-	isIssued, err := db.IsETHTxHashIssued(uniqETHTx)
+	key, err := wallet.Base58CheckDeserialize(issuingETHAcceptedInst.ReceiverAddrStr)
 	if err != nil {
 		return nil, err
-	}
-	if isIssued {
-		fmt.Println("WARNING: already issued for the hash in previous blocks: ", uniqETHTx)
-		return nil, nil
-	}
-
-	logMap, err := metadata.PickNParseLogMapFromReceipt(constructedReceipt)
-	if err != nil {
-		fmt.Println("WARNING: an error occured during parsing log map from receipt: ", err)
-		return nil, err
-	}
-	if logMap == nil {
-		fmt.Println("WARNING: could not find log map out from receipt")
-		return nil, nil
-	}
-
-	// the token might be ETH/ERC20
-	ethereumAddr, ok := logMap["_token"].(rCommon.Address)
-	if !ok {
-		return nil, nil
-	}
-	ethereumToken := ethereumAddr.Bytes()
-	canProcess, err := ac.CanProcessTokenPair(ethereumToken, md.IncTokenID)
-	if err != nil {
-		return nil, err
-	}
-	if !canProcess {
-		fmt.Println("WARNING: pair of incognito token id & ethereum's id is invalid in current block")
-		return nil, nil
-	}
-
-	isValid, err := db.CanProcessTokenPair(ethereumToken, md.IncTokenID)
-	if err != nil {
-		return nil, err
-	}
-	if !isValid {
-		fmt.Println("WARNING: pair of incognito token id & ethereum's id is invalid with previous blocks")
-		return nil, nil
-	}
-
-	addressStr := logMap["_incognito_address"].(string)
-	key, err := wallet.Base58CheckDeserialize(addressStr)
-	if err != nil {
-		return nil, err
-	}
-	amt := logMap["_amount"].(*big.Int)
-	amount := uint64(0)
-	if bytes.Equal(rCommon.HexToAddress(common.ETH_ADDR_STR).Bytes(), ethereumToken) {
-		// convert amt from wei (10^18) to nano eth (10^9)
-		amount = big.NewInt(0).Div(amt, big.NewInt(1000000000)).Uint64()
-	} else { // ERC20
-		amount = amt.Uint64()
 	}
 
 	receiver := &privacy.PaymentInfo{
-		Amount:         amount,
+		Amount:         issuingETHAcceptedInst.IssuingAmount,
 		PaymentAddress: key.KeySet.PaymentAddress,
 	}
 	var propertyID [common.HashSize]byte
-	copy(propertyID[:], md.IncTokenID[:])
+	copy(propertyID[:], issuingETHAcceptedInst.IncTokenID[:])
 	propID := common.Hash(propertyID)
 	tokenParams := &transaction.CustomTokenPrivacyParamTx{
 		PropertyID: propID.String(),
 		// PropertyName:   common.PETHTokenName,
 		// PropertySymbol: common.PETHTokenName,
-		Amount:      amount,
+		Amount:      issuingETHAcceptedInst.IssuingAmount,
 		TokenTxType: transaction.CustomTokenInit,
 		Receiver:    []*privacy.PaymentInfo{receiver},
 		TokenInput:  []*privacy.InputCoin{},
@@ -219,9 +306,9 @@ func (blockgen *BlkTmplGenerator) buildETHIssuanceTx(
 	}
 
 	issuingETHRes := metadata.NewIssuingETHResponse(
-		issuingETHReqAction.TxReqID,
-		uniqETHTx,
-		ethereumToken,
+		issuingETHAcceptedInst.TxReqID,
+		issuingETHAcceptedInst.UniqETHTx,
+		issuingETHAcceptedInst.ExternalTokenID,
 		metadata.IssuingETHResponseMeta,
 	)
 	resTx := &transaction.TxCustomTokenPrivacy{}
@@ -241,8 +328,6 @@ func (blockgen *BlkTmplGenerator) buildETHIssuanceTx(
 	if initErr != nil {
 		return nil, initErr
 	}
-	ac.UniqETHTxsUsed = append(ac.UniqETHTxsUsed, uniqETHTx)
-	ac.DBridgeTokenPair[md.IncTokenID.String()] = ethereumToken
 	fmt.Println("[Decentralized bridge token issuance] Create tx ok.")
 	return resTx, nil
 }
