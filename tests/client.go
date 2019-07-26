c63d699c2bd5391e3106cc0ac1ab032e8a1ab865
0xmerman <hung@autonomous.nyc>
2019-07-26 14:01:59 +0700

    update

--- tests/client.go
+++ tests/client.go
@@ -9,11 +9,11 @@ import (
 )
 
 type Client struct {
-	Ip string
+	Host string
 	Port string
 }
 
-func makeRPCRequest(ip, port, method string, params interface{}) (*rpcserver.JsonResponse, error) {
+func makeRPCRequest(ip, port, method string, params interface{}) (*rpcserver.JsonResponse, *rpcserver.RPCError) {
 	request := rpcserver.JsonRequest{
 		Jsonrpc: "1.0",
 		Method: method,
@@ -22,22 +22,35 @@ func makeRPCRequest(ip, port, method string, params interface{}) (*rpcserver.Jso
 	}
 	requestBytes, err := json.Marshal(&request)
 	if err != nil {
-		return nil, err
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
 	}
 	resp, err := http.Post(ip+":"+port, "application/json", bytes.NewBuffer(requestBytes))
 	if err != nil {
-		return nil, err
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
 	}
 	body := resp.Body
 	defer body.Close()
 	responseBytes, err := ioutil.ReadAll(body)
 	if err != nil {
-		return nil, err
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
 	}
 	response := rpcserver.JsonResponse{}
 	err = json.Unmarshal(responseBytes, &response)
 	if err != nil {
-		return nil, err
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
 	}
 	return &response, nil
+}
+
+func (client *Client) getBlockChainInfo() (map[string]interface{}, *rpcserver.RPCError) {
+	res, rpcError := makeRPCRequest(client.Host, client.Port, "getblockchaininfo", []string{})
+	if rpcError != nil {
+		return nil, rpcError
+	}
+	result := make(map[string]interface{})
+	err := json.Unmarshal(res.Result, &result)
+	if err != nil {
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
+	}
+	return result, res.Error
 }
