aa2bb6995e7148c475da183649d5f7a9b4290f42
0xmerman <hung@autonomous.nyc>
2019-07-28 15:46:10 +0700

    Add Testcase Context to Store local variable

--- tests/engine.go
+++ tests/engine.go
@@ -9,64 +9,83 @@ import (
 
 var (
 	ErrExpectNoError = errors.New("Expect Error is null")
+	ErrExpectError = errors.New("Expect Error is not null")
 	ErrWrongCode = errors.New("Wrong Error Code")
 	ErrWrongMessage = errors.New("Wrong Error Message")
 	ErrResponseNotFound = errors.New("Expected Response Not Found")
 	ErrWrongExpectedResponse = errors.New("Wrong Expected Response")
 	ErrNetworkError = errors.New("No Error and Response from Server")
 	ErrAssertionData = errors.New("Assertion type failure")
+	ErrContextNotFound = errors.New("Key in context not found")
+	ErrWantedKeyNotFound = errors.New("Wanted Key Not Found in Response")
 )
 func executeTest(filename string) (map[string]interface{},error) {
-	var returnedErr *rpcserver.RPCError
-	var response = make(map[string]interface{})
+	var rpcError *rpcserver.RPCError
+	var result = make(map[string]interface{})
 	scenarios, err := readfile(filename)
 	if err != nil {
-		return response, err
+		return result, err
 	}
-	for _, step := range scenarios {
+	for _, step := range scenarios.steps {
 		//command := Command[step.input.name]
 		if !step.input.isWait {
 			var params []interface{}
 			if step.input.fromContext {
 				for _, value := range step.input.params {
 					if contextKey, ok := value.(string); !ok {
-						return response, fmt.Errorf("%+v, expect %+v is %+v", ErrAssertionData, value, "string")
+						return result, fmt.Errorf("%+v, expect %+v is %+v", ErrAssertionData, value, "string")
 					} else {
-					
+						if contextValue, ok := scenarios.context[contextKey]; !ok {
+							return result, fmt.Errorf("%+v, key %+v", ErrContextNotFound, contextKey)
+						} else {
+							params = append(params, contextValue)
+						}
 					}
 				}
+			} else {
+				params = append(params, step.input.params...)
 			}
-			response, returnedErr = makeRPCRequestV2(step.client, step.input.name, step.input.params)
+			result, rpcError = makeRPCRequestV2(step.client, step.input.name, params)
 			//data, err := command(step.client, step.input.params)
-			if err != nil && returnedErr.Code == rpcserver.GetErrorCode(rpcserver.ErrNetwork) {
-				return response, err
+			if rpcError != nil && rpcError.Code == rpcserver.GetErrorCode(rpcserver.ErrNetwork) {
+				return result, err
 			}
 			// check error
 			if step.output.error.isNil {
-				if err != nil {
-				    return response, fmt.Errorf("%+v, get %+v, %+v", ErrExpectNoError, returnedErr.Code, returnedErr.Message)
+				if rpcError != nil {
+				    return result, fmt.Errorf("%+v, get %+v, %+v", ErrExpectNoError, rpcError.Code, rpcError.Message)
 				}
 			} else {
-				if step.output.error.code != returnedErr.Code {
-					return response, fmt.Errorf("%+v, get %+v", ErrWrongCode, returnedErr.Code)
+				if rpcError == nil {
+					return result, fmt.Errorf("%+v, but null", ErrExpectError)
 				}
-				if step.output.error.message != returnedErr.Message {
-					return response, fmt.Errorf("%+v, get %+v", ErrWrongMessage, returnedErr.Message)
+				if step.output.error.code != rpcError.Code {
+					return result, fmt.Errorf("%+v, get %+v", ErrWrongCode, rpcError.Code)
+				}
+				if step.output.error.message != rpcError.Message {
+					return result, fmt.Errorf("%+v, get %+v", ErrWrongMessage, rpcError.Message)
 				}
 			}
 			// check output
 			// if output is empty list then continue
-			//response = data.(map[string]interface{})
+			//result = data.(map[string]interface{})
 			for key, expectedResponse := range step.output.response {
-				if returnedResponse, ok := response[key]; !ok {
-					return response, ErrResponseNotFound
+				if returnedResponse, ok := result[key]; !ok {
+					return result, ErrResponseNotFound
 				} else {
 					if !reflect.DeepEqual(expectedResponse, returnedResponse) {
-						return response, fmt.Errorf("%+v, get %+v", ErrWrongExpectedResponse, returnedResponse)
+						return result, fmt.Errorf("%+v, get %+v", ErrWrongExpectedResponse, returnedResponse)
 					}
+				}
+			}
+			for contextKey, resultKey := range step.store {
+				if resultValue, ok := result[resultKey]; !ok {
+					return result, fmt.Errorf("%+v, key %+v", ErrWantedKeyNotFound, resultKey)
+				} else {
+					scenarios.context[contextKey] = resultValue
 				}
 			}
 		}
 	}
-	return response, returnedErr
+	return result, rpcError
 }
