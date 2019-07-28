ba2491ec519720f3ed3875568add9c4144cdd910
0xmerman <hung@autonomous.nyc>
2019-07-28 15:14:50 +0700

    Update

--- tests/client.go
+++ tests/client.go
@@ -35,7 +35,7 @@ func getMethodName(depthList ...int) string {
 	r, _ := regexp.Compile("\\.(.*)")
 	return strings.ToLower(r.FindStringSubmatch(runtime.FuncForPC(function).Name())[1])
 }
-func makeRPCRequest(ip, port, method string, params ...interface{}) (*rpcserver.JsonResponse, *rpcserver.RPCError) {
+func makeRPCRequest(client* Client, method string, params ...interface{}) (*rpcserver.JsonResponse, *rpcserver.RPCError) {
 	request := rpcserver.JsonRequest{
 		Jsonrpc: "1.0",
 		Method:  method,
@@ -46,7 +46,7 @@ func makeRPCRequest(ip, port, method string, params ...interface{}) (*rpcserver.
 	if err != nil {
 		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
 	}
-	resp, err := http.Post(ip+":"+port, "application/json", bytes.NewBuffer(requestBytes))
+	resp, err := http.Post(client.Host+":"+client.Port, "application/json", bytes.NewBuffer(requestBytes))
 	if err != nil {
 		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
 	}
@@ -64,10 +64,44 @@ func makeRPCRequest(ip, port, method string, params ...interface{}) (*rpcserver.
 	return &response, nil
 }
 
+func makeRPCRequestV2(client* Client, method string, params ...interface{}) (map[string]interface{}, *rpcserver.RPCError) {
+	request := rpcserver.JsonRequest{
+		Jsonrpc: "1.0",
+		Method:  method,
+		Params:  params,
+		Id:      "1",
+	}
+	requestBytes, err := json.Marshal(&request)
+	if err != nil {
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
+	}
+	resp, err := http.Post(client.Host+":"+client.Port, "application/json", bytes.NewBuffer(requestBytes))
+	if err != nil {
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
+	}
+	body := resp.Body
+	defer body.Close()
+	responseBytes, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
+	}
+	response := rpcserver.JsonResponse{}
+	err = json.Unmarshal(responseBytes, &response)
+	if err != nil {
+		return nil, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
+	}
+	result := make(map[string]interface{})
+	rpcError := json.Unmarshal(response.Result, &result)
+	if rpcError != nil {
+		return result, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
+	}
+	return result, response.Error
+}
+
 func (client *Client) getBlockChainInfo(params ...interface{}) (interface{}, *rpcserver.RPCError) {
 	//result := &jsonresult.GetBlockChainInfoResult{}
 	result := make(map[string]interface{})
-	res, rpcError := makeRPCRequest(client.Host, client.Port, getBlockChainInfo, []string{})
+	res, rpcError := makeRPCRequest(client, getBlockChainInfo, []string{})
 	if rpcError != nil {
 		return result, rpcError
 	}
@@ -81,7 +115,7 @@ func (client *Client) getBlockChainInfo(params ...interface{}) (interface{}, *rp
 func (client *Client) createAndSendTransaction(params ...interface{}) (interface{}, *rpcserver.RPCError) {
 	//result := &jsonresult.CreateTransactionResult{}
 	result := make(map[string]interface{})
-	res, rpcError := makeRPCRequest(client.Host, client.Port, getMethodName(), params)
+	res, rpcError := makeRPCRequest(client, createAndSendTransaction, params)
 	if rpcError != nil {
 		return result, rpcError
 	}
@@ -93,3 +127,27 @@ func (client *Client) createAndSendTransaction(params ...interface{}) (interface
 }
 
 
+func (client *Client) getBalanceByPrivatekey(params ...interface{}) (interface{}, *rpcserver.RPCError) {
+	result := make(map[string]interface{})
+	res, rpcError := makeRPCRequest(client, getBalanceByPrivatekey, params)
+	if rpcError != nil {
+		return result, rpcError
+	}
+	err := json.Unmarshal(res.Result, &result)
+	if err != nil {
+		return result, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
+	}
+	return result, nil
+}
+func (client *Client) getTransactionByHash(params ...interface{}) (interface{}, *rpcserver.RPCError) {
+	result := make(map[string]interface{})
+	res, rpcError := makeRPCRequest(client, getTransactionByHash, params)
+	if rpcError != nil {
+		return result, rpcError
+	}
+	err := json.Unmarshal(res.Result, &result)
+	if err != nil {
+		return result, rpcserver.NewRPCError(rpcserver.ErrNetwork, err)
+	}
+	return result, nil
+}
