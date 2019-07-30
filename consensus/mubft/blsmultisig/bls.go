5181fbdc99716bae4d7d8b8263a5cea0c2d14d39
0xakk0r0kamui <hy@autonomous.nyc>
2019-07-30 17:44:36 +0700

    Update

--- consensus/mubft/blsmultisig/bls.go
+++ consensus/mubft/blsmultisig/bls.go
@@ -1,1 +1,5 @@
 package blsmultisig
+
+// type MultiSig struct {
+
+// }
--- consensus/mubft/blsmultisig/hash.go
+++ consensus/mubft/blsmultisig/hash.go
@@ -1,1 +1,26 @@
 package blsmultisig
+
+import (
+	"math/big"
+
+	"golang.org/x/crypto/bn256"
+	"golang.org/x/crypto/sha3"
+)
+
+// P2I is Point to Big Int, in BLS-BFT, it called H1
+func P2I(point *bn256.G1) *big.Int {
+	return big.NewInt(int64(0))
+}
+
+// I2P is Big Int to Point, in BLS-BFT, it called H0
+func I2P(bigInt *big.Int) *bn256.G1 {
+	return nil
+}
+
+// Hash4Block is Hash function for calculate block hash
+// this is different from hash function for calculate transaction hash
+func Hash4Block(data []byte) []byte {
+	hashMachine := sha3.NewLegacyKeccak256()
+	hashMachine.Write(data)
+	return hashMachine.Sum(nil)
+}
