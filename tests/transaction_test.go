e6a6e24594fe2d95508e6fc637ce82dfc98675b9
0xmerman <hung@autonomous.nyc>
2019-07-30 17:02:45 +0700

    Add Custom Token Privacy Integration Test

--- tests/transaction_test.go
+++ tests/transaction_test.go
@@ -37,28 +37,6 @@ func TestCreateAndSendNormalTransaction(t *testing.T) {
 	}
 }
 func TestCreateAndSendCustomTokenTransaction(t *testing.T) {
-	res, err := readfile("./testsdata/transaction/normal_transaction.json")
-	if err != nil {
-		t.Fatal(err)
-	} else {
-		for _, step := range res.steps {
-			log.Println(step)
-		}
-	}
-	testResult, err := executeTest("./testsdata/transaction/normal_transaction.json")
-	if err != nil {
-		if rpcError, ok := err.(*rpcserver.RPCError); ok {
-			if rpcError != nil {
-				t.Fatal(err)
-			}
-		} else {
-			t.Fatal(err)
-		}
-	} else {
-		log.Println(testResult)
-	}
-}
-func TestCreateAndSendCustomTokenPrivacyTransaction(t *testing.T) {
 	res, err := readfile("./testsdata/transaction/custom_token_transaction.json")
 	if err != nil {
 		t.Fatal(err)
@@ -80,4 +58,27 @@ func TestCreateAndSendCustomTokenPrivacyTransaction(t *testing.T) {
 		log.Println(testResult)
 	}
 }
+func TestCreateAndSendCustomTokenPrivacyTransaction(t *testing.T) {
+	res, err := readfile("./testsdata/transaction/custom_token_privacy_transaction.json")
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		for _, step := range res.steps {
+			log.Println(step)
+		}
+	}
+	testResult, err := executeTest("./testsdata/transaction/custom_token_privacy_transaction.json")
+	if err != nil {
+		if rpcError, ok := err.(*rpcserver.RPCError); ok {
+			if rpcError != nil {
+				t.Fatal(err)
+			}
+		} else {
+			t.Fatal(err)
+		}
+	} else {
+		log.Println(testResult)
+	}
+}
+
 
