5abcf46f68671d514dc197b7165acaceacaa3655
0xmerman <hung@autonomous.nyc>
2019-07-30 23:37:04 +0700

    Add Cross Custom Token Test

--- tests/transaction_test.go
+++ tests/transaction_test.go
@@ -102,3 +102,25 @@ func TestCreateAndSendCrossNormalTransaction(t *testing.T) {
 		log.Println(testResult)
 	}
 }
+func TestCreateAndSendCrossCustomTokenTransaction(t *testing.T) {
+	res, err := readfile("./testsdata/transaction/cross_custom_token_transaction.json")
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		for _, step := range res.steps {
+			log.Println(step)
+		}
+	}
+	testResult, err := executeTest("./testsdata/transaction/cross_custom_token_transaction.json")
+	if err != nil {
+		if rpcError, ok := err.(*rpcserver.RPCError); ok {
+			if rpcError != nil {
+				t.Fatal(err)
+			}
+		} else {
+			t.Fatal(err)
+		}
+	} else {
+		log.Println(testResult)
+	}
+}
