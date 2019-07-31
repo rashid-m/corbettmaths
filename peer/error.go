bdeecd17753715eeeb362181a66a792678df4745
sirrush <thaibao@autonomous.nyc>
2019-07-31 09:51:26 +0700

    - refactor peer: change constant error

--- peer/error.go
+++ peer/error.go
@@ -7,12 +7,12 @@ import (
 
 const (
 	// RemotePeer err
-	UnexpectedErr = iota
-	PeerGenerateKeyPairErr
-	CreateP2PNodeErr
-	CreateP2PAddressErr
-	GetPeerIdFromProtocolErr
-	OpeningStreamP2PErr
+	UnexpectedError = iota
+	PeerGenerateKeyPairError
+	CreateP2PNodeError
+	CreateP2PAddressError
+	GetPeerIdFromProtocolError
+	OpeningStreamP2PError
 
 	// PeerConn err
 )
@@ -22,33 +22,33 @@ var ErrCodeMessage = map[int]struct {
 	message string
 }{
 	// -1xxx for peer
-	UnexpectedErr:            {-1001, "Unexpected"},
-	PeerGenerateKeyPairErr:   {-1001, "Can not generate key pair with reader"},
-	CreateP2PNodeErr:         {-1002, "Can not create libp2p node"},
-	CreateP2PAddressErr:      {-1003, "Can not create libp2p address for node"},
-	GetPeerIdFromProtocolErr: {-1004, "Can not get peer id from protocol"},
-	OpeningStreamP2PErr:      {-1005, "Fail in opening stream "},
+	UnexpectedError:            {-1001, "Unexpected"},
+	PeerGenerateKeyPairError:   {-1001, "Can not generate key pair with reader"},
+	CreateP2PNodeError:         {-1002, "Can not create libp2p node"},
+	CreateP2PAddressError:      {-1003, "Can not create libp2p address for node"},
+	GetPeerIdFromProtocolError: {-1004, "Can not get peer id from protocol"},
+	OpeningStreamP2PError:      {-1005, "Fail in opening stream "},
 
 	// -2xxx for peer connection
 }
 
 type PeerError struct {
 	err     error
-	code    int
-	message string
+	Code    int
+	Message string
 
 	peer *Peer
 }
 
 func (e PeerError) Error() string {
-	return fmt.Sprintf("%+v: %+v \n %+v", e.code, e.message, e.err)
+	return fmt.Sprintf("%+v: %+v \n %+v", e.Code, e.Message, e.err)
 }
 
 func NewPeerError(key int, err error, peer *Peer) *PeerError {
 	return &PeerError{
 		err:     errors.Wrap(err, ErrCodeMessage[key].message),
-		code:    ErrCodeMessage[key].code,
-		message: ErrCodeMessage[key].message,
+		Code:    ErrCodeMessage[key].code,
+		Message: ErrCodeMessage[key].message,
 		peer:    peer,
 	}
 }
