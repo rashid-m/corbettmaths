001cd65cb240ff410f6b0851d88fa675455ed11d
0xakk0r0kamui <hy@autonomous.nyc>
2019-08-01 09:57:13 +0700

    Update

--- consensus/mubft/blsmultisig/ec.go
+++ consensus/mubft/blsmultisig/ec.go
@@ -2,16 +2,49 @@ package blsmultisig
 
 import (
 	"fmt"
+	"math/big"
+	"math/rand"
+	"time"
 
-	"golang.org/x/crypto/bn256"
+	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
 )
 
-// Compress is the ...
-func Compress() {
-	fmt.Println(bn256.Order.BitLen())
+// CmprG1 take a point in G1 group and return bytes array
+func CmprG1(pn *bn256.G1) []byte {
+	pnBytesArr := pn.Marshal()
+	xCoorBytes := pnBytesArr[:CBigIntSz]
+	fmt.Println(xCoorBytes)
+	if pnBytesArr[CBigIntSz*2-1]&1 == 1 {
+		xCoorBytes[0] |= 0x80
+	}
+	return xCoorBytes
 }
 
-// Decompress is
-func Decompress() {
-	fmt.Println(bn256.Order.Bits())
+// DecmprG1 is
+func DecmprG1(bytes []byte) *bn256.G1 {
+	// xCoorByte := bytes[:]
+	seed := time.Now().UnixNano()
+	reader := rand.New(rand.NewSource(int64(seed)))
+	_, x, _ := bn256.RandomG1(reader)
+	return x
+}
+
+func xCoor2G1P(xCoor *big.Int, oddPoint bool) *bn256.G1 {
+	pnBytesArr := I2Bytes(xCoor, CBigIntSz)
+	xCoorPow3 := big.NewInt(1)
+	xCoorPow3.Exp(xCoor, big.NewInt(3), bn256.P)
+	yCoorPow2 := big.NewInt(3)
+	yCoorPow2.Add(xCoorPow3, yCoorPow2)
+	yCoorPow2.Mod(yCoorPow2, bn256.P)
+
+	yCoor := big.NewInt(0)
+	yCoor.Exp(yCoorPow2, pAdd1Div4, bn256.P)
+	pn := new(bn256.G1)
+	yCoorByte := I2Bytes(yCoor, CBigIntSz)
+	pnBytesArr = append(pnBytesArr, yCoorByte...)
+	pn.Unmarshal(pnBytesArr)
+	if ((yCoorByte[CBigIntSz-1]&1 == 0) && oddPoint) || ((yCoorByte[CBigIntSz-1]&1 == 1) && !oddPoint) {
+		pn = pn.Neg(pn)
+	}
+	return pn
 }
--- consensus/mubft/blsmultisig/ec_test.go
+++ consensus/mubft/blsmultisig/ec_test.go
@@ -1,17 +1,138 @@
 package blsmultisig
 
-import "testing"
+import (
+	"crypto/rand"
+	"reflect"
+	"testing"
 
-func TestDecompress(t *testing.T) {
+	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
+)
+
+// func TestDecompress(t *testing.T) {
+// 	tests := []struct {
+// 		name string
+// 		bn   *big.Int
+// 	}{}
+// 	j := 0
+// 	for i := 0; i < 10000; i++ {
+// 		bn := privacy.RandScalar()
+// 		x := bn.Mod(bn, bn256.Order)
+// 		if I2Bytes(x, 32)[0] >= 0x8F {
+// 			// if (x.Bit(255) == 1) && (x.Bit(254) == 1) {
+// 			fmt.Println(I2Bytes(x, 32))
+// 			j++
+// 		}
+// 	}
+// 	Decompress()
+// 	fmt.Println(bn256.Order.String())
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			Decompress()
+// 		})
+// 	}
+// }
+
+func TestCmprG1(t *testing.T) {
+	type args struct {
+		pn *bn256.G1
+	}
 	tests := []struct {
 		name string
+		args args
+		want []byte
 	}{
 		// TODO: Add test cases.
 	}
-	Decompress()
+	_, p, _ := bn256.RandomG1(rand.Reader)
+	CmprG1(p)
+	CmprG1(p.Neg(p))
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			Decompress()
+			if got := CmprG1(tt.args.pn); (!reflect.DeepEqual(got, tt.want)) && (len(got) != CCmprPnSz) {
+				t.Errorf("CmprG1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDecmprG1(t *testing.T) {
+	type args struct {
+		bytes []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want *bn256.G1
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DecmprG1(tt.args.bytes); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DecmprG1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func cmptPnG1(oddPoint byte, loop int) (bool, *bn256.G1) {
+	tests := make([]*bn256.G1, loop)
+	for i := 0; i < loop; i++ {
+		_, tests[i], _ = bn256.RandomG1(rand.Reader)
+		for ; tests[i].Marshal()[63]&1 != oddPoint; _, tests[i], _ = bn256.RandomG1(rand.Reader) {
+		}
+		cmprBytesArr := CmprG1(tests[i])
+		pnDeCmpr := DecmprG1(cmprBytesArr)
+		if !reflect.DeepEqual(pnDeCmpr.Marshal(), tests[i].Marshal()) {
+			return false, tests[i]
+		}
+	}
+	return true, nil
+}
+
+//Test compute point in G1 group
+func Test_cmptPnG1(t *testing.T) {
+	type args struct {
+		oddPoint byte
+		loop     int
+	}
+	type res struct {
+		success bool
+		cause   *bn256.G1
+	}
+	tests := []struct {
+		name string
+		args args
+		want res
+	}{
+		{
+			name: "Test with 1000 odd point",
+			args: args{
+				oddPoint: 1,
+				loop:     1000,
+			},
+			want: res{
+				success: true,
+				cause:   nil,
+			},
+		},
+		{
+			name: "Test with 1000 even point",
+			args: args{
+				oddPoint: 0,
+				loop:     1000,
+			},
+			want: res{
+				success: true,
+				cause:   nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if success, cause := cmptPnG1(tt.args.oddPoint, tt.args.loop); !success {
+				t.Errorf("cmptPnG1(%v, %v) failed because %v", tt.args.oddPoint, tt.args.loop, cause.Marshal())
+			}
 		})
 	}
 }
