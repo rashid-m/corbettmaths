dea24f04ea08844754c5dacc8eb043c81a483f3e
0xakk0r0kamui <hy@autonomous.nyc>
2019-08-05 20:30:35 +0700

    Update testcase, reduce number of loop from 10000 to 1000 because time execution

--- consensus/mubft/blsmultisig/ec.go
+++ consensus/mubft/blsmultisig/ec.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"math/big"
 
-	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
+	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/google"
 )
 
 // CmprG1 take a point in G1 group and return bytes array
@@ -17,10 +17,23 @@ func CmprG1(pn *bn256.G1) []byte {
 	return xCoorBytes
 }
 
+// CmprG2 take a point in G1 group and return bytes array
+// ASAP uncomment below code for reduce size of bytes
+func CmprG2(pn *bn256.G2) []byte {
+	// pnBytesArr := pn.Marshal()
+	// xCoorBytes := pnBytesArr[:CBigIntSz]
+	// if pnBytesArr[CBigIntSz*2-1]&1 == 1 {
+	// 	xCoorBytes[0] |= CMaskByte
+	// }
+	// return xCoorBytes
+
+	return pn.Marshal()
+}
+
 // DecmprG1 is
 func DecmprG1(bytes []byte) (*bn256.G1, error) {
 	if len(bytes) != CCmprPnSz {
-		return nil, errors.New(GetFunctionName(DecmprG1) + CErr + CErrInLn)
+		return nil, errors.New(CErr + CErrInps)
 	}
 
 	oddPoint := ((bytes[0] & CMaskByte) != 0x00)
@@ -31,9 +44,41 @@ func DecmprG1(bytes []byte) (*bn256.G1, error) {
 	xCoor.SetBytes(bytes)
 	pn, err := xCoor2G1P(xCoor, oddPoint)
 	if err != nil {
-		return nil, errors.New(GetFunctionName(DecmprG1) + CErr + err.Error())
+		return nil, errors.New(CErr + CErrCmpr + err.Error())
 	}
 	return pn, nil
+}
+
+// DecmprG2 is
+// ASAP uncomment below code for reduce size of bytes
+func DecmprG2(bytes []byte) (*bn256.G2, error) {
+
+	// if len(bytes) != CCmprPnSz {
+	// 	return nil, errors.New(CErr + CErrInLn)
+	// }
+
+	// oddPoint := ((bytes[0] & CMaskByte) != 0x00)
+	// if oddPoint {
+	// 	bytes[0] &= CNotMaskB
+	// }
+	// xCoor := big.NewInt(1)
+	// xCoor.SetBytes(bytes)
+	// pn, err := xCoor2G2P(xCoor, oddPoint)
+	// if err != nil {
+	// 	return nil, errors.New(CErr + err.Error())
+	// }
+	// return pn, nil
+	pn := new(bn256.G2)
+	_, err := pn.Unmarshal(bytes)
+	if err != nil {
+		return nil, errors.New(CErr + CErrCmpr + err.Error())
+	}
+	return pn, nil
+}
+
+func xCoor2G2P(xCoor *big.Int, oddPoint bool) (*bn256.G2, error) {
+	//ASAP, for reduce 128 bytes G2 Point -> 64 bytes
+	return nil, nil
 }
 
 func xCoor2G1P(xCoor *big.Int, oddPoint bool) (*bn256.G1, error) {
