9adb0b69e65af6feaa421a4a8a8dfbc3c0906bbe
sirrush <thaibao@autonomous.nyc>
2019-08-05 11:47:05 +0700

    - refactor privacy

--- privacy/error.go
+++ privacy/error.go
@@ -19,40 +19,40 @@ const (
 )
 
 var ErrCodeMessage = map[int]struct {
-	code    int
-	message string
+	Code    int
+	Message string
 }{
 	UnexpectedErr: {-1, "Unexpected error"},
 
-	InvalidOutputValue: {-2, "Invalid output value"},
-	ProvingErr : {-3, "Zero knowledge proving error"},
-	VerificationErr : {-4, "Zero knowledge verification error"},
-	MarshalErr : {-5, "Marshal payment proof error"},
-	UnmarshalErr : {-6, "Unmarshal payment proof error"},
-	SetBytesProofErr : {-6, "Set bytes payment proof error"},
-	EncryptOutputCoinErr : {-7, "Encrypt output coins error"},
-	DecryptOutputCoinErr : {-8, "Decrypt output coins error"},
-	DecompressTransmissionKeyErr : {-7, "Can not decompress transmission key error"},
+	InvalidOutputValue:           {-2, "Invalid output value"},
+	ProvingErr:                   {-3, "Zero knowledge proving error"},
+	VerificationErr:              {-4, "Zero knowledge verification error"},
+	MarshalErr:                   {-5, "Marshal payment proof error"},
+	UnmarshalErr:                 {-6, "Unmarshal payment proof error"},
+	SetBytesProofErr:             {-6, "Set bytes payment proof error"},
+	EncryptOutputCoinErr:         {-7, "Encrypt output coins error"},
+	DecryptOutputCoinErr:         {-8, "Decrypt output coins error"},
+	DecompressTransmissionKeyErr: {-7, "Can not decompress transmission key error"},
 }
 
 type PrivacyError struct {
-	code    int
-	message string
+	Code    int
+	Message string
 	err     error
 }
 
 func (e PrivacyError) Error() string {
-	return fmt.Sprintf("%+v: %+v %+v", e.code, e.message, e.err)
+	return fmt.Sprintf("%+v: %+v %+v", e.Code, e.Message, e.err)
 }
 
 func (e PrivacyError) GetCode() int {
-	return e.code
+	return e.Code
 }
 
 func NewPrivacyErr(key int, err error) *PrivacyError {
 	return &PrivacyError{
-		err:     errors.Wrap(err, ErrCodeMessage[key].message),
-		code:    ErrCodeMessage[key].code,
-		message: ErrCodeMessage[key].message,
+		err:     errors.Wrap(err, ErrCodeMessage[key].Message),
+		Code:    ErrCodeMessage[key].Code,
+		Message: ErrCodeMessage[key].Message,
 	}
 }
