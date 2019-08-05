9047fa6beb4273c3a75edab2dced7f706ff12348
0xakk0r0kamui <hy@autonomous.nyc>
2019-08-05 20:34:53 +0700

    Update constants Add common variables

--- consensus/mubft/blsmultisig/constant.go
+++ consensus/mubft/blsmultisig/constant.go
@@ -1,6 +1,10 @@
 package blsmultisig
 
-import "math/big"
+import (
+	"math/big"
+
+	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/google"
+)
 
 /** Acronym
  * pn     : point
@@ -22,20 +26,29 @@ const (
 	// CNotMaskB 0b01111111
 	CNotMaskB = 0x7F
 	// CPKSz Public key size
-	CPKSz = 32
+	CPKSz = 128
 	// CSKSz Secret key size
 	CSKSz = 32
 )
 
 const (
-	CErr = " Details error: "
-	// CErrInLn Error input length
-	CErrInLn = "Wrong input length"
+	// CErr Error prefix
+	CErr = "Details error: "
+	// CErrInps Error input length
+	CErrInps = "Input params error"
+	// CErrCmpr Error when work with (de)compress
+	CErrCmpr = "(De)Compress error"
 )
 
 var (
 	// pAdd1Div4 = (p + 1)/4
 	pAdd1Div4, _ = new(big.Int).SetString("c19139cb84c680a6e14116da060561765e05aa45a1c72a34f082305b61f3f52", 16) //
+	// CommonPKs cache list publickey point in an epoch
+	CommonPKs []*bn256.G2
+	// CommonAPs cache list publickey^a_i point in an epoch
+	CommonAPs []*bn256.G2
+	// CommonAis cache list a_i integer in an epoch
+	CommonAis []*big.Int
 )
 
 // PublicKey is bytes of PublicKey point compressed
