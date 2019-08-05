92d10ab0a283b286f8fce3165a0de5ad60367bb1
0xakk0r0kamui <hy@autonomous.nyc>
2019-08-05 20:30:35 +0700

    Update testcase, reduce number of loop from 10000 to 1000 because time execution

--- consensus/mubft/blsmultisig/ec_test.go
+++ consensus/mubft/blsmultisig/ec_test.go
@@ -6,61 +6,8 @@ import (
 	"reflect"
 	"testing"
 
-	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/cloudflare"
+	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/google"
 )
-
-func TestDecmprG1(t *testing.T) {
-	type args struct {
-		bytes []byte
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *bn256.G1
-		wantErr bool
-	}{
-		// {
-		// 	name: "Decompre"
-		// },
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := DecmprG1(tt.args.bytes)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("DecmprG1() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("DecmprG1() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestCmprG1(t *testing.T) {
-	type args struct {
-		pn *bn256.G1
-	}
-	tests := []struct {
-		name string
-		args args
-		want []byte
-	}{
-		// {
-		// 	name: "Compress generator element of G1",
-		// 	args: args{
-		// 		pn: new(bn256.G1).ScalarBaseMult(big.NewInt(1)),
-		// 	},
-		// },
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := CmprG1(tt.args.pn); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CmprG1() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func cmptPnG1(oddPoint byte, loop int) (*bn256.G1, error) {
 	tests := make([]*bn256.G1, loop)
@@ -96,19 +43,19 @@ func Test_cmptPnG1(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "Test with 10000 odd point",
+			name: "Test with 1000 odd point",
 			args: args{
 				oddPoint: 1,
-				loop:     10000,
+				loop:     1000,
 			},
 			want:    nil,
 			wantErr: false,
 		},
 		{
-			name: "Test with 10000 even point",
+			name: "Test with 1000 even point",
 			args: args{
 				oddPoint: 0,
-				loop:     10000,
+				loop:     1000,
 			},
 			want:    nil,
 			wantErr: false,
@@ -127,3 +74,70 @@ func Test_cmptPnG1(t *testing.T) {
 		})
 	}
 }
+
+// func cmptPnG2(oddPoint byte, loop int) (*bn256.G2, error) {
+// 	tests := make([]*bn256.G2, loop)
+// 	var err error
+// 	for i := 0; i < loop; i++ {
+// 		_, tests[i], err = bn256.RandomG2(rand.Reader)
+// 		if err != nil {
+// 			return tests[i], err
+// 		}
+// 		fmt.Println(tests[i].Marshal())
+// 		for ; tests[i].Marshal()[63]&1 != oddPoint; _, tests[i], _ = bn256.RandomG2(rand.Reader) {
+// 		}
+// 		cmprBytesArr := CmprG2(tests[i])
+// 		pnDeCmpr, err := DecmprG2(cmprBytesArr)
+// 		if err != nil {
+// 			return tests[i], err
+// 		}
+// 		if !reflect.DeepEqual(pnDeCmpr.Marshal(), tests[i].Marshal()) {
+// 			return tests[i], errors.New("Not equal")
+// 		}
+// 	}
+// 	return nil, nil
+// }
+
+// func Test_cmptPnG2(t *testing.T) {
+// 	type args struct {
+// 		oddPoint byte
+// 		loop     int
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		args    args
+// 		want    *bn256.G1
+// 		wantErr bool
+// 	}{
+// 		{
+// 			name: "Test with 10000 odd point",
+// 			args: args{
+// 				oddPoint: 1,
+// 				loop:     10000,
+// 			},
+// 			want:    nil,
+// 			wantErr: false,
+// 		},
+// 		{
+// 			name: "Test with 10000 even point",
+// 			args: args{
+// 				oddPoint: 0,
+// 				loop:     10000,
+// 			},
+// 			want:    nil,
+// 			wantErr: false,
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			got, err := cmptPnG2(tt.args.oddPoint, tt.args.loop)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("cmptPnG2() error = %v, wantErr %v", err, tt.wantErr)
+// 				return
+// 			}
+// 			if !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("cmptPnG2() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
