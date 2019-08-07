3650c78670b48caa59c45ac4a0cdd8b7c06c5f0c
sirrush <thaibao@autonomous.nyc>
2019-08-07 10:39:58 +0700

    - refactor transaction: handle error

--- transaction/error.go
+++ transaction/error.go
@@ -8,18 +8,18 @@ import (
 )
 
 const (
-	UnexpectedErr = iota
-	WrongTokenTxType
-	CustomTokenExisted
-	WrongInput
-	WrongSig
-	DoubleSpend
-	TxNotExist
-	RandomCommitmentErr
-	InvalidSanityDataPRV
-	InvalidSanityDataPrivacyToken
-	InvalidDoubleSpendPRV
-	InvalidDoubleSpendPrivacyToken
+	UnexpectedError = iota
+	WrongTokenTxTypeError
+	CustomTokenExistedError
+	WrongInputError
+	WrongSigError
+	DoubleSpendError
+	TxNotExistError
+	RandomCommitmentError
+	InvalidSanityDataPRVError
+	InvalidSanityDataPrivacyTokenError
+	InvalidDoubleSpendPRVError
+	InvalidDoubleSpendPrivacyTokenError
 	InputCoinIsVeryLargeError
 	PaymentInfoIsVeryLargeError
 	TokenIDInvalidError
@@ -38,6 +38,7 @@ const (
 	SndExistedError
 	OutputCommitmentExistError
 	TxProofVerifyFailError
+	VerifyMinerCreatedTxBeforeGettingInBlockError
 )
 
 var ErrCodeMessage = map[int]struct {
@@ -45,40 +46,41 @@ var ErrCodeMessage = map[int]struct {
 	Message string
 }{
 	// for common
-	UnexpectedErr:                            {-1000, "Unexpected error"},
-	WrongTokenTxType:                         {-1001, "Can't handle this TokenTxType"},
-	CustomTokenExisted:                       {-1002, "This token is existed in network"},
-	WrongInput:                               {-1003, "Wrong input transaction"},
-	WrongSig:                                 {-1004, "Wrong signature"},
-	DoubleSpend:                              {-1005, "Double spend"},
-	TxNotExist:                               {-1006, "Not exist tx for this"},
-	RandomCommitmentErr:                      {-1007, "Number of list commitments indices must be corresponding with number of input coins"},
-	InputCoinIsVeryLargeError:                {-1008, "Input coins in tx are very large: %d"},
-	PaymentInfoIsVeryLargeError:              {-1009, "Input coins in tx are very large: %d"},
-	TokenIDInvalidError:                      {-1010, "Invalid TokenID: %+v"},
-	PrivateKeySenderInvalidError:             {-1011, "Invalid private key"},
-	SignTxError:                              {-1012, "Can not sign tx"},
-	DecompressPaymentAddressError:            {-1013, "Can not decompress public key from payment address %+v"},
-	CanNotGetCommitmentFromIndexError:        {-1014, "Can not get commitment from index=%d shardID=%+v"},
-	CanNotDecompressCommitmentFromIndexError: {-1015, "Can not get commitment from index=%d shardID=%+v value=%+v"},
-	InitWithnessError:                        {-1016, "Can not init witness for privacy with param: %s"},
-	WithnessProveError:                       {-1017, "Can not prove with witness hashPrivacy=%+v param: %+s"},
-	EncryptOutputError:                       {-1018, "Can not encrypt output"},
-	DecompressSigPubKeyError:                 {-1019, "Can not decompress sig pubkey of tx"},
-	InitTxSignatureFromBytesError:            {-1020, "Can not init signature for tx from bytes"},
-	VerifyTxSigFailError:                     {-1021, "Verify signature of tx is fail"},
-	DuplicatedOutputSndError:                 {-1022, "Duplicate output"},
-	SndExistedError:                          {-1023, "Snd existed: %s"},
-	OutputCommitmentExistError:               {-1024, "Output's commitment existed"},
-	TxProofVerifyFailError:                   {-1025, "Can not verify proof of tx"},
+	UnexpectedError:                               {-1000, "Unexpected error"},
+	WrongTokenTxTypeError:                         {-1001, "Can't handle this TokenTxType"},
+	CustomTokenExistedError:                       {-1002, "This token is existed in network"},
+	WrongInputError:                               {-1003, "Wrong input transaction"},
+	WrongSigError:                                 {-1004, "Wrong signature"},
+	DoubleSpendError:                              {-1005, "Double spend"},
+	TxNotExistError:                               {-1006, "Not exist tx for this"},
+	RandomCommitmentError:                         {-1007, "Number of list commitments indices must be corresponding with number of input coins"},
+	InputCoinIsVeryLargeError:                     {-1008, "Input coins in tx are very large: %d"},
+	PaymentInfoIsVeryLargeError:                   {-1009, "Input coins in tx are very large: %d"},
+	TokenIDInvalidError:                           {-1010, "Invalid TokenID: %+v"},
+	PrivateKeySenderInvalidError:                  {-1011, "Invalid private key"},
+	SignTxError:                                   {-1012, "Can not sign tx"},
+	DecompressPaymentAddressError:                 {-1013, "Can not decompress public key from payment address %+v"},
+	CanNotGetCommitmentFromIndexError:             {-1014, "Can not get commitment from index=%d shardID=%+v"},
+	CanNotDecompressCommitmentFromIndexError:      {-1015, "Can not get commitment from index=%d shardID=%+v value=%+v"},
+	InitWithnessError:                             {-1016, "Can not init witness for privacy with param: %s"},
+	WithnessProveError:                            {-1017, "Can not prove with witness hashPrivacy=%+v param: %+s"},
+	EncryptOutputError:                            {-1018, "Can not encrypt output"},
+	DecompressSigPubKeyError:                      {-1019, "Can not decompress sig pubkey of tx"},
+	InitTxSignatureFromBytesError:                 {-1020, "Can not init signature for tx from bytes"},
+	VerifyTxSigFailError:                          {-1021, "Verify signature of tx is fail"},
+	DuplicatedOutputSndError:                      {-1022, "Duplicate output"},
+	SndExistedError:                               {-1023, "Snd existed: %s"},
+	OutputCommitmentExistError:                    {-1024, "Output's commitment existed"},
+	TxProofVerifyFailError:                        {-1025, "Can not verify proof of tx"},
+	VerifyMinerCreatedTxBeforeGettingInBlockError: {-1026, "Verify Miner Created Tx Before Getting In Block error"},
 
 	// for PRV
-	InvalidSanityDataPRV:  {-2000, "Invalid sanity data for PRV"},
-	InvalidDoubleSpendPRV: {-2001, "Double spend PRV in blockchain"},
+	InvalidSanityDataPRVError:  {-2000, "Invalid sanity data for PRV"},
+	InvalidDoubleSpendPRVError: {-2001, "Double spend PRV in blockchain"},
 
 	// for privacy token
-	InvalidSanityDataPrivacyToken:  {-3000, "Invalid sanity data for privacy Token"},
-	InvalidDoubleSpendPrivacyToken: {-3001, "Double spend privacy Token in blockchain"},
+	InvalidSanityDataPrivacyTokenError:  {-3000, "Invalid sanity data for privacy Token"},
+	InvalidDoubleSpendPrivacyTokenError: {-3001, "Double spend privacy Token in blockchain"},
 }
 
 type TransactionError struct {
