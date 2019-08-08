8cf47789a763dc7c44ec5bcdbaa2431c1d7ea664
incognitochain <thaibao@autonomous.nyc>
2019-08-08 17:17:45 +0700

    - refactor bootnode

--- bootnode/server/handler.go
+++ bootnode/server/handler.go
@@ -10,19 +10,13 @@ type Handler struct {
 	rpcServer *RpcServer
 }
 
-type PingArgs struct {
-	RawAddress string
-	PublicKey  string
-	SignData   string
-}
-
 // Ping - handler func which receive data from rpc client,
 // add into list current peers and response all of them to client
 func (s Handler) Ping(args *PingArgs, responseMessagePeers *[]wire.RawPeer) error {
 	fmt.Println("Receive ```Ping``` method from ```RPC client``` with data", args)
 
 	// update peer which have just send information to our rpc server
-	err := s.rpcServer.AddOrUpdatePeer(args.RawAddress, args.PublicKey, args.SignData)
+	err := s.rpcServer.AddOrUpdatePeer(args.rawAddress, args.publicKey, args.signData)
 	if err != nil {
 		return err
 	}
@@ -30,7 +24,7 @@ func (s Handler) Ping(args *PingArgs, responseMessagePeers *[]wire.RawPeer) erro
 	s.rpcServer.peersMtx.Lock()
 	defer s.rpcServer.peersMtx.Unlock()
 	// return note list
-	for _, p := range s.rpcServer.Peers {
+	for _, p := range s.rpcServer.peers {
 		*responseMessagePeers = append(*responseMessagePeers, wire.RawPeer{p.RawAddress, p.PublicKey})
 	}
 	fmt.Println("Response", *responseMessagePeers)
