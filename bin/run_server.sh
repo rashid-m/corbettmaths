34b95671c8c46edbfe892fe2498b339309327358
0xBahamoot <lam@autonomous.nyc>
2019-08-08 19:00:25 +0700

    oops

--- bin/run_server.sh
+++ bin/run_server.sh
@@ -1,42 +1,4 @@
 #!/usr/bin/env bash
-
-privatekey=$PRIVATEKEY
-miner_port=9330
-miner_rpc=9334
-dataDir="data"
-ethDataDir="eth-data"
-
-docker -v || bash -c "wget -qO- https://get.docker.com/ | sh"
-
-if [  -z "$privatekey" ]; then
-    echo "Private key not set!"
-    exit
-fi
-if [  -z "$miner_port" ]; then
-    echo "port not set!"
-    exit
-fi
-if [  -z "$miner_rpc" ]; then
-    echo "rpc not set!"
-    exit
-fi
-
-clear="n"
-read -p "Clear database?[y/n]"  clear
-
-ip=`curl http://checkip.amazonaws.com/`
-docker pull incognitochain/incognito
-
-docker rm -f inc_miner
-docker rm -f inc_geth
-
-docker network create --driver bridge inc_net || true
-
-docker run -it --net inc_net -p 8545:8545 -p 30303:30303 -d -v $PWD/${ethDataDir}:/root/.ethereum --name inc_geth ethereum/client-go --rinkeby --syncmode "light" --maxpeers 0 --rpc --rpcaddr "0.0.0.0" --rpcapi db,eth,net,web3,personal,admin --rpccorsdomain "*" --rpcvhosts="*"
-
-docker run --net inc_net -d -e GETH_NAME=inc_geth -e NAME=miner -p $miner_port:$miner_port -p $miner_rpc:$miner_rpc -e DISCOVERPEERSADDRESS='172.104.39.6:9330' -v $PWD/${dataDir}:/data -e PRIVATEKEY="${privatekey}" -e EXTERNALADDRESS="${ip}:9330" -e PORT=$miner_port -e RPC_PORT=$miner_rpc --name inc_miner incognitochain/incognito /run_incognito.sh $clear
-
-#!/usr/bin/env bash
 declare -a ethNodes
 ethNodes[1]=enode://572a275a45e6a49d995948ac4d20cfc7cd78f937809128d2fb321b15e94eacd8508798be0ffec2c73664b9175707c378286f49e0dbb55d8838f37a71a1840305@54.39.158.106:30303
 ethNodes[2]=enode://52da3bb510cb5a695a2faa1729b2c580f0a79c92829d46d2c55d915d166d0e79ecba1b7ac214f38b5104aaa40807f128fe4acfb306ad41c391300b116b6c03c8@128.199.203.81:30303
