59f1b2f75c46bc45a4fbf895d2d4720da8a777f7
0xbunyip <baoduy@autonomous.nyc>
2019-08-09 16:54:49 +0700

    Add integration test for flow burning pErc20

--- tests/it_burn_test.go
+++ tests/it_burn_test.go
@@ -11,6 +11,11 @@ func TestBurnPrivateETH(t *testing.T) {
 	checkError(t, err)
 }
 
+func TestBurnPrivateERC20(t *testing.T) {
+	_, err := executeTest("./testsdata/burn/burnerc20.json")
+	checkError(t, err)
+}
+
 func checkError(t *testing.T, err error) {
 	if err == nil {
 		return
