6e373c38d1be7e03dc4045ab486003ee8cfdfe7e
0xsirrush <thaibao@autonomous.nyc>
2019-08-13 13:28:12 +0700

    - refactor database: params common.Hash

--- database/lvdb/burn.go
+++ database/lvdb/burn.go
@@ -2,14 +2,14 @@ package lvdb
 
 import "github.com/incognitochain/incognito-chain/common"
 
-func (db *db) StoreBurningConfirm(txID []byte, height uint64) error {
-	key := append(burnConfirmPrefix, txID...)
+func (db *db) StoreBurningConfirm(txID common.Hash, height uint64) error {
+	key := append(burnConfirmPrefix, txID[:]...)
 	value := common.Uint64ToBytes(height)
 	return db.Put(key, value)
 }
 
-func (db *db) GetBurningConfirm(txID []byte) (uint64, error) {
-	key := append(burnConfirmPrefix, txID...)
+func (db *db) GetBurningConfirm(txID common.Hash) (uint64, error) {
+	key := append(burnConfirmPrefix, txID[:]...)
 	value, err := db.Get(key)
 	if err != nil {
 		return 0, err
