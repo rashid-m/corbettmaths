789da32fa76aa61757bd41f63e25a02fdc734f84
0xmerman <hung@autonomous.nyc>
2019-08-15 18:00:59 +0700

    Update Document

--- blockchain/beaconproducer.go
+++ blockchain/beaconproducer.go
@@ -189,6 +189,18 @@ func (blockGenerator *BlockGenerator) FinalizeBeaconBlock(beaconBlock *BeaconBlo
 // #3: valid swap instruction
 // #4: bridge instructions
 // #5: accepted reward instructions
+/*
+	Get Shard To Beacon Block Rule:
+	1. Shard To Beacon Blocks will be get from Shard To Beacon Pool (only valid block)
+	2. Process shards independently, for each shard:
+		a. Shard To Beacon Block List must be compatible with current shard state in beacon best state:
+			+ Increased continuosly in height (10, 11, 12,...)
+				Ex: Shard state in beacon best state has height 11 then shard to beacon block list must have first block in list with height 12
+			+ Shard To Beacon Block List must have incremental height in list (10, 11, 12,... NOT 10, 12,...)
+			+ Shard To Beacon Block List can be verify with and only with current shard committee in beacon best state
+			+ DO NOT accept Shard To Beacon Block List that can have two arbitrary blocks that can be verify with two different committee set
+			+ If in Shard To Beacon Block List have one block with Swap Instruction, then this block must be the last block in this list (or only block in this list)
+*/
 func (blockGenerator *BlockGenerator) GetShardState(beaconBestState *BeaconBestState, shardsToBeacon map[byte]uint64) (map[byte][]ShardState, [][]string, map[byte][][]string, [][]string, [][]string) {
 	shardStates := make(map[byte][]ShardState)
 	validStakeInstructions := [][]string{}
