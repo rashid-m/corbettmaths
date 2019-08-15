73e77a298b55c91ee4493f938c7f7f77f0eb4137
0xsirrush <thaibao@autonomous.nyc>
2019-08-15 12:29:32 +0700

    - refactor code metadata

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,75 +1,10 @@
 package metadata
 
 import (
-	"strconv"
-
 	"github.com/incognitochain/incognito-chain/common"
 	"github.com/incognitochain/incognito-chain/database"
 	zkp "github.com/incognitochain/incognito-chain/privacy/zeroknowledge"
 )
-
-type MetadataBase struct {
-	Type int
-}
-
-func NewMetadataBase(thisType int) *MetadataBase {
-	return &MetadataBase{Type: thisType}
-}
-
-func (mb MetadataBase) IsMinerCreatedMetaType() bool {
-	metaType := mb.GetType()
-	for _, mType := range minerCreatedMetaTypes {
-		if metaType == mType {
-			return true
-		}
-	}
-	return false
-}
-
-func (mb *MetadataBase) CalculateSize() uint64 {
-	return 0
-}
-
-func (mb *MetadataBase) Validate() error {
-	return nil
-}
-
-func (mb *MetadataBase) Process() error {
-	return nil
-}
-
-func (mb MetadataBase) GetType() int {
-	return mb.Type
-}
-
-func (mb *MetadataBase) Hash() *common.Hash {
-	record := strconv.Itoa(mb.Type)
-	hash := common.HashH([]byte(record))
-	return &hash
-}
-
-func (mb MetadataBase) CheckTransactionFee(tx Transaction, minFeePerKbTx uint64) bool {
-	txFee := tx.GetTxFee()
-	fullFee := minFeePerKbTx * tx.GetTxActualSize()
-	return !(txFee < fullFee)
-}
-
-func (mb *MetadataBase) BuildReqActions(tx Transaction, bcr BlockchainRetriever, shardID byte) ([][]string, error) {
-	return [][]string{}, nil
-}
-
-func (mb MetadataBase) VerifyMinerCreatedTxBeforeGettingInBlock(
-	txsInBlock []Transaction,
-	txsUsed []int,
-	insts [][]string,
-	instsUsed []int,
-	shardID byte,
-	txr Transaction,
-	bcr BlockchainRetriever,
-	accumulatedValues *AccumulatedValues,
-) (bool, error) {
-	return true, nil
-}
 
 // This is tx struct which is really saved in tx mempool
 type TxDesc struct {
@@ -109,7 +44,6 @@ type BlockchainRetriever interface {
 	GetDatabase() database.DatabaseInterface
 	GetTxValue(txid string) (uint64, error)
 	GetShardIDFromTx(txid string) (byte, error)
-	//GetRPCClient() *rpccaller.RPCClient
 }
 
 // Interface for all types of metadata in tx
@@ -118,7 +52,6 @@ type Metadata interface {
 	Hash() *common.Hash
 	CheckTransactionFee(Transaction, uint64) bool
 	ValidateTxWithBlockChain(tx Transaction, bcr BlockchainRetriever, b byte, db database.DatabaseInterface) (bool, error)
-	// isContinue, ok, err
 	ValidateSanityData(bcr BlockchainRetriever, tx Transaction) (bool, bool, error)
 	ValidateMetadataByItself() bool
 	BuildReqActions(tx Transaction, bcr BlockchainRetriever, shardID byte) ([][]string, error)
