954e1bc0e5e307a475d5ef878355523adb0f2746
0xsirrush <thaibao@autonomous.nyc>
2019-08-15 14:05:02 +0700

    - refactor code mempool

--- mempool/constants.go
+++ mempool/constants.go
@@ -1,11 +1,11 @@
 package mempool
 
 const (
-	// UnminedHeight is the height used for the "block" height field of the
+	// unminedHeight is the height used for the "block" height field of the
 	// contextual transaction information provided in a transaction store
 	// when it has not yet been mined into a block.
-	UnminedHeight = 0x7fffffffffffffff
-	MaxVersion    = 1
+	unminedHeight = 0x7fffffffffffffff
+	maxVersion    = 1
 )
 
 // Beacon pool
@@ -32,10 +32,10 @@ const (
 
 // Cross Shard Pool
 const (
-	MAX_VALID_CROSS_SHARD_IN_POOL   = 1000
 	MAX_PENDING_CROSS_SHARD_IN_POOL = 2000 //per shardID
 
-	VALID_CROSS_SHARD_BLOCK   = 0
-	INVALID_CROSS_SHARD_BLOCK = -1
-	PENDING_CROSS_SHARD_BLOCK = -2
+	/*MAX_VALID_CROSS_SHARD_IN_POOL = 1000
+	VALID_CROSS_SHARD_BLOCK       = 0
+	INVALID_CROSS_SHARD_BLOCK     = -1
+	PENDING_CROSS_SHARD_BLOCK     = -2*/
 )
