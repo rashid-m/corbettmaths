2417a6b533886f85642c67cd220e4229ead5dc48
0xsirrush <thaibao@autonomous.nyc>
2019-08-19 17:28:03 +0700

    - hotfix rpc

--- rpcserver/jsonresult/getbeststateresult.go
+++ rpcserver/jsonresult/getbeststateresult.go
@@ -19,6 +19,27 @@ type CommitteeListsResult struct {
 	BeaconPendingValidator []string          `json:"BeaconPendingValidator"`
 }
 
+func NewCommitteeListsResult(epoch uint64, shardComm map[byte][]string, shardPendingValidator map[byte][]string, beaconCommittee []string, beaconPendingValidator []string) *CommitteeListsResult {
+	result := &CommitteeListsResult{
+		Epoch: epoch,
+	}
+	result.BeaconPendingValidator = make([]string, len(beaconPendingValidator))
+	copy(result.BeaconPendingValidator, beaconPendingValidator)
+	result.BeaconCommittee = make([]string, len(beaconCommittee))
+	copy(result.BeaconCommittee, beaconCommittee)
+	result.ShardCommittee = make(map[byte][]string)
+	for k, v := range shardComm {
+		result.ShardCommittee[k] = make([]string, len(v))
+		copy(result.ShardCommittee[k], v)
+	}
+	result.ShardPendingValidator = make(map[byte][]string)
+	for k, v := range shardPendingValidator {
+		result.ShardPendingValidator[k] = make([]string, len(v))
+		copy(result.ShardPendingValidator[k], v)
+	}
+	return result
+}
+
 type StakeResult struct {
 	PublicKey string `json:"PublicKey"`
 	CanStake  bool   `json:"CanStake"`
