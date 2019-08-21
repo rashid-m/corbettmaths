3016c79c8ef96ebdefea52e40ef0243ac0707307
0xBahamoot <lam@autonomous.nyc>
2019-08-21 11:15:33 +0700

    update

--- common/interfaces.go
+++ common/interfaces.go
@@ -4,6 +4,7 @@ type BlockInterface interface {
 	GetHeight() uint64
 	Hash() *Hash
 	// AddValidationField(validateData string) error
+	GetProducer() string
 	GetValidationField() string
 	GetRound() int
 	GetRoundKey() string
