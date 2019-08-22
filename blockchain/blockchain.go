59fad803f8d0cf3d9cd0b7260a70ae3e8fd7f7f4
0xBahamoot <lam@autonomous.nyc>
2019-08-22 10:51:07 +0700

    update

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1671,36 +1671,6 @@ func DecodeCommitteeKey(committeeKey string) ([]byte, []byte, error) {
 	return iPKBytes, bPKBytes, nil
 }
 
-func ExtractPublickeyList(keyList []incognitokey.CommitteePubKey, keyType string) ([]string, error) {
-	result := []string{}
-	for _, keySet := range keyList {
-		key := keySet.GetMiningKeyBase58(keyType)
-		if key != "" {
-			result = append(result, key)
-		}
-	}
-	return result, nil
-}
-
-func CommitteeKeyListToString(keyList []incognitokey.CommitteePubKey) []string {
-	result := []string{}
-	for _, key := range keyList {
-		keyStr, _ := key.ToBase58()
-		result = append(result, keyStr)
-	}
-	return result
-}
-
-func CommitteeBase58KeyListToStruct(strKeyList []string) []incognitokey.CommitteePubKey {
-	result := []incognitokey.CommitteePubKey{}
-	for _, key := range strKeyList {
-		var keyStruct incognitokey.CommitteePubKey
-		keyStruct.FromBase58(key)
-		result = append(result, keyStruct)
-	}
-	return result
-}
-
 // func (blockchain *BlockChain) BackupCurrentShardState(block *ShardBlock, beaconblks []*BeaconBlock) error {
 
 // 	//Steps:
