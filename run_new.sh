3263b8dccc23f82011007cb3dc7b244c21152dde
0xBahamoot <lam@autonomous.nyc>
2019-08-22 18:57:33 +0700

    update

--- run_new.sh
+++ run_new.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$1" == "1" ]; then
-go run *.go --discoverpeersaddress "0.0.0.0:9330" --miningkeys "bls:14DrMrsYZ58EqrjbuyBuTy3NBmcXB" --nodemode "auto" --datadir "data/1" --listen "0.0.0.0:9435" --externaladdress "0.0.0.0:9435" --norpcauth --rpclisten "0.0.0.0:9335"
+go run *.go --discoverpeersaddress "0.0.0.0:9330" --miningkeys "bls:14DrMrsYZ58EqrjbuyBuTy3NBmcXB" --nodemode "auto" --datadir "data/1" --listen "0.0.0.0:9435" --externaladdress "0.0.0.0:9435" --norpcauth --rpclisten "0.0.0.0:9334" --enablewallet --wallet "wallet1" --walletpassphrase "12345678" --walletautoinit
 fi
 if [ "$1" == "2" ]; then
 go run *.go --discoverpeersaddress "0.0.0.0:9330" --miningkeys "bls:1eDXPfd9jXhMVbhLAALewJymUPob" --nodemode "auto" --datadir "data/2" --listen "0.0.0.0:9436" --externaladdress "0.0.0.0:9436" --norpcauth --rpclisten "0.0.0.0:9336"
