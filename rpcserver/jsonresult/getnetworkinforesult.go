83d43b4ee527a33797051b94a6d1f459d4145f5e
0xsirrush <thaibao@autonomous.nyc>
2019-08-23 10:08:11 +0700

    - refactor code for rpc

--- rpcserver/jsonresult/getnetworkinforesult.go
+++ rpcserver/jsonresult/getnetworkinforesult.go
@@ -1,7 +1,15 @@
 package jsonresult
 
+import (
+	"github.com/incognitochain/incognito-chain/common"
+	"net"
+	"os"
+
+	"github.com/incognitochain/incognito-chain/rpcserver"
+)
+
 type GetNetworkInfoResult struct {
-	Commit 					string                   `json:"commit"`
+	Commit          string                   `json:"commit"`
 	Version         string                   `json:"version"`
 	SubVersion      string                   `json:"SubVersion"`
 	ProtocolVersion string                   `json:"ProtocolVersion"`
@@ -11,4 +19,50 @@ type GetNetworkInfoResult struct {
 	LocalAddresses  []string                 `json:"LocalAddresses"`
 	IncrementalFee  uint64                   `json:"IncrementalFee"`
 	Warnings        string                   `json:"Warnings"`
+}
+
+func NewGetNetworkInfoResult(config rpcserver.RpcServerConfig) (*GetNetworkInfoResult, error) {
+	result := &GetNetworkInfoResult{
+		Commit:          os.Getenv("commit"),
+		ProtocolVersion: config.ProtocolVersion,
+		Version:         rpcserver.RpcServerVersion,
+		SubVersion:      common.EmptyString,
+		NetworkActive:   config.ConnMgr.GetListeningPeer() != nil,
+		LocalAddresses:  []string{},
+	}
+	listener := config.ConnMgr.GetListeningPeer()
+	result.Connections = len(listener.GetPeerConns())
+	result.LocalAddresses = append(result.LocalAddresses, listener.GetRawAddress())
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	networks := []map[string]interface{}{}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			network := map[string]interface{}{}
+			network["name"] = "ipv4"
+			network["limited"] = false
+			network["reachable"] = true
+			network["proxy"] = ""
+			network["proxy_randomize_credentials"] = false
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To16() != nil {
+					network["name"] = "ipv6"
+				}
+			}
+			networks = append(networks, network)
+		}
+	}
+	result.Networks = networks
+	if config.Wallet != nil && config.Wallet.GetConfig() != nil {
+		result.IncrementalFee = config.Wallet.GetConfig().IncrementalFee
+	}
+	result.Warnings = common.EmptyString
+	return result, nil
 }
