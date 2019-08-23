3b7dbe576e6d4e24b21aa38f361185943f48e0e9
0xsirrush <thaibao@autonomous.nyc>
2019-08-23 10:30:31 +0700

    - refactor code for rpc

--- rpcserver/jsonresult/getmininginforesult.go
+++ rpcserver/jsonresult/getmininginforesult.go
@@ -1,5 +1,10 @@
 package jsonresult
 
+import (
+	"github.com/incognitochain/incognito-chain/common"
+	"github.com/incognitochain/incognito-chain/rpcserver"
+)
+
 type GetMiningInfoResult struct {
 	ShardHeight         uint64 `json:"ShardHeight"`
 	BeaconHeight        uint64 `json:"BeaconHeight"`
@@ -10,4 +15,27 @@ type GetMiningInfoResult struct {
 	ShardID             int    `json:"ShardID"`
 	Role                string `json:"Role"`
 	IsEnableMining      bool   `json:"IsEnableMining"`
+}
+
+func NewGetMiningInfoResult(config rpcserver.RpcServerConfig) *GetMiningInfoResult {
+	result := &GetMiningInfoResult{}
+	result.IsCommittee = true
+	result.PoolSize = config.TxMemPool.Count()
+	result.Chain = config.ChainParams.Name
+	result.IsEnableMining = config.Server.IsEnableMining()
+	result.BeaconHeight = config.BlockChain.BestState.Beacon.BeaconHeight
+
+	role, shardID := config.BlockChain.BestState.Beacon.GetPubkeyRole(config.MiningPubKeyB58, 0)
+	result.Role = role
+	if role == common.SHARD_ROLE {
+		result.ShardHeight = config.BlockChain.BestState.Shard[shardID].ShardHeight
+		result.CurrentShardBlockTx = len(config.BlockChain.BestState.Shard[shardID].BestBlock.Body.Transactions)
+		result.ShardID = int(shardID)
+	} else if role == common.VALIDATOR_ROLE || role == common.PROPOSER_ROLE || role == common.PENDING_ROLE {
+		result.ShardID = -1
+	}
+	if role == common.EmptyString {
+		result.IsCommittee = false
+	}
+	return result
 }
