567db1dd2d96213907c8bda9addd3287397c2854
0xBahamoot <lam@autonomous.nyc>
2019-08-26 15:51:13 +0700

    update

--- consensus/blsbft/constants.go
+++ consensus/blsbft/constants.go
@@ -1,18 +1,23 @@
 package blsbft
 
-import "time"
+import (
+	"time"
+
+	"github.com/incognitochain/incognito-chain/common"
+)
 
 const (
-	PROPOSE  = "PROPOSE"
-	LISTEN   = "LISTEN"
-	VOTE     = "VOTE"
-	NEWROUND = "NEWROUND"
-	BLS      = "bls"
-	BRI      = "dsa"
+	PROPOSE       = "PROPOSE"
+	LISTEN        = "LISTEN"
+	VOTE          = "VOTE"
+	NEWROUND      = "NEWROUND"
+	BLS           = "bls"
+	BRI           = "dsa"
+	CONSENSUSNAME = common.BLS_CONSENSUS
 )
 
 //
 const (
-	TIMEOUT             = 5 * time.Second
+	TIMEOUT             = 10 * time.Second
 	MaxNetworkDelayTime = 150 * time.Millisecond // in ms
 )
