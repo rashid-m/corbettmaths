28257c04bf781e939548f3bc02ae639863cbd2ce
0xsirrush <thaibao@autonomous.nyc>
2019-08-26 10:59:36 +0700

    - refactor code for rpc

--- rpcserver/jsonresult/getheaderresult.go
+++ rpcserver/jsonresult/getheaderresult.go
@@ -3,9 +3,8 @@ package jsonresult
 import "github.com/incognitochain/incognito-chain/blockchain"
 
 type GetHeaderResult struct {
-	BlockNum  int    `json:"blocknum"`
-	ShardID   byte   `json:"shardID"`
-	BlockHash string `json:"blockhash"`
-	// Header    blockchain.ShardBlock `json:"header"`
-	Header blockchain.ShardHeader `json:"header"`
+	BlockNum  int                    `json:"Blocknum"`
+	ShardID   byte                   `json:"ShardID"`
+	BlockHash string                 `json:"Blockhash"`
+	Header    blockchain.ShardHeader `json:"Header"`
 }
