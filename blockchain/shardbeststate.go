97ee831f9f3760bbded2481487175b44aee6463c
0xmerman <hung@autonomous.nyc>
2019-08-27 17:09:21 +0700

    update

--- blockchain/shardbeststate.go
+++ blockchain/shardbeststate.go
@@ -24,21 +24,20 @@ import (
 // shared by all callers.
 
 type ShardBestState struct {
-	BestBlockHash         common.Hash                       `json:"BestBlockHash"` // hash of block.
-	BestBlock             *ShardBlock                       `json:"BestBlock"`     // block data
-	BestBeaconHash        common.Hash                       `json:"BestBeaconHash"`
-	BeaconHeight          uint64                            `json:"BeaconHeight"`
-	ShardID               byte                              `json:"ShardID"`
-	Epoch                 uint64                            `json:"Epoch"`
-	ShardHeight           uint64                            `json:"ShardHeight"`
-	MaxShardCommitteeSize int                               `json:"MaxShardCommitteeSize"`
-	MinShardCommitteeSize int                               `json:"MinShardCommitteeSize"`
-	ShardProposerIdx      int                               `json:"ShardProposerIdx"`
-	ShardCommittee        []incognitokey.CommitteePublicKey `json:"ShardCommittee"`
-	ShardPendingValidator []incognitokey.CommitteePublicKey `json:"ShardPendingValidator"`
-	BestCrossShard        map[byte]uint64                   `json:"BestCrossShard"` // Best cross shard block by heigh
-	// TODO: produce merkle root for staking tx
-	StakingTx              map[string]string                 `json:"StakingTx"` //TODO: delete staking transaction for beacon committee when it is swapped out
+	BestBlockHash          common.Hash                       `json:"BestBlockHash"` // hash of block.
+	BestBlock              *ShardBlock                       `json:"BestBlock"`     // block data
+	BestBeaconHash         common.Hash                       `json:"BestBeaconHash"`
+	BeaconHeight           uint64                            `json:"BeaconHeight"`
+	ShardID                byte                              `json:"ShardID"`
+	Epoch                  uint64                            `json:"Epoch"`
+	ShardHeight            uint64                            `json:"ShardHeight"`
+	MaxShardCommitteeSize  int                               `json:"MaxShardCommitteeSize"`
+	MinShardCommitteeSize  int                               `json:"MinShardCommitteeSize"`
+	ShardProposerIdx       int                               `json:"ShardProposerIdx"`
+	ShardCommittee         []incognitokey.CommitteePublicKey `json:"ShardCommittee"`
+	ShardPendingValidator  []incognitokey.CommitteePublicKey `json:"ShardPendingValidator"`
+	BestCrossShard         map[byte]uint64                   `json:"BestCrossShard"` // Best cross shard block by heigh
+	StakingTx              map[string]string                 `json:"StakingTx"`
 	StopStakingRequest     []incognitokey.CommitteePublicKey `json:"StopStakingRequest"`
 	NumTxns                uint64                            `json:"NumTxns"`                // The number of txns in the block.
 	TotalTxns              uint64                            `json:"TotalTxns"`              // The total number of txns in the chain.
