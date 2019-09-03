8ac81492290ceb80b060f2386b8526428e4c8147
0xmerman <hung@autonomous.nyc>
2019-09-03 23:26:32 +0700

    update rpc

--- rpcserver/jsonresult/getshardbeststate.go
+++ rpcserver/jsonresult/getshardbeststate.go
@@ -7,24 +7,24 @@ import (
 )
 
 type GetShardBestState struct {
-	BestBlockHash          common.Hash                    `json:"BestBlockHash"` // hash of block.
-	BestBeaconHash         common.Hash                    `json:"BestBeaconHash"`
-	BeaconHeight           uint64                         `json:"BeaconHeight"`
-	ShardID                byte                           `json:"ShardID"`
-	Epoch                  uint64                         `json:"Epoch"`
-	ShardHeight            uint64                         `json:"ShardHeight"`
-	MaxShardCommitteeSize  int                            `json:"MaxShardCommitteeSize"`
-	MinShardCommitteeSize  int                            `json:"MinShardCommitteeSize"`
-	ShardProposerIdx       int                            `json:"ShardProposerIdx"`
-	ShardCommittee         []incognitokey.CommitteePublicKey `json:"ShardCommittee"`
-	ShardPendingValidator  []incognitokey.CommitteePublicKey `json:"ShardPendingValidator"`
-	BestCrossShard         map[byte]uint64                `json:"BestCrossShard"` // Best cross shard block by heigh
-	StakingTx              map[string]string              `json:"StakingTx"`
-	NumTxns                uint64                         `json:"NumTxns"`                // The number of txns in the block.
-	TotalTxns              uint64                         `json:"TotalTxns"`              // The total number of txns in the chain.
-	TotalTxnsExcludeSalary uint64                         `json:"TotalTxnsExcludeSalary"` // for testing and benchmark
-	ActiveShards           int                            `json:"ActiveShards"`
-	MetricBlockHeight      uint64                         `json:"MetricBlockHeight"`
+	BestBlockHash          common.Hash       `json:"BestBlockHash"` // hash of block.
+	BestBeaconHash         common.Hash       `json:"BestBeaconHash"`
+	BeaconHeight           uint64            `json:"BeaconHeight"`
+	ShardID                byte              `json:"ShardID"`
+	Epoch                  uint64            `json:"Epoch"`
+	ShardHeight            uint64            `json:"ShardHeight"`
+	MaxShardCommitteeSize  int               `json:"MaxShardCommitteeSize"`
+	MinShardCommitteeSize  int               `json:"MinShardCommitteeSize"`
+	ShardProposerIdx       int               `json:"ShardProposerIdx"`
+	ShardCommittee         []string          `json:"ShardCommittee"`
+	ShardPendingValidator  []string          `json:"ShardPendingValidator"`
+	BestCrossShard         map[byte]uint64   `json:"BestCrossShard"` // Best cross shard block by heigh
+	StakingTx              map[string]string `json:"StakingTx"`
+	NumTxns                uint64            `json:"NumTxns"`                // The number of txns in the block.
+	TotalTxns              uint64            `json:"TotalTxns"`              // The total number of txns in the chain.
+	TotalTxnsExcludeSalary uint64            `json:"TotalTxnsExcludeSalary"` // for testing and benchmark
+	ActiveShards           int               `json:"ActiveShards"`
+	MetricBlockHeight      uint64            `json:"MetricBlockHeight"`
 }
 
 func NewGetShardBestState(data *blockchain.ShardBestState) *GetShardBestState {
@@ -43,17 +43,12 @@ func NewGetShardBestState(data *blockchain.ShardBestState) *GetShardBestState {
 		ShardProposerIdx:       data.ShardProposerIdx,
 		TotalTxns:              data.TotalTxns,
 		TotalTxnsExcludeSalary: data.TotalTxnsExcludeSalary,
-
-		//BestCrossShard:         data.BestCrossShard,
-		//ShardPendingValidator:  data.ShardPendingValidator,
-		//StakingTx:              data.StakingTx,
-		//ShardCommittee:         data.ShardCommittee,
 	}
 
-	result.ShardCommittee = make([]incognitokey.CommitteePublicKey, len(data.ShardCommittee))
-	copy(result.ShardCommittee, data.ShardCommittee)
-	result.ShardPendingValidator = make([]incognitokey.CommitteePublicKey, len(data.ShardPendingValidator))
-	copy(result.ShardPendingValidator, data.ShardPendingValidator)
+	result.ShardCommittee = make([]string, len(data.ShardCommittee))
+	copy(result.ShardCommittee, incognitokey.CommitteeKeyListToString(data.ShardCommittee))
+	result.ShardPendingValidator = make([]string, len(data.ShardPendingValidator))
+	copy(result.ShardPendingValidator, incognitokey.CommitteeKeyListToString(data.ShardPendingValidator))
 
 	result.BestCrossShard = make(map[byte]uint64)
 	for k, v := range data.BestCrossShard {
