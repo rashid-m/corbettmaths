d670e5b2592f00daa05625c96eb0611350503ced
0xkraken <hien.n@autonomous.nyc>
2019-09-12 10:55:54 +0700

    refactor http estimate fee

--- rpcserver/rpcservice/commonservice.go
+++ rpcserver/rpcservice/commonservice.go
@@ -4,7 +4,9 @@ import (
 	rCommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/incognitochain/incognito-chain/common"
+	"github.com/incognitochain/incognito-chain/incognitokey"
 	"github.com/incognitochain/incognito-chain/metadata"
+	"github.com/incognitochain/incognito-chain/privacy"
 	"github.com/incognitochain/incognito-chain/transaction"
 	"github.com/incognitochain/incognito-chain/wallet"
 )
@@ -73,4 +75,59 @@ func NewBurningRequestMetadata(senderPrivateKeyStr string, tokenReceivers interf
 
 func GetETHHeaderByHash(ethBlockHash string) (*types.Header, error) {
 	return metadata.GetETHHeader(rCommon.HexToHash(ethBlockHash))
+}
+
+// GetKeySetFromPrivateKeyParams - deserialize a private key string
+// into keyWallet object and fill all keyset in keywallet with private key
+// return key set and shard ID
+func GetKeySetFromPrivateKeyParams(privateKeyWalletStr string) (*incognitokey.KeySet, byte, error) {
+	// deserialize to crate keywallet object which contain private key
+	keyWallet, err := wallet.Base58CheckDeserialize(privateKeyWalletStr)
+	if err != nil {
+		return nil, byte(0), err
+	}
+	// fill paymentaddress and readonly key with privatekey
+	err = keyWallet.KeySet.InitFromPrivateKey(&keyWallet.KeySet.PrivateKey)
+	if err != nil {
+		return nil, byte(0), err
+	}
+
+	// calculate shard ID
+	lastByte := keyWallet.KeySet.PaymentAddress.Pk[len(keyWallet.KeySet.PaymentAddress.Pk)-1]
+	shardID := common.GetShardIDFromLastByte(lastByte)
+
+	return &keyWallet.KeySet, shardID, nil
+}
+
+// GetKeySetFromPaymentAddressParam - deserialize a key string(wallet serialized)
+// into keyWallet - this keywallet may contain
+func GetKeySetFromPaymentAddressParam(paymentAddressStr string) (*incognitokey.KeySet, byte, error) {
+	keyWallet, err := wallet.Base58CheckDeserialize(paymentAddressStr)
+	if err != nil {
+		return nil, byte(0), err
+	}
+
+	// calculate shard ID
+	lastByte := keyWallet.KeySet.PaymentAddress.Pk[len(keyWallet.KeySet.PaymentAddress.Pk)-1]
+	shardID := common.GetShardIDFromLastByte(lastByte)
+
+	return &keyWallet.KeySet, shardID, nil
+}
+
+
+func NewPaymentInfosFromReceiversParam(receiversParam map[string]interface{}) ([]*privacy.PaymentInfo, error){
+	paymentInfos := make([]*privacy.PaymentInfo, 0)
+	for paymentAddressStr, amount := range receiversParam {
+		keyWalletReceiver, err := wallet.Base58CheckDeserialize(paymentAddressStr)
+		if err != nil {
+			return nil, err
+		}
+		paymentInfo := &privacy.PaymentInfo{
+			Amount:         uint64(amount.(float64)),
+			PaymentAddress: keyWalletReceiver.KeySet.PaymentAddress,
+		}
+		paymentInfos = append(paymentInfos, paymentInfo)
+	}
+
+	return paymentInfos, nil
 }
--- rpcserver/rpcservice/txmempoolservice.go
+++ rpcserver/rpcservice/txmempoolservice.go
@@ -2,6 +2,7 @@ package rpcservice
 import (
 	"github.com/incognitochain/incognito-chain/common"
 	"github.com/incognitochain/incognito-chain/mempool"
+	"github.com/incognitochain/incognito-chain/privacy"
 )
 
 type TxMemPoolService struct {
@@ -11,5 +12,18 @@ type TxMemPoolService struct {
 func (txMemPoolService TxMemPoolService) GetPoolCandidate() map[common.Hash]string {
 	return txMemPoolService.TxMemPool.GetClonedPoolCandidate()
 }
+
+
+func (txMemPoolService TxMemPoolService) FilterMemPoolOutcoinsToSpent(outCoins []*privacy.OutputCoin) ([]*privacy.OutputCoin, error) {
+	remainOutputCoins := make([]*privacy.OutputCoin, 0)
+
+	for _, outCoin := range outCoins {
+		if txMemPoolService.TxMemPool.ValidateSerialNumberHashH(outCoin.CoinDetails.GetSerialNumber().Compress()) == nil {
+			remainOutputCoins = append(remainOutputCoins, outCoin)
+		}
+	}
+	return remainOutputCoins, nil
+}
+
 
 
