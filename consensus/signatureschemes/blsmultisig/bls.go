d1960a41f81f5f93cbf6369b2eb8270de34b5419
0xakk0r0kamui <hy@autonomous.nyc>
2019-09-13 16:44:48 +0700

    Big Update, needed remove db: - Optimize bls validation - Change Producer Single Signature, from BLS to ECDSA

--- consensus/signatureschemes/blsmultisig/bls.go
+++ consensus/signatureschemes/blsmultisig/bls.go
@@ -1,9 +1,10 @@
 package blsmultisig
 
 import (
+	"bytes"
 	"errors"
 	"math/big"
-	"reflect"
+	"sync"
 
 	bn256 "github.com/ethereum/go-ethereum/crypto/bn256/google"
 )
@@ -27,10 +28,6 @@ func Sign(data, skBytes []byte, selfIdx int, committee []PublicKey) ([]byte, err
 
 // Verify verify BLS sig on given data and list public key
 func Verify(sig, data []byte, signersIdx []int, committee []PublicKey) (bool, error) {
-	// if len(skBytes) != CSKSz {
-	// 	return []byte{0}, NewBLSSignatureError(InvalidPrivateKeyErr, errors.New(ErrCodeMessage[InvalidPrivateKeyErr].Message))
-	// }
-	// sk := B2I(skBytes)
 	for _, idx := range signersIdx {
 		if (idx < 0) || (idx >= len(committee)) {
 			return false, NewBLSSignatureError(InvalidCommitteeInfoErr, errors.New(ErrCodeMessage[InvalidCommitteeInfoErr].Message))
@@ -39,19 +36,48 @@ func Verify(sig, data []byte, signersIdx []int, committee []PublicKey) (bool, er
 	if len(signersIdx) > len(committee) || (len(committee) < 1) {
 		return false, NewBLSSignatureError(InvalidCommitteeInfoErr, errors.New(ErrCodeMessage[InvalidCommitteeInfoErr].Message))
 	}
-	gG2Pn := new(bn256.G2)
-	gG2Pn.ScalarBaseMult(big.NewInt(1))
-	sigPn, err := DecmprG1(sig)
+	wg := sync.WaitGroup{}
+	// done := make(chan bool, 1)
+	// errChan := make(chan error, 1)
+	var err error
+	lPair := new(bn256.GT)
+	rPair := new(bn256.GT)
+	sigPn := new(bn256.G1)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		gG2Pn := new(bn256.G2)
+		gG2Pn.ScalarBaseMult(big.NewInt(1))
+		sigPn, err = DecmprG1(sig)
+		if err != nil {
+			return
+			// errChan <- err
+			// return false, NewBLSSignatureError(DecompressFromByteErr, err)
+		}
+		lPair = bn256.Pair(sigPn, gG2Pn)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		apk := APKGen(committee, signersIdx)
+		dataPn := B2G1P(data)
+		rPair = bn256.Pair(dataPn, apk)
+	}()
+	// go func() {
+	wg.Wait()
+	// close(done)
+	// }()
+	// select {
+	// case <-done:
+	// case err := <-errChan:
 	if err != nil {
 		return false, NewBLSSignatureError(DecompressFromByteErr, err)
 	}
-	lPair := bn256.Pair(sigPn, gG2Pn)
-	apk := APKGen(committee, signersIdx)
-	dataPn := B2G1P(data)
-	rPair := bn256.Pair(dataPn, apk)
-	if !reflect.DeepEqual(lPair.Marshal(), rPair.Marshal()) {
+	// }
+	if !bytes.Equal(lPair.Marshal(), rPair.Marshal()) {
 		return false, nil
 	}
+	// fmt.Printf("ConsLog %v %v %v %v %v %v %v\n", e1.Seconds(), e2.Seconds(), e3.Seconds(), e4.Seconds(), e5.Seconds(), e6.Seconds(), e7.Seconds())
 	return true, nil
 }
 
