07a21ca43511d7ad0dd1748535f1273ab6c03fde
0xkraken <hien.n@autonomous.nyc>
2019-09-16 11:24:49 +0700

    update

--- rpcserver/http.go
+++ rpcserver/http.go
@@ -38,9 +38,9 @@ type HttpServer struct {
 	txMemPoolService  *rpcservice.TxMemPoolService
 	databaseService   *rpcservice.DatabaseService
 	networkService    *rpcservice.NetworkService
-	poolStateService * rpcservice.PoolStateService
-	txService *rpcservice.TxService
-	walletService *rpcservice.WalletService
+	poolStateService  *rpcservice.PoolStateService
+	txService         *rpcservice.TxService
+	walletService     *rpcservice.WalletService
 }
 
 func (httpServer *HttpServer) Init(config *RpcServerConfig) {
@@ -61,18 +61,18 @@ func (httpServer *HttpServer) Init(config *RpcServerConfig) {
 	httpServer.blockService = &rpcservice.BlockService{BlockChain: httpServer.config.BlockChain, DB: httpServer.config.Database}
 	httpServer.outputCoinService = &rpcservice.CoinService{BlockChain: httpServer.config.BlockChain}
 	httpServer.txMemPoolService = &rpcservice.TxMemPoolService{TxMemPool: httpServer.config.TxMemPool}
-	httpServer.databaseService = &rpcservice.DatabaseService{DB: *httpServer.config.Database}
+	httpServer.databaseService = &rpcservice.DatabaseService{DB: httpServer.config.Database}
 	httpServer.networkService = &rpcservice.NetworkService{ConnMgr: httpServer.config.ConnMgr}
 	httpServer.poolStateService = &rpcservice.PoolStateService{}
 	httpServer.txService = &rpcservice.TxService{
-		DB: httpServer.config.Database,
-		BlockChain: httpServer.config.BlockChain,
-		Wallet: httpServer.config.Wallet,
+		DB:           httpServer.config.Database,
+		BlockChain:   httpServer.config.BlockChain,
+		Wallet:       httpServer.config.Wallet,
 		FeeEstimator: httpServer.config.FeeEstimator,
-		TxMemPool: httpServer.config.TxMemPool,
+		TxMemPool:    httpServer.config.TxMemPool,
 	}
 	httpServer.walletService = &rpcservice.WalletService{
-		Wallet: httpServer.config.Wallet,
+		Wallet:     httpServer.config.Wallet,
 		BlockChain: httpServer.config.BlockChain,
 	}
 }
