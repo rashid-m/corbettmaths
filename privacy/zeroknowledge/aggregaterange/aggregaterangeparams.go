3cd4dcadb70b0fa81a573ad5c381ed8855366c8b
0xkraken <hien.n@autonomous.nyc>
2019-09-23 11:57:55 +0700

    refactor aggregrate range

--- privacy/zeroknowledge/aggregaterange/aggregaterangeparams.go
+++ privacy/zeroknowledge/aggregaterange/aggregaterangeparams.go
@@ -1,7 +1,6 @@
 package aggregaterange
 
 import (
-	"math/big"
 	"sync"
 
 	"github.com/incognitochain/incognito-chain/common"
@@ -12,29 +11,29 @@ import (
 
 // bulletproofParams includes all generator for aggregated range proof
 type bulletproofParams struct {
-	g []*privacy.EllipticPoint
-	h []*privacy.EllipticPoint
-	u *privacy.EllipticPoint
+	g []*privacy.Point
+	h []*privacy.Point
+	u *privacy.Point
 }
 
 func newBulletproofParams(m int) *bulletproofParams {
 	gen := new(bulletproofParams)
 	capacity := maxExp * m // fixed value
-	gen.g = make([]*privacy.EllipticPoint, capacity)
-	gen.h = make([]*privacy.EllipticPoint, capacity)
+	gen.g = make([]*privacy.Point, capacity)
+	gen.h = make([]*privacy.Point, capacity)
 
 	var wg sync.WaitGroup
 	wg.Add(capacity)
 	for i := 0; i < capacity; i++ {
 		go func(i int, wg *sync.WaitGroup) {
 			defer wg.Done()
-			gen.g[i] = privacy.PedCom.G[0].Hash(int64(5 + i))
-			gen.h[i] = privacy.PedCom.G[0].Hash(int64(5 + i + maxOutputNumber*maxExp))
+			gen.g[i] = privacy.HashToPoint(int64(5 + i))
+			gen.h[i] = privacy.HashToPoint(int64(5 + i + maxOutputNumber*maxExp))
 		}(i, &wg)
 	}
 	wg.Wait()
-	gen.u = new(privacy.EllipticPoint)
-	gen.u = gen.h[0].Hash(int64(5 + 2*maxOutputNumber*64))
+	gen.u = new(privacy.Point)
+	gen.u = privacy.HashToPoint(int64(5 + 2*maxOutputNumber*maxExp))
 
 	return gen
 }
@@ -44,8 +43,8 @@ func addBulletproofParams(extraNumber int) *bulletproofParams {
 	newCapacity := currentCapacity + maxExp * extraNumber
 
 	for i := 0; i < newCapacity - currentCapacity; i++ {
-		AggParam.g = append(AggParam.g, privacy.PedCom.G[0].Hash(int64(5 + i + currentCapacity)))
-		AggParam.h = append(AggParam.h, privacy.PedCom.G[0].Hash(int64(5 + i + currentCapacity + maxOutputNumber*maxExp)))
+		AggParam.g = append(AggParam.g, privacy.HashToPoint(int64(5 + i + currentCapacity)))
+		AggParam.h = append(AggParam.h, privacy.HashToPoint(int64(5 + i + currentCapacity + maxOutputNumber*maxExp)))
 	}
 
 	return AggParam
@@ -53,17 +52,17 @@ func addBulletproofParams(extraNumber int) *bulletproofParams {
 
 var AggParam = newBulletproofParams(numOutputParam)
 
-func generateChallengeForAggRange(AggParam *bulletproofParams, values [][]byte) *big.Int {
-	bytes := AggParam.g[0].Compress()
-	for i := 1; i < len(AggParam.g); i++ {
-		bytes = append(bytes, AggParam.g[i].Compress()...)
+func generateChallengeForAggRange(AggParam *bulletproofParams, values [][]byte) *privacy.Scalar {
+	bytes := []byte{}
+	for i := 0; i < len(AggParam.g); i++ {
+		bytes = append(bytes, AggParam.g[i].ToBytes()[:]...)
 	}
 
 	for i := 0; i < len(AggParam.h); i++ {
-		bytes = append(bytes, AggParam.h[i].Compress()...)
+		bytes = append(bytes, AggParam.h[i].ToBytes()[:]...)
 	}
 
-	bytes = append(bytes, AggParam.u.Compress()...)
+	bytes = append(bytes, AggParam.u.ToBytes()[:]...)
 
 	for i := 0; i < len(values); i++ {
 		bytes = append(bytes, values[i]...)
@@ -71,7 +70,6 @@ func generateChallengeForAggRange(AggParam *bulletproofParams, values [][]byte) 
 
 	hash := common.HashB(bytes)
 
-	res := new(big.Int).SetBytes(hash)
-	res.Mod(res, privacy.Curve.Params().N)
+	res := new(privacy.Scalar).FromBytes(privacy.SliceToArray(hash))
 	return res
 }
