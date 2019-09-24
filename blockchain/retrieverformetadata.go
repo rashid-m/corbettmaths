a10eb2a2fe44ace2b89d35003e6184a43e858e9a
0xmerman <hung@autonomous.nyc>
2019-09-24 15:57:13 +0700

    update staking metadata

--- blockchain/retrieverformetadata.go
+++ blockchain/retrieverformetadata.go
@@ -71,58 +71,34 @@ func ListPubKeyFromListPayment(listPaymentAddresses []privacy.PaymentAddress) []
 	return pubKeys
 }
 
-func (blockchain *BlockChain) GetAllCommitteeValidatorCandidate() (map[byte][]string, map[byte][]string, []string, []string, []string, []string, []string, []string, error) {
-	SC := make(map[byte][]string)
-	SPV := make(map[byte][]string)
+func (blockchain *BlockChain) GetAllCommitteeValidatorCandidate() (map[byte][]incognitokey.CommitteePublicKey, map[byte][]incognitokey.CommitteePublicKey, []incognitokey.CommitteePublicKey, []incognitokey.CommitteePublicKey, []incognitokey.CommitteePublicKey, []incognitokey.CommitteePublicKey, []incognitokey.CommitteePublicKey, []incognitokey.CommitteePublicKey, error) {
+	SC := make(map[byte][]incognitokey.CommitteePublicKey)
+	SPV := make(map[byte][]incognitokey.CommitteePublicKey)
 	if blockchain.IsTest {
-		return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, nil
+		return SC, SPV, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, nil
 	}
 	beaconBestState := BeaconBestState{}
 	temp, err := blockchain.config.DataBase.FetchBeaconBestState()
 	if err != nil {
-		return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
+		return SC, SPV, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, nil
 	} else {
 		if err := json.Unmarshal(temp, &beaconBestState); err != nil {
 			Logger.log.Error(err)
-			return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
+			return SC, SPV, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, nil
 		}
 	}
 	for shardID, committee := range beaconBestState.GetShardCommittee() {
-		SC[shardID], err = incognitokey.CommitteeKeyListToString(committee)
-		if err != nil {
-			return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
-		}
+		SC[shardID] = append([]incognitokey.CommitteePublicKey{}, committee...)
 	}
-	for shardID, committee := range beaconBestState.GetShardPendingValidator() {
-		SPV[shardID], err = incognitokey.CommitteeKeyListToString(committee)
-		if err != nil {
-			return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
-		}
+	for shardID, pendingValidator := range beaconBestState.GetShardPendingValidator() {
+		SPV[shardID] = append([]incognitokey.CommitteePublicKey{}, pendingValidator...)
 	}
-	BC, err := incognitokey.CommitteeKeyListToString(beaconBestState.BeaconCommittee)
-	if err != nil {
-		return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
-	}
-	BPV, err := incognitokey.CommitteeKeyListToString(beaconBestState.BeaconPendingValidator)
-	if err != nil {
-		return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
-	}
-	CBWFCR, err := incognitokey.CommitteeKeyListToString(beaconBestState.CandidateBeaconWaitingForCurrentRandom)
-	if err != nil {
-		return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
-	}
-	CBWFNR, err := incognitokey.CommitteeKeyListToString(beaconBestState.CandidateBeaconWaitingForNextRandom)
-	if err != nil {
-		return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
-	}
-	CSWFCR, err := incognitokey.CommitteeKeyListToString(beaconBestState.CandidateShardWaitingForCurrentRandom)
-	if err != nil {
-		return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
-	}
-	CSWFNR, err := incognitokey.CommitteeKeyListToString(beaconBestState.CandidateShardWaitingForNextRandom)
-	if err != nil {
-		return SC, SPV, []string{}, []string{}, []string{}, []string{}, []string{}, []string{}, err
-	}
+	BC := beaconBestState.BeaconCommittee
+	BPV := beaconBestState.BeaconPendingValidator
+	CBWFCR := beaconBestState.CandidateBeaconWaitingForCurrentRandom
+	CBWFNR := beaconBestState.CandidateBeaconWaitingForNextRandom
+	CSWFCR := beaconBestState.CandidateShardWaitingForCurrentRandom
+	CSWFNR := beaconBestState.CandidateShardWaitingForNextRandom
 	return SC, SPV, BC, BPV, CBWFCR, CBWFNR, CSWFCR, CSWFNR, nil
 }
 
