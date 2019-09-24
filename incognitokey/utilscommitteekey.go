7382077004a00b3ddecb34c81fbee66cc9fed546
0xakk0r0kamui <hy@autonomous.nyc>
2019-09-24 13:04:32 +0700

    Add some helpful function

--- incognitokey/utilscommitteekey.go
+++ incognitokey/utilscommitteekey.go
@@ -1,5 +1,13 @@
 package incognitokey
 
+import (
+	"encoding/json"
+	"reflect"
+
+	"github.com/incognitochain/incognito-chain/common"
+	"github.com/incognitochain/incognito-chain/common/base58"
+)
+
 func ExtractPublickeysFromCommitteeKeyList(keyList []CommitteePublicKey, keyType string) ([]string, error) {
 	result := []string{}
 	for _, keySet := range keyList {
@@ -31,12 +39,71 @@ func CommitteeBase58KeyListToStruct(strKeyList []string) ([]CommitteePublicKey, 
 		return []CommitteePublicKey{}, nil
 	}
 	result := []CommitteePublicKey{}
+	keyStruct := new(CommitteePublicKey)
 	for _, key := range strKeyList {
-		var keyStruct CommitteePublicKey
-		if err := keyStruct.FromBase58(key); err != nil {
+
+		if err := keyStruct.FromString(key); err != nil {
 			return nil, err
 		}
-		result = append(result, keyStruct)
+		result = append(result, *keyStruct)
 	}
 	return result, nil
+}
+
+func Equal(keyString1 string, keyString2 string) bool {
+	var pubKey1 CommitteePublicKey
+	var pubKey2 CommitteePublicKey
+	keyBytes1, ver, err := base58.Base58Check{}.Decode(keyString1)
+	if (ver != common.ZeroByte) || (err != nil) {
+		// errors.New("wrong input")
+		return false
+	}
+	keyBytes2, ver, err := base58.Base58Check{}.Decode(keyString2)
+	if (ver != common.ZeroByte) || (err != nil) {
+		// return errors.New("wrong input")
+		return false
+	}
+	err = json.Unmarshal(keyBytes1, pubKey1)
+	if err != nil {
+		// return errors.New("wrong input")
+		return false
+	}
+	err = json.Unmarshal(keyBytes2, pubKey2)
+	if err != nil {
+		// return errors.New("wrong input")
+		return false
+	}
+	if reflect.DeepEqual(pubKey1, pubKey2) {
+		return true
+	}
+	return false
+}
+
+func IsOneMiner(keyString1 string, keyString2 string) bool {
+	var pubKey1 CommitteePublicKey
+	var pubKey2 CommitteePublicKey
+	keyBytes1, ver, err := base58.Base58Check{}.Decode(keyString1)
+	if (ver != common.ZeroByte) || (err != nil) {
+		// errors.New("wrong input")
+		return false
+	}
+	keyBytes2, ver, err := base58.Base58Check{}.Decode(keyString2)
+	if (ver != common.ZeroByte) || (err != nil) {
+		// return errors.New("wrong input")
+		return false
+	}
+	err = json.Unmarshal(keyBytes1, pubKey1)
+	if err != nil {
+		// return errors.New("wrong input")
+		return false
+	}
+	err = json.Unmarshal(keyBytes2, pubKey2)
+	if err != nil {
+		// return errors.New("wrong input")
+		return false
+	}
+	if reflect.DeepEqual(pubKey1.MiningPubKey, pubKey2.MiningPubKey) {
+		return true
+	}
+	return false
 }
