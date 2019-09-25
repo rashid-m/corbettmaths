8cace630bd15641bdb3485cfcea128837b4af7a5
0xkumi <dung.q@autonomous.nyc>
2019-09-25 17:47:45 +0700

    update

---
+++ peerv2/grpc_server.go
@@ -0,0 +1,12 @@
+package peerv2
+
+import (
+	"google.golang.org/grpc"
+)
+
+type GRPCService_Server struct {
+}
+
+func (self *GRPCService_Server) registerServices(grpsServer *grpc.Server) {
+
+}
--- peerv2/utils.go
+++ peerv2/utils.go
@@ -23,16 +23,6 @@ func ParseListenner(s, defaultIP string, defaultPort int) (string, int) {
 	return splitStr[0], 0
 }
 
-//func IDFromPublicKey(pk crypto.PubKey) (peer.ID, error) {
-//	b, err := pk.Bytes()
-//	if err != nil {
-//		return "", err
-//	}
-//	var alg uint64 = multihash.SHA2_256
-//	hash, _ := multihash.Sum(b, alg, -1)
-//	return peer.ID(hash), nil
-//}
-
 func must(err error) {
 	if err != nil {
 		panic(err)
