0a8af341f6e4c815c7d5ce1fa53fe3825dfd77ec
0xbunyip <>
2019-09-26 18:15:12 +0700

    Subscribe, unsub and queue messages

--- peerv2/conn_manager.go
+++ peerv2/conn_manager.go
@@ -10,6 +10,7 @@ import (
 	"github.com/incognitochain/incognito-chain/incognitokey"
 	"github.com/incognitochain/incognito-chain/wire"
 	"github.com/libp2p/go-libp2p-core/peer"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/multiformats/go-multiaddr"
 )
 
@@ -21,6 +22,10 @@ type ConnManager struct {
 	LocalHost            *Host
 	DiscoverPeersAddress string
 	IdentityKey          *incognitokey.CommitteePublicKey
+
+	ps       *pubsub.PubSub
+	subs     map[string]*pubsub.Subscription
+	messages chan *pubsub.Message
 
 	cd ConsensusData
 }
@@ -53,6 +58,12 @@ func (cm *ConnManager) Start() {
 	gRPCService := GRPCService_Server{}
 	gRPCService.registerServices(cm.LocalHost.GRPC.GetGRPCServer())
 
+	// Pubsub
+	// TODO(@0xbunyip): handle error
+	cm.ps, _ = pubsub.NewFloodSub(context.Background(), cm.LocalHost.Host)
+	cm.subs = map[string]*pubsub.Subscription{}
+	cm.messages = make(chan *pubsub.Message, 1000)
+
 	go cm.manageRoleSubscription()
 }
 
@@ -72,6 +83,7 @@ func (cm *ConnManager) manageRoleSubscription() {
 			continue
 		}
 
+		// Get new topics
 		topics := lastTopics
 		if role == common.ShardRole || role == common.BeaconRole {
 			var err error
@@ -82,7 +94,11 @@ func (cm *ConnManager) manageRoleSubscription() {
 			}
 		}
 
-		cm.subscribeNewTopics(topics, lastTopics)
+		err := cm.subscribeNewTopics(topics, lastTopics)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
 
 		lastRole = role
 		lastShardID = shardID
@@ -91,7 +107,7 @@ func (cm *ConnManager) manageRoleSubscription() {
 }
 
 // subscribeNewTopics subscribes to new topics and unsubcribes any topics that aren't needed anymore
-func (cm *ConnManager) subscribeNewTopics(topics []string, subscribed []string) {
+func (cm *ConnManager) subscribeNewTopics(topics []string, subscribed []string) error {
 	found := func(s string, l []string) bool {
 		for _, m := range l {
 			if s == m {
@@ -101,20 +117,47 @@ func (cm *ConnManager) subscribeNewTopics(topics []string, subscribed []string) 
 		return false
 	}
 
+	// Subscribe to new topics
 	for _, t := range topics {
 		if found(t, subscribed) {
 			continue
 		}
+
 		fmt.Println("subscribing", t)
-		// TODO(@0xbunyip): sub here
+		s, err := cm.ps.Subscribe(t)
+		if err != nil {
+			return err
+		}
+		cm.subs[t] = s
+		go processSubscriptionMessage(cm.messages, s)
 	}
 
+	// Unsubscribe to old ones
 	for _, t := range subscribed {
 		if found(t, topics) {
 			continue
 		}
-		// TODO(@0xbunyip): unsub here
+
 		fmt.Println("unsubscribing", t)
+		s := cm.subs[t]
+		s.Cancel()
+		delete(cm.subs, t)
+	}
+	return nil
+}
+
+// processSubscriptionMessage listens to a topic and pushes all messages to a queue to be processed later
+func processSubscriptionMessage(inbox chan *pubsub.Message, sub *pubsub.Subscription) {
+	ctx := context.Background()
+	for {
+		msg, err := sub.Next(ctx)
+		if err != nil {
+			log.Println(err)
+			return
+			// TODO(@0xbunyip): check if topic is unsubbed then return, otherwise just continue
+		}
+
+		inbox <- msg
 	}
 }
 
