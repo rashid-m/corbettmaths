3c1ceae7f11cba93f50c35590891b7f3e28cadda
0xmerman <hung@autonomous.nyc>
2019-09-30 22:19:28 +0700

    add error

--- blockchain/btc/error.go
+++ blockchain/btc/error.go
@@ -18,19 +18,29 @@ const (
 	NonceError
 	WrongTypeError
 	TimeParseError
+	BlockHashParseError
+	GetBlockHashResultError
+	GetBlockHeaderResultError
+	ParseNonceResultError
+	ParseTimestampResultError
 )
 
 var ErrCodeMessage = map[int]struct {
 	code    int
 	message string
 }{
-	UnExpectedError:         {-1, "Unexpected error"},
-	APIError:                {-2, "API Error"},
-	TimestampError:          {-3, "Timestamp Error"},
-	UnmashallJsonBlockError: {-4, "Unmarshall json block is failed"},
-	NonceError:              {-5, "Nonce Error"},
-	WrongTypeError:              {-6, "Wrong Type Error"},
-	TimeParseError:              {-7, "Time Parse Error"},
+	UnExpectedError:           {-1, "Unexpected error"},
+	APIError:                  {-2, "API Error"},
+	TimestampError:            {-3, "Timestamp Error"},
+	UnmashallJsonBlockError:   {-4, "Unmarshall json block is failed"},
+	NonceError:                {-5, "Nonce Error"},
+	WrongTypeError:            {-6, "Wrong Type Error"},
+	TimeParseError:            {-7, "Time Parse Error"},
+	BlockHashParseError:       {-8, "Block Hash Parse Error"},
+	GetBlockHashResultError:   {-9, "Get Block Hash Result Error"},
+	GetBlockHeaderResultError: {-10, "Get Block Header Result Error"},
+	ParseNonceResultError:     {-11, "Parse Nonce Result Error"},
+	ParseTimestampResultError: {-12, "Parse Timestamp Result Error"},
 }
 
 type BTCAPIError struct {
