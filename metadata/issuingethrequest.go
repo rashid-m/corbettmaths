a16ef4240ebce2d1983360e7ed29cf4f40ca0c52
0xkraken <hien.n@autonomous.nyc>
2019-10-07 11:58:05 +0700

    uncomment metadata

--- metadata/issuingethrequest.go
+++ metadata/issuingethrequest.go
@@ -1,6 +1,6 @@
 package metadata
 
-/*import (
+import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
@@ -285,4 +285,3 @@ func PickAndParseLogMapFromReceipt(constructedReceipt *types.Receipt, ethContrac
 	}
 	return ParseETHLogData(logData)
 }
-*/
