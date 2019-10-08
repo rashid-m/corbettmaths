499ead7e4f0115ce2de09ddd040fb0474b8e4869
0xkumi <dung.q@autonomous.nyc>
2019-10-08 09:31:08 +0700

    update SetBeaconBestState

--- blockchain/revert.go
+++ blockchain/revert.go
@@ -703,8 +703,7 @@ func (blockchain *BlockChain) revertBeaconBestState() error {
 	if err := json.Unmarshal(prevBST, &beaconBestState); err != nil {
 		return err
 	}
-
-	blockchain.BestState.Beacon = &beaconBestState
+	SetBeaconBestState(&beaconBestState)
 	blockchain.config.BeaconPool.SetBeaconState(beaconBestState.BeaconHeight)
 	blockchain.config.ShardToBeaconPool.SetShardState(blockchain.BestState.Beacon.GetBestShardHeight())
 
