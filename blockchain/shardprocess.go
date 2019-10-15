ba20fabf5383abd06e45ca127da42ef0b6711faa
0xkumi <dung.q@autonomous.nyc>
2019-10-15 13:07:09 +0700

    remove log, update connection

--- blockchain/shardprocess.go
+++ blockchain/shardprocess.go
@@ -138,8 +138,7 @@ func (blockchain *BlockChain) InsertShardBlock(shardBlock *ShardBlock, isValidat
 	if err := blockchain.BestState.Shard[shardID].verifyBestStateWithShardBlock(shardBlock, true, shardID); err != nil {
 		return err
 	}
-	Logger.log.Infof("SHARD %+v | Update ShardBestState, block height %+v with hash %+v \n", shardBlock.Header.ShardID, shardBlock.Header.Height, blockHash)
-
+	Logger.log.Infof("SHARD %+v | BackupCurrentShardState, block height %+v with hash %+v \n", shardBlock.Header.ShardID, shardBlock.Header.Height, blockHash)
 	// Backup beststate
 	err = blockchain.config.DataBase.CleanBackup(false, shardBlock.Header.ShardID)
 	if err != nil {
@@ -154,6 +153,8 @@ func (blockchain *BlockChain) InsertShardBlock(shardBlock *ShardBlock, isValidat
 	if err != nil {
 		return err
 	}
+
+	Logger.log.Infof("SHARD %+v | Update ShardBestState, block height %+v with hash %+v \n", shardBlock.Header.ShardID, shardBlock.Header.Height, blockHash)
 	if err := blockchain.BestState.Shard[shardID].updateShardBestState(blockchain, shardBlock, beaconBlocks); err != nil {
 		errRevert := blockchain.revertShardBestState(shardID)
 		if errRevert != nil {
@@ -161,6 +162,8 @@ func (blockchain *BlockChain) InsertShardBlock(shardBlock *ShardBlock, isValidat
 		}
 		return err
 	}
+
+	Logger.log.Infof("SHARD %+v | Update NumOfBlocksByProducers, block height %+v with hash %+v \n", shardBlock.Header.ShardID, shardBlock.Header.Height, blockHash)
 	// update number of blocks produced by producers to shard best state
 	blockchain.BestState.Shard[shardID].updateNumOfBlocksByProducers(shardBlock)
 
