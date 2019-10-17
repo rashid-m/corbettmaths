28b60794fed75e65af5a9862e311d9b632990487
0xkraken <hien.n@autonomous.nyc>
2019-10-17 15:13:43 +0700

    check validate param http slash

--- rpcserver/http_slash.go
+++ rpcserver/http_slash.go
@@ -8,12 +8,16 @@ import (
 
 func (httpServer *HttpServer) handleGetProducersBlackList(params interface{}, closeChan <-chan struct{}) (interface{}, *rpcservice.RPCError) {
 	// beaconHeight := httpServer.config.BlockChain.BestState.Beacon.BeaconHeight
-
 	arrayParams := common.InterfaceSlice(params)
-	if len(arrayParams) == 0 {
+	if arrayParams == nil || len(arrayParams) < 1 {
 		return nil, nil
 	}
-	beaconHeight := uint64(arrayParams[0].(float64))
+	beaconHeightParam, ok := arrayParams[0].(float64)
+	if !ok {
+		return nil, nil
+	}
+	beaconHeight := uint64(beaconHeightParam)
+
 	producersBlackList, err := httpServer.databaseService.GetProducersBlackList(beaconHeight)
 	if err != nil {
 		return false, rpcservice.NewRPCError(rpcservice.UnexpectedError, err)
@@ -24,15 +28,15 @@ func (httpServer *HttpServer) handleGetProducersBlackList(params interface{}, cl
 func (httpServer *HttpServer) handleGetProducersBlackListDetail(params interface{}, closeChan <-chan struct{}) (interface{}, *rpcservice.RPCError) {
 	// beaconHeight := httpServer.config.BlockChain.BestState.Beacon.BeaconHeight
 	arrayParams := common.InterfaceSlice(params)
-	if len(arrayParams) == 0 {
+	if arrayParams == nil || len(arrayParams) < 1 {
 		return nil, nil
 	}
 
-	param0, ok := arrayParams[0].(float64)
+	beaconHeightParam, ok := arrayParams[0].(float64)
 	if !ok {
 		return nil, nil
 	}
-	beaconHeight := uint64(param0)
+	beaconHeight := uint64(beaconHeightParam)
 
 	producersBlackList, err := httpServer.databaseService.GetProducersBlackList(beaconHeight)
 	if err != nil {
