7e29fcd9c38197e5a27167988ad683c249b60acb
0xbunyip <>
2019-10-18 16:47:22 +0700

    Increase #blocks for each grpc request
    Mempool requires 2 succeeding blocks to insert to validPool. We increase
    
    - Increase #blocks each request from 1 to 20
    - Add logging to trace mempool block insertion
    - Enable rpc for staker node

--- peerv2/block_requester.go
+++ peerv2/block_requester.go
@@ -9,6 +9,8 @@ import (
 	"google.golang.org/grpc"
 )
 
+// TODO(@0xbunyip): cache all requests to prevent querying the same height multiple times
+
 type BlockRequester struct {
 	conn *grpc.ClientConn
 }
@@ -89,9 +91,10 @@ func (c *BlockRequester) GetBlockBeaconByHeight(
 			FromPool:   false,
 		},
 	)
-	log.Printf("Received block beacon data %v", reply)
 	if err != nil {
 		return nil, err
+	} else if reply != nil {
+		log.Printf("Received block beacon data len: %v", len(reply.Data))
 	}
 	return reply.Data, nil
 }
