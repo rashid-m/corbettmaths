8234cc1063d4bbd3b8ad165746b6d6eef6b97e21
minhduccm <minhduccm90@gmail.com>
2019-10-22 08:00:37 +0700

    Create mainnet-run.sh file and add logshipper to incognito nodes

--- bin/bridge/mainnet-run.sh
+++ bin/bridge/mainnet-run.sh
@@ -7,8 +7,8 @@ run()
   latest_tag=$1
   current_tag=$2
   data_dir="data"
-  eth_data_dir="eth-kovan-data"
-  logshipper_data_dir="logshipper-data"
+  eth_data_dir="eth-mainnet-data"
+  logshipper_data_dir="logshipper-mainnet-data"
 
   docker -v || bash -c "wget -qO- https://get.docker.com/ | sh"
 
@@ -18,8 +18,8 @@ run()
     chmod -R 777 $PWD/${eth_data_dir}
   fi
 
-  docker rm -f inc_miner
-  docker rm -f inc_kovan
+  docker rm -f inc_mainnet
+  docker rm -f eth_mainnet
   if [ "$current_tag" != "" ]
   then
     docker image rm -f incognitochain/incognito:${current_tag}
@@ -28,9 +28,9 @@ run()
   docker pull incognitochain/incognito:${latest_tag}
   docker network create --driver bridge inc_net || true
 
-  docker run -ti --restart=always --net inc_net -d -p 8545:8545  -p 30303:30303 -p 30303:30303/udp -v $PWD/${eth_data_dir}:/home/parity/.local/share/io.parity.ethereum/ --name inc_kovan  parity/parity:stable --light  --chain kovan  --jsonrpc-interface all --jsonrpc-hosts all  --jsonrpc-apis all --mode last  --base-path=/home/parity/.local/share/io.parity.ethereum/
+  docker run -ti --restart=always --net inc_net -d -p 8545:8545  -p 30303:30303 -p 30303:30303/udp -v $PWD/${eth_data_dir}:/home/parity/.local/share/io.parity.ethereum/ --name eth_mainnet  parity/parity:stable --light --jsonrpc-interface all --jsonrpc-hosts all  --jsonrpc-apis all --mode last --base-path=/home/parity/.local/share/io.parity.ethereum/
 
-  docker run --restart=always --net inc_net -p 9334:9334 -p 9433:9433 -e GETH_NAME=inc_kovan -e MININGKEY=${validator_key} -v $PWD/${data_dir}:/data -d --name inc_miner incognitochain/incognito:${latest_tag}
+  docker run --restart=always --net inc_net -p 9334:9334 -p 9433:9433 -e GETH_NAME=eth_mainnet -e MININGKEY=${validator_key} -v $PWD/${data_dir}:/data -d --name inc_mainnet incognitochain/incognito:${latest_tag}
 
   if [ $is_shipping_logs -eq 1 ]
   then
