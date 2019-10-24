bf104451196bb6f5c02bae10c96ac8d3e8ebf219
0xmerman <hung@autonomous.nyc>
2019-10-24 10:18:51 +0700

    Resolve Conflict

--- blockchain/shardproducer.go
+++ blockchain/shardproducer.go
@@ -593,8 +593,8 @@ func (blockchain *BlockChain) generateInstruction(shardID byte, beaconHeight uin
 			return instructions, shardPendingValidator, shardCommittee, err
 		}
 
-		//TODO: 0xmerman
-		shardCommittee = append([]string{fixedProducerShardCommittee}, shardCommittee...)
+		////TODO: 0xmerman
+		//shardCommittee = append([]string{fixedProducerShardCommittee}, shardCommittee...)
 
 		// NOTE: shardCommittee must be finalized before building Bridge instruction here
 		// shardCommittee must include all producers and validators in the right order
