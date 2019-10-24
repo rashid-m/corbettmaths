c116c29a8dda7dd88df08b2ff4c21bc90a4121f4
0xsirrush <thaibao@autonomous.nyc>
2019-10-24 17:24:14 +0700

    - deploy for mainnet

--- bin/build_mainnet.sh
+++ bin/build_mainnet.sh
@@ -11,7 +11,7 @@ fi
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o incognito ../*.go
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o bootnode ../bootnode/*.go
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 ENV=testnet go build -ldflags '-w' -o incognito-test ../tests/*.go
-cp ../keylist-mainnet.json .
+cp ../keylist-mainnet.json ./keylist.json
 cp ../sample-config.conf .
 
 commit=`git show --summary --oneline | cut -d ' ' -f 1`
