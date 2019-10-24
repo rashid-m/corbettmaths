877c701f6fbbbb1cb5884eba1256bf84bb97e5a0
0xkraken <hien.n@autonomous.nyc>
2019-10-24 18:02:54 +0700

    estimate fee for pToken with rate between native token and privacy token

--- rpcserver/jsonresult/estimatefeeresult.go
+++ rpcserver/jsonresult/estimatefeeresult.go
@@ -3,12 +3,14 @@ package jsonresult
 type EstimateFeeResult struct {
 	EstimateFeeCoinPerKb uint64
 	EstimateTxSizeInKb   uint64
+	IsFeePToken          bool
 }
 
-func NewEstimateFeeResult(estimateFeeCoinPerKb uint64, estimateTxSizeInKb uint64) *EstimateFeeResult {
+func NewEstimateFeeResult(estimateFeeCoinPerKb uint64, estimateTxSizeInKb uint64, isFeePToken bool) *EstimateFeeResult {
 	result := &EstimateFeeResult{
 		EstimateFeeCoinPerKb: estimateFeeCoinPerKb,
 		EstimateTxSizeInKb:   estimateTxSizeInKb,
+		IsFeePToken:          isFeePToken,
 	}
 	return result
 }
