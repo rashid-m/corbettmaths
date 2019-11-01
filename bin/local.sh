0daf6c9f19076895b908c5b12f99cdb2b44629e0
0xsirrush <thaibao@autonomous.nyc>
2019-11-01 16:48:26 +0700

    - Cherry pick: update logrotate

--- bin/local.sh
+++ bin/local.sh
@@ -6,10 +6,12 @@ fi
 if [ -f ./bootnode ]; then
     rm -rf ./bootnode
 fi
+echo "!23"
 
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o incognito ../*.go
 env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-w' -o bootnode ../bootnode/*.go
 cp ../keylist.json .
+cp ../sample-config.conf .
 
 commit=`git show --summary --oneline | cut -d ' ' -f 1`
 docker build --build-arg commit=$commit . -t incognito && echo "Commit: $commit"
