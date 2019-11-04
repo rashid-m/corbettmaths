2148ddced868946f8e23cb2f3a2554284c8fb445
0xkumi <dung.q@autonomous.nyc>
2019-11-04 14:36:55 +0700

    add log monitor

--- consensus/blsbft/util.go
+++ consensus/blsbft/util.go
@@ -2,6 +2,7 @@ package blsbft
 
 import (
 	"fmt"
+	"github.com/incognitochain/incognito-chain/metrics"
 	"reflect"
 	"strconv"
 	"strings"
@@ -88,6 +89,7 @@ func (e *BLSBFT) isHasMajorityVotes() bool {
 			delete(e.EarlyVotes, roundKey)
 		}
 	}
+	metrics.SetGlobalParam("NVote", len(e.RoundData.Votes))
 	if len(e.RoundData.Votes) > 2*committeeSize/3 {
 		return true
 	}
@@ -152,5 +154,6 @@ func (e *BLSBFT) InitRoundData() {
 	e.RoundData.BlockHash = common.Hash{}
 	e.RoundData.NotYetSendVote = true
 	e.RoundData.LastProposerIndex = e.Chain.GetLastProposerIndex()
+	e.RoundData.TimeStart = time.Now()
 	e.UpdateCommitteeBLSList()
 }
