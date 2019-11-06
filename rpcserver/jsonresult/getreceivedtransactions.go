56965a1ddab31f6c7673a88749baed282e440293
0xsirrush <thaibao@autonomous.nyc>
2019-11-06 17:02:25 +0700

    - update RPC for tx

--- rpcserver/jsonresult/getreceivedtransactions.go
+++ rpcserver/jsonresult/getreceivedtransactions.go
@@ -1,11 +1,18 @@
 package jsonresult
 
-import "github.com/incognitochain/incognito-chain/common"
+import (
+	"github.com/incognitochain/incognito-chain/common"
+	"github.com/incognitochain/incognito-chain/privacy"
+)
 
 type ReceivedTransaction struct {
-	Hash           string
-	ReceivedAmount map[common.Hash]uint64
-	FromShardID    byte
+	Hash          string
+	ReceivedInfos map[common.Hash]ReceivedInfo
+	FromShardID   byte
+}
+
+type ReceivedInfo struct {
+	OutputCoin privacy.OutputCoin
 }
 
 type ListReceivedTransaction struct {
