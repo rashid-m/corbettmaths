7c09c83c597c4829fa1bfc49469a6c8c8389c020
0xsirrush <thaibao@autonomous.nyc>
2019-11-11 15:40:00 +0700

    - not by pass

--- blockchain/shardproducer.go
+++ blockchain/shardproducer.go
@@ -479,7 +479,6 @@ func (blockGenerator *BlockGenerator) buildResponseTxsFromBeaconInstructions(bea
 			case metadata.PDEContributionMeta:
 				if len(l) >= 4 && l[2] == "refund" {
 					newTx, err = blockGenerator.buildPDERefundContributionTx(l[3], producerPrivateKey, shardID)
-					time.Sleep(1 * time.Second)
 				}
 
 			default:
