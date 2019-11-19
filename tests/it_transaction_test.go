077b775fcc58f175d180d6931cb71ff5e74d7877
0xmerman <hung@autonomous.nyc>
2019-11-19 23:17:03 +0700

    Update Integration Test

--- tests/it_transaction_test.go
+++ tests/it_transaction_test.go
@@ -30,23 +30,7 @@ func TestCreateAndSendNormalTransaction(t *testing.T) {
 		}
 	}
 }
-func TestCreateAndSendCustomTokenTransaction(t *testing.T) {
-	var err error
-	_, err = readfile("./testsdata/transaction/custom_token_transaction.json")
-	if err != nil {
-		t.Fatal(err)
-	}
-	_, err = executeTest("./testsdata/transaction/custom_token_transaction.json")
-	if err != nil {
-		if rpcError, ok := err.(*rpcservice.RPCError); ok {
-			if rpcError != nil {
-				t.Fatal(err)
-			}
-		} else {
-			t.Fatal(err)
-		}
-	}
-}
+
 func TestCreateAndSendCustomTokenPrivacyTransaction(t *testing.T) {
 	var err error
 	_, err = readfile("./testsdata/transaction/custom_token_privacy_transaction.json")
