77629ca3e4a44f59e310b37fb284b87f533b70ad
0xsirrush <thaibao@autonomous.nyc>
2019-11-21 11:43:50 +0700

    - getkey consensus role of validator

--- rpcserver/rpcinterface.go
+++ rpcserver/rpcinterface.go
@@ -155,6 +155,7 @@ var HttpHandler = map[string]httpHandler{
 	getChainMiningStatus:        (*HttpServer).handleGetChainMiningStatus,
 	getPublickeyMining:          (*HttpServer).handleGetPublicKeyMining,
 	getPublicKeyRole:            (*HttpServer).handleGetPublicKeyRole,
+	getRoleByValidatorKey:       (*HttpServer).handleGetValidatorKeyRole,
 	getIncognitoPublicKeyRole:   (*HttpServer).handleGetIncognitoPublicKeyRole,
 	getMinerRewardFromMiningKey: (*HttpServer).handleGetMinerRewardFromMiningKey,
 	getProducersBlackList:       (*HttpServer).handleGetProducersBlackList,
