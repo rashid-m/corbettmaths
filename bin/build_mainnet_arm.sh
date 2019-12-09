392fd7cc3e8ddd2d420a0896386a32d0c979b676
0xsirrush <thaibao@autonomous.nyc>
2019-12-09 15:01:21 +0700

    buil arm64 docker

--- bin/build_mainnet_arm.sh
+++ bin/build_mainnet_arm.sh
@@ -9,12 +9,12 @@ if [ -f ./bootnode ]; then
 fi
 
 
-env CGO_ENABLED=0 GOOS=linux GOARM=7	GOARCH=arm go build -ldflags '-w' -o incognito ../*.go
-env CGO_ENABLED=0 GOOS=linux GOARM=7	GOARCH=arm go build -ldflags '-w' -o bootnode ../bootnode/*.go
-env CGO_ENABLED=0 GOOS=linux GOARM=7	GOARCH=arm ENV=testnet go build -ldflags '-w' -o incognito-test ../tests/*.go
+env CGO_ENABLED=0 GOOS=linux GOARM=7	GOARCH=arm64 go build -ldflags '-w' -o incognito ../*.go
+env CGO_ENABLED=0 GOOS=linux GOARM=7	GOARCH=arm64 go build -ldflags '-w' -o bootnode ../bootnode/*.go
+env CGO_ENABLED=0 GOOS=linux GOARM=7	GOARCH=arm64 ENV=testnet go build -ldflags '-w' -o incognito-test ../tests/*.go
 cp ../keylist-mainnet.json ./keylist.json
 cp ../sample-config.conf .
 
 commit=`git show --summary --oneline | cut -d ' ' -f 1`
-docker build -f Dockerfile-mainnet --build-arg commit=$commit . -t incognitochain/incognito-mainnet:${tag} && docker push incognitochain/incognito-mainnet:${tag} && echo "Commit: $commit"
+docker build -f Dockerfile-mainnet --build-arg commit=$commit . -t incognitochain/incognito-mainnet-arm:${tag} && docker push incognitochain/incognito-mainnet-arm:${tag} && echo "Commit: $commit"
 docker rmi -f $(docker images --filter "dangling=true" -q)
