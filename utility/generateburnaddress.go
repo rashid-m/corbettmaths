aaee7a8c47aee21ea86646cdc5e276ed663b871d
0xkraken <hien.n@autonomous.nyc>
2019-12-26 16:13:32 +0700

    add public viewing key in burning address

--- utility/generateburnaddress.go
+++ utility/generateburnaddress.go
@@ -13,15 +13,19 @@ func main() {
 			burnPubKeyE := privacy.HashToPointFromIndex(int64(i), privacy.CStringBurnAddress)
 			burnPubKey := burnPubKeyE.ToBytesS()
 			if burnPubKey[len(burnPubKey) - 1] == 0 {
+				// generate public viewing key Vk = H(Pk)
+				viewKey := privacy.HashToPoint(burnPubKey).ToBytesS()
 				burnKey := wallet.KeyWallet{
 					KeySet: incognitokey.KeySet{
 						PaymentAddress: privacy.PaymentAddress{
 							Pk: burnPubKey,
+							Tk: viewKey,
 						},
 					},
 				}
 
 				burnPaymentAddress := burnKey.Base58CheckSerialize(wallet.PaymentAddressType)
+
 				fmt.Printf("Burning payment address : %s at index %d\n", burnPaymentAddress, i)
 				fmt.Println("======================================")
 				fmt.Printf("Burning public key bytes: %v\n", burnKey.KeySet.PaymentAddress.Pk)
@@ -30,11 +34,12 @@ func main() {
 			}
 		}
 	}
-
 }
 
 // result
-// Burning payment address : 15pGE5XZc7gsHSLgQxgZRVt7UsXBodiyTYe6xtvXipTFQPMpdQqtXhkzriL at index 68
-// ======================================
-// Burning public key bytes: [127 76 149 36 97 166 59 24 204 39 108 209 42 199 106 173 88 95 221 184 142 215 198 51 10 150 125 89 73 86 24 0]
-// ======================================
+//======================================
+//Burning payment address : 12RxahVABnAVCGP3LGwCn8jkQxgw7z1x14wztHzn455TTVpi1wBq9YGwkRMQg3J4e657AbAnCvYCJSdA9czBUNuCKwGSRQt55Xwz8WA at index 68
+//======================================
+//Burning public key bytes: [127 76 149 36 97 166 59 24 204 39 108 209 42 199 106 173 88 95 221 184 142 215 198 51 10 150 125 89 73 86 24 0]
+//======================================
+
