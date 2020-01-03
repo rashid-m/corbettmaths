a16ca9e9e451e36f6276bbcf8f627e9b4975e34e
0xbunyip <baoduy@autonomous.nyc>
2020-01-03 12:04:35 +0700

    Increase BlockRequester and ConnManager DialTimeout to 5s

--- peerv2/constants.go
+++ peerv2/constants.go
@@ -15,7 +15,7 @@ const (
 	UpdateHighwayListTimestep = 10 * time.Minute // RPC to update list of highways
 	RequesterDialTimestep     = 10 * time.Second // Check gRPC connection
 	MaxTimePerRequest         = 10 * time.Second // Time per request
-	DialTimeout               = 2 * time.Second  // Timeout for dialing's context
+	DialTimeout               = 5 * time.Second  // Timeout for dialing's context
 	RequesterKeepaliveTime    = 10 * time.Minute
 	RequesterKeepaliveTimeout = 20 * time.Second
 )
