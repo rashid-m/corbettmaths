ad4056bf3bfd77ccdae19462bd36a50bec3e43ff
0xsirrush <thaibao@autonomous.nyc>
2020-01-03 10:46:40 +0700

    - Change mainnet burning address at beacon heigh 150,500 and testnet at 250,000

--- blockchain/constants.go
+++ blockchain/constants.go
@@ -28,8 +28,8 @@ const (
 	LowerBoundPercentForIncDAO   = 3
 	UpperBoundPercentForIncDAO   = 10
 	GetValidBlock                = 20
-	TestRandom                   = true
-	NumberOfFixedBlockValidators = 4
+	TestRandom                   = false
+	NumberOfFixedBlockValidators = 22
 )
 
 // burning addresses
@@ -147,7 +147,7 @@ func init() {
 		panic(err)
 	}
 
-	var IsTestNet = true
+	var IsTestNet = false
 	if IsTestNet {
 		for i := 0; i < TestNetMinBeaconCommitteeSize; i++ {
 			PreSelectBeaconNodeTestnetSerializedPubkey = append(PreSelectBeaconNodeTestnetSerializedPubkey, keylist.Beacon[i].CommitteePublicKey)
