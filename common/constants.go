27f2fe96354f128321d544922b51ed01829703e7
0xmerman <hung@autonomous.nyc>
2020-02-08 03:23:48 +0700

    params: update max shard number for testing

--- common/constants.go
+++ common/constants.go
@@ -75,7 +75,7 @@ const (
 	ValidatorRole  = "validator"
 	PendingRole    = "pending"
 	WaitingRole    = "waiting"
-	MaxShardNumber = 8
+	MaxShardNumber = 2
 
 	BlsConsensus    = "bls"
 	BridgeConsensus = "dsa"
