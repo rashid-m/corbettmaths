d86f2eb1a063d0769039265e75f8b50a0f7c1832
0xsirrush <thaibao@autonomous.nyc>
2020-02-13 11:22:54 +0700

    - fix error code - withdraw reward with multi tokens - update RPC get balance of bridge token(in case use pri-key)

--- blockchain/constants.go
+++ blockchain/constants.go
@@ -28,8 +28,8 @@ const (
 	LowerBoundPercentForIncDAO    = 3
 	UpperBoundPercentForIncDAO    = 10
 	GetValidBlock                 = 20
-	TestRandom                    = true
-	NumberOfFixedBlockValidators  = 4
+	TestRandom                    = false
+	NumberOfFixedBlockValidators  = 22
 	BEACON_ID                     = -1         // CommitteeID of beacon chain, used for highway
 	ValidateTimeForSpamRequestTxs = 1581565837 // GMT: Thursday, February 13, 2020 3:50:37 AM. From this time, block will be checked spam request-reward tx
 )
@@ -149,7 +149,7 @@ func init() {
 		panic(err)
 	}
 
-	var IsTestNet = true
+	var IsTestNet = false
 	if IsTestNet {
 		for i := 0; i < TestNetMinBeaconCommitteeSize; i++ {
 			PreSelectBeaconNodeTestnetSerializedPubkey = append(PreSelectBeaconNodeTestnetSerializedPubkey, keylist.Beacon[i].CommitteePublicKey)
