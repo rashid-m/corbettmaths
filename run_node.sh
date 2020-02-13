cb447562b8c5407071a791d108cc8644fd6b0e23
0xmerman <hung@autonomous.nyc>
2020-02-13 23:25:28 +0700

    script: update

--- run_node.sh
+++ run_node.sh
@@ -73,7 +73,7 @@ fi
 if [ "$1" == "fullnode-testnet-b" ]; then
 GO111MODULE=on GETH_NAME=kovan.infura.io/v3/1138a1e99b154b10bae5c382ad894361 GETH_PROTOCOL=https GETH_PORT="" ./incognito --testnet true --nodemode "relay" --relayshards "all" --externaladdress "127.0.0.1:9533" --enablewallet --wallet "wallet" --walletpassphrase "12345678" --walletautoinit --norpcauth --datadir "../testnet/fullnode" --discoverpeersaddress "testnet-bootnode.incognito.org:9330" --norpcauth --rpclisten "0.0.0.0:7334" --rpcwslisten "127.0.0.1:17338" > ../testnet/log.txt 2> ../testnet/error_log.txt &
 fi
-if [ "$1" == "fullnode-testnet-b" ]; then
+if [ "$1" == "fullnode-testnet-b5" ]; then
 GO111MODULE=on GETH_NAME=kovan.infura.io/v3/1138a1e99b154b10bae5c382ad894361 GETH_PROTOCOL=https GETH_PORT="" ./incognito --testnet true --nodemode "relay" --relayshards "[5]" --externaladdress "127.0.0.1:9534" --enablewallet --wallet "wallet" --walletpassphrase "12345678" --walletautoinit --norpcauth --datadir "../testnet/shard5" --discoverpeersaddress "testnet-bootnode.incognito.org:9330" --norpcauth --rpclisten "0.0.0.0:5334" --rpcwslisten "127.0.0.1:15338" > ../testnet/shard5/log.txt 2> ../testnet/shard5/error_log.txt &
 fi
 if [ "$1" == "fullnode-mainnet" ]; then
