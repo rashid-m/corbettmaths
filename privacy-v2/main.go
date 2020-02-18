c578fafb382901d58b76c907455b9b550c0d440d
anpham-incognito <an@incognito.org>
2020-02-18 16:59:42 +0700

    can now check a utxo output belong to an address

--- privacy-v2/main.go
+++ privacy-v2/main.go
@@ -1,19 +1,91 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"math/big"
 
 	"github.com/incognitochain/incognito-chain/privacy-v2/mlsag"
-	C25519 "github.com/incognitochain/incognito-chain/privacy/curve25519"
+	"github.com/incognitochain/incognito-chain/privacy-v2/onetime_address"
+	ota "github.com/incognitochain/incognito-chain/privacy-v2/onetime_address"
+
+	"github.com/incognitochain/incognito-chain/common"
+	"github.com/incognitochain/incognito-chain/privacy"
+	"github.com/incognitochain/incognito-chain/privacy-v2/onetime_address/address"
 )
 
-func main() {
+func testMlsag() {
 	fmt.Println("Running test")
-	keyInputs := []C25519.Key{}
-	for i := 0; i < 3; i += 1 {
-		privateKey := C25519.RandomScalar()
+	keyInputs := []privacy.Scalar{}
+	for i := 0; i < 8; i += 1 {
+		privateKey := privacy.RandomScalar()
 		keyInputs = append(keyInputs, *privateKey)
 	}
-	signature := mlsag.SignCore(keyInputs, "Hello", 5)
+	numFake := 3
+	pi := common.RandInt() % numFake
+	ring := mlsag.NewRandomRing(keyInputs, numFake, pi)
+	signer := mlsag.NewMlsagWithDefinedRing(keyInputs, ring, pi, numFake)
+
+	signature, err := signer.Sign("Hello")
+	if err != nil {
+		fmt.Println("There is something wrong with signing")
+		fmt.Println(err)
+	}
+	// ring = mlsag.NewRandomRing(keyInputs, numFake, pi)
+	check, err := mlsag.Verify(signature, ring, "Hello")
+	if err != nil {
+		fmt.Println("There is something wrong with verifying")
+		fmt.Println(err)
+	}
+	fmt.Println("Check signature:")
+	fmt.Println(check)
+
+	b, err := signature.ToHex()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Printing signature")
 	fmt.Println(signature)
+	fmt.Println("==============")
+	fmt.Println("Printing signature hex")
+	fmt.Println(b)
+	sig, _ := new(mlsag.Signature).FromHex(b)
+
+	fmt.Println("==============")
+	fmt.Println("Printing signature from hex")
+	fmt.Println(sig)
+
+	fmt.Println("==============")
+	fmt.Println("Checking 2 signatures are the same")
+	b1, _ := sig.ToBytes()
+	b2, _ := signature.ToBytes()
+	fmt.Println(bytes.Equal(b1, b2))
+}
+
+func main() {
+	// aliceAddress := address.GenerateRandomAddress()
+
+	const n int = 10
+	money := make([]*big.Int, n)
+	peopleAddresses := make([]*address.PrivateAddress, n)
+	peoplePublicAddresses := make([]*address.PublicAddress, n)
+	for i := 0; i < n; i += 1 {
+		money[i], _ = new(big.Int).SetString("100", 10)
+		peopleAddresses[i] = address.GenerateRandomAddress()
+		peoplePublicAddresses[i] = peopleAddresses[i].GetPublicAddress()
+	}
+	outputs, err := onetime_address.CreateOutputs(peoplePublicAddresses, money)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := 0; i < len(outputs); i += 1 {
+		fmt.Println("===========")
+		fmt.Println(ota.IsUtxoOfAddress(peopleAddresses[i], outputs[i]))
+		break
+		// nxt := (i + 1) % len(outputs)
+		// fmt.Println(ota.IsUtxoOfAddress(peopleAddresses[i], outputs[nxt]))
+	}
 }
