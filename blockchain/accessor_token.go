6f866a10b7dc2f44a37b7375f2bf08ceff66176c
0xmerman <hung@autonomous.nyc>
2020-02-18 15:10:30 +0700

    hotfix: withdraw reward metadata

--- blockchain/accessor_token.go
+++ blockchain/accessor_token.go
@@ -1,7 +1,9 @@
 package blockchain
 
 import (
+	"encoding/json"
 	"github.com/incognitochain/incognito-chain/common"
+	"github.com/incognitochain/incognito-chain/dataaccessobject/rawdb"
 	"github.com/incognitochain/incognito-chain/dataaccessobject/statedb"
 )
 
@@ -22,10 +24,37 @@ func (blockchain *BlockChain) GetAllCoinIDV2(shardID byte) ([]common.Hash, error
 	for k, _ := range tokenStates {
 		tokenIDs = append(tokenIDs, k)
 	}
+	brigdeTokenIDs, _, err := blockchain.GetAllBridgeTokens()
+	if err != nil {
+		return nil, err
+	}
+	for _, bridgeTokenID := range brigdeTokenIDs {
+		if _, found := tokenStates[bridgeTokenID]; !found {
+			tokenIDs = append(tokenIDs, bridgeTokenID)
+		}
+	}
 	return tokenIDs, nil
 }
 
 // Check Privacy Custom token ID is existed
 func (blockchain *BlockChain) PrivacyCustomTokenIDExistedV2(tokenID *common.Hash, shardID byte) bool {
 	return statedb.PrivacyTokenIDExisted(blockchain.BestState.Shard[shardID].GetCopiedTransactionStateDB(), *tokenID)
+}
+
+func (blockchain *BlockChain) GetAllBridgeTokens() ([]common.Hash, []*rawdb.BridgeTokenInfo, error) {
+	bridgeTokenIDs := []common.Hash{}
+	allBridgeTokens := []*rawdb.BridgeTokenInfo{}
+	bridgeStateDB := blockchain.BestState.Beacon.GetCopiedFeatureStateDB()
+	allBridgeTokensBytes, err := statedb.GetAllBridgeTokens(bridgeStateDB)
+	if err != nil {
+		return bridgeTokenIDs, allBridgeTokens, err
+	}
+	err = json.Unmarshal(allBridgeTokensBytes, &allBridgeTokens)
+	if err != nil {
+		return bridgeTokenIDs, allBridgeTokens, err
+	}
+	for _, bridgeTokenInfo := range allBridgeTokens {
+		bridgeTokenIDs = append(bridgeTokenIDs, *bridgeTokenInfo.TokenID)
+	}
+	return bridgeTokenIDs, allBridgeTokens, nil
 }
