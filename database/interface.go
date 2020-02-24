fe6e75ccf608bef0aba27b62f495e2b3a7cef53b
HoangNguyen <hoang@incognito.org>
2020-02-24 11:56:41 +0700

    fix return data

--- database/interface.go
+++ database/interface.go
@@ -204,7 +204,7 @@ type DatabaseInterface interface {
 	GetAllRecordsPortalByPrefix(beaconHeight uint64, prefix []byte) ([][]byte, [][]byte, error)
 	TrackCustodianDepositCollateral(key []byte, value []byte) error
 	TrackReqPTokens(key []byte, value []byte) error
-	GetItemPortalByPrefix(prefix []byte) ([]byte, error)
+	GetItemPortalByPrefix(prefix []byte) (byte, error)
 	StorePortingRequestItem(key []byte, value interface{}) error
 	StoreExchangeRatesRequestItem(key []byte, value interface{}) error
 	StoreFinalExchangeRatesItem(key []byte, value interface{}) error
