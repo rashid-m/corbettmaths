25aefffddc07ca6dfd2ffb3a272e63af5b1b2e96
0xsirrush <thaibao@autonomous.nyc>
2020-03-04 11:35:44 +0700

    - remove not use code

--- peerv2/wrapper/wrapper.go
+++ peerv2/wrapper/wrapper.go
@@ -3,9 +3,7 @@ package wrapper
 import (
 	"encoding/json"
 	"runtime"
-	"time"
 
-	"github.com/incognitochain/incognito-chain/common"
 	"github.com/klauspost/compress/zstd"
 )
 
@@ -19,7 +17,7 @@ func init() {
 
 // EnCom: encode an interface{} to bytes and compress to shorted bytes slice
 func EnCom(data interface{}) ([]byte, error) {
-	s := time.Now()
+	//s := time.Now()
 	// var buf bytes.Buffer
 	// e := gob.NewEncoder(&buf)
 	// err := e.Encode(data)
@@ -27,15 +25,15 @@ func EnCom(data interface{}) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	Logger.Infof("[stream] Time Encode %v", time.Since(s).Seconds())
-	s = time.Now()
+	//Logger.Infof("[stream] Time Encode %v", time.Since(s).Seconds())
+	//s = time.Now()
 	// compresser, err := zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedBestCompression))
 	// if err != nil {
 	// 	return nil, err
 	// }
 	res := compresser.EncodeAll(b, nil)
-	Logger.Infof("[stream] Time Compress %v", time.Since(s).Seconds())
-	Logger.Infof("[stream] Time %v, Len encode %v len compress %v Ratio %v", time.Since(s).Seconds(), len(b), len(res), float64(len(b))/float64(len(res)))
+	//Logger.Debugf("[stream] Time Compress %v", time.Since(s).Seconds())
+	//Logger.Debugf("[stream] Time %v, Len encode %v len compress %v Ratio %v", time.Since(s).Seconds(), len(b), len(res), float64(len(b))/float64(len(res)))
 	return res, nil
 }
 
@@ -53,27 +51,5 @@ func DeCom(data []byte, out interface{}) error {
 	// d := gob.NewDecoder(buf)
 
 	err = json.Unmarshal(rawdata, out) //d.Decode(out)
-	return err
-}
-
-// EnCom: encode an interface{} to bytes and compress to shorted bytes slice
-func OldEnCom(data interface{}) ([]byte, error) {
-	s := time.Now()
-	jsonBytes, err := json.Marshal(data)
-	if err != nil {
-		return nil, err
-	}
-	Logger.Infof("[stream] Time Encode %v", time.Since(s).Seconds())
-	s = time.Now()
-	messageBytes, err := common.GZipFromBytes(jsonBytes)
-	Logger.Infof("[stream] Time Compress %v", time.Since(s).Seconds())
-	Logger.Infof("[stream] Time %v, Len encode %v len compress %v Ratio %v", time.Since(s).Seconds(), len(jsonBytes), len(messageBytes), float64(len(jsonBytes))/float64(len(messageBytes)))
-	return messageBytes, nil
-}
-
-// DeCom: decode bytes to an interface{}
-func OldDeCom(data []byte, out interface{}) error {
-	jsonDecodeBytes, err := common.GZipToBytes(data)
-	err = json.Unmarshal([]byte(jsonDecodeBytes), out)
 	return err
 }
