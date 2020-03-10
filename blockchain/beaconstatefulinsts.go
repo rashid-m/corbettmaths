855619b048083da3053e37db9a8e61c0a412ebfa
HoangNguyen <hoang@incognito.org>
2020-03-10 11:08:50 +0700

    add instruct min aspect ratio

--- blockchain/beaconstatefulinsts.go
+++ blockchain/beaconstatefulinsts.go
@@ -790,6 +790,7 @@ func (blockchain *BlockChain) autoCheckAndCreatePortalLiquidationInsts(
 
 
 	// case 2: check collateral's value (locked collateral amount) drops below MinRatio
+	buildMinAspectRatioCollateralLiquidationInst()
 	return [][]string{}, nil
 
 }
