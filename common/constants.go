35672b35f7bba98f016da2f966c6c9caccb77d8c
0xsirrush <thaibao@autonomous.nyc>
2020-03-19 16:34:02 +0700

    - refactor

--- common/constants.go
+++ common/constants.go
@@ -134,6 +134,10 @@ const (
 	PDEWithdrawalRejectedChainStatus = "rejected"
 )
 
+const (
+	HexEmptyRoot = "56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
+)
+
 var (
-	EmptyRoot = HexToHash("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
+	EmptyRoot = HexToHash(HexEmptyRoot)
 )
