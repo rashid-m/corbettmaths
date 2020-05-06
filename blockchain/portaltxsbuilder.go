fe0b4b91c97b208cdb66a55acab5114c3cb8b491
minhduccm <minhduccm90@gmail.com>
2020-05-06 08:43:40 +0700

    Refund fee for rejected porting request

--- blockchain/portaltxsbuilder.go
+++ blockchain/portaltxsbuilder.go
@@ -315,7 +315,7 @@ func (blockGenerator *BlockGenerator) buildPortalRejectedRedeemRequestTx(
 		return nil, nil
 	}
 	if redeemReqContent.ShardID != shardID {
-		Logger.log.Errorf("ERROR: ShardID unexpected expect %v, but got %+v", shardID, redeemReqContent.ShardID)
+		Logger.log.Errorf("ERROR: unexpected ShardID, expect %v, but got %+v", shardID, redeemReqContent.ShardID)
 		return nil, nil
 	}
 
@@ -377,7 +377,7 @@ func (blockGenerator *BlockGenerator) buildPortalRejectedRedeemRequestTx(
 		return nil, initErr
 	}
 
-	Logger.log.Errorf("Suucc: %+v", err)
+	Logger.log.Info("[Shard buildPortalRejectedRedeemRequestTx] Finished...")
 	return resTx, nil
 }
 
@@ -524,4 +524,52 @@ func (blockGenerator *BlockGenerator) buildPortalAcceptedWithdrawRewardTx(
 		}
 		return resTx, nil
 	}
+}
+
+// buildPortalRefundPortingFeeTx builds portal refund porting fee tx
+func (blockGenerator *BlockGenerator) buildPortalRefundPortingFeeTx(
+	contentStr string,
+	producerPrivateKey *privacy.PrivateKey,
+	shardID byte,
+) (metadata.Transaction, error) {
+	Logger.log.Info("[Portal refund porting fee] Starting...")
+	contentBytes := []byte(contentStr)
+	var portalPortingRequest metadata.PortalPortingRequestContent
+	err := json.Unmarshal(contentBytes, &portalPortingRequest)
+	if err != nil {
+		Logger.log.Errorf("ERROR: an error occured while unmarshaling portal porting request content: %+v", err)
+		return nil, nil
+	}
+	if portalPortingRequest.ShardID != shardID {
+		return nil, nil
+	}
+
+	meta := metadata.NewPortalFeeRefundResponse(
+		common.PortalPortingRequestRejectedChainStatus,
+		portalPortingRequest.TxReqID,
+		metadata.PortalPortingResponseMeta,
+	)
+
+	keyWallet, err := wallet.Base58CheckDeserialize(portalPortingRequest.IncogAddressStr)
+	if err != nil {
+		Logger.log.Errorf("ERROR: an error occured while deserializing receiver address string: %+v", err)
+		return nil, nil
+	}
+	receiverAddr := keyWallet.KeySet.PaymentAddress
+
+	// the returned currency is PRV
+	resTx := new(transaction.Tx)
+	err = resTx.InitTxSalary(
+		portalPortingRequest.PortingFee,
+		&receiverAddr,
+		producerPrivateKey,
+		blockGenerator.chain.BestState.Shard[shardID].GetCopiedTransactionStateDB(),
+		meta,
+	)
+	if err != nil {
+		Logger.log.Errorf("ERROR: an error occured while initializing portal refund porting fee tx: %+v", err)
+		return nil, nil
+	}
+	Logger.log.Info("[Portal refund porting fee] Finished...")
+	return resTx, nil
 }
