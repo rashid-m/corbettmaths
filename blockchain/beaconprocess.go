91fc4271df91fcbdf11c651e192581aae988aa97
0xsirrush <thaibao@autonomous.nyc>
2020-05-25 11:41:27 +0700

    - deploy testnet portal v2

--- blockchain/beaconprocess.go
+++ blockchain/beaconprocess.go
@@ -1340,12 +1340,12 @@ func (blockchain *BlockChain) processStoreBeaconBlock(
 	}
 
 	// execute, store Portal Instruction
-	if (blockchain.config.ChainParams.Net == Mainnet) || (blockchain.config.ChainParams.Net == Testnet && beaconBlock.Header.Height > 1500000) {
-		err = blockchain.processPortalInstructions(newBestState.featureStateDB, beaconBlock)
-		if err != nil {
-			return NewBlockChainError(ProcessPortalInstructionError, err)
-		}
+	//if (blockchain.config.ChainParams.Net == Mainnet) || (blockchain.config.ChainParams.Net == Testnet && beaconBlock.Header.Height > 1500000) {
+	err = blockchain.processPortalInstructions(newBestState.featureStateDB, beaconBlock)
+	if err != nil {
+		return NewBlockChainError(ProcessPortalInstructionError, err)
 	}
+	//}
 
 	// execute, store Ralaying Instruction
 	err = blockchain.processRelayingInstructions(beaconBlock)
