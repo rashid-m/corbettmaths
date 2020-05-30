d01fb1db865f7917fdc646b163cfcee80e6e8d8f
0xkraken <hien.n@autonomous.nyc>
2020-05-30 10:35:00 +0700

    remove checkpoint portal

--- blockchain/beaconprocess.go
+++ blockchain/beaconprocess.go
@@ -1320,11 +1320,11 @@ func (blockchain *BlockChain) processStoreBeaconBlock(beaconBlock *BeaconBlock, 
 	}
 
 	// execute, store
-	if (blockchain.config.ChainParams.Net == Mainnet) || (blockchain.config.ChainParams.Net == Testnet && beaconBlock.Header.Height > 1483000) {
-		err = blockchain.processPortalInstructions(tempBeaconBestState.featureStateDB, beaconBlock)
-		if err != nil {
-			return NewBlockChainError(ProcessPortalInstructionError, err)
-		}
+	//if (blockchain.config.ChainParams.Net == Mainnet) || (blockchain.config.ChainParams.Net == Testnet && beaconBlock.Header.Height > 1483000) {
+	//}
+	err = blockchain.processPortalInstructions(tempBeaconBestState.featureStateDB, beaconBlock)
+	if err != nil {
+		return NewBlockChainError(ProcessPortalInstructionError, err)
 	}
 
 	// execute, store
