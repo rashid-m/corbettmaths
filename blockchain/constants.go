fc6a4192d2a6d7bcae34b651db5138eda0eed95c
hung <hung@autnomous.nyc>
2020-06-19 11:59:16 +0700

    cleancode: hash

--- blockchain/constants.go
+++ blockchain/constants.go
@@ -2,6 +2,7 @@ package blockchain
 
 import (
 	"encoding/json"
+	"github.com/incognitochain/incognito-chain/metrics"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -217,4 +218,22 @@ const (
 	StakeAction   = "stake"
 	AssignAction  = "assign"
 	StopAutoStake = "stopautostake"
+)
+
+var (
+	shardInsertBlockTimer                  = metrics.NewRegisteredTimer("shard/insert", nil)
+	shardVerifyPreprocesingTimer           = metrics.NewRegisteredTimer("shard/verify/preprocessing", nil)
+	shardVerifyPreprocesingForPreSignTimer = metrics.NewRegisteredTimer("shard/verify/preprocessingpresign", nil)
+	shardVerifyWithBestStateTimer          = metrics.NewRegisteredTimer("shard/verify/withbeststate", nil)
+	shardVerifyPostProcessingTimer         = metrics.NewRegisteredTimer("shard/verify/postprocessing", nil)
+	shardStoreBlockTimer                   = metrics.NewRegisteredTimer("shard/storeblock", nil)
+	shardUpdateBestStateTimer              = metrics.NewRegisteredTimer("shard/updatebeststate", nil)
+
+	beaconInsertBlockTimer                  = metrics.NewRegisteredTimer("beacon/insert", nil)
+	beaconVerifyPreprocesingTimer           = metrics.NewRegisteredTimer("beacon/verify/preprocessing", nil)
+	beaconVerifyPreprocesingForPreSignTimer = metrics.NewRegisteredTimer("beacon/verify/preprocessingpresign", nil)
+	beaconVerifyWithBestStateTimer          = metrics.NewRegisteredTimer("beacon/verify/withbeststate", nil)
+	beaconVerifyPostProcessingTimer         = metrics.NewRegisteredTimer("beacon/verify/postprocessing", nil)
+	beaconStoreBlockTimer                   = metrics.NewRegisteredTimer("beacon/storeblock", nil)
+	beaconUpdateBestStateTimer              = metrics.NewRegisteredTimer("beacon/updatebeststate", nil)
 )
