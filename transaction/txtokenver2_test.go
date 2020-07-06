d5755f29e965dbad862550d30eba615ddf25aad1
darius <datle@incognito.org>
2020-07-06 17:42:18 +0700

    refractor; add test for TxTokenSalary (ongoing)

--- transaction/txtokenver2_test.go
+++ transaction/txtokenver2_test.go
@@ -34,6 +34,11 @@ import (
 // 	trie.Logger.Init(common.NewBackend(nil).Logger("test", true))
 // 	return
 // }()
+var (
+	hasPrivacyForPRV bool = true
+	hasPrivacyForToken bool = true
+	shardID byte = byte(0)
+)
 
 func TestInitTxPrivacyToken(t *testing.T) {
 	for loop := 0; loop < numOfLoops; loop++ {
@@ -48,59 +53,24 @@ func TestInitTxPrivacyToken(t *testing.T) {
 			tempCoin,err := coin.NewCoinFromPaymentInfo(paymentInfo[i%len(dummyPrivateKeys)])
 			assert.Equal(t, nil, err)
 			assert.Equal(t, false, tempCoin.IsEncrypted())
-
-// to obtain a PlainCoin to feed into input of TX, we need to conceal & decrypt it (it makes sure all fields are right, as opposed to just casting the type to PlainCoin)
 			tempCoin.ConcealOutputCoin(keySets[i%len(dummyPrivateKeys)].PaymentAddress.GetPublicView())
 			assert.Equal(t, true, tempCoin.IsEncrypted())
 			assert.Equal(t, true, tempCoin.GetSharedRandom() == nil)
 			pastCoins[i] = tempCoin
 		}
 
-// use the db's interface to write our simulated pastCoins to the database
-// we do need to re-format the data into bytes first
-		forceSaveCoins(dummyDB, pastCoins, 0, common.PRVCoinID,t)
+		// store a bunch of sample OTA coins in PRV
+		forceSaveCoins(dummyDB, pastCoins, 0, common.PRVCoinID, t)
 
-		// message to receiver
+		// sample message to receiver
 		msgCipherText := []byte("haha dummy ciphertext")
-		// receiverTK, _ := new(privacy.Point).FromBytesS(keySets[0].PaymentAddress.Tk)
-
-		initAmount := uint64(10000)
-		tokenPayments := []*privacy.PaymentInfo{{PaymentAddress: keySets[0].PaymentAddress, Amount: initAmount, Message: msgCipherText}}
-
-		myOnlyInputCoin,err := pastCoins[0].Decrypt(keySets[0])
-		inputCoinsPRV := []coin.PlainCoin{myOnlyInputCoin}
-		paymentInfoPRV := []*privacy.PaymentInfo{key.InitPaymentInfo(keySets[0].PaymentAddress,uint64(15000),[]byte("test out"))}
-
-		// token param for init new token
-		tokenParam := &TokenParam{
-			PropertyID:     "",
-			PropertyName:   "Token 1",
-			PropertySymbol: "T1",
-			Amount:         initAmount,
-			TokenTxType:    CustomTokenInit,
-			Receiver:       tokenPayments,
-			TokenInput:     []coin.PlainCoin{},
-			Mintable:       false,
-			Fee:            0,
-		}
-
-		hasPrivacyForPRV := true
-		hasPrivacyForToken := true
-		shardID := byte(0)
-		paramToCreateTx := NewTxTokenParams(&keySets[0].PrivateKey,
-			paymentInfoPRV, inputCoinsPRV, 1000, tokenParam, dummyDB, nil,
-			hasPrivacyForPRV, hasPrivacyForToken, shardID, []byte{},nil)
-
-		// init tx
+		paramToCreateTx,tokenParam := getParamsForTxTokenInit(pastCoins[0], dummyDB)
+		// create tx for token init
 		tx := &TxTokenVersion2{}
+		
 		err = tx.Init(paramToCreateTx)
-		// fmt.Println(err)
 		assert.Equal(t, nil, err)
 
-		// assert.Equal(t, len(msgCipherText), 
-		inf := tx.TxTokenData.TxNormal.GetProof().GetOutputCoins()[0].GetInfo()
-
-		assert.Equal(t,true,bytes.Equal([]byte(inf),msgCipherText))
 		// convert to JSON string and revert
 		txJsonString := tx.JSONString()
 		txHash := tx.Hash()
@@ -109,112 +79,86 @@ func TestInitTxPrivacyToken(t *testing.T) {
 		txHash1 := tx1.Hash()
 		assert.Equal(t, txHash, txHash1)
 
-		// // get actual tx size
+		// size checks
 		txActualSize := tx.GetTxActualSize()
 		assert.Greater(t, txActualSize, uint64(0))
-		// txPrivacyTokenActualSize := tx.GetTxPrivacyTokenActualSize()
-		// assert.Greater(t, txPrivacyTokenActualSize, uint64(0))
+		sigPubKey := tx.GetSigPubKey()
+		assert.Equal(t, common.SigPubKeySize, len(sigPubKey))
+		// param checks
+		inf := tx.GetTxTokenData().TxNormal.GetProof().GetOutputCoins()[0].GetInfo()
+		assert.Equal(t,true,bytes.Equal([]byte(inf),msgCipherText))
 		retrievedFee := tx.GetTxFee()
 		assert.Equal(t, uint64(1000),retrievedFee)
-		err = tx.ValidateTxWithBlockChain(nil, nil, nil, shardID, dummyDB)
-		assert.Equal(t, nil, err)
+		theAmount := tx.GetTxTokenData().GetAmount()
+		assert.Equal(t, tokenParam.Amount, theAmount)
+		isUniqueReceiver, _, uniqueAmount, tokenID := tx.GetTransferData()
+		assert.Equal(t, true, isUniqueReceiver)
+		assert.Equal(t, tokenParam.Amount, uniqueAmount)
+		assert.Equal(t, tx.GetTokenID(), tokenID)
 
+		// sanity check
 		isValidSanity, err := tx.ValidateSanityData(nil, nil, nil, 0)
 		assert.Equal(t, true, isValidSanity)
 		assert.Equal(t, nil, err)
+		// validate signatures, proofs, etc. Only do after sanity checks are passed
 		isValidTxItself, err := tx.ValidateTxByItself(hasPrivacyForPRV, dummyDB, nil, nil, shardID, false, nil, nil)
 		assert.Equal(t, true, isValidTxItself)
 		assert.Equal(t, nil, err)
+		// check double spend using `blockchain data` in this db
+		err = tx.ValidateTxWithBlockChain(nil, nil, nil, shardID, dummyDB)
+		assert.Equal(t, nil, err)
 
-		// testTxTokenV2DeletedProof(tx,t)
-
-		theAmount := tx.GetTxTokenData().GetAmount()
-
-		assert.Equal(t, initAmount, theAmount)
-
-		isUniqueReceiver, _, uniqueAmount, tokenID := tx.GetTransferData()
-		assert.Equal(t, true, isUniqueReceiver)
-		assert.Equal(t, initAmount, uniqueAmount)
-		assert.Equal(t, tx.GetTokenID(), tokenID)
-
-		sigPubKey := tx.GetSigPubKey()
-		assert.Equal(t, common.SigPubKeySize, len(sigPubKey))
-
-		// save the token-init tx's effects into the db
+		// save the fee outputs into the db
 		// get output coin token from tx
 		tokenOutputs := tx.GetTxTokenData().TxNormal.GetProof().GetOutputCoins()
 		feeOutputs := tx.GetTxBase().GetProof().GetOutputCoins()
-		forceSaveCoins(dummyDB, tokenOutputs, 0, *tx.GetTokenID(), t)
 		forceSaveCoins(dummyDB, feeOutputs, 0, common.PRVCoinID, t)
-		// err = statedb.StorePrivacyTokenTx(dummyDB,*tx.GetTokenID(), *tx.Hash())
-		statedb.StorePrivacyToken(dummyDB, *tx.GetTokenID(), tokenParam.PropertyName, tokenParam.PropertySymbol, statedb.InitToken, tokenParam.Mintable, tokenParam.Amount, []byte{}, *tx.Hash())
-		assert.Equal(t,nil,err)
-		statedb.StoreCommitments(dummyDB,*tx.GetTokenID(), [][]byte{tokenOutputs[0].GetCommitment().ToBytesS()}, shardID)
-
-		// token check
-		exists := statedb.PrivacyTokenIDExisted(dummyDB,*tx.GetTokenID())
-		// val,exists := tokenMap[*tx.GetTokenID()]
-		fmt.Printf("Token created : %v\n",exists)
-		assert.Equal(t, true, exists)
-
-		transferAmount := uint64(69)
-		paymentInfo2 := []*privacy.PaymentInfo{{PaymentAddress: keySets[1].PaymentAddress, Amount: transferAmount, Message: msgCipherText}}
-
-		prvCoinsToPayTransfer := make([]coin.PlainCoin,0)
-		tokenCoinsToTransfer := make([]coin.PlainCoin,0)
-		for _,c := range feeOutputs{
-			pc,_ := c.Decrypt(keySets[0])
-			// s,_ := json.Marshal(pc.(*coin.CoinV2))
-			fmt.Printf("Tx Fee : %x has received %d in PRV\n",pc.GetPublicKey().ToBytesS(),pc.GetValue())
-			prvCoinsToPayTransfer = append(prvCoinsToPayTransfer,pc)
-		}
-		for _,c := range tokenOutputs{
-			pc,err := c.Decrypt(keySets[0])
-			// s,_ := json.Marshal(pc.(*coin.CoinV2))
-			fmt.Printf("Tx Token : %x has received %d in token T1\n",pc.GetPublicKey().ToBytesS(),pc.GetValue())
-			assert.Equal(t,nil,err)
-			tokenCoinsToTransfer = append(tokenCoinsToTransfer,pc)
-		}
-		// // token param for transfer token
-		tokenParam2 := &TokenParam{
-			PropertyID:     tx.GetTokenID().String(),
-			PropertyName:   "Token 1",
-			PropertySymbol: "T1",
-			Amount:         transferAmount,
-			TokenTxType:    CustomTokenTransfer,
-			Receiver:       paymentInfo2,
-			TokenInput:     tokenCoinsToTransfer,
-			Mintable:       false,
-			Fee:            0,
-		}
-
-		paramToCreateTx2 := NewTxTokenParams(&keySets[0].PrivateKey,
-			[]*key.PaymentInfo{}, prvCoinsToPayTransfer, 15, tokenParam2, dummyDB, nil,
-			hasPrivacyForPRV, hasPrivacyForToken, shardID, []byte{},nil)
-
-		// // init tx
+		
+		// tx token transfer
+		paramToCreateTx2, tokenParam2 := getParamForTxTokenTransfer(tx, dummyDB, t)
+		_ = tokenParam2
 		tx2 := &TxTokenVersion2{}
+
 		err = tx2.Init(paramToCreateTx2)
-		// fmt.Println(err)
+		// should fail because db does not have this token yet
+		assert.NotEqual(t, nil, err)
+		// store the token
+		exists := statedb.PrivacyTokenIDExisted(dummyDB,*tx.GetTokenID())
+		assert.Equal(t, false, exists)
+		statedb.StorePrivacyToken(dummyDB, *tx.GetTokenID(), tokenParam.PropertyName, tokenParam.PropertySymbol, statedb.InitToken, tokenParam.Mintable, tokenParam.Amount, []byte{}, *tx.Hash())
+
+		statedb.StoreCommitments(dummyDB,*tx.GetTokenID(), [][]byte{tokenOutputs[0].GetCommitment().ToBytesS()}, shardID)
+		// check it exists
+		exists = statedb.PrivacyTokenIDExisted(dummyDB,*tx.GetTokenID())
+		assert.Equal(t, true, exists)
+		err = tx2.Init(paramToCreateTx2)
+		// still fails because the token's `init` coin (10000 T1) is not stored yet
+		assert.NotEqual(t, nil, err)
+		// add the coin. Tx creation shouldsucceed  now
+		forceSaveCoins(dummyDB, tokenOutputs, 0, *tx.GetTokenID(), t)
+		err = tx2.Init(paramToCreateTx2)
 		assert.Equal(t, nil, err)
 
+		msgCipherText = []byte("doing a transfer")
 		assert.Equal(t, true, bytes.Equal(msgCipherText, tx2.GetTxTokenData().TxNormal.GetProof().GetOutputCoins()[0].GetInfo()))
-
-
-		err = tx2.ValidateTxWithBlockChain(nil, nil, nil, shardID, dummyDB)
-		assert.Equal(t, nil, err)
 
 		isValidSanity, err = tx2.ValidateSanityData(nil, nil, nil, 0)
 		assert.Equal(t, true, isValidSanity)
 		assert.Equal(t, nil, err)
 
+		// before the token init tx is written into db, this should not pass
 		isValidTxItself, err = tx2.ValidateTxByItself(hasPrivacyForPRV, dummyDB, nil, nil, shardID, false, nil, nil)
 		assert.Equal(t, true, isValidTxItself)
+		assert.Equal(t, nil, err)
+		
+		err = tx2.ValidateTxWithBlockChain(nil, nil, nil, shardID, dummyDB)
 		assert.Equal(t, nil, err)
 		
 		testTxTokenV2InvalidFee(dummyDB, paramToCreateTx2, t)
 		testTxTokenV2OneFakeOutput(tx2, dummyDB, paramToCreateTx2, t)
 		testTxTokenV2OneDoubleSpentInput(tx2, dummyDB, t)
+
+		testTxTokenV2Salary(tx.GetTokenID(), dummyDB, t)
 	}
 }
 
@@ -279,4 +223,137 @@ func testTxTokenV2OneDoubleSpentInput(tokenTx *TxTokenVersion2, db *statedb.Stat
 		err = tokenTx.ValidateTxWithBlockChain(nil, nil ,nil, 0, db)
 		assert.NotEqual(t,nil,err)
 		
+}
+
+func getParamForTxTokenTransfer(txTokenInit *TxTokenVersion2, db *statedb.StateDB, t *testing.T) (*TxTokenParams,*TokenParam){
+	transferAmount := uint64(69)
+	msgCipherText := []byte("doing a transfer")
+	paymentInfo2 := []*privacy.PaymentInfo{{PaymentAddress: keySets[1].PaymentAddress, Amount: transferAmount, Message: msgCipherText}}
+
+	feeOutputs := txTokenInit.GetTxBase().GetProof().GetOutputCoins()
+	prvCoinsToPayTransfer := make([]coin.PlainCoin,0)
+	tokenOutputs := txTokenInit.GetTxTokenData().TxNormal.GetProof().GetOutputCoins()
+	tokenCoinsToTransfer := make([]coin.PlainCoin,0)
+	for _,c := range feeOutputs{
+		pc,_ := c.Decrypt(keySets[0])
+		// s,_ := json.Marshal(pc.(*coin.CoinV2))
+		fmt.Printf("Tx Fee : %x has received %d in PRV\n",pc.GetPublicKey().ToBytesS(),pc.GetValue())
+		prvCoinsToPayTransfer = append(prvCoinsToPayTransfer,pc)
+	}
+	for _,c := range tokenOutputs{
+		pc,err := c.Decrypt(keySets[0])
+		// s,_ := json.Marshal(pc.(*coin.CoinV2))
+		fmt.Printf("Tx Token : %x has received %d in token T1\n",pc.GetPublicKey().ToBytesS(),pc.GetValue())
+		assert.Equal(t,nil,err)
+		tokenCoinsToTransfer = append(tokenCoinsToTransfer,pc)
+	}
+	// // token param for transfer token
+	tokenParam2 := &TokenParam{
+		PropertyID:     txTokenInit.GetTokenID().String(),
+		PropertyName:   "Token 1",
+		PropertySymbol: "T1",
+		Amount:         transferAmount,
+		TokenTxType:    CustomTokenTransfer,
+		Receiver:       paymentInfo2,
+		TokenInput:     tokenCoinsToTransfer,
+		Mintable:       false,
+		Fee:            0,
+	}
+
+	paramToCreateTx2 := NewTxTokenParams(&keySets[0].PrivateKey,
+		[]*key.PaymentInfo{}, prvCoinsToPayTransfer, 15, tokenParam2, db, nil,
+		hasPrivacyForPRV, hasPrivacyForToken, shardID, []byte{},db)
+	return  paramToCreateTx2, tokenParam2
+}
+
+func getParamsForTxTokenInit(theInputCoin coin.Coin, db *statedb.StateDB) (*TxTokenParams,*TokenParam){
+	msgCipherText := []byte("haha dummy ciphertext")
+	initAmount := uint64(10000)
+	tokenPayments := []*privacy.PaymentInfo{{PaymentAddress: keySets[0].PaymentAddress, Amount: initAmount, Message: msgCipherText}}
+
+	myOnlyInputCoin,_ := theInputCoin.Decrypt(keySets[0])
+	inputCoinsPRV := []coin.PlainCoin{myOnlyInputCoin}
+	paymentInfoPRV := []*privacy.PaymentInfo{key.InitPaymentInfo(keySets[0].PaymentAddress,uint64(15000),[]byte("test out"))}
+
+	// token param for init new token
+	tokenParam := &TokenParam{
+		PropertyID:     "",
+		PropertyName:   "Token 1",
+		PropertySymbol: "T1",
+		Amount:         initAmount,
+		TokenTxType:    CustomTokenInit,
+		Receiver:       tokenPayments,
+		TokenInput:     []coin.PlainCoin{},
+		Mintable:       false,
+		Fee:            0,
+	}
+
+	paramToCreateTx := NewTxTokenParams(&keySets[0].PrivateKey,
+		paymentInfoPRV, inputCoinsPRV, 1000, tokenParam, db, nil,
+		hasPrivacyForPRV, hasPrivacyForToken, shardID, []byte{},db)
+	return paramToCreateTx, tokenParam
+}
+
+func testTxTokenV2Salary(tokenID *common.Hash, db *statedb.StateDB, t *testing.T){
+	numOfPrivateKeys := 2
+	for loop := 0; loop < numOfLoops; loop++ {
+		var err error
+		preparePaymentKeys(numOfPrivateKeys,t)
+
+		// create 2 otaCoins, the second one will already be stored in the db
+		theCoins := make([]*coin.CoinV2, 2)
+		theCoinsGeneric := make([]coin.Coin,2)
+		for i, _ := range theCoins {
+			var tempCoin *coin.CoinV2
+			var err error
+			for{
+				tempCoin,err = coin.NewCoinFromPaymentInfo(paymentInfo[i])
+				otaPublicKeyBytes := tempCoin.GetPublicKey().ToBytesS()
+				// want an OTA in shard 0
+				if otaPublicKeyBytes[31]==0{
+					break
+				}
+			}
+			assert.Equal(t, nil, err)
+			assert.Equal(t, false, tempCoin.IsEncrypted())
+			// tempCoin.ConcealData(keySets[i].PaymentAddress.GetPublicView())
+			// assert.Equal(t, true, tempCoin.IsEncrypted())
+			// assert.Equal(t, true, tempCoin.GetSharedRandom() == nil)
+			// _, err = tempCoin.Decrypt(keySets[i])
+			// assert.Equal(t,nil,err)
+			theCoins[i] = tempCoin
+			theCoinsGeneric[i] = tempCoin
+		}
+		forceSaveCoins(db, []coin.Coin{theCoinsGeneric[1]}, 0, *tokenID, t)
+
+		// creating the TX object
+		txsal := TxTokenVersion2{}
+		// actually making the salary TX
+		err = txsal.InitTxTokenSalary(theCoins[0], dummyPrivateKeys[0], db, nil, tokenID, "Token 1")
+		isValidSanity, err := txsal.ValidateSanityData(nil, nil, nil, 0)
+		assert.Equal(t, true, isValidSanity)
+		assert.Equal(t, nil, err)
+
+		// simulate bridgeDB already having this token in it
+		txDatabaseWrapper.isBridgeTokenExistedByType = func(stateDB *statedb.StateDB, incTokenID common.Hash, isCentralized bool) (bool,error){
+			return true, nil
+		}
+
+		isValidTxItself, err := txsal.ValidateTxByItself(hasPrivacyForPRV, db, nil, nil, shardID, false, nil, nil)
+		assert.Equal(t, true, isValidTxItself)
+		assert.Equal(t, nil, err)
+
+		// this other coin is already in db so it must be rejected by double spend check
+		txsal = TxTokenVersion2{}
+		err = txsal.InitTxTokenSalary(theCoins[1], dummyPrivateKeys[0], db, nil, tokenID, "Token 1")
+		assert.Equal(t,nil,err)
+		isValidSanity, err = txsal.ValidateSanityData(nil, nil, nil, 0)
+		assert.Equal(t, true, isValidSanity)
+		assert.Equal(t, nil, err)
+		isValidTxItself, err = txsal.ValidateTxByItself(hasPrivacyForPRV, db, nil, nil, shardID, false, nil, nil)
+		assert.Equal(t, true, isValidTxItself)
+		assert.Equal(t, nil, err)
+
+		// TODO: add an integrated double spend check in txtokenver2
+	}
 }
