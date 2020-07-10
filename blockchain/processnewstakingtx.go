38e3b103703958978a49ca5014444e1d2a92d709
0xkumi <dung.q@autonomous.nyc>
2020-07-10 12:34:53 +0700

    build process

---
+++ blockchain/processnewstakingtx.go
@@ -0,0 +1,128 @@
+package blockchain
+
+import (
+	"fmt"
+	"github.com/incognitochain/incognito-chain/common"
+	"github.com/incognitochain/incognito-chain/dataaccessobject/rawdbv2"
+	"github.com/incognitochain/incognito-chain/dataaccessobject/statedb"
+	"strconv"
+	"strings"
+	"time"
+)
+
+/*
+	This hot fox include:
+	- init build process when init chain state
+	- build process implementation (proecssnewstakingtx.go)
+	- database access (accessor_newstakingtx.go)
+*/
+
+const NEWSTAKINGTX_HEIGHT_SWITCH = 1000000
+
+func (blockchain *BlockChain) buildNewStakingTx() {
+	bDB := blockchain.GetDatabase()
+	stakingInfo, err := rawdbv2.GetMapStakingTxNew(bDB)
+	if err != nil {
+		stakingInfo = &rawdbv2.StakingTXInfo{
+			MStakingTX: make(map[int]map[string]string),
+			Height:     2,
+		}
+		for i := 0; i < blockchain.GetActiveShardNumber(); i++ {
+			stakingInfo.MStakingTX[i] = make(map[string]string)
+		}
+	}
+
+	for {
+		if stakingInfo.Height > NEWSTAKINGTX_HEIGHT_SWITCH {
+			break
+		}
+		nextRequestHeight := stakingInfo.Height + 500
+		if nextRequestHeight > NEWSTAKINGTX_HEIGHT_SWITCH {
+			nextRequestHeight = NEWSTAKINGTX_HEIGHT_SWITCH
+		}
+
+		if nextRequestHeight > blockchain.GetBeaconHeight() {
+			nextRequestHeight = blockchain.GetBeaconHeight()
+		}
+
+		if nextRequestHeight <= stakingInfo.Height {
+			time.Sleep(time.Second)
+			continue
+		}
+
+		fmt.Println("NEWTX: get blocks", stakingInfo.Height+1, nextRequestHeight)
+		blocks, err := FetchBeaconBlockFromHeight(bDB, stakingInfo.Height+1, nextRequestHeight)
+		if err != nil {
+			Logger.log.Error(err)
+			panic(err)
+		}
+		for _, block := range blocks {
+			newMap, err := blockchain.GetMapStakingTxFromBeaconBlock(stakingInfo.MStakingTX, block)
+			if err != nil {
+				Logger.log.Error(err)
+				panic(err)
+			}
+			stakingInfo.Height = block.GetHeight()
+			stakingInfo.MStakingTX = newMap
+			if block.GetHeight() == NEWSTAKINGTX_HEIGHT_SWITCH || (block.GetHeight() < NEWSTAKINGTX_HEIGHT_SWITCH && block.GetHeight()%100 == 0) {
+				rawdbv2.StoreMapStakingTxNew(bDB, block.GetHeight(), newMap)
+			}
+		}
+
+	}
+}
+
+func (blockchain *BlockChain) GetMapStakingTxFromBeaconBlock(curMap map[int]map[string]string, bcBlk *BeaconBlock) (map[int]map[string]string, error) {
+	beaconConsensusRootHash, err := blockchain.GetBeaconConsensusRootHash(blockchain.GetDatabase(), bcBlk.GetHeight())
+	if err != nil {
+		return nil, fmt.Errorf("Beacon Consensus Root Hash of Height %+v not found ,error %+v", bcBlk.GetHeight(), err)
+	}
+	beaconConsensusStateDB, err := statedb.NewWithPrefixTrie(beaconConsensusRootHash, statedb.NewDatabaseAccessWarper(blockchain.GetDatabase()))
+	if err != nil {
+		return nil, err
+	}
+	_, autoStaking := statedb.GetRewardReceiverAndAutoStaking(beaconConsensusStateDB, blockchain.GetShardIDs())
+	for _, l := range bcBlk.Body.Instructions {
+		switch l[0] {
+		case SwapAction:
+			for _, outPublicKey := range strings.Split(l[2], ",") {
+				// If out public key has auto staking then ignore this public key
+				res, ok := autoStaking[outPublicKey]
+				if ok && res {
+					continue
+				}
+				sid, err := strconv.Atoi(l[4])
+				if err != nil {
+					panic(err)
+				}
+				delete(curMap[sid], outPublicKey)
+			}
+		case StakeAction:
+			switch l[2] {
+			case "shard":
+				shard := strings.Split(l[1], ",")
+				newShardCandidates := []string{}
+				newShardCandidates = append(newShardCandidates, shard...)
+				if len(l) == 6 {
+					for i, v := range strings.Split(l[3], ",") {
+						txHash, err := common.Hash{}.NewHashFromStr(v)
+						if err != nil {
+							continue
+						}
+						shardID, _, _, _, err := blockchain.GetTransactionByHash(*txHash)
+						if err != nil {
+							continue
+						}
+						// if transaction belong to this shard then add to shard beststate
+						curMap[int(shardID)][newShardCandidates[i]] = v
+					}
+				}
+			default:
+				continue
+			}
+		default:
+			continue
+		}
+	}
+	return curMap, nil
+}
