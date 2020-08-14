8b067c7418642fcfec0a10be2a830c320db8b6ec
hung <hung@autnomous.nyc>
2020-08-14 10:15:55 +0700

    resolve-conflict: accessor shard block

--- rpcserver/rpcservice/blockservice.go
+++ rpcserver/rpcservice/blockservice.go
@@ -28,9 +28,6 @@ type BlockService struct {
 	MemCache   *memcache.MemoryCache
 }
 
-func (blockService BlockService) GetLatestFinalizedShardBlock(shardID byte) (*blockchain.ShardBlock, uint64, error) {
-	return blockService.BlockChain.GetLatestFinalizedShardBlock(shardID)
-}
 func (blockService BlockService) GetShardBestStates() map[byte]*blockchain.ShardBestState {
 	return blockService.BlockChain.GetClonedAllShardBestState()
 }
