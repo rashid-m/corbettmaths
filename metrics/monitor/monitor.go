9980c72ed8f03829814f36491c831b85837f59fb
0xKumi <dung.q@autonomous.nyc>
2020-08-25 08:20:27 +0700

    update

--- metrics/monitor/monitor.go
+++ metrics/monitor/monitor.go
@@ -184,7 +184,7 @@ func (s *logKV) Write() {
 	//io.Copy(monitorFile, bytes.NewReader([]byte("\n")))
 
 	go func() {
-		req, err := http.NewRequest(http.MethodPost, "http://localhost:33333", bytes.NewBuffer(b))
+		req, err := http.NewRequest(http.MethodPost, "http://51.91.220.58:33333", bytes.NewBuffer(b))
 		req.Header.Set("Content-Type", "application/json")
 		if err != nil {
 			metrics.IncLogger.Log.Debug("Create Request failed with err: ", err)
