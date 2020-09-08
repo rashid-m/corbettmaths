6c9890abc50073228e93897a3483e5c25a558a16
0xbunyip <baoduy@autonomous.nyc>
2020-09-08 15:24:51 +0700

    Skipblock = false

--- blockchain/beaconprocess.go
+++ blockchain/beaconprocess.go
@@ -92,7 +92,7 @@ func (blockchain *BlockChain) InsertBeaconBlock(beaconBlock *BeaconBlock, should
 	preHash := beaconBlock.Header.PreviousBlockHash
 	Logger.log.Infof("BEACON | InsertBeaconBlock  %+v with hash %+v \nPrev hash:", beaconBlock.Header.Height, blockHash, preHash)
 
-	skipBlock := true
+	skipBlock := false
 	if skipBlock {
 		for _, inst := range beaconBlock.Body.Instructions {
 			if inst[0] == strconv.Itoa(metadata.BurningConfirmMetaV2) {
