4c016cad00ea18de944a61b00f069bafb8faca81
hung <hung@autnomous.nyc>
2020-09-14 16:07:49 +0700

    refactor code

--- blockchain/slashing/slashmissingsignature.go
+++ blockchain/slashing/slashmissingsignature.go
@@ -5,6 +5,13 @@ import (
 	"github.com/incognitochain/incognito-chain/incognitokey"
 )
 
+type SlashMissingSignature interface {
+	AddMissingSignature(validationData string, committees []*incognitokey.CommitteePublicKey) error
+	GetAllSlashingPenalty() map[string]uint64
+	GetSlashingPenalty(key *incognitokey.CommitteePublicKey)
+	Reset()
+}
+
 type SignatureCounter struct {
 	epoch            int
 	missingSignature map[string]uint
