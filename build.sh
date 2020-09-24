2e3e68039d3315f24acb4cde3771465565ceaba7
hung <hung@autnomous.nyc>
2020-09-24 15:27:38 +0700

    script: add go test to build script

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@ echo "Start build Incognito"
 
 git pull
 
-echo "Package install"
-dep ensure -v
+GO111MODULE=on go get -v
 
 APP_NAME="incognito"
 
+GO111MODULE=on go test -cover ./blockchain/committeestate/*.go
+GO111MODULE=on go test -cover ./dataaccessobject/statedb/*.go
+
 echo "go build -o $APP_NAME"
-go build -o $APP_NAME
+GO111MODULE=on go build -o $APP_NAME
 
 echo "cp ./$APP_NAME $GOPATH/bin/$APP_NAME"
 mv ./$APP_NAME $GOPATH/bin/$APP_NAME
