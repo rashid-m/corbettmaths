3391b124706104e0e699c8a3bb237dee7b18cea7
0xkumi <dung.q@autonomous.nyc>
2020-11-05 17:03:26 +0700

    add flow

--- devframework/dev/connect.go
+++ devframework/dev/connect.go
@@ -11,8 +11,12 @@ func main() {
 	})
 	sim.ConnectNetwork()
 	sim.Network.On(F.MSG_BLOCK_BEACON, func(msg interface{}) {
-		fmt.Println(msg)
+		//process 1st listenner
+		fmt.Println("1 process receive", msg)
 	})
-
+	sim.Network.On(F.MSG_BLOCK_BEACON, func(msg interface{}) {
+		//process 2nd listenner
+		fmt.Println("2 process receive", msg)
+	})
 	sim.Pause()
 }
