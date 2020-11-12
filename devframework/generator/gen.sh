dedd01c4e7fada81a7d1b12e294562321e52d70d
0xkumi <dung.q@autonomous.nyc>
2020-11-12 15:01:38 +0700

    remove devframework

--- devframework/generator/gen.sh
+++
@@ -1,3 +0,0 @@
-go run localRPC.go
-go run remoteRPC.go
-cat apispec.go | sed 's/package main/package rpcclient \/\/This file is auto generated. Please do not change if you dont know what you are doing/' > ../rpcclient/interfaces.go
--- devframework/mock/pubsub.go
+++
@@ -1,9 +0,0 @@
-package mock
-
-import "github.com/incognitochain/incognito-chain/pubsub"
-
-type Pubsub struct{}
-
-func (ps *Pubsub) PublishMessage(message *pubsub.Message) {
-	return
-}
