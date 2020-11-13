25628d16feafb6882af6f326b87b567f8eb73598
hung <hung@autnomous.nyc>
2020-11-13 11:48:26 +0700

    [rpc] update

--- rpcserver/http_testing.go
+++ rpcserver/http_testing.go
@@ -90,7 +90,7 @@ func (httpServer *HttpServer) handleGetCommitteeState(params interface{}, closeC
 		return nil, rpcservice.NewRPCError(rpcservice.UnexpectedError, err2)
 	}
 
-	currentValidator, substituteValidator, nextEpochShardCandidate, currentEpochShardCandidate, _, _, _, _, _ := statedb.GetAllCandidateSubstituteCommittee(stateDB, shardIDs)
+	currentValidator, substituteValidator, nextEpochShardCandidate, currentEpochShardCandidate, _, _, rewardReceivers, autoStaking, stakingTx := statedb.GetAllCandidateSubstituteCommittee(stateDB, shardIDs)
 	currentValidatorStr := make(map[int][]string)
 	for shardID, v := range currentValidator {
 		tempV, _ := incognitokey.CommitteeKeyListToString(v)
@@ -109,6 +109,9 @@ func (httpServer *HttpServer) handleGetCommitteeState(params interface{}, closeC
 		"substitute":       substituteValidatorStr,
 		"nextCandidate":    nextEpochShardCandidateStr,
 		"currentCandidate": currentEpochShardCandidateStr,
+		"rewardReceivers":  rewardReceivers,
+		"autoStaking":      autoStaking,
+		"stakingTx":        stakingTx,
 	}, nil
 }
 
