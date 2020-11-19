55feafb385db488c4e1e002631ca8bb70d03840e
darius <datle@incognito.org>
2020-11-19 10:31:52 +0700

    call init logger for v2 bulletproofs

--- privacy/privacy_v2/payment_v2_log.go
+++ privacy/privacy_v2/payment_v2_log.go
@@ -2,6 +2,7 @@ package privacy_v2
 
 import (
 	"github.com/incognitochain/incognito-chain/common"
+	bp "github.com/incognitochain/incognito-chain/privacy/privacy_v2/bulletproofs"
 )
 
 type PaymentV2Logger struct {
@@ -10,6 +11,7 @@ type PaymentV2Logger struct {
 
 func (logger *PaymentV2Logger) Init(inst common.Logger) {
 	logger.Log = inst
+	bp.Logger.Init(inst)
 }
 
 const (
