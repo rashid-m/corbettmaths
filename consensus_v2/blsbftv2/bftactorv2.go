03644ddd60aca4808edeb701d164d79e959fbb86
0xkumi <dung.q@autonomous.nyc>
2021-01-05 15:33:50 +0700

    Revert "using commitee pubkey when send vote"
    This reverts commit ecfda6f467e31186bfd488ae014d59046bbc6839.

--- consensus_v2/blsbftv2/bftactorv2.go
+++ consensus_v2/blsbftv2/bftactorv2.go
@@ -152,7 +152,8 @@ func (e *BLSBFT_V2) Start() error {
 						b.votes[voteMsg.Validator] = &voteMsg // store it
 						vid, v := GetValidatorIndex(e.Chain.GetBestView(), voteMsg.Validator)
 						if v != nil {
-							e.Logger.Infof("%v Receive vote (%d) for block %s from validator %d %v", e.ChainKey, len(e.receiveBlockByHash[voteMsg.BlockHash].votes), voteMsg.BlockHash, vid, voteMsg.Validator)
+							vbase58, _ := v.ToBase58()
+							e.Logger.Infof("%v Receive vote (%d) for block %s from validator %d %v", e.ChainKey, len(e.receiveBlockByHash[voteMsg.BlockHash].votes), voteMsg.BlockHash, vid, vbase58)
 						} else {
 							e.Logger.Infof("%v Receive vote (%d) for block from unknown validator", e.ChainKey, len(e.receiveBlockByHash[voteMsg.BlockHash].votes), voteMsg.BlockHash, voteMsg.Validator)
 						}
@@ -167,7 +168,8 @@ func (e *BLSBFT_V2) Start() error {
 					e.receiveBlockByHash[voteMsg.BlockHash].votes[voteMsg.Validator] = &voteMsg
 					vid, v := GetValidatorIndex(e.Chain.GetBestView(), voteMsg.Validator)
 					if v != nil {
-						e.Logger.Infof("%v Receive vote (%d) for block %s from validator %d %v", e.ChainKey, len(e.receiveBlockByHash[voteMsg.BlockHash].votes), voteMsg.BlockHash, vid, voteMsg.Validator)
+						vbase58, _ := v.ToBase58()
+						e.Logger.Infof("%v Receive vote (%d) for block %s from validator %d %v", e.ChainKey, len(e.receiveBlockByHash[voteMsg.BlockHash].votes), voteMsg.BlockHash, vid, vbase58)
 					} else {
 						e.Logger.Infof("%v Receive vote (%d) for block from unknown validator", e.ChainKey, len(e.receiveBlockByHash[voteMsg.BlockHash].votes), voteMsg.BlockHash, voteMsg.Validator)
 					}
@@ -241,6 +243,7 @@ func (e *BLSBFT_V2) Start() error {
 					if createdBlk, err := e.proposeBlock(userProposeKey, proposerPk, proposeBlock); err != nil {
 						e.Logger.Critical(UnExpectedError, errors.New("can't propose block"))
 						e.Logger.Critical(err)
+
 					} else {
 						e.Logger.Infof("%v proposer block %v round %v time slot %v blockTimeSlot %v with hash %v", e.ChainKey, createdBlk.GetHeight(), e.currentTimeSlot-common.CalculateTimeSlot(bestView.GetBlock().GetProposeTime()), e.currentTimeSlot, common.CalculateTimeSlot(createdBlk.GetProduceTime()), createdBlk.Hash().String())
 					}
@@ -310,8 +313,7 @@ func NewInstance(chain ChainInterface, chainKey string, chainID int, node NodeIn
 
 func GetValidatorIndex(view multiview.View, validator string) (int, *incognitokey.CommitteePublicKey) {
 	for id, c := range view.GetCommittee() {
-		base58PKKey, _ := c.ToBase58()
-		if validator == base58PKKey {
+		if validator == c.GetMiningKeyBase58(common.BlsConsensus) {
 			return id, &c
 		}
 	}
@@ -378,7 +380,7 @@ func (e *BLSBFT_V2) processIfBlockGetEnoughVote(blockHash string, v *ProposeBloc
 	v.hasNewVote = false
 	if validVote > 2*len(view.GetCommittee())/3 {
 		e.Logger.Infof("%v Commit block %v , height: %v", e.ChainKey, blockHash, v.block.GetHeight())
-		committeeBLSString, err := incognitokey.CommitteeKeyListToString(view.GetCommittee())
+		committeeBLSString, err := incognitokey.ExtractPublickeysFromCommitteeKeyList(view.GetCommittee(), common.BlsConsensus)
 		//fmt.Println(committeeBLSString)
 		if err != nil {
 			e.Logger.Error(err)
@@ -486,8 +488,8 @@ func CreateVote(userKey *signatureschemes2.MiningKey, block common.BlockInterfac
 	Vote.BRI = bridgeSig
 	Vote.BlockHash = block.Hash().String()
 
-	base58PKKey, _ := committees[selfIdx].ToBase58()
-	Vote.Validator = base58PKKey
+	userPk := userKey.GetPublicKey()
+	Vote.Validator = userPk.GetMiningKeyBase58(common.BlsConsensus)
 	Vote.PrevBlockHash = block.GetPrevHash().String()
 	err = Vote.signVote(userKey)
 	if err != nil {
