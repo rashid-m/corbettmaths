39b8bf019219e9c6ae978f06bcf51303c200ed5c
thanhnguyen <thanh.n@incognito.org>
2021-01-15 10:14:59 +0700

    Remove checking keys in staking metadata

--- metadata/staking.go
+++ metadata/staking.go
@@ -125,8 +125,7 @@ func (stakingMetadata StakingMetadata) ValidateSanityData(chainRetriever ChainRe
 		return false, false, errors.New("invalid Stake Beacon Amount")
 	}
 
-	funderAddr, err := AssertPaymentAddressAndTxVersion(stakingMetadata.FunderPaymentAddress, tx.GetVersion())
-	if err != nil {
+	if _, err := AssertPaymentAddressAndTxVersion(stakingMetadata.FunderPaymentAddress, tx.GetVersion()); err != nil {
 		return false, false, errors.New(fmt.Sprintf("invalid funder address: %v", err))
 	}
 
@@ -141,11 +140,6 @@ func (stakingMetadata StakingMetadata) ValidateSanityData(chainRetriever ChainRe
 	}
 	if !CommitteePublicKey.CheckSanityData() {
 		return false, false, errors.New("Invalid Commitee Public Key of Candidate who join consensus")
-	}
-
-	if !bytes.Equal(CommitteePublicKey.IncPubKey, funderAddr.Pk){
-		Logger.log.Infof("BUGLOG IncPubkey != funder.PK: %v != %v\n", CommitteePublicKey.IncPubKey, funderAddr.Pk)
-		return false, false, errors.New("IncPubkey must equal the public key of the funder")
 	}
 
 	return true, true, nil
