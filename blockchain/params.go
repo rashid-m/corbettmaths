777f44290b2cd28f27de623d2c76d73acde34f89
minhduccm <minhduccm90@gmail.com>
2021-03-13 15:44:16 +0700

    Update checkpoint for bft2

--- blockchain/params.go
+++ blockchain/params.go
@@ -405,7 +405,7 @@ func SetupParam() {
 		},
 		CheckForce:                     false,
 		ChainVersion:                   "version-chain-main.json",
-		ConsensusV2Epoch:               3066,
+		ConsensusV2Epoch:               3071,
 		Timeslot:                       40,
 		BeaconHeightBreakPointBurnAddr: 150500,
 		BNBRelayingHeaderChainID:       MainnetBNBChainID,
