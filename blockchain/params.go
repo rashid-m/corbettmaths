1b75499a4a900c0b37226e65984598ffdc7ad989
thachtb <thach.bao@incognito.org>
2021-03-23 16:47:35 +0700

    add default params for testnet.

--- blockchain/params.go
+++ blockchain/params.go
@@ -277,8 +277,11 @@ func SetupParam() {
 					MinUnshieldAmts: map[string]uint64{
 						portalcommonv4.PortalBTCIDStr: 500000, // 1000000 nano pbtc = 100000 satoshi
 					},
-					BatchNumBlks:               15, // ~ 10 mins
-					MinConfirmationIncBlockNum: 3,
+					BatchNumBlks:                15, // ~ 10 mins
+					MinConfirmationIncBlockNum:  3,
+					PortalReplacementAddress:    "12S5Lrs1XeQLbqN4ySyKtjAjd2d7sBP2tjFijzmp6avrrkQCNFMpkXm3FPzj2Wcu2ZNqJEmh9JriVuRErVwhuQnLmWSaggobEWsBEci",
+					MaxFeePercentageForEachStep: 20, // ~ 20% from previous fee
+					TimeSpaceForFeeReplacement:  5 * time.Minute,
 				},
 			},
 		},
