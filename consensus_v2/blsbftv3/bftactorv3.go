558b7048c1fffbf0ee37233b1059b616ce857473
hung <hung@autonomous.nyc>
2021-03-29 15:16:10 +0700

    [dcs] fix bug validate and vote

--- consensus_v2/blsbftv3/bftactorv3.go
+++ consensus_v2/blsbftv3/bftactorv3.go
@@ -254,6 +254,7 @@ func (e *BLSBFT_V3) run() error {
 						delete(e.receiveBlockByHash, h)
 					}
 				}
+
 				//rule 1: get history of vote for this height, vote if (round is lower than the vote before) or (round is equal but new proposer) or (there is no vote for this height yet)
 				sort.Slice(validProposeBlock, func(i, j int) bool {
 					return validProposeBlock[i].block.GetProduceTime() < validProposeBlock[j].block.GetProduceTime()
@@ -457,47 +458,51 @@ func (e *BLSBFT_V3) validateAndVote(
 	v *ProposeBlockInfo,
 ) error {
 	//already vote for this proposed block
+
 	if v.isVoted {
 		return nil
 	}
 
-	//not connected
-	e.Logger.Info("validateAndVote")
-	view := e.Chain.GetViewByHash(v.block.GetPrevHash())
-	if view == nil {
-		e.Logger.Info("view is null")
-		return errors.New("View not connect")
-	}
-
-	//TODO: using context to validate block
-	_, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-
-	if err := e.Chain.ValidatePreSignBlock(v.block, v.committees); err != nil {
-		e.Logger.Error(err)
-		return err
-	}
-
-	//if valid then vote
-	for _, userKey := range e.UserKeySet {
-		Vote, err := CreateVote(&userKey, v.block, v.committees)
-		if err != nil {
-			e.Logger.Error(err)
-			return NewConsensusError(UnExpectedError, err)
+	if !v.isValid {
+		//not connected
+		view := e.Chain.GetViewByHash(v.block.GetPrevHash())
+		if view == nil {
+			e.Logger.Info("view is null")
+			return errors.New("View not connect")
 		}
 
-		msg, err := MakeBFTVoteMsg(Vote, e.ChainKey, e.currentTimeSlot, v.block.GetHeight())
-		if err != nil {
-			e.Logger.Error(err)
-			return NewConsensusError(UnExpectedError, err)
-		}
+		//TODO: using context to validate block
+		_, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
 
-		v.isValid = true
-		v.isVoted = true
-		e.voteHistory[v.block.GetHeight()] = v.block
-		e.Logger.Info(e.ChainKey, "sending vote...")
-		go e.ProcessBFTMsg(msg.(*wire.MessageBFT))
-		go e.Node.PushMessageToChain(msg, e.Chain)
+		if err := e.Chain.ValidatePreSignBlock(v.block, v.committees); err != nil {
+			e.Logger.Error(err)
+			return err
+		}
+	}
+	v.isValid = true
+
+	if !v.isVoted {
+		//if valid then vote
+		for _, userKey := range e.UserKeySet {
+			Vote, err := CreateVote(&userKey, v.block, v.committees)
+			if err != nil {
+				e.Logger.Error(err)
+				return NewConsensusError(UnExpectedError, err)
+			}
+
+			msg, err := MakeBFTVoteMsg(Vote, e.ChainKey, e.currentTimeSlot, v.block.GetHeight())
+			if err != nil {
+				e.Logger.Error(err)
+				return NewConsensusError(UnExpectedError, err)
+			}
+
+			v.isVoted = true
+			e.voteHistory[v.block.GetHeight()] = v.block
+			e.Logger.Infof("e.ChainKey %+v sending vote for block hash %+v height %+v", e.ChainKey, v.block.Hash().String(), v.block.GetHeight())
+			go e.ProcessBFTMsg(msg.(*wire.MessageBFT))
+			go e.Node.PushMessageToChain(msg, e.Chain)
+		}
 	}
 
 	return nil
