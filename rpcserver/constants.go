13913f7a5b6a873ae95d645ae966aa7f02baca29
thachtb <thach.bao@incognito.org>
2021-04-13 11:30:42 +0700

    fixed typo replace by fee portal.

--- rpcserver/constants.go
+++ rpcserver/constants.go
@@ -304,7 +304,7 @@ const (
 	getPortalReplacementFeeStatus              = "getportalreplacebyfeestatus"
 	createAndSendTxWithPortalSubmitConfirmedTx = "createandsendtxwithportalsubmitconfirmedtx"
 	getPortalSubmitConfirmedTx                 = "getportalsubmitconfirmedtxstatus"
-	getSignedRawReplaceFeeTransaction          = "getporalsignedrawreplacebyfeetransaction"
+	getSignedRawReplaceFeeTransaction          = "getportalsignedrawreplacebyfeetransaction"
 )
 
 const (
