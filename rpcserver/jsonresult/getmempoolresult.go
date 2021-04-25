92bca2b1381b01c2c4afeb5c9dbf61b5d8b4322a
minhduccm <minhduccm90@gmail.com>
2021-04-25 17:29:38 +0700

    Update GetMempoolInfo rpc to including tpKey in its result

--- rpcserver/jsonresult/getmempoolresult.go
+++ rpcserver/jsonresult/getmempoolresult.go
@@ -3,6 +3,7 @@ package jsonresult
 import (
 	"sort"
 
+	"github.com/incognitochain/incognito-chain/common"
 	"github.com/incognitochain/incognito-chain/metadata"
 )
 
@@ -18,7 +19,7 @@ type GetMempoolInfo struct {
 
 func NewGetMempoolInfo(txMempool interface {
 	MaxFee() uint64
-	ListTxsDetail() []metadata.Transaction
+	ListTxsDetail() ([]common.Hash, []metadata.Transaction)
 	Count() int
 	Size() uint64
 }) *GetMempoolInfo {
@@ -28,11 +29,11 @@ func NewGetMempoolInfo(txMempool interface {
 		MempoolMaxFee: txMempool.MaxFee(),
 	}
 	// get list data from mempool
-	listTxsDetail := txMempool.ListTxsDetail()
-	if len(listTxsDetail) > 0 {
+	tpKeys, txs := txMempool.ListTxsDetail()
+	if len(txs) > 0 {
 		result.ListTxs = make([]GetMempoolInfoTx, 0)
-		for _, tx := range listTxsDetail {
-			item := NewGetMempoolInfoTx(tx)
+		for idx, tx := range txs {
+			item := NewGetMempoolInfoTx(tpKeys[idx], tx)
 			result.ListTxs = append(result.ListTxs, *item)
 		}
 	}
@@ -45,26 +46,17 @@ func NewGetMempoolInfo(txMempool interface {
 	return result
 }
 
-// GetTxDetailsFromMempool is to get all txs along with their detailed info from mempool
-func GetTxDetailsFromMempool(txMempool interface {
-	MaxFee() uint64
-	ListTxsDetail() []metadata.Transaction
-	Count() int
-	Size() uint64
-}) []metadata.Transaction {
-	txDetailsInMempool := txMempool.ListTxsDetail()
-	return txDetailsInMempool
-}
-
 type GetMempoolInfoTx struct {
 	TxID     string `json:"TxID"`
 	LockTime int64  `json:"LockTime"`
+	TpKey    string `json:"TpKey"`
 }
 
-func NewGetMempoolInfoTx(tx metadata.Transaction) *GetMempoolInfoTx {
+func NewGetMempoolInfoTx(tpKey common.Hash, tx metadata.Transaction) *GetMempoolInfoTx {
 	result := &GetMempoolInfoTx{
 		LockTime: tx.GetLockTime(),
 		TxID:     tx.Hash().String(),
+		TpKey:    tpKey.String(),
 	}
 	return result
 }
