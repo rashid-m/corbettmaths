1370b847d09fe7a048c100ea3afb6306f80d8448
hungngo <hung@autonomous.nyc>
2021-05-04 15:03:39 +0700

    [reward] staking flow v2 go with reward v1

--- blockchain/committeestate/reward_v2.go
+++ blockchain/committeestate/reward_v2.go
@@ -10,14 +10,13 @@ func (b *BeaconCommitteeEngineV2) SplitReward(
 	map[common.Hash]uint64, map[common.Hash]uint64,
 	map[common.Hash]uint64, map[common.Hash]uint64, error,
 ) {
+
 	devPercent := uint64(env.DAOPercent)
 	allCoinTotalReward := env.TotalReward
 	rewardForBeacon := map[common.Hash]uint64{}
 	rewardForShard := map[common.Hash]uint64{}
 	rewardForIncDAO := map[common.Hash]uint64{}
 	rewardForCustodian := map[common.Hash]uint64{}
-	lenBeaconCommittees := uint64(len(b.GetBeaconCommittee()))
-	lenShardCommittees := uint64(len(b.GetShardCommittee()[env.ShardID]))
 
 	if len(allCoinTotalReward) == 0 {
 		Logger.log.Info("Beacon Height %+v, 😭 found NO reward", env.BeaconHeight)
@@ -25,24 +24,57 @@ func (b *BeaconCommitteeEngineV2) SplitReward(
 	}
 
 	for key, totalReward := range allCoinTotalReward {
-		totalRewardForDAOAndCustodians := devPercent * totalReward / 100
-		totalRewardForShardAndBeaconValidators := totalReward - totalRewardForDAOAndCustodians
-		shardWeight := float64(lenShardCommittees)
-		beaconWeight := 2 * float64(lenBeaconCommittees) / float64(env.ActiveShards)
-		totalValidatorWeight := shardWeight + beaconWeight
+		rewardForBeacon[key] += 2 * ((100 - devPercent) * totalReward) / ((uint64(env.ActiveShards) + 2) * 100)
+		totalRewardForDAOAndCustodians := uint64(devPercent) * totalReward / uint64(100)
 
-		rewardForShard[key] = uint64(shardWeight * float64(totalRewardForShardAndBeaconValidators) / totalValidatorWeight)
 		Logger.log.Infof("[test-salary] totalRewardForDAOAndCustodians tokenID %v - %v\n",
 			key.String(), totalRewardForDAOAndCustodians)
 
 		if env.IsSplitRewardForCustodian {
-			rewardForCustodian[key] += env.PercentCustodianReward * totalRewardForDAOAndCustodians / 100
+			rewardForCustodian[key] += uint64(env.PercentCustodianReward) * totalRewardForDAOAndCustodians / uint64(100)
 			rewardForIncDAO[key] += totalRewardForDAOAndCustodians - rewardForCustodian[key]
 		} else {
 			rewardForIncDAO[key] += totalRewardForDAOAndCustodians
 		}
-		rewardForBeacon[key] += totalReward - (rewardForShard[key] + totalRewardForDAOAndCustodians)
+
+		rewardForShard[key] = totalReward - (rewardForBeacon[key] + totalRewardForDAOAndCustodians)
 	}
 
 	return rewardForBeacon, rewardForShard, rewardForIncDAO, rewardForCustodian, nil
+	// Next version
+	//devPercent := uint64(env.DAOPercent)
+	//allCoinTotalReward := env.TotalReward
+	//rewardForBeacon := map[common.Hash]uint64{}
+	//rewardForShard := map[common.Hash]uint64{}
+	//rewardForIncDAO := map[common.Hash]uint64{}
+	//rewardForCustodian := map[common.Hash]uint64{}
+	//lenBeaconCommittees := uint64(len(b.GetBeaconCommittee()))
+	//lenShardCommittees := uint64(len(b.GetShardCommittee()[env.ShardID]))
+	//
+	//if len(allCoinTotalReward) == 0 {
+	//	Logger.log.Info("Beacon Height %+v, 😭 found NO reward", env.BeaconHeight)
+	//	return rewardForBeacon, rewardForShard, rewardForIncDAO, rewardForCustodian, nil
+	//}
+	//
+	//for key, totalReward := range allCoinTotalReward {
+	//	totalRewardForDAOAndCustodians := devPercent * totalReward / 100
+	//	totalRewardForShardAndBeaconValidators := totalReward - totalRewardForDAOAndCustodians
+	//	shardWeight := float64(lenShardCommittees)
+	//	beaconWeight := 2 * float64(lenBeaconCommittees) / float64(env.ActiveShards)
+	//	totalValidatorWeight := shardWeight + beaconWeight
+	//
+	//	rewardForShard[key] = uint64(shardWeight * float64(totalRewardForShardAndBeaconValidators) / totalValidatorWeight)
+	//	Logger.log.Infof("[test-salary] totalRewardForDAOAndCustodians tokenID %v - %v\n",
+	//		key.String(), totalRewardForDAOAndCustodians)
+	//
+	//	if env.IsSplitRewardForCustodian {
+	//		rewardForCustodian[key] += env.PercentCustodianReward * totalRewardForDAOAndCustodians / 100
+	//		rewardForIncDAO[key] += totalRewardForDAOAndCustodians - rewardForCustodian[key]
+	//	} else {
+	//		rewardForIncDAO[key] += totalRewardForDAOAndCustodians
+	//	}
+	//	rewardForBeacon[key] += totalReward - (rewardForShard[key] + totalRewardForDAOAndCustodians)
+	//}
+	//
+	//return rewardForBeacon, rewardForShard, rewardForIncDAO, rewardForCustodian, nil
 }
