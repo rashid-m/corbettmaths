c3f62ece29ae167e638e5c9e19ba1788def5fed1
0xkumi <dung.q@autonomous.nyc>
2021-05-17 17:16:25 +0700

    add log

--- blockchain/shardproducer.go
+++ blockchain/shardproducer.go
@@ -365,6 +365,7 @@ func (blockGenerator *BlockGenerator) getTransactionForNewBlock(
 		}
 		go blockGenerator.txPool.RemoveTx(txToRemove, false)
 	} else {
+		fmt.Println("Get beacon block", bView.BeaconHeight, curView.BeaconHeight)
 		txsToAdd = chain.TxPool.GetTxsTranferForNewBlock(
 			blockGenerator.chain,
 			curView,
