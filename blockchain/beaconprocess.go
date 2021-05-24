c9bacaca7c39b590c7f7642ba33933a9da057bd5
tin-incognito <tin@incognito.org>
2021-05-24 09:35:07 +0700

    [config] add log for wrong propose time block

--- blockchain/beaconprocess.go
+++ blockchain/beaconprocess.go
@@ -103,6 +103,12 @@ CONTINUE_VERIFY:
 // var bcAllTime time.Duration
 
 func (blockchain *BlockChain) InsertBeaconBlock(beaconBlock *types.BeaconBlock, shouldValidate bool) error {
+
+	if beaconBlock.Header.Height == 2353313 {
+		Logger.log.Info("beaconBlock.Header.ProposeTime:", beaconBlock.Header.ProposeTime)
+		panic("Reach to beacon block height 2353313")
+	}
+
 	blockHash := beaconBlock.Header.Hash()
 	preHash := beaconBlock.Header.PreviousBlockHash
 	Logger.log.Infof("BEACON | InsertBeaconBlock  %+v with hash %+v", beaconBlock.Header.Height, blockHash)
