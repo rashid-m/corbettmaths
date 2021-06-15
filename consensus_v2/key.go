64120d838c2d2dcc969cd3ef40469e6ff08bf13b
0xkraken <hien.n@autonomous.nyc>
2021-06-15 15:32:46 +0700

    Add portal sigs into bftv3

--- consensus_v2/key.go
+++ consensus_v2/key.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/incognitochain/incognito-chain/consensus_v2/blsbftv3"
+
 	"github.com/incognitochain/incognito-chain/blockchain"
 	"github.com/incognitochain/incognito-chain/blockchain/types"
 	"github.com/incognitochain/incognito-chain/common"
@@ -195,10 +197,11 @@ func (engine *Engine) ExtractBridgeValidationData(block types.BlockInterface) ([
 func (engine *Engine) ExtractPortalV4ValidationData(block types.BlockInterface) ([]*portalprocessv4.PortalSig, error) {
 	if block.GetVersion() == 2 {
 		return blsbftv2.ExtractPortalV4ValidationData(block)
+	} else if block.GetVersion() == 3 {
+		return blsbftv3.ExtractPortalV4ValidationData(block)
 	}
 	return nil, blsbft.NewConsensusError(blsbft.ConsensusTypeNotExistError, errors.New(block.GetConsensusType()))
 }
-
 
 func LoadUserKeyFromIncPrivateKey(privateKey string) (string, error) {
 	wl, err := wallet.Base58CheckDeserialize(privateKey)
