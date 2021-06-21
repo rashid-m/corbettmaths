5c3866cca75926c7e9e1dde5d6132b150ed2ae56
Lampard <thanh23497@gmail.com>
2021-06-21 05:28:39 +0100

    disable old trade request at beacon level

--- blockchain/beaconstatefulinsts.go
+++ blockchain/beaconstatefulinsts.go
@@ -535,24 +535,27 @@ func (blockchain *BlockChain) handlePDEInsts(
 		}
 	}
 
-	// handle trade
-	sortedTradesActions := sortPDETradeInstsByFee(
-		beaconHeight,
-		currentPDEState,
-		pdeTradeActionsByShardID,
-	)
-	for _, tradeAction := range sortedTradesActions {
-		actionContentBytes, _ := json.Marshal(tradeAction)
-		actionContentBase64Str := base64.StdEncoding.EncodeToString(actionContentBytes)
-		newInst, err := blockchain.buildInstructionsForPDETrade(actionContentBase64Str, tradeAction.ShardID, metadata.PDETradeRequestMeta, currentPDEState, beaconHeight)
-		if err != nil {
-			Logger.log.Error(err)
-			continue
-		}
-		if len(newInst) > 0 {
-			instructions = append(instructions, newInst...)
+	if !blockchain.IsAfterPrivacyV2CheckPoint(beaconHeight) { // disable old pDEX trades after privacy V2 break point
+		// handle trade
+		sortedTradesActions := sortPDETradeInstsByFee(
+			beaconHeight,
+			currentPDEState,
+			pdeTradeActionsByShardID,
+		)
+		for _, tradeAction := range sortedTradesActions {
+			actionContentBytes, _ := json.Marshal(tradeAction)
+			actionContentBase64Str := base64.StdEncoding.EncodeToString(actionContentBytes)
+			newInst, err := blockchain.buildInstructionsForPDETrade(actionContentBase64Str, tradeAction.ShardID, metadata.PDETradeRequestMeta, currentPDEState, beaconHeight)
+			if err != nil {
+				Logger.log.Error(err)
+				continue
+			}
+			if len(newInst) > 0 {
+				instructions = append(instructions, newInst...)
+			}
 		}
 	}
+
 
 	// handle cross pool trade
 	sortedTradableActions, untradableActions := categorizeNSortPDECrossPoolTradeInstsByFee(
