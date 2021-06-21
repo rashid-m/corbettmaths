814db5abf760cbb590bbca66ff87d1cfffe34630
Lampard <thanh23497@gmail.com>
2021-06-21 08:34:37 +0100

    cherry-pick 1aeb5be878f50163325ef2accd3b7df8c4c40e1a

--- blockchain/shardchain.go
+++ blockchain/shardchain.go
@@ -76,20 +76,21 @@ func (chain *ShardChain) GetBestState() *ShardBestState {
 }
 
 func (chain *ShardChain) AddView(view multiview.View) bool {
-	curBestView := chain.multiView.GetBestView().GetHash().String()
+	curBestView := chain.multiView.GetBestView()
 	added := chain.multiView.AddView(view)
-	if added {
-		go func(chain *ShardChain) {
+	if (curBestView != nil) && (added) {
+		go func(chain *ShardChain, curBestView multiview.View) {
 			sBestView := chain.GetBestState()
-			if (curBestView != sBestView.GetHash().String()) && (chain.TxPool != nil) {
+			if (curBestView.GetHash().String() != sBestView.GetHash().String()) && (chain.TxPool != nil) {
 				bcHash := sBestView.GetBeaconHash()
 				bcView, err := chain.Blockchain.GetBeaconViewStateDataFromBlockHash(bcHash, true)
 				if err != nil {
-					Logger.log.Infof("Can not get beacon view from hash %, sView Hash %v, err %v", bcHash.String(), sBestView.GetHash().String(), err)
+					Logger.log.Errorf("Can not get beacon view from hash %, sView Hash %v, err %v", bcHash.String(), sBestView.GetHash().String(), err)
+				} else {
+					chain.TxPool.FilterWithNewView(chain.Blockchain, sBestView, bcView)
 				}
-				chain.TxPool.FilterWithNewView(chain.Blockchain, sBestView, bcView)
 			}
-		}(chain)
+		}(chain, curBestView)
 	}
 	return added
 }
