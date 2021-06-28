7432eac086f56b84f14dc09539d48e4003e83dc8
0xmerman <0xmerman>
2021-06-28 11:09:31 +0700

    [resolve-conflict] 2

---
+++ blockchain/utils.go
@@ -0,0 +1,12 @@
+package blockchain
+
+import "bytes"
+
+func IsBridgeTxHashUsedInBlock(uniqTx []byte, uniqTxsUsed [][]byte) bool {
+	for _, item := range uniqTxsUsed {
+		if bytes.Equal(uniqTx, item) {
+			return true
+		}
+	}
+	return false
+}
