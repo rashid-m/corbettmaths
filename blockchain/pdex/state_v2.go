b55c2987dfde10a4196d84f2137520a4ca3f9aee
tin-incognito <tin@incognito.org>
2021-09-18 16:24:09 +0700

    [pdex] Update coding style

--- blockchain/pdex/state_v2.go
+++ blockchain/pdex/state_v2.go
@@ -693,29 +693,6 @@ func (s *stateV2) updatePoolPairsToDB(env StateEnvironment, stateChange *v2utils
 		if err != nil {
 			return err
 		}
-		// store / delete orders
-		ordersByID := make(map[string]*Order)
-		for _, ord := range poolPairState.orderbook.orders {
-			ordersByID[ord.Id()] = ord
-		}
-		for orderID, changed := range poolPairChange.OrderIDs {
-			if changed {
-				if order, exists := ordersByID[orderID]; exists {
-					// update order in db
-					orderState := statedb.NewPdexv3OrderStateWithValue(poolPairID, *order)
-					err = statedb.StorePdexv3Order(env.StateDB(), *orderState)
-					if err != nil {
-						return err
-					}
-				} else {
-					// delete order from db
-					err = statedb.DeletePdexv3Order(env.StateDB(), poolPairID, orderID)
-					if err != nil {
-						return err
-					}
-				}
-			}
-		}
 	}
 	return nil
 }
