2b3c8cbebe596fdc4eda8d8f8c7a550296785893
thachtb <thach.bao@incognito.org>
2021-11-02 16:10:24 +0700

    Cherry pick burn pbsc for deposit to sc

--- blockchain/beaconprocess.go
+++ blockchain/beaconprocess.go
@@ -958,6 +958,7 @@ func (blockchain *BlockChain) processStoreBeaconBlock(
 		strconv.Itoa(metadata.BurningBSCConfirmMeta),
 		strconv.Itoa(metadataCommon.BurningPRVERC20ConfirmMeta),
 		strconv.Itoa(metadataCommon.BurningPRVBEP20ConfirmMeta),
+		strconv.Itoa(metadataCommon.BurningPBSCConfirmForDepositToSCMeta),
 	}
 	if err := blockchain.storeBurningConfirm(newBestState.featureStateDB, beaconBlock.Body.Instructions, beaconBlock.Header.Height, metas); err != nil {
 		return NewBlockChainError(StoreBurningConfirmError, err)
