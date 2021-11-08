f8e05a0ff0de5162b7b589bd79a2b17323830e48
tin-incognito <tin@incognito.org>
2021-11-08 09:57:04 +0700

    [pdex] restore beacon view when init

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -594,7 +594,7 @@ func (blockchain *BlockChain) RestoreBeaconViews() error {
 
 	blockchain.BeaconChain.multiView.Reset()
 	for _, v := range allViews {
-		if err := v.RestoreBeaconViewStateFromHash(blockchain, true, false); err != nil {
+		if err := v.RestoreBeaconViewStateFromHash(blockchain, true, true); err != nil {
 			return NewBlockChainError(BeaconError, err)
 		}
 		v.pdeStates, err = pdex.InitStatesFromDB(v.featureStateDB, v.BeaconHeight)
