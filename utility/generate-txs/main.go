3c8668a38256c891835f9fa1f6e601a09323c218
thanhn-inc <thanh.n@incognito.org>
2021-11-11 01:38:03 +0000

    Update gen-txs

--- utility/generate-txs/main.go
+++ utility/generate-txs/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"github.com/incognitochain/incognito-chain/common"
 	"github.com/incognitochain/incognito-chain/dataaccessobject/statedb"
-	"github.com/incognitochain/incognito-chain/privacy/coin"
+	"github.com/incognitochain/incognito-chain/privacy"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -18,61 +18,112 @@ import (
 )
 
 func main() {
-	//==========Write
-	transactions := []string{}
-	db, err := incdb.Open("leveldb", filepath.Join("./", "./"))
+	initEnvironment()
+
+	var transactions []string
+	db, err := incdb.Open("leveldb", filepath.Join("./data", "./"))
 	if err != nil {
 		fmt.Print("could not open connection to leveldb")
 		fmt.Print(err)
 		panic(err)
 	}
-	stateDB, _ := statedb.NewWithPrefixTrie(common.EmptyRoot, statedb.NewDatabaseAccessWarper(db))
-	privateKeys := readTxsFromFile("private-keys-shard-1-1.json")
-	fmt.Println(len(privateKeys))
+	stateDB, err := statedb.NewWithPrefixTrie(common.EmptyRoot, statedb.NewDatabaseAccessWarper(db))
+	if err != nil {
+		panic(err)
+	}
+	privateKeys := []string{
+		"1111111CL65f56Khc7NhYHcxFabrWrNE3tbEHJpd4diMJiKd9NHmLAYgdHRDV23rb4qwjYBMohicB5hDhhFb9qxTbBprCt19yKiUu4ajTLB",
+	}
+
 	for _, privateKey := range privateKeys {
-		txs := initTx("1000", privateKey, stateDB)
+		txs, err := initTx("1000", privateKey, stateDB)
+		if err != nil {
+			panic(err)
+		}
 		transactions = append(transactions, txs[0])
 	}
-	fmt.Println(len(transactions))
-	file, _ := json.MarshalIndent(transactions, "", " ")
-	_ = ioutil.WriteFile("shard1-1-init-txs.json", file, 0644)
+	file, err := json.MarshalIndent(transactions, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("shard2-2-init-txs.json", file, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
-func readTxsFromFile(filename string) []string {
+
+func initEnvironment() {
+	common.MaxShardNumber = 8
+}
+
+func readTxsFromFile(filename string) ([]string, error) {
 	// Open our jsonFile
 	jsonFile, err := os.Open(filename)
 	// if we os.Open returns an error then handle it
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 	fmt.Println("Successfully Opened ", filename)
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer func() {
+		err = jsonFile.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
 
 	var result []string
-	json.Unmarshal([]byte(byteValue), &result)
-	// defer the closing of our jsonFile so that we can parse it later on
-	defer jsonFile.Close()
-	return result
+	err = json.Unmarshal(byteValue, &result)
+	if err != nil {
+		 return nil, err
+	}
+	return result, nil
 }
-func initTx(amount string, privateKey string, stateDB *statedb.StateDB) []string {
-	var initTxs []string
-	var initAmount, _ = strconv.Atoi(amount) // amount init
-	testUserkeyList := []string{
+
+func initTx(amount string, privateKey string, stateDB *statedb.StateDB) ([]string, error) {
+	testKeyList := []string{
 		privateKey,
 	}
-	for _, val := range testUserkeyList {
 
-		testUserKey, _ := wallet.Base58CheckDeserialize(val)
-		testUserKey.KeySet.InitFromPrivateKey(&testUserKey.KeySet.PrivateKey)
+	initTxs := make([]string, 0)
+	var initAmount, err = strconv.Atoi(amount) // amount init
+	if err != nil {
+		return nil, err
+	}
 
-		testSalaryTX := transaction.Tx{}
+	for _, val := range testKeyList {
+		w, err := wallet.Base58CheckDeserialize(val)
+		if err != nil {
+			return nil, err
+		}
+		err = w.KeySet.InitFromPrivateKey(&w.KeySet.PrivateKey)
+		if err != nil {
+			return nil, err
+		}
 
-		// TODO Privacy
-		testSalaryTX.InitTxSalary(uint64(initAmount), coin.NewTxRandom(), &testUserKey.KeySet.PaymentAddress, &testUserKey.KeySet.PrivateKey,
-			stateDB,
-			nil,
-		)
-		initTx, _ := json.Marshal(testSalaryTX)
+		// Generate a new OTA coin
+		otaCoin, err := privacy.NewCoinFromPaymentInfo(&privacy.PaymentInfo{PaymentAddress: w.KeySet.PaymentAddress, Amount: uint64(initAmount)})
+		if err != nil {
+			return nil, err
+		}
+
+		// Initialize the tx
+		testSalaryTX := transaction.TxVersion2{}
+		err = testSalaryTX.InitTxSalary(otaCoin, &(w.KeySet.PrivateKey), stateDB, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		initTx, err := json.Marshal(testSalaryTX)
+		if err != nil {
+			return nil, err
+		}
 		initTxs = append(initTxs, string(initTx))
 	}
-	return initTxs
+
+	return initTxs, nil
 }
