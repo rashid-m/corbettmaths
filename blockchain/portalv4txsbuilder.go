6aa5092cb5c53ce9e922086e0a616a30bc3a3b59
thanhn-inc <thanh.n@incognito.org>
2022-01-21 02:05:32 +0000

    Update buildPortalv4Response

--- blockchain/portalv4txsbuilder.go
+++ blockchain/portalv4txsbuilder.go
@@ -2,6 +2,8 @@ package blockchain
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/incognitochain/incognito-chain/wallet"
 
 	"github.com/incognitochain/incognito-chain/common"
 	"github.com/incognitochain/incognito-chain/metadata"
@@ -9,8 +11,6 @@ import (
 	"github.com/incognitochain/incognito-chain/privacy"
 	"github.com/incognitochain/incognito-chain/privacy/coin"
 	"github.com/incognitochain/incognito-chain/transaction"
-	"github.com/incognitochain/incognito-chain/wallet"
-	"github.com/pkg/errors"
 )
 
 // buildPortalAcceptedShieldingRequestTx builds response tx for the shielding request tx with status "accepted"
@@ -21,48 +21,54 @@ func (curView *ShardBestState) buildPortalAcceptedShieldingRequestTx(
 	producerPrivateKey *privacy.PrivateKey,
 	shardID byte,
 ) (metadata.Transaction, error) {
-	Logger.log.Errorf("[buildPortalAcceptedShieldingRequestTx] Starting...")
-
+	prefix := fmt.Sprintf("[buildPortalAcceptedShieldingRequestTx]")
+	Logger.log.Infof("%v ContentStr: %v\n", contentStr)
 	contentBytes := []byte(contentStr)
 	var acceptedShieldingReq metadata.PortalShieldingRequestContent
 	err := json.Unmarshal(contentBytes, &acceptedShieldingReq)
 	if err != nil {
-		Logger.log.Errorf("[buildPortalAcceptedShieldingRequestTx]: an error occured while unmarshaling portal custodian deposit content: %+v", err)
-		return nil, nil
+		Logger.log.Errorf("%v an error occurred while unmarshalling instruction's content: %v\n",
+			prefix, err)
+		return nil, fmt.Errorf("%v cannot unmarshal instruction's content: %v", prefix, err)
 	}
 	if acceptedShieldingReq.ShardID != shardID {
-		Logger.log.Errorf("[buildPortalAcceptedShieldingRequestTx]: ShardID unexpected expect %v, but got %+v", shardID, acceptedShieldingReq.ShardID)
-		return nil, nil
+		Logger.log.Errorf("%v expected shardID %v, got %v\n", prefix, shardID, acceptedShieldingReq.ShardID)
+		return nil, fmt.Errorf("%v expected shardID %v, got %v", prefix, shardID, acceptedShieldingReq.ShieldingUTXO)
 	}
 
 	meta := metadata.NewPortalShieldingResponse(
 		"accepted",
 		acceptedShieldingReq.TxReqID,
-		acceptedShieldingReq.IncogAddressStr,
+		acceptedShieldingReq.Receiver,
 		acceptedShieldingReq.MintingAmount,
 		acceptedShieldingReq.TokenID,
 		metadataCommon.PortalV4ShieldingResponseMeta,
 	)
 
-	keyWallet, err := wallet.Base58CheckDeserialize(acceptedShieldingReq.IncogAddressStr)
-	if err != nil {
-		Logger.log.Errorf("[buildPortalAcceptedShieldingRequestTx]: an error occured while deserializing custodian address string: %+v", err)
-		return nil, nil
-	}
-	// in case the returned currency is privacy custom token
-	receiver := &privacy.PaymentInfo{
-		Amount:         acceptedShieldingReq.MintingAmount,
-		PaymentAddress: keyWallet.KeySet.PaymentAddress,
-	}
-
 	tokenIDPointer, _ := new(common.Hash).NewHashFromStr(acceptedShieldingReq.TokenID)
 	tokenID := *tokenIDPointer
 	if tokenID == common.PRVCoinID {
-		Logger.log.Errorf("[buildPortalAcceptedShieldingRequestTx]: cannot minting PRV in shield request")
-		return nil, errors.New("[buildPortalAcceptedShieldingRequestTx]: cannot mint PRV in shield request")
+		Logger.log.Errorf("%v cannot minting PRV in shield request\n", prefix)
+		return nil, fmt.Errorf("%v cannot mint PRV in shield request", prefix)
 	}
 
-	txParam := transaction.TxSalaryOutputParams{Amount: receiver.Amount, ReceiverAddress: &receiver.PaymentAddress, TokenID: &tokenID}
+	txParam := transaction.TxSalaryOutputParams{
+		Amount:  acceptedShieldingReq.MintingAmount,
+		TokenID: &tokenID,
+	}
+	keyWallet, err := wallet.Base58CheckDeserialize(acceptedShieldingReq.Receiver)
+	if err == nil { // receiver is a payment address
+		txParam.ReceiverAddress = &keyWallet.KeySet.PaymentAddress
+	} else { // receiver is an OTAReceiver
+		otaReceiver := new(privacy.OTAReceiver)
+		err = otaReceiver.FromString(acceptedShieldingReq.Receiver)
+		if err != nil {
+			return nil, fmt.Errorf("parseOTA receiver from %v error: %v", acceptedShieldingReq.Receiver, err)
+		}
+		txParam.TxRandom = &otaReceiver.TxRandom
+		txParam.PublicKey = &otaReceiver.PublicKey
+	}
+
 	makeMD := func(c privacy.Coin) metadata.Metadata {
 		if c != nil && c.GetSharedRandom() != nil {
 			meta.SetSharedRandom(c.GetSharedRandom().ToBytesS())
