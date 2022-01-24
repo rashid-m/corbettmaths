b9f55546daef3d84cd3499688c1a7819b1146c2a
thanhn-inc <thanh.n@incognito.org>
2022-01-24 05:26:09 +0000

    Update featureKey

--- incognitokey/featurekey.go
+++ incognitokey/featurekey.go
@@ -2,15 +2,14 @@ package incognitokey
 
 import (
 	"github.com/incognitochain/incognito-chain/common"
-	"github.com/incognitochain/incognito-chain/common/base58"
 	"github.com/incognitochain/incognito-chain/privacy/operation"
 	"math/big"
 )
 
 // OTDepositKey represents a pair of one-time depositing key for shielding.
 type OTDepositKey struct {
-	PrivateKey string
-	PublicKey  string
+	PrivateKey []byte
+	PublicKey  []byte
 }
 
 // GenerateOTDepositKey generates a new OTDepositKey from the keySet with the given tokenID and index.
@@ -27,35 +26,29 @@ func (keySet *KeySet) GenerateOTDepositKey(tokenIDStr string, index uint64) (*OT
 	privateKey := operation.HashToScalar(append(masterDepositSeed, indexBig.Bytes()...))
 	pubKey := new(operation.Point).ScalarMultBase(privateKey)
 
-	privateKeyStr := base58.Base58Check{}.Encode(privateKey.ToBytesS(), 0)
-	pubKeyStr := base58.Base58Check{}.Encode(pubKey.ToBytesS(), 0)
-
 	return &OTDepositKey{
-		PrivateKey: privateKeyStr,
-		PublicKey:  pubKeyStr,
+		PrivateKey: privateKey.ToBytesS(),
+		PublicKey:  pubKey.ToBytesS(),
 	}, nil
 }
 
 // GenerateOTDepositKeyFromPrivateKey generates a new OTDepositKey from the given privateKey, tokenID and index.
-func GenerateOTDepositKeyFromPrivateKey(privateKey []byte, tokenIDStr string, index uint64) (*OTDepositKey, error) {
+func GenerateOTDepositKeyFromPrivateKey(incPrivateKey []byte, tokenIDStr string, index uint64) (*OTDepositKey, error) {
 	tokenID, err := new(common.Hash).NewHashFromStr(tokenIDStr)
 	if err != nil {
 		return nil, err
 	}
 
 	tmp := append([]byte(common.PortalV4DepositKeyGenSeed), tokenID[:]...)
-	masterDepositSeed := common.SHA256(append(privateKey[:], tmp...))
+	masterDepositSeed := common.SHA256(append(incPrivateKey[:], tmp...))
 	indexBig := new(big.Int).SetUint64(index)
 
-	depositPubKey := operation.HashToScalar(append(masterDepositSeed, indexBig.Bytes()...))
-	pubKey := new(operation.Point).ScalarMultBase(depositPubKey)
-
-	privateKeyStr := base58.Base58Check{}.Encode(depositPubKey.ToBytesS(), 0)
-	pubKeyStr := base58.Base58Check{}.Encode(pubKey.ToBytesS(), 0)
+	privateKey := operation.HashToScalar(append(masterDepositSeed, indexBig.Bytes()...))
+	pubKey := new(operation.Point).ScalarMultBase(privateKey)
 
 	return &OTDepositKey{
-		PrivateKey: privateKeyStr,
-		PublicKey:  pubKeyStr,
+		PrivateKey: privateKey.ToBytesS(),
+		PublicKey:  pubKey.ToBytesS(),
 	}, nil
 }
 
@@ -63,14 +56,11 @@ func GenerateOTDepositKeyFromPrivateKey(privateKey []byte, tokenIDStr string, in
 func GenerateOTDepositKeyFromMasterDepositSeed(masterDepositSeed []byte, index uint64) (*OTDepositKey, error) {
 	indexBig := new(big.Int).SetUint64(index)
 
-	depositPubKey := operation.HashToScalar(append(masterDepositSeed, indexBig.Bytes()...))
-	pubKey := new(operation.Point).ScalarMultBase(depositPubKey)
-
-	privateKeyStr := base58.Base58Check{}.Encode(depositPubKey.ToBytesS(), 0)
-	pubKeyStr := base58.Base58Check{}.Encode(pubKey.ToBytesS(), 0)
+	privateKey := operation.HashToScalar(append(masterDepositSeed, indexBig.Bytes()...))
+	pubKey := new(operation.Point).ScalarMultBase(privateKey)
 
 	return &OTDepositKey{
-		PrivateKey: privateKeyStr,
-		PublicKey:  pubKeyStr,
+		PrivateKey: privateKey.ToBytesS(),
+		PublicKey:  pubKey.ToBytesS(),
 	}, nil
 }
