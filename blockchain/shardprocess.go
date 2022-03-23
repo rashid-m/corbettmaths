0a8ec74e9cf1459479ceee9ff6bbcca78e593a04
hungngo <hung@autonomous.nyc>
2022-03-23 17:46:59 +0700

    [log] validate committee sig error

--- blockchain/shardprocess.go
+++ blockchain/shardprocess.go
@@ -222,7 +222,7 @@ WAITFORBEACON:
 	if shouldValidate {
 		committeesStr, _ := incognitokey.CommitteeKeyListToString(signingCommittees)
 		if err := blockchain.config.ConsensusEngine.ValidateBlockCommitteSig(shardBlock, signingCommittees); err != nil {
-			Logger.log.Errorf("Validate block %v shard %v with committee %v return error %v", shardBlock.GetHeight(), shardBlock.GetShardID(), committeesStr, err)
+			Logger.log.Errorf("Validate block %v shard %v, hash %+v, validationData %+v, with committee %v return error %v", shardBlock.GetHeight(), *shardBlock.Hash(), shardBlock.GetValidationField(), shardBlock.GetShardID(), committeesStr, err)
 			return err
 		}
 		Logger.log.Infof("SHARD %+v | Verify Pre Processing, block height %+v with hash %+v", shardID, blockHeight, blockHash)
