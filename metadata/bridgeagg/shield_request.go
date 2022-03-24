53ad91715d877c91c19e34f27ac8a13bbbf68d31
tin-incognito <tin@incognito.org>
2022-03-24 17:08:42 +0700

    [bridgeagg] Add shield request for unifiedtoken

--- metadata/bridgeagg/shield_request.go
+++ metadata/bridgeagg/shield_request.go
@@ -39,36 +39,26 @@ func NewShieldRequestWithValue(
 	}
 }
 
-func (s *ShieldData) ValidateTxWithBlockChain(tx metadataCommon.Transaction, chainRetriever metadataCommon.ChainRetriever, shardViewRetriever metadataCommon.ShardViewRetriever, beaconViewRetriever metadataCommon.BeaconViewRetriever, shardID byte, transactionStateDB *statedb.StateDB) (bool, error) {
+func (request *ShieldRequest) ValidateTxWithBlockChain(tx metadataCommon.Transaction, chainRetriever metadataCommon.ChainRetriever, shardViewRetriever metadataCommon.ShardViewRetriever, beaconViewRetriever metadataCommon.BeaconViewRetriever, shardID byte, transactionStateDB *statedb.StateDB) (bool, error) {
 	return true, nil
 }
 
-func (s *ShieldData) ValidateSanityData(chainRetriever metadataCommon.ChainRetriever, shardViewRetriever metadataCommon.ShardViewRetriever, beaconViewRetriever metadataCommon.BeaconViewRetriever, beaconHeight uint64, tx metadataCommon.Transaction) (bool, bool, error) {
+func (request *ShieldRequest) ValidateSanityData(chainRetriever metadataCommon.ChainRetriever, shardViewRetriever metadataCommon.ShardViewRetriever, beaconViewRetriever metadataCommon.BeaconViewRetriever, beaconHeight uint64, tx metadataCommon.Transaction) (bool, bool, error) {
 
 	return true, true, nil
 }
 
-func (s *ShieldData) ValidateMetadataByItself() bool {
+func (request *ShieldRequest) ValidateMetadataByItself() bool {
 	return true
 }
 
-func (s *ShieldData) Hash() *common.Hash {
-	record := iReq.BlockHash.String()
-	record += string(iReq.TxIndex)
-	proofStrs := iReq.ProofStrs
-	for _, proofStr := range proofStrs {
-		record += proofStr
-	}
-	record += iReq.MetadataBase.Hash().String()
-	record += iReq.IncTokenID.String()
-
-	// final hash
-	hash := common.HashH([]byte(record))
-	return &hash
+func (request *ShieldRequest) Hash() *common.Hash {
+	return nil
 }
 
-func (s *ShieldData) BuildReqActions(tx metadataCommon.Transaction, chainRetriever metadataCommon.ChainRetriever, shardViewRetriever metadataCommon.ShardViewRetriever, beaconViewRetriever metadataCommon.BeaconViewRetriever, shardID byte, shardHeight uint64) ([][]string, error) {
-
+func (request *ShieldRequest) BuildReqActions(tx metadataCommon.Transaction, chainRetriever metadataCommon.ChainRetriever, shardViewRetriever metadataCommon.ShardViewRetriever, beaconViewRetriever metadataCommon.BeaconViewRetriever, shardID byte, shardHeight uint64) ([][]string, error) {
+	content, err := metadataCommon.NewActionWithValue(request, *tx.Hash(), nil).StringSlice(metadataCommon.ShieldUnifiedTokenRequestMeta)
+	return [][]string{content}, err
 }
 
 func (request *ShieldRequest) CalculateSize() uint64 {
