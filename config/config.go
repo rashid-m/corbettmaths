df22ef17d009798e1ff021fa89b77ac3283df2d7
0xakk0r0kamui <hy@autonomous.nyc>
2022-04-06 04:41:16 +0700

    Fix conflict

--- config/config.go
+++ config/config.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"strconv"
 
+	"github.com/incognitochain/incognito-chain/common"
 	"github.com/incognitochain/incognito-chain/metadata/rpccaller"
 	"github.com/incognitochain/incognito-chain/utils"
 	"github.com/jessevdk/go-flags"
@@ -118,8 +119,8 @@ type config struct {
 	NumIndexerWorkers   int64  `mapstructure:"num_indexer_workers" long:"numindexerworkers" description:"Number of workers for caching output coins"`
 	IndexerAccessTokens string `mapstructure:"indexer_access_token" long:"indexeraccesstoken" description:"The access token for caching output coins"`
 	UseOutcoinDatabase  []bool `mapstructure:"use_coin_data" long:"usecoindata" description:"Store output coins by known OTA keys"`
-	SyncMode            string    `mapstructure:"sync_mode" long:"syncmode" description:"smart contract of prv bep20"`
-	EnableFFStorage     bool      `mapstructure:"enable_ffstorage" long:"ffstorage" description:"enable_ffstorage"`
+	SyncMode            string `mapstructure:"sync_mode" long:"syncmode" description:"smart contract of prv bep20"`
+	EnableFFStorage     bool   `mapstructure:"enable_ffstorage" long:"ffstorage" description:"enable_ffstorage"`
 }
 
 // normalizeAddresses returns a new slice with all the passed peer addresses
