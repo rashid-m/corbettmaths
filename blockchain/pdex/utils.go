b43fa08240e9f9649670275695072b8de592673a
tin-incognito <tin@incognito.org>
2022-04-27 11:20:47 +0700

    [hotfix-pdex] Sort auto withdraw orders

--- blockchain/pdex/utils.go
+++ blockchain/pdex/utils.go
@@ -995,3 +995,17 @@ func addMakingVolume(
 	}
 	return base
 }
+
+func getSortedPoolPairIDs(poolPairs map[string]*PoolPairState) []string {
+	// To store the keys in slice in sorted order
+	keys := make([]string, len(poolPairs))
+	i := 0
+	for poolPairID := range poolPairs {
+		keys[i] = poolPairID
+		i++
+	}
+	sort.SliceStable(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+	return keys
+}
