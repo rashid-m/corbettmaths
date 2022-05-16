5d8236d7de6660f334db4a8525a2960371aae975
0xKumi <dung.q@autonomous.nyc>
2022-05-16 09:19:49 +0700

    refactor

--- multiview/shardmultiview.go
+++ multiview/shardmultiview.go
@@ -2,7 +2,6 @@ package multiview
 
 import (
 	"github.com/incognitochain/incognito-chain/blockchain/types"
-	"github.com/incognitochain/incognito-chain/common"
 )
 
 type ShardMultiView struct {
@@ -15,24 +14,10 @@ func NewShardMultiView() *ShardMultiView {
 	return sv
 }
 
-func (s *ShardMultiView) AddView(v View) (int, error) {
-	added := s.multiView.addView(v)
-	res := 0
-	if added {
-		res = 1
-	}
-	return res, nil
-}
-
-func (s *ShardMultiView) AddViewWithFinalizedHash(v View, newFinalizedHash *common.Hash) (res int, err error) {
+func (s *ShardMultiView) AddView(v View) (res int, err error) {
 	added := s.multiView.addView(v)
 	shardBlock := v.GetBlock()
-
-	if shardBlock.GetVersion() >= types.INSTANT_FINALITY_VERSION {
-		if newFinalizedHash != nil {
-			err = s.FinalizeView(*newFinalizedHash)
-		}
-	} else {
+	if shardBlock.GetVersion() < types.INSTANT_FINALITY_VERSION {
 		err = s.FinalizeView(*s.GetExpectedFinalView().GetHash())
 	}
 	if added {
