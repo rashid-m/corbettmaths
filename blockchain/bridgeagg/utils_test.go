b47a3412655cb031e799dc6bbc3f2f42d2d7f38b
0xkraken <hien.n@autonomous.nyc>
2022-07-01 13:25:25 +0700

    Add list unified tokens testnet2

--- blockchain/bridgeagg/utils_test.go
+++ blockchain/bridgeagg/utils_test.go
@@ -179,158 +179,6 @@ func TestCalculateAmountByDecimal(t *testing.T) {
 	}
 }
 
-func Test_updateRewardReserve(t *testing.T) {
-	type args struct {
-		lastUpdatedRewardReserve uint64
-		currentRewardReserve     uint64
-		newRewardReserve         uint64
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    uint64
-		want1   uint64
-		wantErr bool
-	}{
-		{
-			name: "First time modify",
-			args: args{
-				lastUpdatedRewardReserve: 0,
-				currentRewardReserve:     0,
-				newRewardReserve:         100,
-			},
-			want:    100,
-			want1:   100,
-			wantErr: false,
-		},
-		{
-			name: "Second time modify - not yet update reward",
-			args: args{
-				lastUpdatedRewardReserve: 100,
-				currentRewardReserve:     100,
-				newRewardReserve:         200,
-			},
-			want:    200,
-			want1:   200,
-			wantErr: false,
-		},
-		{
-			name: "Second time modify - has updated reward - deltaY > 0",
-			args: args{
-				lastUpdatedRewardReserve: 100,
-				currentRewardReserve:     90,
-				newRewardReserve:         200,
-			},
-			want:    200,
-			want1:   190,
-			wantErr: false,
-		},
-		{
-			name: "Second time modify - has updated reward - deltaY < 0",
-			args: args{
-				lastUpdatedRewardReserve: 100,
-				currentRewardReserve:     110,
-				newRewardReserve:         200,
-			},
-			want:    200,
-			want1:   210,
-			wantErr: false,
-		},
-		{
-			name: "newY < deltaY",
-			args: args{
-				lastUpdatedRewardReserve: 100,
-				currentRewardReserve:     10,
-				newRewardReserve:         50,
-			},
-			want:    0,
-			want1:   0,
-			wantErr: true,
-		},
-		{
-			name: "deltaY < 0",
-			args: args{
-				lastUpdatedRewardReserve: 100,
-				currentRewardReserve:     200,
-				newRewardReserve:         50,
-			},
-			want:    50,
-			want1:   150,
-			wantErr: false,
-		},
-		{
-			name: "deltaY < 0",
-			args: args{
-				lastUpdatedRewardReserve: 100,
-				currentRewardReserve:     200,
-				newRewardReserve:         50,
-			},
-			want:    50,
-			want1:   150,
-			wantErr: false,
-		},
-		{
-			name: "Set reward to 0",
-			args: args{
-				lastUpdatedRewardReserve: 100,
-				currentRewardReserve:     10,
-				newRewardReserve:         90,
-			},
-			want:    0,
-			want1:   0,
-			wantErr: true,
-		},
-		{
-			name: "Not change value",
-			args: args{
-				lastUpdatedRewardReserve: 100,
-				currentRewardReserve:     10,
-				newRewardReserve:         100,
-			},
-			want:    100,
-			want1:   10,
-			wantErr: false,
-		},
-		{
-			name: "newReward = deltaY + 1",
-			args: args{
-				lastUpdatedRewardReserve: 5000000000,
-				currentRewardReserve:     88,
-				newRewardReserve:         4999999913,
-			},
-			want:    4999999913,
-			want1:   1,
-			wantErr: false,
-		},
-		{
-			name: "newReward == 0 && lastUpdatedRewardReserve == 0",
-			args: args{
-				lastUpdatedRewardReserve: 0,
-				currentRewardReserve:     0,
-				newRewardReserve:         0,
-			},
-			want:    0,
-			want1:   0,
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := updateRewardReserve(tt.args.lastUpdatedRewardReserve, tt.args.currentRewardReserve, tt.args.newRewardReserve)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("updateRewardReserve() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("updateRewardReserve() got = %v, want %v", got, tt.want)
-			}
-			if got1 != tt.want1 {
-				t.Errorf("updateRewardReserve() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
-
 func readTestCases(fileName string) ([]byte, error) {
 	raw, err := ioutil.ReadFile("testdata/" + fileName)
 	if err != nil {
@@ -373,6 +221,8 @@ func TestA(t *testing.T) {
 		"0x0000000000000000000000000000000000000000",
 		"0x0000000000000000000000000000000000000000",
 		"0x0000000000000000000000000000000000000000",
+		"0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee",
+		"0x3813e82e6f7098b9583FC0F33a962D02018B6803",
 	}
 
 	prefixs := []string{
@@ -392,6 +242,8 @@ func TestA(t *testing.T) {
 		common.PLGPrefix,
 		common.FTMPrefix,
 		common.BSCPrefix,
+		common.BSCPrefix,
+		common.PLGPrefix,
 	}
 
 	for i := 0; i < len(prefixs); i++ {
