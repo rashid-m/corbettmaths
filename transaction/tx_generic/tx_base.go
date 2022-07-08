42f449c982b18d1ca496da55976eec12d443e20c
0xkraken <hien.n@autonomous.nyc>
2022-07-08 10:55:19 +0700

    Add logs

--- transaction/tx_generic/tx_base.go
+++ transaction/tx_generic/tx_base.go
@@ -590,6 +590,7 @@ func (tx *TxBase) ValidateDoubleSpendWithBlockchain(shardID byte, stateDB *state
 		}
 
 		if isMint := len(tx.Proof.GetOutputCoins()) == 1 && len(tx.Proof.GetInputCoins()) == 0 && tx.GetMetadata() != nil && tx.GetMetadata().IsMinerCreatedMetaType(); afterCoinOriginHeight && !isMint {
+			utils.Logger.Log.Infof("ValidateTxSalary afterCoinOriginHeight %v - isMint %v", afterCoinOriginHeight, isMint)
 			cptype := privacy.CoinPrivacyTypeTransfer
 			coinSenderShardID, _, coinPrivacyType, err := privacy.DeriveShardInfoFromCoin(outCoin.GetPublicKey().ToBytesS())
 			if err != nil {
