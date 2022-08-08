da2b924523610284d560bb4b2141f027bdb6eb38
0xKumi <dung.q@autonomous.nyc>
2022-08-09 01:41:10 +0700

    getting committee from block by cache

--- blockchain/shardbeststate.go
+++ blockchain/shardbeststate.go
@@ -679,7 +679,7 @@ func (shardBestState *ShardBestState) getSigningCommittees(
 		return shardBestState.GetShardCommittee(), shardBestState.GetShardCommittee(), nil
 	}
 	if shardBlock.Header.Version >= types.MULTI_VIEW_VERSION && shardBlock.Header.Version <= types.LEMMA2_VERSION || shardBlock.Header.Version >= types.INSTANT_FINALITY_VERSION_V2 {
-		committees, err := bc.GetShardCommitteeFromBeaconHash(shardBlock.CommitteeFromBlock(), shardBlock.Header.ShardID)
+		committees, err := bc.BeaconChain.CommitteesFromViewHashForShard(shardBlock.CommitteeFromBlock(), shardBlock.Header.ShardID)
 		if err != nil {
 			return []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, err
 		}
@@ -687,8 +687,9 @@ func (shardBestState *ShardBestState) getSigningCommittees(
 		return committees, signingCommittees, nil
 	}
 	if shardBlock.Header.Version >= types.BLOCK_PRODUCINGV3_VERSION && shardBlock.Header.Version <= types.INSTANT_FINALITY_VERSION {
-		committees, err := bc.GetShardCommitteeFromBeaconHash(shardBlock.CommitteeFromBlock(), shardBlock.Header.ShardID)
+		committees, err := bc.BeaconChain.CommitteesFromViewHashForShard(shardBlock.CommitteeFromBlock(), shardBlock.Header.ShardID)
 		if err != nil {
+			log.Println("error committee from block", shardBlock.CommitteeFromBlock(), shardBlock.Header.ShardID)
 			return []incognitokey.CommitteePublicKey{}, []incognitokey.CommitteePublicKey{}, err
 		}
 		timeSlot := shardBestState.CalculateTimeSlot(shardBlock.Header.ProposeTime)
