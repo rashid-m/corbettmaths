21db257550751a182aab985ce8f74cf2dc3b9b45
Rocky <thach@incognito.org>
2022-08-31 08:01:43 +0700

    blink.sh patch, workaround hub.docker.com invalid json response

---
+++ bin/blink_patch1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SERVICE_FILE=/etc/systemd/system/IncognitoUpdater.service
+RUN_NODE_FILE=$(cat $SERVICE_FILE | grep ExecStart | cut -d \" \" -f3)
+
+sed -i s'|echo "Latest tag is ${latest_tag}"|echo "Latest tag is ${latest_tag}\nif [[ -z $latest_tag ]]; then\n  echo "Cannot get tags from docker hub for now. Skip this round!"\n  exit 0\nfi|' $RUN_NODE_FILE
