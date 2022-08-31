adaf25816d502c0d578ee015435e5d7f297f8f3c
Rocky <thach@incognito.org>
2022-08-31 09:47:09 +0700

    blink.sh patch, workaround hub.docker.com invalid json response

--- bin/blink_patch1.sh
+++ bin/blink_patch1.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+# check super user
+if [ $(whoami) != root ]; then
+cat << EOF
+	!!! Please run with sudo or su, otherwise it won't work
+	!!! Script now exits
+EOF
+	exit 1
+fi
+
 SERVICE_FILE=/etc/systemd/system/IncognitoUpdater.service
 RUN_NODE_FILE=$(cat $SERVICE_FILE | grep ExecStart | cut -d " " -f3)
 
-sed -i s'|echo "Latest tag is ${latest_tag}"|echo "Latest tag is ${latest_tag}\nif [[ -z $latest_tag ]]; then\n  echo "Cannot get tags from docker hub for now. Skip this round!"\n  exit 0\nfi|' $RUN_NODE_FILE
+sed -i s'|echo "Latest tag is ${latest_tag}"|echo "Latest tag is ${latest_tag}"\nif [[ -z $latest_tag ]]; then\n  echo "Cannot get tags from docker hub for now. Skip this round!"\n  exit 0\nfi\n|' $RUN_NODE_FILE
+
+systemctl enable IncognitoUpdater.service
+systemctl start IncognitoUpdater.service
