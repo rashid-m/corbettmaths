7b673f0167c13bca80f008dd181ac94c9bdd16f7
Rocky <thach@incognito.org>
2022-09-05 16:56:14 +0700

    pnode updater patch, using docker hub APIv2

--- bin/pnode_updater_patch1.sh
+++ bin/pnode_updater_patch1.sh
@@ -8,7 +8,14 @@ cat << EOF
 EOF
 	exit 1
 fi
+apt install jq -y
 
 UPDATER_BASH_FILE=/home/nuc/aos/ability/incognito/start.sh
 
-sed -i s'|latest_tag=${sorted_tags\[0\]}|latest_tag=${sorted_tags\[0\]}\nif [[ -z $latest_tag ]]; then\n  echo "Cannot get tags from docker hub for now. Skip this round!"\n  exit 0\nfi\n|' $UPDATER_BASH_FILE
+if [[ -z $(grep "Cannot get tags from docker hub for now" $UPDATER_BASH_FILE) ]]; then
+	sed -i s'|latest_tag=${sorted_tags\[0\]}|latest_tag=${sorted_tags\[0\]}\nif [[ -z $latest_tag ]]; then\n  echo "Cannot get tags from docker hub for now. Skip this round!"\n  exit 0\nfi\n|' $UPDATER_BASH_FILE
+fi
+
+if [[ -z $(grep "hub.docker.com/v2/" $UPDATER_BASH_FILE) ]]; then
+	sed -i s'!tags=`curl.*!tags=$(curl -sX GET https://hub.docker.com/v2/namespaces/incognitochain/repositories/incognito-mainnet/tags?page_size=100 | jq ".results[].name" | tr -d "\\"")!' $UPDATER_BASH_FILE
+fi
