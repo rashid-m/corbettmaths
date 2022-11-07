fa50078d1f8481e5fbd732e3f9e61cc5e3cbf28a
minhduccm <minhduccm90@gmail.com>
2022-11-07 12:07:53 +0700

    Update build script to separate deployment for fixed nodes and community nodes.

--- build.sh
+++ build.sh
@@ -39,6 +39,8 @@ if [[ $env == "testnet" ]]; then
     docker build --build-arg commit=$commit . -t incognitochaintestnet/incognito:${tag} && docker push incognitochaintestnet/incognito:${tag} && echo "Commit: $commit"
 elif [ $env == "mainnet" ]; then
     docker build --build-arg commit=$commit . -t incognitochain/incognito-mainnet:${tag} && docker push incognitochain/incognito-mainnet:${tag} && echo "Commit: $commit"
+elif [ $env == "fixednodes" ]; then
+    docker build --build-arg commit=$commit . -t incognitochain/incognito-fixed-nodes:${tag} && docker push incognitochain/incognito-fixed-nodes:${tag} && echo "Commit: $commit"
 elif [ $env == "local" ]; then
     DOCKER_BUILDKIT=1 docker build --build-arg commit=$commit . -t incognitochain:local
 fi
